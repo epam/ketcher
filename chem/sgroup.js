38d513d25a3f259aa27b71d2b033d35628e02776
Mikhail Kozhevnikov <kozhevnikov@ggasoftware.com>
2011-02-24 16:57:26 +0100

    SRU group display adjusted

--- chem/sgroup.js
+++ chem/sgroup.js
@@ -289,18 +289,19 @@ chem.SGroup.GroupSru = {
 		var bb = this.bracketBox.extend(vext, vext);
 		chem.SGroup.drawBrackets(set, paper, settings, styles, bb);
 		var connectivity = this.data.connectivity || 'eu';
-		var connectivityIndex = paper.text(bb.p1.x + settings.lineWidth * 1, bb.p0.y, connectivity)
-			.attr({'font' : settings.font, 'font-size' : settings.fontszsub});
-		var connectivityIndexBox = connectivityIndex.getBBox();
-		connectivityIndex.translate(0.5 * connectivityIndexBox.width, 0.3 * connectivityIndexBox.height);
-		set.push(connectivityIndex);
-		if (this.data.subscript) {
-			var subscript = paper.text(bb.p1.x + settings.lineWidth * 2, bb.p1.y, this.data.subscript)
+		if (connectivity != 'ht') {
+			var connectivityIndex = paper.text(bb.p1.x + settings.lineWidth * 1, bb.p0.y, connectivity)
 				.attr({'font' : settings.font, 'font-size' : settings.fontszsub});
-			var subscriptBox = subscript.getBBox();
-			subscript.translate(0.5 * subscriptBox.width, -0.3 * subscriptBox.height);
-			set.push(subscript);
+			var connectivityIndexBox = connectivityIndex.getBBox();
+			connectivityIndex.translate(0.5 * connectivityIndexBox.width, 0.3 * connectivityIndexBox.height);
+			set.push(connectivityIndex);
 		}
+		this.data.subscript = this.data.subscript || 'n';
+		var subscript = paper.text(bb.p1.x + settings.lineWidth * 2, bb.p1.y, this.data.subscript)
+			.attr({'font' : settings.font, 'font-size' : settings.fontszsub});
+		var subscriptBox = subscript.getBBox();
+		subscript.translate(0.5 * subscriptBox.width, -0.3 * subscriptBox.height);
+		set.push(subscript);
 		return set;
 	},
 

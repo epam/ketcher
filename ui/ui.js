3a0f3a132d8accf56276d85b07545af098690544
Mikhail Kozhevnikov <kozhevnikov@ggasoftware.com>
2011-08-23 11:52:48 +0200

    no initial scrollbars

--- ui/ui.js
+++ ui/ui.js
@@ -276,7 +276,7 @@ ui.init = function ()
     this.selectMode('select_simple');
     
     // Init renderer
-    this.render =  new rnd.Render(this.client_area, this.scale, {atomColoring: true}, new util.Vec2(ui.client_area.clientWidth, ui.client_area.clientHeight - 4));
+    this.render =  new rnd.Render(this.client_area, this.scale, {atomColoring: true});
     
     this.render.onAtomClick = this.onClick_Atom;
     this.render.onAtomDblClick = this.onDblClick_Atom;

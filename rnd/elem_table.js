06fd11b172879ce53fb0452c303b1a0ea9cc864c
Mikhail Kozhevnikov <kozhevnikov@ggasoftware.com>
2011-09-05 14:12:00 +0200

    auto-size the element table

--- rnd/elem_table.js
+++ rnd/elem_table.js
@@ -15,24 +15,30 @@ if (!window.Prototype)
 if (!window.rnd)
 	throw new Error("rnd should be defined prior to loading this file");
 
-rnd.ElementTable = function (clientArea, opts, element)
+rnd.ElementTable = function (clientArea, opts, isTable)
 {
 	opts = opts || {};
 	clientArea = $(clientArea);
 	clientArea.innerHTML = "";
-	this.paper = new Raphael(clientArea);
-	this.size = new util.Vec2();
-	this.viewSz = new util.Vec2(clientArea['clientWidth'] || 100, clientArea['clientHeight'] || 100);
-	this.bb = new util.Box2Abs(new util.Vec2(), this.viewSz);
-
 	var table = this;
 	this.onClick = opts.onClick || function(elemNum){table.setElementSelected(elemNum, !table.items[elemNum].selected);}
 
-	this.elemHalfSz = new util.Vec2(16, 16);
+	var hsz = opts.buttonHalfSize || 16;
+	this.elemHalfSz = new util.Vec2(hsz, hsz);
 	this.elemSz = this.elemHalfSz.scaled(2);
 	this.spacing = new util.Vec2(3, 3);
-	this.cornerRadius = 7;
-	this.orig = this.elemSz.scaled(1.0);
+	this.cornerRadius = 0;
+	this.orig = this.elemSz.scaled(0);
+
+	if (isTable) {
+		this.size = new util.Vec2((this.elemSz.x + this.spacing.x) * 18 + this.spacing.x, (this.elemSz.y + this.spacing.y) *9 + this.spacing.y);
+		clientArea.style.width = (this.size.x).toString() + 'px';
+		clientArea.style.height = (this.size.y).toString() + 'px';
+	}
+	this.viewSz = new util.Vec2(clientArea['clientWidth'] || 100, clientArea['clientHeight'] || 100);
+
+	this.paper = new Raphael(clientArea, this.viewSz.x, this.viewSz.y);
+	this.bb = new util.Box2Abs(new util.Vec2(), this.viewSz);
 
 	this.fillColor = opts.fillColor || '#def';
 	this.fillColorSelected = opts.fillColorSelected || '#fcb';
@@ -56,14 +62,13 @@ rnd.ElementTable = function (clientArea, opts, element)
 rnd.ElementTable.prototype.renderTable = function () {
 	var table = this;
 	chem.Element.elements.each(function(id, elem){
-		var centre = new util.Vec2(this.orig.x + elem.xpos * this.elemSz.x + (elem.xpos - 1) * this.spacing.x, this.orig.y + elem.ypos * this.elemSz.y + (elem.ypos - 1) * this.spacing.y);
+		var centre = new util.Vec2(this.orig.x + (elem.xpos - 1) * (this.spacing.x + this.elemSz.x) + this.elemHalfSz.x + this.spacing.x, this.orig.y + (elem.ypos - 1) * (this.spacing.y + this.elemSz.y) + this.elemHalfSz.y + this.spacing.y);
 		var box = this.paper.rect(centre.x - this.elemHalfSz.x, centre.y - this.elemHalfSz.y, this.elemSz.x, this.elemSz.y, this.cornerRadius).attr(this.frameAttrs);
-		var label = this.paper.text(centre.x, centre.y, elem.label).attr(this.fontAttrs).attr('fill', elem.color);
+		var label = this.paper.text(centre.x, centre.y, elem.label).attr(this.fontAttrs).attr('fill', elem.labelColor);
 		box.node.onclick = function () {table.onClick(id);};
 		label.node.onclick = function () {table.onClick(id);};
 		this.items[id] = {'box':box, 'label':label, 'selected':false};
 	}, this);
-
 }
 
 rnd.ElementTable.prototype.renderSingle = function (element) {

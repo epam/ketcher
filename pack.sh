f1d34db9144a5c2477695a271ad60ae1b975c47f
Mikhail Rybalkin <rybalkin@ggasoftware.com>
2011-11-22 14:12:39 +0300

    Suffix name for building archive

--- pack.sh
+++ pack.sh
@@ -1,1 +1,1 @@
-tar -czf ketcher.tar.gz LICENSE.GPL base64.js chem/ favicon.ico ketcher-server-wsgi.py ketcher-server.py ketcher.css ketcher.html ketcher.js loading.gif png/ prototype-min.js raphael-min.js rnd/ ui/ util/
+tar -czf ketcher$1.tar.gz LICENSE.GPL base64.js chem/ favicon.ico ketcher-server-wsgi.py ketcher-server.py ketcher.css ketcher.html ketcher.js loading.gif png/ prototype-min.js raphael-min.js rnd/ ui/ util/

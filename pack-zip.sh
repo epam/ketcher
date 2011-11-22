d67a9c25d783bbd8d72f6630f1c8467e2a78e5d5
Mikhail Rybalkin <rybalkin@ggasoftware.com>
2011-11-22 16:38:43 +0400

    Linux script for packing Ketcher into zip archive

---
+++ pack-zip.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+name=$1
+
+if [ -z $name ]; then
+  echo "specify name";
+  exit;
+fi
+
+rm -rf ./$name 
+
+mkdir $name
+cp -r LICENSE.GPL LICENSE.GPL base64.js chem/ favicon.ico ketcher-server-wsgi.py ketcher-server.py ketcher.css ketcher.html ketcher.js loading.gif png/ prototype-min.js raphael-min.js rnd/ ui/ util/ $name/
+
+zip -r -9 $name.zip $name
+
+rm -rf ./$name 
+

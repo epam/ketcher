b61848947169a4b347d27924110951e706aabcf4
Roman Balabanov <rbalabanov@ggasoftware.com>
2012-01-16 17:45:43 +0400

    Reaxys Generics - deployment scripts fixed

--- pack-zip.sh
+++ pack-zip.sh
@@ -10,7 +10,7 @@ fi
 rm -rf ./$name 
 
 mkdir $name
-cp -r LICENSE.GPL LICENSE.GPL base64.js chem/ favicon.ico ketcher-server-wsgi.py ketcher-server.py ketcher.css ketcher.html ketcher.js loading.gif png/ prototype-min.js raphael-min.js raphael.js rnd/ ui/ util/ reaxis/ $name/
+cp -r LICENSE.GPL LICENSE.GPL base64.js chem/ favicon.ico ketcher-server-wsgi.py ketcher-server.py ketcher.css ketcher.html ketcher.js loading.gif png/ prototype-min.js raphael-min.js raphael.js rnd/ ui/ util/ reaxys/ $name/
 
 zip -r -9 $name.zip $name
 

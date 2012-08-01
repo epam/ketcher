854e475a46ddc550c45ea188c03754a01456738d
Boris Karulin <karulin@ggasoftware.com>
2012-08-01 20:57:40 +0400

    normalized pattern size when added on atom

--- ui/actions.js
+++ ui/actions.js
@@ -692,7 +692,8 @@ ui.Action.fromPatternOnElement = function (id, pattern, on_atom)
         var atom_pos = ui.render.atomGetPos(id);
 
         pos = util.Vec2.diff(atom_pos, ui.render.atomGetPos(nei_id));
-        pos = pos.scaled(0.5 / Math.cos(angle));
+        //pos = pos.scaled(0.5 / Math.cos(angle));
+        pos.normalize();
         v = pos.negated();
         pos.add_(atom_pos);
         angle = Math.PI - 2 * angle;

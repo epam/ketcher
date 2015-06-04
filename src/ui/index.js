449c6cf026fcb67fd7e9550b7cf07cf86e2cc754
Aleksey Elyseev <aelyseev@gmail.com>
2015-06-04 23:18:15 +0300

    Ui folder

---
+++ src/ui/index.js
@@ -0,0 +1,14 @@
+/*global require, global:false*/
+
+require('../vendor/prototype-min');
+require('./actions');
+require('./keys');
+require('./server');
+require('./ui');
+require('./dialog/crap');
+require('./dialog/io');
+require('./dialog/selectdialog');
+require('./dialog/sgroup');
+require('./dialog/template-custom');
+
+global.ui = global.ui || {};

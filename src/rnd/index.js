5919ee24df4bfded5ed25bc21d7f773638e5c52a
Aleksey Elyseev <aelyseev@gmail.com>
2015-06-05 00:52:58 +0300

    Rnd files

---
+++ src/rnd/index.js
@@ -0,0 +1,10 @@
+/*global require, global:false*/
+
+require('./editor');
+require('./render');
+require('./restruct');
+require('./restruct_rendering');
+require('./templates');
+require('./visel');
+
+global.rnd = global.rnd || {};

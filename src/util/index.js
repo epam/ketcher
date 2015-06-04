3cdce000e0c84d2b2918d2b50a3e10ea7a080ab9
Aleksey Elyseev <aelyseev@gmail.com>
2015-06-04 20:32:56 +0300

    Entry point for util

---
+++ src/util/index.js
@@ -0,0 +1,10 @@
+/*global require, global:false, rnd*/
+
+require('./ajax');
+require('./common');
+require('./map');
+require('./pool');
+require('./promise');
+require('./set');
+require('./vec2');
+require('./xhr');

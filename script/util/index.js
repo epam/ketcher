2aead5d5509a8d4a0141daee86997112a9e7704d
Nikolay Kuznetsov <Nikolay_Kuznetsov@epam.com>
2015-06-25 20:30:39 +0300

    Remove Vec2 & Box2Abs from util. namespace

--- script/util/index.js
+++ script/util/index.js
@@ -4,4 +4,5 @@ global.util = require('./common');
 global.util.Map = require('./map');
 global.util.Pool = require('./pool');
 global.util.Set = require('./set');
-require('./vec2');
+global.util.Vec2 = require('./vec2').Vec2;
+global.util.Box2Abs = require('./vec2').Box2Abs;

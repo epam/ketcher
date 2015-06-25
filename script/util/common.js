a7e601103dd9ed53687cc5af134c6adec0949544
Nikolay Kuznetsov <Nikolay_Kuznetsov@epam.com>
2015-06-25 19:56:15 +0300

    Make util/common.js exports more explicit

--- script/util/common.js
+++ script/util/common.js
@@ -1,6 +1,4 @@
-/*global require, global:false, util:false*/
-
-var util = {}; // jshint ignore:line
+/*global module*/
 
 Array.prototype.swap = function (i1, i2) { //eslint-disable-line
 	var tmp = this[i1];
@@ -8,20 +6,20 @@ Array.prototype.swap = function (i1, i2) { //eslint-disable-line
 	this[i2] = tmp;
 };
 
-util.tfx = function tfx(v) {
+var tfx = function (v) {
 	return (v - 0).toFixed(8);
 };
 
 // "each" function for an array
-util.each = function (array, func, context) {
-	util.assert(!util.isNullOrUndefined(array), 'array must be defined');
+var each = function (array, func, context) {
+	assert(!isNullOrUndefined(array), 'array must be defined');
 	for (var i = 0; i < array.length; ++i) {
 		func.call(context, array[i], i);
 	}
 };
 
-util.map_each = function (map, func, context) {
-	util.assert(!util.isNullOrUndefined(map), 'map must be defined');
+var map_each = function (map, func, context) {
+	assert(!isNullOrUndefined(map), 'map must be defined');
 	for (var key in map) {
 		if (map.hasOwnProperty(key)) {
 			func.call(context, key, map[key]);
@@ -29,7 +27,7 @@ util.map_each = function (map, func, context) {
 	}
 };
 
-util.find = function (array, func, context) {
+var find = function (array, func, context) {
 	for (var i = 0; i < array.length; ++i) {
 		if (func.call(context, array[i], i)) {
 			return i;
@@ -38,7 +36,7 @@ util.find = function (array, func, context) {
 	return -1;
 };
 
-util.findAll = function (array, func, context) {
+var findAll = function (array, func, context) {
 	var i;
 	var ret = [];
 	for (i = 0; i < array.length; ++i) {
@@ -49,7 +47,7 @@ util.findAll = function (array, func, context) {
 	return ret;
 };
 
-util.array = function (arrayLike) {
+var array = function (arrayLike) {
 	var a = [];
 	var i = arrayLike.length;
 	while (--i >= 0) {
@@ -58,7 +56,7 @@ util.array = function (arrayLike) {
 	return a;
 };
 
-util.isEmpty = function (obj) {
+var isEmpty = function (obj) {
 	for (var v in obj) {
 		if (obj.hasOwnProperty(v)) {
 			return false;
@@ -67,7 +65,7 @@ util.isEmpty = function (obj) {
 	return true;
 };
 
-util.stopEventPropagation = function (event) {
+var stopEventPropagation = function (event) {
 	if ('stopPropagation' in event) {// Mozilla, Opera, Safari
 		event.stopPropagation();
 	} else if ('cancelBubble' in event) {// IE
@@ -77,7 +75,7 @@ util.stopEventPropagation = function (event) {
 	}
 };
 
-util.preventDefault = function (event) {
+var preventDefault = function (event) {
 	if ('preventDefault' in event) {
 		event.preventDefault();
 	}
@@ -88,7 +86,7 @@ util.preventDefault = function (event) {
 	return false;
 };
 
-util.setElementTextContent = function (element, text) {
+var setElementTextContent = function (element, text) {
 	if ('textContent' in element) {// Mozilla, Opera, Safari
 		element.textContent = text;
 	} else if ('innerText' in element) {// IE and others (except Mozilla)
@@ -98,7 +96,7 @@ util.setElementTextContent = function (element, text) {
 	}
 };
 
-util.getElementTextContent = function (element) {
+var getElementTextContent = function (element) {
 	if ('textContent' in element) {// Mozilla, Opera, Safari
 		return element.textContent;
 	} else if ('innerText' in element) {// IE and others (except Mozilla)
@@ -108,7 +106,7 @@ util.getElementTextContent = function (element) {
 	throw Error('Browser unrecognized');
 };
 
-util.stringPadded = function (string, width, leftAligned) {
+var stringPadded = function (string, width, leftAligned) {
 	var str = string + '';
 	var space = '';
 	while (str.length + space.length < width) {
@@ -118,7 +116,7 @@ util.stringPadded = function (string, width, leftAligned) {
 	return (leftAligned) ? string + space : space + string;
 };
 
-util.idList = function (object) {
+var idList = function (object) {
 	var list = [];
 	for (var aid in object) {
 		if (object.hasOwnProperty(aid)) {
@@ -128,7 +126,7 @@ util.idList = function (object) {
 	return list;
 };
 
-util.mapArray = function (src, map) {
+var mapArray = function (src, map) {
 	var dst = [];
 	for (var i = 0; i < src.length; ++i) {
 		dst.push(map[src[i]]);
@@ -136,15 +134,15 @@ util.mapArray = function (src, map) {
 	return dst;
 };
 
-util.arrayMax = function (array) {
+var arrayMax = function (array) {
 	return Math.max.apply(Math, array);
 };
 
-util.arrayMin = function (array) {
+var arrayMin = function (array) {
 	return Math.min.apply(Math, array);
 };
 
-util.map = function (src, func, context) {
+var map = function (src, func, context) {
 	var dst = [];
 	for (var i = 0; i < src.length; ++i) {
 		dst.push(func.call(context, src[i]));
@@ -152,21 +150,21 @@ util.map = function (src, func, context) {
 	return dst;
 };
 
-util.apply = function (array, func) {
+var apply = function (array, func) {
 	for (var i = 0; i < array.length; ++i) {
 		array[i] = func(array[i]);
 	}
 };
 
-util.ifDef = function (dst, src, prop, def) {
+var ifDef = function (dst, src, prop, def) {
 	dst[prop] = !Object.isUndefined(src[prop]) ? src[prop] : def;
 };
 
-util.ifDefList = function (dst, src, prop, def) {
-	dst[prop] = !Object.isUndefined(src[prop]) && src[prop] !== null ? util.array(src[prop]) : def;
+var ifDefList = function (dst, src, prop, def) {
+	dst[prop] = !Object.isUndefined(src[prop]) && src[prop] !== null ? array(src[prop]) : def;
 };
 
-util.identityMap = function (array) {
+var identityMap = function (array) {
 	var map = {};
 	for (var i = 0; i < array.length; ++i) {
 		map[array[i]] = array[i];
@@ -174,38 +172,38 @@ util.identityMap = function (array) {
 	return map;
 };
 
-util.strip = function (src) {
+var strip = function (src) {
 	return src.replace(/\s*$/, '').replace(/^\s*/, '');
 };
 
-util.stripRight = function (src) {
+var stripRight = function (src) {
 	return src.replace(/\s*$/, '');
 };
 
-util.stripQuotes = function (str) {
+var stripQuotes = function (str) {
 	if (str[0] === '"' && str[str.length - 1] === '"') {
 		return str.substr(1, str.length - 2);
 	}
 	return str;
 };
 
-util.paddedFloat = function (number, width, precision) {
+var paddedFloat = function (number, width, precision) {
 	var numStr = number.toFixed(precision).replace(',', '.');
 	if (numStr.length > width) {
 		throw new Error('number does not fit');
 	}
-	return util.stringPadded(numStr, width);
+	return stringPadded(numStr, width);
 };
 
-util.paddedInt = function (number, width) {
+var paddedInt = function (number, width) {
 	var numStr = number.toFixed(0);
 	if (numStr.length > width) {
 		throw new Error('number does not fit');
 	}
-	return util.stringPadded(numStr, width);
+	return stringPadded(numStr, width);
 };
 
-util.arrayAddIfMissing = function (array, item) {
+var arrayAddIfMissing = function (array, item) {
 	for (var i = 0; i < array.length; ++i) {
 		if (array[i] === item) {
 			return false;
@@ -215,30 +213,30 @@ util.arrayAddIfMissing = function (array, item) {
 	return true;
 };
 
-util.assert = function (condition, comment) {
+var assert = function (condition, comment) {
 	if (!condition) {
 		throw new Error(comment ? ('Assertion failed: ' + comment) : 'Assertion failed');
 	}
 };
 
-util.assertDefined = function(v) {
-	util.assert(!util.isNullOrUndefined(v));
+var assertDefined = function(v) {
+	assert(!isNullOrUndefined(v));
 };
 
-util.isUndefined = function (variable) {
+var isUndefined = function (variable) {
 	return Object.isUndefined(variable); // use prototype.js method for now
 };
 
-util.isNull = function (variable) {
+var isNull = function (variable) {
 	return variable === null;
 };
 
-util.isNullOrUndefined = function (v) {
-	return util.isUndefined(v) || util.isNull(v);
+var isNullOrUndefined = function (v) {
+	return isUndefined(v) || isNull(v);
 };
 
-util.arrayRemoveByValue = function (array, item) {
-	util.assert(!util.isUndefined(array) && !util.isNull(array), 'array must be defined');
+var arrayRemoveByValue = function (array, item) {
+	assert(!isUndefined(array) && !isNull(array), 'array must be defined');
 	var idx = array.indexOf(item);
 	var cnt = 0;
 	while (idx >= 0) {
@@ -249,13 +247,13 @@ util.arrayRemoveByValue = function (array, item) {
 	return cnt;
 };
 
-util.listNextRotate = function (list, value) {
+var listNextRotate = function (list, value) {
 	return list[(list.indexOf(value) + 1) % list.length];
 };
 
 // similar to Object.assign
 // http://www.2ality.com/2014/01/object-assign.html
-util.extend = function (dest, src) {
+var extend = function (dest, src) {
 	for (var prop in src) {
 		if (src.hasOwnProperty(prop)) {
 			dest[prop] = src[prop];
@@ -264,11 +262,11 @@ util.extend = function (dest, src) {
 	return dest;
 };
 
-util.isObject = function (obj) {
+var isObject = function (obj) {
 	return obj === Object(obj);
 };
 
-util.eachAsync = function (list, process, timeGap, startTimeGap) {
+var eachAsync = function (list, process, timeGap, startTimeGap) {
 	return new Promise(function (resolve) {
 		var i = 0;
 		var n = list.length;
@@ -284,4 +282,41 @@ util.eachAsync = function (list, process, timeGap, startTimeGap) {
 	});
 };
 
-module.exports = util;
+module.exports = {
+	tfx: tfx,
+	each: each,
+	find: find,
+	findAll: findAll,
+	array: array,
+	isEmpty: isEmpty,
+	stopEventPropagation: stopEventPropagation,
+	preventDefault: preventDefault,
+	setElementTextContent: setElementTextContent,
+	getElementTextContent: getElementTextContent,
+	stringPadded: stringPadded,
+	idList: idList,
+	mapArray: mapArray,
+	arrayMax: arrayMax,
+	arrayMin: arrayMin,
+	map: map,
+	apply: apply,
+	ifDef: ifDef,
+	ifDefList: ifDefList,
+	identityMap: identityMap,
+	strip: strip,
+	stripRight: stripRight,
+	stripQuotes: stripQuotes,
+	paddedFloat: paddedFloat,
+	paddedInt: paddedInt,
+	arrayAddIfMissing: arrayAddIfMissing,
+	assert: assert,
+	assertDefined: assertDefined,
+	isUndefined: isUndefined,
+	isNull: isNull,
+	isNullOrUndefined: isNullOrUndefined,
+	arrayRemoveByValue: arrayRemoveByValue,
+	listNextRotate: listNextRotate,
+	extend: extend,
+	isObject: isObject,
+	eachAsync: eachAsync
+};

181f28a222e941caf5ee87e1a729de4df4f7b623
Nikolay Kuznetsov <Nikolay_Kuznetsov@epam.com>
2015-06-27 18:58:33 +0300

    Remove obsolete graphic tests

--- script/index.js
+++ script/index.js
@@ -1,9 +1,5 @@
 /*global require,exports,global:false*/
 
-var Raphael = require('./raphael-ext');
-
-var Box2Abs = require('./util/box2abs');
-var Vec2 = require('./util/vec2');
 var util = require('./util');
 
 require('./chem');
@@ -80,43 +76,6 @@ ketcher.onStructChange = function (handler) {
 	ui.render.addStructChangeHandler(handler);
 };
 
-ketcher.testSegment = function (clientArea) {
-	var sz = 600;
-	var bx = 50;
-	var by = 50;
-	var bw = sz;
-	var bh = sz;
-	var a;
-	var b;
-	var c;
-	var add;
-	var i;
-	var j;
-	var list = [];
-
-	b = new Box2Abs(bx, by, bx + bw, by + bh);
-	c = new Raphael(clientArea);
-
-	for (i = 0; i < 100000; ++i) {
-		a = new Vec2(Math.random() * sz + 50, Math.random() * sz + 50);
-		b = new Vec2(Math.random() * sz + 50, Math.random() * sz + 50);
-		add = true;
-		for (j = 0; j < list.length; ++j) {
-			if (Vec2.segmentIntersection(a, b, list[j][0], list[j][1])) {
-				add = false;
-				break;
-			}
-		}
-		if (add) {
-			list.push([a, b]);
-		}
-	}
-	c.rect(50, 50, sz, sz).attr({stroke: '#0f0'});
-	for (j = 0; j < list.length; ++j) {
-		c.path('M{0},{1}L{2},{3}', list[j][0].x, list[j][0].y, list[j][1].x, list[j][1].y).attr({'stroke-width': 1, stroke: '#000'});
-	}
-};
-
 // TODO: replace window.onload with something like <https://github.com/ded/domready>
 // to start early
 global.onload = function () {
@@ -142,49 +101,3 @@ global.onload = function () {
 	// Initialize UI
 	ui.init(paramHash);
 };
-
-/*
- ketcher.testShiftRayBox = function (clientArea)
- {
- var bx = 50, by = 180, bw = 220, bh = 40;
-
- var b = new Box2Abs(bx, by, bx + bw, by + bh);
- var c = new Raphael(clientArea);
-
- c.rect(0, 0, 300, 300).attr({stroke:'#0f0'});
- c.rect(bx, by, bw, bh).attr({stroke:'#00f'});
-
- for (var i = 0; i < 3; ++i)
- {
- var p = new Vec2(Math.random() * 300, Math.random() * 300);
- var d = new Vec2(Math.random() * 300, Math.random() * 300);
- var p1 = p.add(d);
- c.circle(p.x, p.y, 4).attr({fill:'#0f0'});
- c.path("M{0},{1}L{2},{3}", p.x, p.y, p1.x, p1.y).attr({'stroke-width':'3','stroke':'#f00'});
- var t = Math.max(0, Vec2.shiftRayBox(p, d, b));
- var p0 = p.addScaled(d, t / d.length());
- c.path("M{0},{1}L{2},{3}", p0.x, p0.y, p1.x, p1.y).attr({'stroke-width':'1','stroke':'#000'});
- }
- }
-
- ketcher.runTest = function(test, context) {
- var runs = [], nr = 5, diff;
-
- for (var r = 0; r < nr; ++r) {
- var start = (new Date).getTime();
- for ( var n = 0; (diff = (new Date).getTime() - start) < 1000; n++ )
- test.call(context);
- runs.push( diff / n );
- }
-
- var avg = 0, std = 0, t;
- for (r = 0; r < nr; ++r) {
- t = runs[r];
- avg += t;
- std += t * t;
- }
- avg /= nr;
- std = Math.sqrt(std - avg * avg) / nr;
- console.log(avg + ' ' + std);
- }
- */

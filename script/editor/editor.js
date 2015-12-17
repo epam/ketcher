2f08fcd6caef3ea756de380836d56e10dfb5d2f7
Daniil Savchuk <Daniil_Savchuk@epam.com>
2015-12-17 18:39:14 +0300

    +refactor :: move selectionhelper to self module

--- script/editor/editor.js
+++ script/editor/editor.js
@@ -11,13 +11,14 @@ var ReStruct = require('../rnd/restruct')
 var EditorTool = require('./editortool');
 var HoverHelper = require('./hoverhelper');
 var LassoHelper = require('./lassohelper');
+var SelectionHelper = require('./selectionhelper');
 
 var ui = global.ui;
 
 var Editor = function (render)
 {
 	this.render = render;
-	this._selectionHelper = new Editor.SelectionHelper(this);
+	this._selectionHelper = new SelectionHelper(this);
 };
 
 Editor.prototype.selectAll = function () {
@@ -102,47 +103,6 @@ Editor.prototype.getSelectionStruct = function () {
 		(dst.rxnArrows.count() || dst.rxnPluses.count());
 
 	return dst;
-};
-
-Editor.SelectionHelper = function (editor) {
-	this.editor = editor;
-};
-Editor.SelectionHelper.prototype.setSelection = function (selection, add) {
-	if (!('selection' in this) || !add) {
-		this.selection = {};
-		for (var map1 in ReStruct.maps) this.selection[map1] = []; // TODO it should NOT be mandatory
-	}
-	if (selection && 'id' in selection && 'map' in selection) {
-		(selection[selection.map] = selection[selection.map] || []).push(selection.id);
-	}
-	if (selection) {
-		for (var map2 in this.selection) {
-			if (map2 in selection) {
-				for (var i = 0; i < selection[map2].length; i++) {
-					if (this.selection[map2].indexOf(selection[map2][i]) < 0) {
-						this.selection[map2].push(selection[map2][i]);
-					}
-				}
-			}
-		}
-	}
-	this.editor.render.setSelection(this.selection);
-	this.editor.render.update();
-
-	ui.updateClipboardButtons(); // TODO notify ui about selection
-};
-Editor.SelectionHelper.prototype.isSelected = function (item) {
-	var render = this.editor.render;
-	var ctab = render.ctab;
-	if (item.map == 'frags' || item.map == 'rgroups') {
-		var atoms = item.map == 'frags' ?
-			ctab.frags.get(item.id).fragGetAtoms(render, item.id) :
-			ctab.rgroups.get(item.id).getAtoms(render);
-		return !Object.isUndefined(this.selection['atoms'])
-			 && Set.subset(Set.fromList(atoms), Set.fromList(this.selection['atoms']));
-	}
-	return 'selection' in this && !Object.isUndefined(this.selection[item.map]) &&
-	this.selection[item.map].indexOf(item.id) > -1;
 };
 
 Editor.EraserTool = function (editor, mode) {

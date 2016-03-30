e74332fe1d43d33491b5b2de82350d55331c6929
Nikolay Kuznetsov <Nikolay_Kuznetsov@epam.com>
2016-03-30 13:22:17 +0300

    Rewrite contextedit validation to single regexp. Address #40

--- script/ui/contextedit.js
+++ script/ui/contextedit.js
@@ -45,58 +45,26 @@ function serialize(lc) {
 	if (!lc.charge)
 		return lc.label;
 	var sign = lc.charge < 0 ? '-' : '+';
-	return lc.label + Math.abs(lc.charge) + sign;
+	var charge = Math.abs(lc.charge);
+	return lc.label + (charge > 1 ? charge: '') + sign;
 }
 
-// TODO: rewrite me to single regexp
 function deserialize(value) {
-	var label = '';
-	var charge = 0;
-	var value_arr = value.toArray();
-
-	if (value == '*') {
-		label = 'A';
+	var match = value.match(/^([a-z*]{1,3})(\d[-+]|[-+]\d|[-+])?$/i);
+	if (match) {
+		var label = match[1] == '*' ? 'A' : match[1].capitalize();
+		var charge = 0;
+		if (match[2]) {
+			charge = parseInt(match[2]);
+			if (isNaN(charge)) // NaN => [-+]
+				charge = 1;
+			if (match[2].endsWith('-'))
+				charge = -charge;
+		}
+		// Not consistant
+		if (label == 'A' || label == 'Q' || label == 'X' || element.getElementByLabel(label) != null)
+			return { label: label, charge: charge };
 	}
-	else if (value.match(/^[*][1-9]?[+-]$/i)) {
-		label = 'A';
-
-		if (value.length == 2)
-			charge = 1;
-		else
-			charge = parseInt(value_arr[1]);
-
-		if (value_arr[2] == '-')
-			charge *= -1;
-	}
-	else if (value.match(/^[A-Z]{1,2}$/i)) {
-		label = value.capitalize();
-	}
-	else if (value.match(/^[A-Z]{1,2}[0][+-]?$/i)) {
-		if (value.match(/^[A-Z]{2}/i))
-			label = value.substr(0, 2).capitalize();
-		else
-			label = value_arr[0].capitalize();
-	}
-	else if (value.match(/^[A-Z]{1,2}[1-9]?[+-]$/i)) {
-		if (value.match(/^[A-Z]{2}/i))
-			label = value.substr(0, 2).capitalize();
-		else
-			label = value_arr[0].capitalize();
-
-		var match = value.match(/[0-9]/i);
-
-		if (match != null)
-			charge = parseInt(match[0]);
-		else
-			charge = 1;
-
-		if (value_arr[value.length - 1] == '-')
-			charge *= -1;
-	}
-	// Not consistant
-	if (label == 'A' || label == 'Q' || label == 'X' || label == 'R' || element.getElementByLabel(label) != null)
-		return { label: label, charge: charge };
-
 	return null;
 }
 

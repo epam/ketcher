48e5e45978da6d442955fab3cd57731e1a815bab
Nikolay Kuznetsov <Nikolay_Kuznetsov@epam.com>
2016-07-15 12:28:17 +0300

    New default: no smart layout

--- script/index.js
+++ script/index.js
@@ -65,7 +65,9 @@ window.onload = function () {
 	var params = queryString.parse(document.location.search);
 	if (params.api_path)
 		ketcher.api_path = params.api_path;
-	ketcher.server = api(ketcher.api_path);
+	ketcher.server = api(ketcher.api_path, {
+		'indigo-smart-layout': 'false'
+	});
 	ui.init(Object.assign({}, params), ketcher.server);
 };
 

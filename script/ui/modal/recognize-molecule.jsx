3e5423d9c76be660a74a49a3b72525c8db8fce6c
Maria_Kuznetsova <Maria_Kuznetsova@epam.com>
2016-08-16 12:53:42 +0300

    Loading picture

--- script/ui/modal/recognize-molecule.jsx
+++ script/ui/modal/recognize-molecule.jsx
@@ -3,54 +3,15 @@ import { h, Component, render } from 'preact';
 
 import Dialog from './dialog';
 
-/*function readSingleFile(e) {
-  var file = e.target.files[0];
-  if (!file) {
-    return;
-  }
-  var reader = new FileReader();
-  reader.onload = function(e) {
-    var contents = e.target.result;
-    //displayContents(contents);
-  };
-  reader.readAsText(file);
-}
-
-/*function displayContents(contents) {
-  var element = document.getElementById('file-content');
-  element.innerHTML = contents;
-}*/
-
-function readURL(input) {
-        if (input.files && input.files[0]) {
-            console.log("22222");
-            var reader = new FileReader();
-
-            reader.onload = function (e) {
-               /* $('#blah')
-                    .attr('src', e.target.result)
-                    .width(150)
-                    .height(200);*/
-                    $('#pic').attr('src', e.target.result);
-            };
-
-            reader.readAsDataURL(input.files[0]);
-        }
-}
-
 class RecognizeMolecule extends Component {
     result () {
         return `Yo!`;
     }
     onChange(event) {
         var URL = window.URL || window.webkitURL;
-        var imageUrl,
-        image;
-
-        var reader = new FileReader();
-
+        var imageUrl;
+        var image;
         var file = document.getElementById('input').files[0];
-
         if (URL) {
             imageUrl = URL.createObjectURL(file);
             image = document.getElementById("pic");
@@ -65,8 +26,8 @@ class RecognizeMolecule extends Component {
             <Dialog caption="Import From Image"
                     name="recognize-molecule" params={props.params}
                     result={() => this.result()}>
-              <textarea></textarea>
               <img id="pic" src="" />
+              <img />
               <label class="open">
                  <input type="checkbox"></input>
                  Load as a fragment and copy to the Clipboard 

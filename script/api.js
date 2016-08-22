f45285e30abc7587b49c48aab188fca803677886
Nikolay Kuznetsov <Nikolay_Kuznetsov@epam.com>
2016-08-22 16:53:08 +0300

    Extend API to support imago pollig. Address #278

--- script/api.js
+++ script/api.js
@@ -1,43 +1,93 @@
+function pollDeferred(process, complete, timeGap, startTimeGap) {
+	return new Promise(function (resolve, reject) {
+		function iterate() {
+			process().then(function (val) {
+				try {
+					var finish = complete(val);
+					if (finish)
+						resolve(val);
+					else
+						setTimeout(iterate, timeGap);
+				} catch (e) {
+					reject(e);
+				}
+			}, function (err) {
+				return reject(err);
+			});
+		}
+		setTimeout(iterate, startTimeGap || 0);
+	});
+}
+
+function parametrizeUrl(url, params) {
+	return url.replace(/:(\w+)/g, function (_, val) {
+		return params[val];
+	});
+}
+
 function api(base, defaultOptions) {
 	var baseUrl = !base || /\/$/.test(base) ? base : base + '/';
 
-	function request(method, url, defaultData) {
+	function request(method, url, data, headers) {
+		if (data && method == 'GET')
+			url = parametrizeUrl(url, data);
+		return fetch(baseUrl + url, {
+			method: method,
+			headers: Object.assign({
+				Accept: 'application/json'
+			}, headers),
+			body: method != 'GET' ? data : null
+		}).then(function (response) {
+			if (response.ok)
+				return response.json();
+			// console.info('PLAIN RESPONSE', response);
+			throw response.json();
+		});
+	}
+
+	function indigoCall(method, url, defaultData) {
 		return function (data, options) {
 			var body = Object.assign({}, defaultData, data);
 			body.indigo_options = Object.assign(body.indigo_options || {},
 			                                    defaultOptions, options);
-			return fetch(baseUrl + url, {
-				method: method,
-				headers: {
-					'Accept': 'application/json',
-					'Content-Type': 'application/json'
-				},
-				body: JSON.stringify(body)
-			}).then(function (response) {
-				if (response.ok)
-					return response.json();
-				// console.info('PLAIN RESPONSE', response);
-				throw response.json();
+			return request(method, url, JSON.stringify(body), {
+				'Content-Type': 'application/json'
 			});
 		};
 	}
 
-	var info = fetch(baseUrl + 'info', { method: 'GET' }).then(function (res) {
-		return res.json();
-	}, function () {
+
+	var info = request('GET', 'indigo/info').then(null, function () {
 		throw Error('Server is not compatible');
 	});
 
 	return Object.assign(info, {
-		convert: request('POST', 'convert'),
-		layout: request('POST', 'layout'),
-		clean: request('POST', 'clean'),
-		aromatize: request('POST', 'aromatize'),
-		dearomatize: request('POST', 'dearomatize'),
-		calculateCip: request('POST', 'calculate_cip'),
-		automap: request('POST', 'automap'),
-		check: request('POST', 'check'),
-		calculate: request('POST', 'calculate')
+		convert: indigoCall('POST', 'indigo/convert'),
+		layout: indigoCall('POST', 'indigo/layout'),
+		clean: indigoCall('POST', 'indigo/clean'),
+		aromatize: indigoCall('POST', 'indigo/aromatize'),
+		dearomatize: indigoCall('POST', 'indigo/dearomatize'),
+		calculateCip: indigoCall('POST', 'indigo/calculate_cip'),
+		automap: indigoCall('POST', 'indigo/automap'),
+		check: indigoCall('POST', 'indigo/check'),
+		calculate: indigoCall('POST', 'indigo/calculate'),
+		recognize: function (blob) {
+			var req = request('POST', 'imago/uploads', blob, {
+				'Content-Type': blob.type || 'application/octet-stream'
+			});
+			var status = request.bind(null, 'GET', 'imago/uploads/:id');
+			return req.then(function (res) {
+				return pollDeferred(
+					status.bind(null, { id: res.upload_id }),
+					function complete(res) {
+						if (res.state === 'FAILURE')
+							throw res;
+						return res.state === 'SUCCESS';
+					}, 500, 300);
+			}).then(function correct(res) {
+				return { struct: res.metadata.mol_str };
+			});
+		}
 	});
 }
 

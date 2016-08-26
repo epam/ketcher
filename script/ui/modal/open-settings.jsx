42df58283f1a6a1ba120ee8dde4cfc4f25a5c076
Maria_Kuznetsova <Maria_Kuznetsova@epam.com>
2016-08-26 12:55:24 +0300

    Fabric for options

--- script/ui/modal/open-settings.jsx
+++ script/ui/modal/open-settings.jsx
@@ -4,6 +4,17 @@ import { h, Component, render } from 'preact';
 import Dialog from './dialog';
 
 class OpenSettings extends Component {
+    opts = [
+        {name: 'render-atom-ids-visible',
+        type: 'boolean',
+        defaultValue: false,
+        tab: 'chemul'},
+        {name: 'render-bond-ids-visible',
+        type: 'boolean',
+        defaultValue: false,
+        tab: 'chemul'}
+    ]
+
      constructor(props) {
         super(props);
         this.state = {
@@ -13,11 +24,38 @@ class OpenSettings extends Component {
     result () {
         return `Yo!`;
     }
-    showInfo (ev) {
+    /*showInfo (ev) {
         console.log(ev)
         this.setState({
             selectedItem: true //?
         })
+    }*/
+    createHtmlCheckbox(value, name, div) {
+        var input = $('<input>');
+        if (value === true) {
+            input.attr({'type': 'checkbox', 'checked': true, 'id': name});
+        } else {
+            input.attr({'type': 'checkbox', 'checked': false, 'id': name});
+        }
+        div.append(input);
+        input.change(function () {
+            onChangeInSetupIndigo();
+        });
+    }
+    createHtmlElement(values, name, container, func) {
+        func(values, name, container);
+        container.append(name);
+        container.append($('<br>'));
+    }
+    fillHtmlContainer(container) {
+        this.props.opts = opts;
+          this.state.opts = {
+            'render-bond-ids-visible': true,
+            'hide-chiral': true
+          }
+        container.empty();
+        for (i = 0; i < opts.length; i++)
+            createHtmlElement(opts[i].defaultValue, opts[i].name, container, createHtmlCheckbox);
     }
     render (props) {
         return (
@@ -34,7 +72,7 @@ class OpenSettings extends Component {
             <div>
                 <ul class="accordion-menu">
                     <li class="has-children">
-                        <input type="checkbox" id="atoms" checked></input>
+                        <input type="checkbox" class="menu" id="atoms" checked></input>
                             <label for="atoms">Atoms</label>
                                 <ul>
                                     <li><output>Atom label margin</output></li>
@@ -42,14 +80,14 @@ class OpenSettings extends Component {
                                 </ul>
                     </li>
                     <li class="has-children">
-                        <input type="checkbox" id="attachedData" checked></input>
+                        <input type="checkbox" class="menu" id="attachedData" checked></input>
                             <label for="attachedData">Attached data</label>
                                 <ul>
                                     <li><output>Diplay attached data</output></li>
                                 </ul>
                     </li>
                     <li class="has-children">
-                        <input type="checkbox" id="bonds" checked></input>
+                        <input type="checkbox" class="menu" id="bonds" checked></input>
                             <label for="bonds">Bonds</label>
                                 <ul>
                                     <li><output>Aromatic bond as circle</output></li>
@@ -57,13 +95,10 @@ class OpenSettings extends Component {
                                 </ul>
                     </li>
                     <li class="has-children">
-                        <input type="checkbox" id="chemul" checked></input>
+                        <input type="checkbox" class="menu" id="chemul" checked></input>
                             <label for="chemul">Chemul</label>
                                 <ul>
-                                    <select>
-                                      <option value="">on</option>
-                                      <option value="">off</option>
-                                    </select>
+                                    { test() }
                                     <li> <output>Do not show the 'Chiral' flag</output> <input type="checkbox" checked/></li>
                                     <li><output>Show the Data S-Group Tool</output></li>
                                 </ul>
@@ -75,9 +110,36 @@ class OpenSettings extends Component {
     }
 }
 
+function test() {
+    var opts = [
+        {name: 'render-atom-ids-visible',
+        type: 'boolean',
+        defaultValue: false,
+        tab: 'chemul'},
+        {name: 'render-bond-ids-visible',
+        type: 'boolean',
+        defaultValue: false,
+        tab: 'chemul'}
+    ]
+
+    var stationComponents = opts.map(function(station) {
+            return (
+            <li>
+                <output>Some text</output>
+                <select>
+                <option value="">on</option>
+                <option value="" selected="selected">off</option>
+                </select>
+            </li>);
+        });
+    return <div>{stationComponents}</div>;
+}
+
 
 export default function dialog(params) {
     var overlay = $$('.overlay')[0];
+    var t = test();
+    console.info('test', test);
     return render((
         <OpenSettings params={params}/>
     ), overlay);

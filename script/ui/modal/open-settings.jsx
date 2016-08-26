6ab3bb13b640fe064ec984fb74a1652f53358d1d
Maria_Kuznetsova <Maria_Kuznetsova@epam.com>
2016-08-26 19:19:43 +0300

    State

--- script/ui/modal/open-settings.jsx
+++ script/ui/modal/open-settings.jsx
@@ -5,87 +5,99 @@ import Dialog from './dialog';
 import defaultOptions from './options';
 
 class OpenSettings extends Component {
-    /*opts = [
-        {name: 'showAtomIds',
-        type: 'boolean',
-        defaultValue: false,
-        tab: 'atoms'},
-        {name: 'hideImplicitHydrogen',
-        type: 'boolean',
-        defaultValue: false,
-        tab: 'atoms'},
-
-        {name: 'Display attached data',
-        type: 'boolean',
-        defaultValue: false,
-        tab: 'attachedData'},
-
-        {name: 'bondLength',
-        type: 'number',
-        defaultValue: 75,
-        values: [75, 100, 150],
-        tab: 'bonds'},
-        {name: 'showSelectionRegions',
-        type: 'boolean',
-        defaultValue: false,
-        tab: 'bonds'},
-        {name: 'showBondIds',
-        type: 'boolean',
-        defaultValue: false,
-        tab: 'bonds'},
-        {name: 'showHalfBondIds',
-        type: 'boolean',
-        defaultValue: false,
-        tab: 'bonds'},
-        {name: 'showLoopIds',
-        type: 'boolean',
-        defaultValue: false,
-        tab: 'bonds'},
-
-        {name: 'autoScale',
-        type: 'boolean',
-        defaultValue: false,
-        tab: 'scaling'},
-        {name: 'autoScaleMargin',
-        type: 'number',
-        defaultValue: 4,
-        values: [2, 4, 8],
-        tab: 'scaling'},
-
-        {name: 'Do not show the "Chiral" flag',
-        type: 'boolean',
-        defaultValue: false,
-        tab: 'chemul'},
-        {name: 'Show the Data S-Group Tool',
-        type: 'boolean',
-        defaultValue: false,
-        tab: 'chemul'}
-    ]*/
-
      constructor(props) {
         super(props);
-        /*this.state = {
-            selectedItem: null
-        }*/
         this.props.opts = defaultOptions();
+        var tmp = {};
+        for (var i = 0; i < this.props.opts.length; i++) {
+            tmp[this.props.opts[i].name] = this.props.opts[i].defaultValue;
+        }
+        this.setState(tmp);
+        console.log("state", this.state);
+
+        this.changeState = this.changeState.bind(this);
     }
     result () {
         return `Yo!`;
     }
-    /*showInfo (ev) {
-        console.log(ev)
+    uploadSettings(ev) {
         this.setState({
-            selectedItem: true //?
-        })
-    }*/
+                file: ev.target.files[0]
+        });
+    }
+    load (ev) {
+        var file = toString(this.state.file);
+        var userOpts = file.split('},');
+        console.log("userOpts: ", userOpts)
+        //
+        //this.props.opts = Object.assign(this.props.opts, userOpts);
+    }
+
+    changeState(ev, value) {
+        console.log("ev", ev.target.id);
+        console.log("state1", this.state);
+        console.log("11111111111");
+        var tmp = {};
+        tmp[ev.target.id] = ev.target.value;
+        Object.assign(this.state, tmp);
+        console.log("state1", this.state);
+        console.log("22222222222");
+    }
+
+    createSelectListItem(f, values, defaultOption, name) {
+        var listComponents = values.map(function(value) {
+            var booleanValue = false;
+            if (value === "on")
+                booleanValue = true;
+
+            if (value === defaultOption) {
+                return ( <option value={booleanValue} selected="selected" > {value} </option>);
+            } else {
+                return ( <option value={booleanValue}> {value} </option>);
+            }
+        });
+        return (
+            <li>
+                <output> { name } </output>
+                <select  id={name} onChange = { ev => f(ev, name) }>{listComponents}</select>
+            </li>
+            );
+    }
+
+    test(opts, tab) {
+        var f = this.changeState;
+        var createItem = this.createSelectListItem;
+        var optsComponents = opts.map(function(elem) {
+            var values;
+            var defaultValue;
+            if (elem.type === 'boolean') {
+                values = ['on', 'off'];
+                if (elem.defaultValue === true)
+                    defaultValue = "on";
+                 else
+                    defaultValue = "off";
+            } else{
+                values = elem.values;
+                defaultValue = elem.defaultValue;
+            }
+            if (elem.tab === tab) {
+                return (
+                    createItem(f, values, defaultValue, elem.name)
+                );
+            }
+        });
+        return <div>{optsComponents}</div>;
+    }
+
     render (props) {
         return (
             <Dialog caption="Settings"
                     name="open-settings" params={props.params}
                     result={() => this.result()}
                      buttons={[
-                        <button>Load...</button>,
-                        <button>Save as...</button>,
+                        <input type="file" onChange={ ev => this.uploadSettings(ev) }/>,
+                        <button onClick={ ev => this.load(ev) }>Load</button>,
+                        <button onClick={ ev => this.changeState(ev) }>Save as...</button>,
                         <button>Reset</button>,
                         <button>Apply and Save</button>,
                         <button>Apply</button>,
@@ -96,35 +108,35 @@ class OpenSettings extends Component {
                         <input type="checkbox" class="menu" id="atoms" checked></input>
                             <label for="atoms">Atoms</label>
                                 <ul>
-                                    { test(props.opts, "atoms") }
+                                    { this.test(props.opts, "atoms") }
                                 </ul>
                     </li>
                     <li class="has-children">
                         <input type="checkbox" class="menu" id="attachedData" checked></input>
                             <label for="attachedData">Attached data</label>
                                 <ul>
-                                    { test(props.opts, "attachedData") }
+                                    { this.test(props.opts, "attachedData") }
                                 </ul>
                     </li>
                     <li class="has-children">
                         <input type="checkbox" class="menu" id="bonds" checked></input>
                             <label for="bonds">Bonds</label>
                                 <ul>
-                                    { test(props.opts, "bonds") }
+                                    { this.test(props.opts, "bonds") }
                                 </ul>
                     </li>
                     <li class="has-children">
                         <input type="checkbox" class="menu" id="scaling" checked></input>
                             <label for="scaling">Scaling</label>
                                 <ul>
-                                    { test(props.opts, "scaling") }
+                                    { this.test(props.opts, "scaling") }
                                 </ul>
                     </li>
                     <li class="has-children">
                         <input type="checkbox" class="menu" id="chemul" checked></input>
                             <label for="chemul">Chemul</label>
                                 <ul>
-                                    { test(props.opts, "chemul") }
+                                    { this.test(props.opts, "chemul") }
                                 </ul>
                     </li>    
                 </ul>
@@ -132,42 +144,6 @@ class OpenSettings extends Component {
             </Dialog>
         );
     }
-}
-
-function createSelectListItem(values, defaultOption) {
-    var listComponents = values.map(function(value) {
-        if (value === defaultOption) {
-            return ( <option value="" selected="selected" > {value} </option>);
-        } else {
-            return ( <option value=""> {value} </option>);
-        }
-    });
-    return <select>{listComponents}</select>;
-}
-
-function test(opts, tab) {
-    var optsComponents = opts.map(function(elem) {
-        var values;
-        var defaultValue;
-        if (elem.type === 'boolean') {
-            values = ['on', 'off'];
-            if (elem.defaultValue === true)
-                defaultValue = "on";
-             else
-                defaultValue = "off";
-        } else{
-            values = elem.values;
-            defaultValue = elem.defaultValue;
-        }
-        if (elem.tab === tab) {
-            return (
-            <li>
-                <output> { elem.name } </output>
-                {createSelectListItem(values, defaultValue)}
-            </li>);
-        }
-    });
-    return <div>{optsComponents}</div>;
 }
 
 

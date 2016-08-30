efff3e79a748e635a06fb1fe62be07dcef8a3e7c
Victor_Komlev <Victor_Komlev@epam.com>
2016-08-30 16:42:24 +0300

    Check struct schema

--- script/ui/modal/check-struct.jsx
+++ script/ui/modal/check-struct.jsx
@@ -4,103 +4,54 @@ import { h, Component, render } from 'preact';
 import Dialog from './dialog';
 import molfile from '../../chem/molfile';
 
-class CheckStruct extends Component {
+const checkScheckSchema = [
+	{ title: 'Valence', value: 'valence' },
+	{ title: 'Radical', value: 'radicals'},
+	{ title: 'Pseudoatom', value: 'pseudoatoms'},
+	{ title: 'Stereochemistry', value: 'stereo' },
+	{ title: 'Query', value: 'query' },
+	{ title: 'Overlapping Atoms', value: 'overlapping_atoms'},
+	{ title: '3D Structure', value: '3d'}
+];
 
+class CheckStruct extends Component {
     constructor(props) {
       super(props);
       this.state = {
       		tabIndex: 0,
-      		data: null,
-      		checker:[],
+      		data: {},
+      		checker: {},
       	}
-      this.props.checked = [false,false,false,false,false,false,false];
-      this.tabs = ['Check', 'Settings'];
-      this.checkbox = ['Valence Check','Radical Check','Pseudoatom Check',
-      				'Stereochemistry Check','Query Check',
-      				'Overlapping Atoms Check','3D Structure Check'];
-  }
+  	}
     changeTab(ev, index) {
       this.setState({
         tabIndex: index,
       });
+      if (index == 0)
+      	  this.doCheck();
       ev.preventDefault();
     }
-    doAdd(parametr){
-    	this.setState({
-    		checker:this.state.checker.concat([parametr])
-    		})
-    };
-    doDelete(parametr){
-    	this.setState({
-    		checker:this.state.checker.splice(this.state.checker.indexOf(parametr),1)
-    		})
-    };
     doCheck() {
+    	var checks = Object.keys(this.state.checker).filter(v => this.state.checker[v]);
         this.props.server.check({ struct: molfile.stringify(this.props.struct),
-                                  checks: this.state.checker})
+                                  checks: checks})
             .then(res => this.setState({
                 	data: res
                 })
            	)
-           	return JSON.stringify(this.state.data);
     };
-    doChecked(index){
-    	if (this.props.checked[index] == false){
-    		this.props.checked[index] = true;
-    	}else{
-    		this.props.checked[index] =false;
-    	}
-    };
-    UpdateCheckerList(ev,index){
-    	this.doChecked(index);
-    	if(index==0){
-    		this.doAdd('valence');
-    	}else{
-    		this.doDelete('valence');
-    	}
-    	if(index==1){
-    		this.doAdd('radicals');
-    	}else{
-    		this.doDelete('radicals');
-    	}
-    	if(index==2){
-    		this.doAdd('pseudoatoms');
-    	}else{
-    		this.doDelete('pseudoatoms');
-    	}
-    	if(index==3){
-    		this.doAdd('stereo');
-    	}else{
-    		this.doDelete('stereo');
-    	}
-    	if(index==4){
-    		this.doAdd('query');
-    	}else{
-    		this.doDelete('query');
-    	}
-    	if(index==5){
-    		this.doAdd('overlapping_atoms');
-    	}else{
-    		this.doDelete('overlapping_atoms');
-    	}
-    	if(index==6){
-    		this.doAdd('3d');
-    	}else{
-    		this.doDelete('3d');
-    	}
-    	this.doCheck();
-    };
-
-    result () {
-        return `Yo!`;
+    checkItem(val) {
+    	this.state.checker[val] = !this.state.checker[val];
+    	this.setState(this.state.checker);
     }
     render (props) {
+    	var tabs = ['Check', 'Settings'];
         return (
             <Dialog caption="Structure Check"
                     name="check-struct" params={props}
-                    result={() => this.result()} buttons={[ "Cancel"]}>
+                    buttons={[ "Cancel"]}>
               <ul class="tabs">
-                { this.tabs.map((caption, index) => (
+                { tabs.map((caption, index) => (
                   <li class={this.state.tabIndex == index ? 'active' : ''}>
                      <a onClick={ ev => this.changeTab(ev, index) }>{caption}</a>
                   </li>
@@ -108,15 +59,22 @@ class CheckStruct extends Component {
                 }
               </ul>
               {[(
-                <div tabTitle = "Check">
-                    <output>{this.doCheck()}</output>
+                <div>
+                <ul>{
+					checkScheckSchema.filter(item => !!this.state.data[item.value]).map(item =>(
+                		<li><label>{item.title} Error<input type="text" disabled="true"
+                		                        value={this.state.data[item.value]}/></label></li>
+                	))
+                }
+                </ul>
                 </div>
                 ), (
-                <div tabTitle = "Setting">
-                <ul>{this.checkbox.map((caption,index) =>(
-                	<label><input type="checkbox" checked={this.props.checked[index]} onChange={ev => this.UpdateCheckerList(ev,index)}/>
-                	{caption}<br />
-                	</label>
+                <div>
+                <ul>{checkScheckSchema.map((item) =>(
+                	<li><label><input type="checkbox" checked={this.state.checker[item.value]}
+                				onClick={ev => this.checkItem(item.value)}/>
+                	{item.title} Check
+                	</label></li>
                 	))
                 }
                 </ul>

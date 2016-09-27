7d6798e59ba713ba408f294307cfeaed6a009fe3
Victor_Komlev <Victor_Komlev@epam.com>
2016-09-27 17:42:34 +0300

    change the list ordering

--- script/ui/modal/check-struct.jsx
+++ script/ui/modal/check-struct.jsx
@@ -11,8 +11,8 @@ const checkScheckSchema = [
 	{ title: 'Stereochemistry', value: 'stereo' },
 	{ title: 'Query', value: 'query' },
 	{ title: 'Overlapping Atoms', value: 'overlapping_atoms'},
-	{ title: '3D Structure', value: '3d'},
-	{ title: 'Overlapping Bonds', value: 'ovrlapping_bonds'}
+	{ title: 'Overlapping Bonds', value: 'ovrlapping_bonds'},
+	{ title: '3D Structure', value: '3d'}
 ];
 
 class CheckStruct extends Component {

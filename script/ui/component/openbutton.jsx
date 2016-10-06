dec265712f5b0981fa6ba19a9c0a633be5363e1f
Nikolay Kuznetsov <Nikolay_Kuznetsov@epam.com>
2016-10-06 17:48:50 +0300

    Extract open file button to components

---
+++ script/ui/component/openbutton.jsx
@@ -0,0 +1,81 @@
+import { h, Component } from 'preact';
+/** @jsx h */
+
+class OpenButton extends Component {
+	constructor(props) {
+		super(props);
+		fileOpener(props.server).then(f => {
+			this.setState({
+				opener: f
+			});
+		});
+	}
+	open(ev) {
+		let files = ev.target.files;
+		let noop = () => null;
+		let { onLoad=noop, onError=noop } = this.props;
+		if (this.state.opener && files.length)
+			this.state.opener(files[0]).then(onLoad, onError);
+		ev.preventDefault();
+	}
+	render() {
+		let { children, accept } = this.props;
+		return (
+			<label disabled={ !this.state.opener } { ...this.props }>
+				{ children }
+				<input onChange={ ev => this.open(ev) }
+					accept={ accept } type="file"/>
+			</label>
+		);
+	}
+}
+
+function fileOpener (server) {
+	return new Promise((resolve, reject) => {
+		// TODO: refactor return
+		if (global.FileReader)
+			resolve(throughFileReader);
+
+		else if (global.ActiveXObject) {
+			try {
+				var fso = new ActiveXObject('Scripting.FileSystemObject');
+				resolve(file => Promise.resolve(throughFileSystemObject(fso, file)));
+			} catch (e) {
+				reject(e);
+			}
+		} else if (server) {
+			resolve(server.then(() => {
+				throw "Server doesn't still support echo method";
+				//return resolve(throughForm2IframePosting);
+			}));
+		} else
+			reject(new Error("Your browser does not support "  +
+							 "opening files locally"));
+	});
+}
+
+function throughFileReader(file) {
+	return new Promise((resolve, reject) => {
+		var rd = new FileReader();
+		rd.onload = event => {
+			var content = rd.result;
+			if (file.msClose)
+				file.msClose();
+			resolve(content);
+		};
+		rd.onerror = event => {
+			reject(event);
+		};
+		rd.readAsText(file, 'UTF-8');
+	});
+}
+
+function throughFileSystemObject(fso, file) {
+	// IE9 and below
+	var fd =  fso.OpenTextFile(file.name, 1),
+	content = fd.ReadAll();
+	fd.Close();
+	return content;
+}
+
+export default OpenButton;
--- script/ui/modal/open.jsx
+++ script/ui/modal/open.jsx
@@ -3,6 +3,7 @@ import { h, Component, render } from 'preact';
 
 import { map as formatMap } from '../structformat';
 import Dialog from '../component/dialog';
+import OpenButton from '../component/openbutton';
 
 var ui = global.ui;
 
@@ -11,14 +12,8 @@ class Open extends Component {
 		super(props);
 		this.state = {
 			structStr: '',
-			fragment: false,
-			fileOpener: null
+			fragment: false
 		};
-		fileOpener(props.server).then(f => {
-			this.setState({
-				fileOpener: f
-			});
-		});
 	}
 	result() {
 		return this.state.structStr ? {
@@ -26,48 +21,34 @@ class Open extends Component {
 			fragment: this.state.fragment
 		} : null;
 	}
-	changeStructStr(ev) {
+	changeStructStr(target) {
 		this.setState({
-			structStr: ev.target.value.trim()
+			structStr: target.value ? target.value.trim() : target
 		});
 	}
-	changeFragment(ev) {
+	changeFragment(target) {
 		this.setState({
-			fragment: ev.target.checked
+			fragment: target.checked
 		});
-	}
-	open(ev) {
-		var files = ev.target.files;
-		if (this.state.fileOpener && files.length) {
-			this.state.fileOpener(files[0]).then(structStr => {
-				this.setState({ structStr });
-			}, ui.echo);
-		}
-		ev.preventDefault();
-	}
-	accept() {
-		return Object.keys(formatMap).reduce((res, key) => (
-			res.concat(formatMap[key].mime, ...formatMap[key].ext)
-		), []).join(',');
 	}
 	render () {
-		let { structStr, fragment, fileOpener } = this.state;
+		let { structStr, fragment } = this.state;
 		return (
 			<Dialog caption="Open Structure"
 				name="open" result={() => this.result() }
 				params={this.props}
 				buttons={[(
-					<label className="open" disabled={!fileOpener}>
+					<OpenButton className="open" accept={structMime()}
+								server={this.props.server}
+								onLoad={ s => this.changeStructStr(s) }>
 						Open From File…
-						<input onChange={ ev => this.open(ev) }
-							accept={ this.accept() } type="file"/>
-					</label>
+					</OpenButton>
 				), "Cancel", "OK"]}>
 				<textarea value={structStr}
-			              onInput={ ev => this.changeStructStr(ev) } />
+			              onInput={ ev => this.changeStructStr(ev.target) } />
 				<label>
 				<input type="checkbox" checked={fragment}
-			           onClick={ev => this.changeFragment(ev)} />
+			           onClick={ev => this.changeFragment(ev.target)} />
 				    Load as a fragment
 			    </label>
 			</Dialog>
@@ -75,51 +56,12 @@ class Open extends Component {
 	}
 }
 
-function fileOpener (server) {
-	function throughFileReader(file) {
-		return new Promise((resolve, reject) => {
-			var rd = new FileReader();
-			rd.onload = event => {
-				var content = rd.result;
-				if (file.msClose)
-					file.msClose();
-				resolve(content);
-			};
-			rd.onerror = event => {
-				reject(event);
-			};
-			rd.readAsText(file, 'UTF-8');
-		});
-	}
-	function throughFileSystemObject(fso, file) {
-		// IE9 and below
-		var fd =  fso.OpenTextFile(file.name, 1),
-		content = fd.ReadAll();
-		fd.Close();
-		return content;
-	}
-	return new Promise((resolve, reject) => {
-		// TODO: refactor return
-		if (global.FileReader)
-			resolve(throughFileReader);
-
-		else if (global.ActiveXObject) {
-			try {
-				var fso = new ActiveXObject('Scripting.FileSystemObject');
-				resolve(file => Promise.resolve(throughFileSystemObject(fso, file)));
-			} catch (e) {
-				reject(e);
-			}
-		}
-		else
-			resolve(server.then(() => {
-				throw "Server doesn't still support echo method";
-				//return resolve(throughForm2IframePosting);
-			}, () => {
-				throw new Error("Standalone mode!");
-			}));
-	});
+function structMime() {
+	return Object.keys(formatMap).reduce((res, key) => (
+		res.concat(formatMap[key].mime, ...formatMap[key].ext)
+	), []).join(',');
 }
+
 
 export default function dialog(params) {
 	var overlay = $$('.overlay')[0];

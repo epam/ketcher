8929245ab00820c2a0bc19960b2e64e5d6ba14e2
Nikita_Ryzhov <Nikita_Ryzhov@epam.com>
2016-10-11 20:38:19 +0300

    Component Tabs

---
+++ script/ui/component/tabs.jsx
@@ -0,0 +1,34 @@
+import {h, Component} from 'preact';
+/** @jsx h */
+
+class Tabs extends Component {
+	constructor(props) {
+		super(props);
+		this.state.tabIndex = 0;
+	}
+	changeTab(ev, index) {
+		this.setState({	tabIndex: index });
+		this.props.changeTab(index);
+	}
+
+	render() {
+		let {children, captions, ...props} = this.props;
+		return (
+			<div {...props}>
+				<ul className="tabs">
+					{ captions.map((caption, index) => (
+						<li className={this.state.tabIndex == index ? 'active' : ''}
+							 onClick={ ev => this.changeTab(ev, index)}>
+							{caption}
+						</li>
+					)) }
+				</ul>
+				<div className="tabs-content">
+					{ children[this.state.tabIndex] }
+				</div>
+			</div>
+		);
+	}
+}
+
+export default Tabs;

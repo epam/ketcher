572b389ca956bd2327d54e8ba26844615b1500fe
Nikolay Kuznetsov <Nikolay_Kuznetsov@epam.com>
2016-10-13 15:12:52 +0300

    Struct render could accept struct string

--- script/ui/component/structrender.jsx
+++ script/ui/component/structrender.jsx
@@ -1,30 +1,48 @@
 import { h, Component } from 'preact';
 /** @jsx h */
 
+import Struct from '../../chem/struct';
+import molfile from '../../chem/molfile';
 import Render from '../../render';
 
-function renderStruct(el, struct) {
+function renderStruct(el, struct, options={}) {
 	if (el) {
 		if (struct.prerender)           // Should it sit here?
 			el.innerHTML = struct.prerender;
 		else {
 			var rnd = new Render(el, 0, {
-				'autoScale': true,
-				'autoScaleMargin': 0,
-				'maxBondLength': 30
+				autoScale: true,
+				autoScaleMargin: 0,
+				maxBondLength: 30,
+				...options
 			});
 			rnd.setMolecule(struct);
 			rnd.update();
 			console.info('render!');//, el.innerHTML);
-			//tmpl.prerender = el.innerHTML;
+			//struct.prerender = el.innerHTML;
 		}
 	}
 }
 
 class StructRender extends Component {
-	render ({ struct, Tag="div", ...props }) {
-		return (
-			<Tag ref={ el => renderStruct(el, struct) } {...props}/>
+	constructor(props) {
+		super(props);
+		if (!(props.struct instanceof Struct)) try {
+			this.props.struct = molfile.parse(props.struct);
+		} catch (e) {
+			alert("Could not parse structure\n" + e);
+			this.props.struct = null;
+		}
+	}
+	shouldComponentUpdate() {
+		return false;
+	}
+	render () {
+		let { struct, Tag="div", options, ...props } = this.props;
+		return struct ? (
+			<Tag ref={ el => renderStruct(el, struct, options) } {...props}/>
+		) : (
+			<Tag>No molecule</Tag>
 		);
 	}
 }

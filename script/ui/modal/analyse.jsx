df14326f39d455ce9fd6ed7d241ae390e759364a
Nikita_Ryzhov <Nikita_Ryzhov@epam.com>
2016-10-21 12:14:20 +0300

    Choose 'decimal places' in Analyse

--- script/ui/modal/analyse.jsx
+++ script/ui/modal/analyse.jsx
@@ -4,20 +4,55 @@ import { h, Component, render } from 'preact';
 import Dialog from '../component/dialog';
 
 class Analyse extends Component {
-	render () {
+	constructor() {
+		super();
+		this.state = {
+			roundWeight: 3,
+			roundMass: 3
+		}
+	}
+
+	regExpReplace(str, round) {
+		return (str.replace(/[0-9]*\.[0-9]*/g, (str) => (+str).toFixed(round)));
+	}
+
+	changeRound(name, value) {
+		let newState = {};
+		newState[name] = +value;
+		this.setState(newState);
+	}
+
+	selectContent(roundName) {
+		let selectList = [];
+		for (let i = 0; i < 8; i++) {
+			selectList.push(i == this.state[roundName] ? <option value={i} selected>{i + ' decimal places'}</option> :
+				<option value={i}>{i + ' decimal places'}</option>)
+		}
+		return (<select onChange={el => this.changeRound(roundName, el.target.value)}>
+			{selectList}
+		</select>)
+	}
+
+	render (props, state) {
 		return (
 			<Dialog caption="Calculated Values"
 					name="analyse" params={this.props}
 					buttons={["Cancel"]}>
 				<ul>{[
-						{ name: 'Chemical Formula', key: 'gross' },
-						{ name: 'Molecular Weight', key: 'molecular-weight' },
-						{ name: 'Exact Mass', key: 'monoisotopic-mass' },
-						{ name: 'Elemental Analysis', key: 'mass-composition' }
-						].map(v => (
-							<label>{v.name}<output>{this.props[v.key]}</output></label>
-						))
-					}</ul>
+					{ name: 'Chemical Formula', key: 'gross' },
+					{ name: 'Molecular Weight', key: 'molecular-weight', round: 'roundWeight' },
+					{ name: 'Exact Mass', key: 'monoisotopic-mass', round: 'roundMass' },
+					{ name: 'Elemental Analysis', key: 'mass-composition' }
+				].map(v => (
+					<li>
+						<label>{v.name}:</label>
+						{v.round ?
+							<input value={typeof props[v.key] == 'number' ? props[v.key].toFixed(state[v.round]) : this.regExpReplace(props[v.key], state[v.round]) } readonly/> :
+							<input value={props[v.key]} readonly/>}
+						{v.round ? this.selectContent(v.round) : null}
+					</li>
+				))
+				}</ul>
 			</Dialog>
 		);
 	}

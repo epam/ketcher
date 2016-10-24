4cce56680826800510f44b396d0627ba2f58b6ca
Nikita_Ryzhov <Nikita_Ryzhov@epam.com>
2016-10-24 16:39:46 +0300

    Checking system fonts

---
+++ script/ui/component/systemfonts.jsx
@@ -0,0 +1,49 @@
+import { h, Component } from 'preact';
+import FontFaceObserver from "font-face-observer";
+/** @jsx h */
+
+let commonFonts = [
+	"Arial",
+	"Arial Black",
+	"Comic Sans MS",
+	"Courier New",
+	"Georgia",
+	"Impact", "Charcoal",
+	"Lucida Console", "Monaco",
+	"Palatino Linotype", "Book Antiqua", "Palatino",
+	"Tahoma", "Geneva",
+	"Times New Roman", "Times",
+	"Verdana",
+	"Symbol",
+	"MS Serif", "MS Sans Serif", "New York",
+	"Droid Sans", "Droid Serif", "Droid Sans Mono", "Roboto"
+];
+
+class SystemFonts extends Component {
+	constructor(props) {
+		super(props);
+		this.state = { availableFonts: [] };
+		let observer, tmp;
+
+		commonFonts.forEach((fontName) => {
+			observer = new FontFaceObserver(fontName);
+			observer.check().then(() => {
+				tmp = this.state.availableFonts;
+				tmp.push(fontName);
+				this.setState({ availableFonts: tmp })
+			});
+		});
+	}
+	render() {
+		let {...props} = this.props;
+		let content = this.state.availableFonts.map((fontName) =>
+			<div style={{ fontFamily: fontName }}>{fontName}</div>);
+		return (
+			<div {...props}>
+				{content}
+			</div>
+		);
+	}
+}
+
+export default SystemFonts;

e45fe0202e93c51a54b727b784e08c87ee8c0866
Nikita_Ryzhov <Nikita_Ryzhov@epam.com>
2016-12-09 13:04:48 +0300

    Fix syntax bug
    Fix syntax bug

--- script/render/restruct/reatom.js
+++ script/render/restruct/reatom.js
@@ -299,7 +299,7 @@ function showRadical(atom, render) {
 		hshift = 1.6 * options.lineWidth;
 		radical.path.push(
 			draw.radicalBullet(paper, ps.add(new Vec2(-hshift, 0)), options),
-			draw.radicalBullet(paper, ps.add(new Vec2(hshift, 0))), options);
+			draw.radicalBullet(paper, ps.add(new Vec2(hshift, 0)), options));
 		radical.path.attr('fill', atom.color);
 		break;
 	case 2:
@@ -310,7 +310,7 @@ function showRadical(atom, render) {
 		hshift = 1.6 * options.lineWidth;
 		radical.path.push(
 			draw.radicalCap(paper, ps.add(new Vec2(-hshift, 0)), options),
-			draw.radicalCap(paper, ps.add(new Vec2(hshift, 0))), options);
+			draw.radicalCap(paper, ps.add(new Vec2(hshift, 0)), options));
 		radical.path.attr('stroke', atom.color);
 		break;
 	}

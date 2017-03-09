2e1342810f501aaab607facfa45ee608be5baa3e
Nikita_Ryzhov <Nikita_Ryzhov@epam.com>
2017-03-09 18:45:51 +0300

    Reset atom pros after change atom label. Address #403.

--- script/editor/tool/atom.js
+++ script/editor/tool/atom.js
@@ -10,7 +10,7 @@ function AtomTool(editor, atomProps) {
 			return new AtomTool(editor, atomProps);
 
 		var action = Action.fromAtomsAttrs(editor.render.ctab, editor.selection().atoms,
-		                                   atomProps);
+		                                   atomProps, true);
 		editor.update(action);
 		editor.selection(null);
 		return null;

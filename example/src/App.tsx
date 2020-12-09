b441a6a3e351acf02db37b86a2420b4ee07ed30d
Andrei Mazol <72735611+AndreiMazol@users.noreply.github.com>
2020-12-09 16:48:29 +0300

    #148 update miew version (#157)
    - add Miew npm package

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,7 +1,11 @@
 import React from 'react'
-
+//@ts-ignore
+import Miew from 'miew'
+import 'miew/dist/Miew.min.css'
+//@ts-ignore
 import { Editor } from 'ketcher-react'
 import 'ketcher-react/dist/index.css'
+;(global as any).Miew = Miew
 
 const App = () => {
   return (

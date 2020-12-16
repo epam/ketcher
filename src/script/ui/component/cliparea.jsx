b76538daa1c9f59dc718e03adccaae279f4faa29
Anna Kuznetsova <40429134+Aniuta907@users.noreply.github.com>
2020-12-16 15:10:53 +0300

    add isFormElement code from the old version

--- src/script/ui/component/cliparea.jsx
+++ src/script/ui/component/cliparea.jsx
@@ -106,7 +106,7 @@ class ClipArea extends Component {
 }
 
 function isFormElement(el) {
-  return el.closest('form') != null || el.tagName === 'SELECT'
+  return el.closest('form') != null || ['INPUT', 'SELECT', 'TEXTAREA', 'OPTION', 'LABEL'].includes(el.tagName)
 }
 
 function autofocus(cliparea) {

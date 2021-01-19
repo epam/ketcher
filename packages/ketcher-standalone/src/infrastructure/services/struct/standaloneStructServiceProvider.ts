8fb93790c85c7a66d1e3de4ee5320a9d21525380
Evgeniya_Semenova <evgeniya_semenova@epam.com>
2021-01-19 14:10:36 +0300

    #241 Hide simple objects for standalone mode

--- packages/ketcher-standalone/src/infrastructure/services/struct/standaloneStructServiceProvider.ts
+++ packages/ketcher-standalone/src/infrastructure/services/struct/standaloneStructServiceProvider.ts
@@ -2,9 +2,13 @@ import { StructService, StructServiceProvider } from './structService.types'
 import StandaloneStructService from './standaloneStructService'
 
 class StandaloneStructServiceProvider implements StructServiceProvider {
+
+  constructor() {
+    ;(global as any).ketcher.standalone = true
+  }
+
   // @ts-ignore
   createStructService(baseUrl: string, options: any): StructService {
-    ;(global as any).ketcher.standalone = true
     return new StandaloneStructService(options)
   }
 }

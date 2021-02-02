73e03ea8df6ae9795685a3ce2055c093aeece7ae
TomasLight <art.ser.ignatiev@gmail.com>
2021-02-02 17:52:07 +0300

    #244 update buttons config

--- packages/ketcher-react/src/script/index.ts
+++ packages/ketcher-react/src/script/index.ts
@@ -1,5 +1,5 @@
 /****************************************************************************
- * Copyright 2018 EPAM Systems
+ * Copyright 2021 EPAM Systems
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -38,5 +38,5 @@ async function buildKetcherAsync({
   return builder.build()
 }
 
-export type { Config }
+export type { Config, ButtonsConfig }
 export default buildKetcherAsync

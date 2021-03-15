a7e2d2332d997e8a79765b0625d7d6496e34fcbd
Evgeniya Semenova <evgeniya_semenova@epam.com>
2021-03-15 11:46:58 +0300

    update copyright year

--- packages/ketcher-react/src/script/render/restruct/reloop.js
+++ packages/ketcher-react/src/script/render/restruct/reloop.js
@@ -1,5 +1,5 @@
 /****************************************************************************
- * Copyright 2020 EPAM Systems
+ * Copyright 2021 EPAM Systems
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  ***************************************************************************/
-import Visel from './visel'
 import ReObject from './ReObject'
 import util from '../util'
 import { Bond, Vec2, scale } from 'ketcher-core'

e5075ae5ac1e0fc07305b87661021dc355fb86b9
ensemenova <77324533+ensemenova@users.noreply.github.com>
2021-03-17 12:27:45 +0300

    make generation of svg to a separate element (#402)

--- packages/ketcher-react/src/script/ui/views/components/StructEditor/StructEditor.jsx
+++ packages/ketcher-react/src/script/ui/views/components/StructEditor/StructEditor.jsx
@@ -122,9 +122,8 @@ class StructEditor extends Component {
       <Tag
         onMouseDown={event => event.preventDefault()}
         className={clsx(classes.canvas, className)}
-        {...props}
-        ref={this.editorRef}>
-        {/* svg here */}
+        {...props}>
+        <div ref={this.editorRef}>{/* svg here */}</div>
         <div className={classes.measureLog} ref={this.logRef} />
         {indigoVerification && (
           <div className={classes.spinnerOverlay}>

9f82281c5ea9560904123cc1d82b31cc27d9ccee
Magomed Gasanov <magomed-gasanov1996@yandex.ru>
2021-06-02 11:48:46 +0300

    Add options "new..." to Enhanced Stereo window

--- packages/ketcher-react/src/script/ui/dialog/toolbox/enhancedStereo.tsx
+++ packages/ketcher-react/src/script/ui/dialog/toolbox/enhancedStereo.tsx
@@ -7,43 +7,6 @@ import { StereoLabel } from 'ketcher-core'
 
 import { findStereoAtoms } from '../../../editor/actions/utils'
 
-function findStereLabels(struct, aids): Array<string> {
-  const stereoaids = findStereoAtoms(struct, aids)
-  return stereoaids.map(aid => struct.atoms.get(aid).stereoLabel)
-}
-
-function maxOfAnds(stereLabels): number {
-  const numbers = stereLabels.map(label => {
-    return label.match(/&/) ? +label.match(/\d+/)?.join() : 0
-  })
-  return Math.max(...numbers)
-}
-
-function maxOfOrs(stereLabels): number {
-  const numbers = stereLabels.map(label => {
-    return label.match(/or/) ? +label.match(/\d+/)?.join() : 0
-  })
-  return Math.max(...numbers)
-}
-
-const enhancedStereoSchema = {
-  title: 'Enhanced Stereo',
-  type: 'object',
-  properties: {
-    type: {
-      title: 'Stereo Label',
-      enum: [StereoLabel.Abs, StereoLabel.And, StereoLabel.Or],
-      enumNames: ['ABS', 'AND', 'OR']
-    },
-    andNumber: {
-      type: 'integer'
-    },
-    orNumber: {
-      type: 'integer'
-    }
-  }
-}
-
 interface EnhancedStereoResult {
   andNumber: number
   orNumber: number
@@ -74,6 +37,44 @@ const EnhancedStereo: React.FC<Props> = props => {
   const { struct, formState, init, ...rest } = props
   const { result, valid } = formState
 
+  const stereoLabels: Array<string> = findStereLabels(
+    struct,
+    Array.from(struct.atoms.keys())
+  )
+
+  const maxAnd: number = maxOfAnds(stereoLabels)
+  const maxOr: number = maxOfOrs(stereoLabels)
+
+  const enhancedStereoSchema = {
+    title: 'Enhanced Stereo',
+    type: 'object',
+    properties: {
+      type: {
+        title: 'Stereo Label',
+        enum: [
+          StereoLabel.Abs,
+          StereoLabel.And,
+          StereoLabel.Or,
+          StereoLabel.And + (maxAnd + 1),
+          StereoLabel.Or + (maxOr + 1)
+        ],
+        enumNames: [
+          'ABS',
+          'Add to AND',
+          'Add to OR',
+          'Create new AND Group',
+          'Create new OR Group'
+        ]
+      },
+      andNumber: {
+        type: 'integer'
+      },
+      orNumber: {
+        type: 'integer'
+      }
+    }
+  }
+
   return (
     <Dialog
       title="Enhanced Stereochemistry"
@@ -86,7 +87,8 @@ const EnhancedStereo: React.FC<Props> = props => {
         <Field
           name="type"
           component={FieldSet}
-          struct={struct}
+          maxAnd={maxAnd}
+          maxOr={maxOr}
           labelPos={false}
         />
       </Form>
@@ -100,54 +102,97 @@ interface FieldSetProps {
   value: StereoLabel
   onChange: (value: string) => void
   type?: string
-  struct: any
+  maxAnd: number
+  maxOr: number
 }
 
 const FieldSet: React.FC<FieldSetProps> = props => {
-  const { struct, schema, value, onChange, type = 'radio', ...rest } = props
-
-  const stereoLabels: Array<string> = findStereLabels(
-    struct,
-    Array.from(struct.atoms.keys())
-  )
-
-  const maxAnd: number = maxOfAnds(stereoLabels)
-  const maxOr: number = maxOfOrs(stereoLabels)
+  const {
+    maxAnd,
+    maxOr,
+    schema,
+    value,
+    onChange,
+    type = 'radio',
+    ...rest
+  } = props
 
   return (
     <fieldset>
-      {schema.enum.map((val, index) => (
-        <li key={schema.enumNames[index]} className="stereo-label-item">
-          <label className="stereo-label-name">
-            <input
-              type={type}
-              checked={val === value}
-              value={val}
-              onChange={() => onChange(val)}
-              {...rest}
-            />
-            {schema.enumNames[index]}
-          </label>
-          {val === StereoLabel.And && (
-            <Field
-              name="andNumber"
-              schema={[...Array(maxAnd + 1)].map((_e, i) => i + 1)}
-              type="text"
-              className="label-group-select"
-            />
-          )}
-          {val === StereoLabel.Or && (
-            <Field
-              name="orNumber"
-              schema={[...Array(maxOr + 1)].map((_e, i) => i + 1)}
-              type="text"
-              className="label-group-select"
-            />
-          )}
-        </li>
-      ))}
+      {schema.enum.map(
+        (val, index) =>
+          shouldDisplay(val, maxAnd, maxOr) && (
+            <li key={schema.enumNames[index]} className="stereo-label-item">
+              <label className="stereo-label-name">
+                <input
+                  type={type}
+                  checked={val === value}
+                  value={val}
+                  onChange={() => onChange(val)}
+                  {...rest}
+                />
+                {schema.enumNames[index]}
+              </label>
+              {val === StereoLabel.And && (
+                <>
+                  <Field
+                    name="andNumber"
+                    // The line below creates an array [1...maxAnd]
+                    schema={[...Array(maxAnd)].map((_e, i) => i + 1)}
+                    type="text"
+                    className="label-group-select"
+                  />
+                  Group
+                </>
+              )}
+              {val === StereoLabel.Or && (
+                <>
+                  <Field
+                    name="orNumber"
+                    // The line below creates an array [1...maxOr]
+                    schema={[...Array(maxOr)].map((_e, i) => i + 1)}
+                    type="text"
+                    className="label-group-select"
+                  />
+                  Group
+                </>
+              )}
+            </li>
+          )
+      )}
     </fieldset>
   )
+}
+
+function findStereLabels(struct, aids): Array<string> {
+  const stereoaids = findStereoAtoms(struct, aids)
+  return stereoaids.map(aid => struct.atoms.get(aid).stereoLabel)
+}
+
+function maxOfAnds(stereLabels): number {
+  const numbers = stereLabels.map(label => {
+    return label.match(/&/) ? +label.match(/\d+/)?.join() : 0
+  })
+  return Math.max(...numbers)
+}
+
+function maxOfOrs(stereLabels): number {
+  const numbers = stereLabels.map(label => {
+    return label.match(/or/) ? +label.match(/\d+/)?.join() : 0
+  })
+  return Math.max(...numbers)
+}
+
+function shouldDisplay(type, maxAnd, maxOr) {
+  if (type === StereoLabel.Or && maxOr === 0) {
+    return false
+  }
+
+  if (type === StereoLabel.And && maxAnd === 0) {
+    return false
+  }
+
+  return true
 }
 
 export default connect(state => ({

ecef3ecc578d464f30bef637b25130d51ae5ae15
Andrei Mazol <Andrei_Mazol@epam.com>
2021-06-07 17:18:49 +0300

    update priority of operations

--- packages/ketcher-react/src/script/editor/operations/atom/index.ts
+++ packages/ketcher-react/src/script/editor/operations/atom/index.ts
@@ -88,7 +88,7 @@ class AtomDelete extends BaseOperation {
   data: Data
 
   constructor(atomId?: any) {
-    super(OperationType.ATOM_DELETE, 2)
+    super(OperationType.ATOM_DELETE, 3)
     this.data = { aid: atomId, atom: null, pos: null }
   }
 
--- packages/ketcher-react/src/script/editor/operations/bond/index.ts
+++ packages/ketcher-react/src/script/editor/operations/bond/index.ts
@@ -95,7 +95,7 @@ class BondDelete extends BaseOperation {
   data: Data
 
   constructor(bondId?: any) {
-    super(OperationType.BOND_DELETE, 1)
+    super(OperationType.BOND_DELETE, 2)
     this.data = { bid: bondId, bond: null, begin: null, end: null }
   }
 

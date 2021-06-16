f4a4b00f5a740e6d6a276f1ca2194f6a9e6e31bd
Walt Seymour <waltonseymour@users.noreply.github.com>
2021-06-16 08:34:26 -0400

    Export ketcher and buildKetcherAsync (#591)
    * export ketcher and buildKetcherAsync
    
    * prepare

--- packages/ketcher-react/src/index.tsx
+++ packages/ketcher-react/src/index.tsx
@@ -14,3 +14,5 @@
  * limitations under the License.
  ***************************************************************************/
 export * from './Editor'
+export * from './script'
+export * from './script/ketcher'

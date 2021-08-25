334db02ff3b9b0f50786185bb58ff7450d98f18e
Andrei Prokofev <andrei_prokofev1@epam.com>
2021-08-25 15:19:12 +0300

    #723 Structure with some sgroups cannot be opened

--- packages/ketcher-react/src/script/ui/state/shared.js
+++ packages/ketcher-react/src/script/ui/state/shared.js
@@ -6,6 +6,7 @@ import {
 } from 'ketcher-core'
 
 import { getStereoAtomsMap } from '../../editor/actions/bond'
+import { supportedSGroupTypes } from './constants'
 
 export function onAction(action) {
   if (action && action.dialog) {
@@ -57,6 +58,21 @@ export function load(struct, options) {
     return parseStruct(struct, server, options).then(
       struct => {
         const { rescale, fragment } = options
+
+        if (struct.sgroups.some(sGroup => !supportedSGroupTypes[sGroup.type])) {
+          const isConfirmed = window.confirm(
+            `Unsupported S-group type found. Would you like to import structure without it?`
+          )
+
+          if (!isConfirmed) {
+            return
+          }
+
+          struct.sgroups = struct.sgroups.filter(
+            (key, sGroup) => supportedSGroupTypes[sGroup.type]
+          )
+        }
+
         if (rescale) {
           struct.rescale() // TODO: move out parsing?
 

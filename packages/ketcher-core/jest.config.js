047bafed3511781fe7755a0fac4746b8da967a64
Andrei Mazol <Andrei_Mazol@epam.com>
2021-09-21 17:22:44 +0300

    rename entities files

--- packages/ketcher-core/jest.config.js
+++ packages/ketcher-core/jest.config.js
@@ -12,7 +12,7 @@ module.exports = {
   moduleNameMapper: {
     'application(.*)$': '<rootDir>/src/application/$1',
     'domain(.*)$': '<rootDir>/src/domain/$1',
-    'infrastructure(.*)$': '<rootDir>/src/infrastructure/$1'
-    // "utils(.*)$": "<rootDir>/src/utils/$1",
+    'infrastructure(.*)$': '<rootDir>/src/infrastructure/$1',
+    'utils(.*)$': '<rootDir>/src/utils/$1'
   }
 }

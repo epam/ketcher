1548204666bf4ad9c45ba1b5fd2e8602e8685d8b
Kirill Kapytov <Kirill_Kapytov@epam.com>
2021-09-28 17:58:39 +0300

    Set template name length limit and add name check

--- packages/ketcher-react/src/script/ui/dialog/template/template-attach.jsx
+++ packages/ketcher-react/src/script/ui/dialog/template/template-attach.jsx
@@ -42,7 +42,8 @@ class Attach extends Component {
     return name &&
       (name !== this.tmpl.struct.name ||
         atomid !== this.tmpl.props.atomid ||
-        bondid !== this.tmpl.props.bondid)
+        bondid !== this.tmpl.props.bondid) &&
+      name.trim().length
       ? { name, attach: { atomid, bondid } }
       : null
   }
@@ -67,7 +68,12 @@ class Attach extends Component {
         params={prop}>
         <label>
           Template name:
-          <Input value={name} onChange={onNameEdit} placeholder="tmpl" />
+          <Input
+            value={name}
+            onChange={onNameEdit}
+            placeholder="tmpl"
+            maxLength="128"
+          />
         </label>
         <label>Choose attachment atom and bond:</label>
         <StructEditor

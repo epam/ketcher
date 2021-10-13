68c880a5b5066d6f6586b5c3d0472df8ff923abd
Elena Odnoshivkina <Elena_Odnoshivkina@epam.com>
2021-10-13 16:13:57 +0300

    add remove FGs function

--- packages/ketcher-react/src/script/ui/component/ContextMenu/FunctionalGroupContextMenu.tsx
+++ packages/ketcher-react/src/script/ui/component/ContextMenu/FunctionalGroupContextMenu.tsx
@@ -1,100 +1,86 @@
 import { ContextMenu, MenuItem } from 'react-contextmenu'
 import { connect } from 'react-redux'
-import LassoHelper from '../../../../script/editor/tool/helper/lasso'
 import Action from '../../../editor/shared/action'
 import { FunctionalGroupsProvider } from 'ketcher-core'
-import { fromSgroupAttrs } from '../../../editor/actions/sgroup'
+import {
+  fromSgroupAttrs,
+  fromSgroupDeletion
+} from '../../../editor/actions/sgroup'
 import { fromAtomsAttrs } from '../../../editor/actions/atom'
+import { isEqual } from 'lodash/fp'
 
 function FGContextMenu(prop) {
-  const isGgroupSelected = () => {
-    const atoms = prop.editor._selection.atoms
-    const sGroups = prop.editor.render.ctab.molecule.sgroups
-    return sGroups.find(sg => sg.atoms === atoms)
-  }
   const editor = prop.editor
-  const lassoHelper = new LassoHelper(0, editor, 0)
-  const disableMenu =
-    prop.status.activeTool?.tool === 'select' && isGgroupSelected
+  const rnd = editor.render
+  const ctab = rnd?.ctab
 
-  const handleClick = function (event) {
-    const selectFragment = lassoHelper.fragment
-    const rnd = editor.render
-    const ctab = rnd.ctab
+  const atoms = editor._selection?.atoms
+  const sGroups = ctab?.molecule.sgroups
 
-    const ci = prop.editor.findItem(
-      event,
-      selectFragment
-        ? [
-            'frags',
-            'sgroups',
-            'sgroupData',
-            'rgroups',
-            'rxnArrows',
-            'rxnPluses',
-            'enhancedFlags',
-            'simpleObjects',
-            'texts'
-          ]
-        : [
-            'atoms',
-            'bonds',
-            'sgroups',
-            'sgroupData',
-            'rgroups',
-            'rxnArrows',
-            'rxnPluses',
-            'enhancedFlags',
-            'simpleObjects',
-            'texts'
-          ]
-    )
-    if (ci.map === 'sgroups') {
-      const provider = FunctionalGroupsProvider.getInstance()
-      const types = provider.getFunctionalGroupsList()
+  let isSGgroupSelected = false
+  let selectedSGroupId
 
-      const sgroup = ctab.sgroups.get(ci.id).item
-
-      if (
-        types.some(type => type.name === sgroup.data.name) &&
-        sgroup.type === 'SUP'
-      ) {
-        const action = new Action()
-        const functionalGroup = ctab.molecule.functionalGroups.get(ci.id)
-        functionalGroup.isExpanded = !functionalGroup.isExpanded
-        const relatedSGroup = ctab.sgroups.get(functionalGroup.relatedSGroupId)
-        const atoms = relatedSGroup.item.atoms
-        action.mergeWith(fromSgroupAttrs(rnd.ctab, ci.id, functionalGroup))
-        atoms.forEach(aid => {
-          action.mergeWith(
-            fromAtomsAttrs(rnd.ctab, aid, ctab.atoms.get(aid).a, false)
-          )
-        })
-        prop.editor.update(action)
+  if (atoms && sGroups) {
+    sGroups.forEach(sg => {
+      if (isEqual(atoms, sg.atoms)) {
+        isSGgroupSelected = true
+        selectedSGroupId = sg.id
       }
+    })
+  }
+  const disableMenu =
+    prop.status.activeTool?.tool === 'select' && isSGgroupSelected
+
+  const handleExpand = function () {
+    const provider = FunctionalGroupsProvider.getInstance()
+    const types = provider.getFunctionalGroupsList()
+
+    const sgroup = ctab.sgroups.get(selectedSGroupId).item
+
+    if (
+      types.some(type => type.name === sgroup.data.name) &&
+      sgroup.type === 'SUP'
+    ) {
+      const action = new Action()
+      const functionalGroup = ctab.molecule.functionalGroups.get(
+        selectedSGroupId
+      )
+      functionalGroup.isExpanded = !functionalGroup.isExpanded
+      const relatedSGroup = ctab.sgroups.get(functionalGroup.relatedSGroupId)
+      const atoms = relatedSGroup.item.atoms
+      action.mergeWith(
+        fromSgroupAttrs(rnd.ctab, selectedSGroupId, functionalGroup)
+      )
+      atoms.forEach(aid => {
+        action.mergeWith(
+          fromAtomsAttrs(rnd.ctab, aid, ctab.atoms.get(aid).a, false)
+        )
+      })
+      editor.update(action)
     }
   }
 
-  return (
-    disableMenu && (
-      <ContextMenu id="fgid">
-        <MenuItem onClick={event => handleClick(event)}>
-          Expand/Contract Abbreviation
-        </MenuItem>
-        <MenuItem
-          onClick={() => {
-            console.log('here')
-          }}>
-          Remove Abbreviation
-        </MenuItem>
-      </ContextMenu>
-    )
-  )
+  const handleRemove = function () {
+    editor.update(fromSgroupDeletion(ctab, selectedSGroupId))
+  }
+
+  return disableMenu ? (
+    <ContextMenu id="fgid">
+      <MenuItem onClick={handleExpand}>
+        {`${
+          ctab.molecule.functionalGroups.get(selectedSGroupId).isExpanded
+            ? 'Contract'
+            : 'Expand'
+        }` + ' Abbreviation'}
+      </MenuItem>
+      <MenuItem onClick={handleRemove}>Remove Abbreviation</MenuItem>
+    </ContextMenu>
+  ) : null
 }
 
 const mapStateToProps = state => ({
   status: state.actionState || {},
-  editor: state.editor
+  editor: state.editor || {}
 })
 
 const FGContextMenuContainer = connect(mapStateToProps)(FGContextMenu)

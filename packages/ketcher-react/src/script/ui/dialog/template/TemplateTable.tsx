e8c39443c68c857b57e44217e8e6d54e7e641fd1
Elena Odnoshivkina <Elena_Odnoshivkina@epam.com>
2021-10-18 11:34:16 +0300

    removing AutoSizer component from Template table

--- packages/ketcher-react/src/script/ui/dialog/template/TemplateTable.tsx
+++ packages/ketcher-react/src/script/ui/dialog/template/TemplateTable.tsx
@@ -14,8 +14,6 @@
  * limitations under the License.
  ***************************************************************************/
 
-import { AutoSizer } from 'react-virtualized'
-
 import { FC } from 'react'
 import { Struct } from 'ketcher-core'
 import StructRender from '../../component/structrender'
@@ -73,63 +71,51 @@ const TemplateTable: FC<TemplateTableProps> = props => {
     height: `${ITEM_SIZE.height}px`
   }
 
-  return (
+  return !ITEMS_COUNT ? (
+    <EmptySearchResult textInfo="No items found" />
+  ) : (
     <div className={classes.table} style={{ minWidth: ITEM_SIZE.width }}>
-      <AutoSizer>
-        {({ height, width }) => {
-          return !ITEMS_COUNT ? (
-            <EmptySearchResult textInfo="No items found" />
-          ) : (
+      <div className={classes.tableContent}>
+        {templates.map((tmpl, i) => {
+          return (
             <div
-              className={classes.tableContent}
-              style={{
-                width: width,
-                height: height,
-                overflowY: 'scroll'
-              }}>
-              {templates.map((tmpl, i) => {
-                return (
-                  <div
-                    className={
-                      tmpl === selected
-                        ? `${classes.td} ${classes.selected}`
-                        : classes.td
-                    }
-                    title={greekify(tmplName(tmpl, i))}
-                    key={
-                      tmpl.struct.name !== selected?.struct.name
-                        ? tmpl.struct.name
-                        : `${tmpl.struct.name}_selected`
-                    }
-                    style={tmplStyles}>
-                    <RenderTmpl
-                      tmpl={tmpl}
-                      className={classes.struct}
-                      onClick={() => onSelect(tmpl)}
-                    />
-                    <div className={classes.btnContainer}>
-                      {tmpl.props.group === 'User Templates' && (
-                        <button
-                          className={classes.deleteButton}
-                          onClick={() => onDelete!(tmpl)}>
-                          Delete
-                        </button>
-                      )}
-                      {tmpl.props.group !== 'Functional Groups' && (
-                        <button
-                          className={classes.attachButton}
-                          onClick={() => onAttach!(tmpl)}>
-                          Edit
-                        </button>
-                      )}
-                    </div>
-                  </div>
-                )
-              })}
+              className={
+                tmpl === selected
+                  ? `${classes.td} ${classes.selected}`
+                  : classes.td
+              }
+              title={greekify(tmplName(tmpl, i))}
+              key={
+                tmpl.struct.name !== selected?.struct.name
+                  ? tmpl.struct.name
+                  : `${tmpl.struct.name}_selected`
+              }
+              style={tmplStyles}>
+              <RenderTmpl
+                tmpl={tmpl}
+                className={classes.struct}
+                onClick={() => onSelect(tmpl)}
+              />
+              <div className={classes.btnContainer}>
+                {tmpl.props.group === 'User Templates' && (
+                  <button
+                    className={classes.deleteButton}
+                    onClick={() => onDelete!(tmpl)}>
+                    Delete
+                  </button>
+                )}
+                {tmpl.props.group !== 'Functional Groups' && (
+                  <button
+                    className={classes.attachButton}
+                    onClick={() => onAttach!(tmpl)}>
+                    Edit
+                  </button>
+                )}
+              </div>
             </div>
           )
-        }}
-      </AutoSizer>
+        })}
+      </div>
     </div>
   )
 }

1fba5ace535649a95dcd61ce82373dfc6cf0d73f
ElenaOdnoshivkina <89905163+ElenaOdnoshivkina@users.noreply.github.com>
2021-10-22 10:58:27 +0300

    adding polyfill

--- packages/ketcher-react/src/script/ui/App/App.tsx
+++ packages/ketcher-react/src/script/ui/App/App.tsx
@@ -14,6 +14,7 @@
  * limitations under the License.
  ***************************************************************************/
 
+import 'intersection-observer';
 import {
   BottomToolbarContainer,
   LeftToolbarContainer,

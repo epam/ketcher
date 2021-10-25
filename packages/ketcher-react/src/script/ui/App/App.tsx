194075d32d0e79a3633d2b0b421a3d04904d7c0d
Elena Odnoshivkina <Elena_Odnoshivkina@epam.com>
2021-10-25 09:56:59 +0300

    move polyfill into Editor

--- packages/ketcher-react/src/script/ui/App/App.tsx
+++ packages/ketcher-react/src/script/ui/App/App.tsx
@@ -14,7 +14,6 @@
  * limitations under the License.
  ***************************************************************************/
 
-import 'intersection-observer'
 import {
   BottomToolbarContainer,
   LeftToolbarContainer,

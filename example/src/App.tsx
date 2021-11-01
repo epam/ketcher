dd46760bbfdaea73df537843f570321bbf58e0b7
Elena Odnoshivkina <Elena_Odnoshivkina@epam.com>
2021-11-02 00:12:07 +0300

    adding error modal for example package

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -3,13 +3,13 @@ import 'ketcher-react/dist/index.css'
 
 import { Ketcher, RemoteStructServiceProvider } from 'ketcher-core'
 
-;
-
 import { Editor } from 'ketcher-react'
 // @ts-ignore
 import Miew from 'miew'
+import ErrorModal from './ErrorModal/ErrorModal'
+import { useState } from 'react'
 
-(global as any).Miew = Miew
+;(global as any).Miew = Miew
 
 let structServiceProvider: any = new RemoteStructServiceProvider(
   process.env.API_PATH || process.env.REACT_APP_API_PATH!
@@ -20,15 +20,24 @@ if (process.env.MODE === 'standalone') {
 }
 
 const App = () => {
+  const [hasError, setHasError] = useState(false)
+  const [errorMessage, setErrorMessage] = useState('')
+
   return (
-    <Editor
-      errorHandler={(message: string) => alert(message)}
-      staticResourcesUrl={process.env.PUBLIC_URL}
-      structServiceProvider={structServiceProvider}
-      onInit={(ketcher: Ketcher) => {
-        ;(global as any).ketcher = ketcher
-      }}
-    />
+    <>
+      <Editor
+        errorHandler={(message: string) => {
+          setHasError(true)
+          setErrorMessage(message)
+        }}
+        staticResourcesUrl={process.env.PUBLIC_URL}
+        structServiceProvider={structServiceProvider}
+        onInit={(ketcher: Ketcher) => {
+          ;(global as any).ketcher = ketcher
+        }}
+      />
+      {hasError && <ErrorModal message={errorMessage} update={setHasError} />}
+    </>
   )
 }
 

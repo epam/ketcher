f39beb9d2ca0041fb454f11678bc561e942372fa
Kirill Kapytov <Kirill_Kapytov@epam.com>
2021-11-17 13:55:37 +0300

    Use text transform and remove useless style lines

--- packages/ketcher-react/src/script/ui/views/modal/components/toolbox/Attach/Attach.tsx
+++ packages/ketcher-react/src/script/ui/views/modal/components/toolbox/Attach/Attach.tsx
@@ -31,7 +31,7 @@ const AttachPoints = (props: Props) => {
   const { formState, ...rest } = props
   return (
     <Dialog
-      title="ATTACHMENT POINTS"
+      title="Attachment Point"
       className={classes.attachPoints}
       result={() => formState.result}
       valid={() => formState.valid}

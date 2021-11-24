32c7b36c26bedb95e8012b5a34af0af16893ed12
Kirill Kapytov <Kirill_Kapytov@epam.com>
2021-11-24 15:21:39 +0300

    Change style classes

--- example/src/PeptidesToggler/PeptidesToggler.tsx
+++ example/src/PeptidesToggler/PeptidesToggler.tsx
@@ -8,7 +8,7 @@ const PeptidesToggler = ({ toggle }: TogglerProps) => {
   return (
     <label className="switch">
       <input type="checkbox" onChange={e => toggle(e.target.checked)} />
-      <span className="slider round" />
+      <span className="slider" />
     </label>
   )
 }

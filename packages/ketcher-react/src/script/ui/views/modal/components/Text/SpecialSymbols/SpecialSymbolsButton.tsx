203aad9017dc3ccbe7ede544da8c1943baf705fc
Kirill Kapytov <75835582+r1z3rISGOD@users.noreply.github.com>
2021-12-09 15:16:42 +0300

    #1044 Text Tool: Add special symbols (#1048)
    * Add special symbols button and ability to insert symbols to text tool
    
    * Used prettier
    
    * Used stylelint
    
    * refactoring and renaming of icon
    
    * fixing problem with formatting of inserted symbols
    
    Co-authored-by: Kirill Kapytov <Kirill_Kapytov@epam.com>
    Co-authored-by: Elena Odnoshivkina <Elena_Odnoshivkina@epam.com>

---
+++ packages/ketcher-react/src/script/ui/views/modal/components/Text/SpecialSymbols/SpecialSymbolsButton.tsx
@@ -0,0 +1,75 @@
+/****************************************************************************
+ * Copyright 2021 EPAM Systems
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ***************************************************************************/
+
+import { useState } from 'react'
+import { SpecialSymbolsList } from '../SpecialSymbolsList/SpecialSymbolsList'
+import classes from './SpecialSymbolsButton.module.less'
+import Icon from '../../../../../component/view/icon'
+import { EditorState, Modifier } from 'draft-js'
+
+const SpecialSymbolsButton = ({ editorState, setEditorState, styles }) => {
+  const [showSpecialSymbols, setShowSpecialSymbols] = useState(false)
+
+  const handleClose = event => {
+    event.stopPropagation()
+    event.preventDefault()
+    setShowSpecialSymbols(false)
+  }
+
+  const closeSymbolsList = event => {
+    if (!event.currentTarget.contains(event.relatedTarget)) {
+      handleClose(event)
+    }
+  }
+
+  const addSymbol = (e, value) => {
+    e.preventDefault()
+    const selection = editorState.getSelection()
+    const contentState = editorState.getCurrentContent()
+    let nextEditorState
+    const nextContentState = Modifier.replaceText(
+      contentState,
+      selection,
+      value,
+      styles
+    )
+    nextEditorState = EditorState.push(
+      editorState,
+      nextContentState,
+      'insert-characters'
+    )
+    setEditorState(nextEditorState)
+    setShowSpecialSymbols(false)
+  }
+
+  return (
+    <div onBlur={closeSymbolsList}>
+      <button
+        title="symbols"
+        onMouseDown={e => {
+          e.preventDefault()
+          setShowSpecialSymbols(!showSpecialSymbols)
+        }}
+        className={classes.textButton}
+      >
+        <Icon name="text-special-symbols" />
+      </button>
+      {showSpecialSymbols && <SpecialSymbolsList select={addSymbol} />}
+    </div>
+  )
+}
+
+export { SpecialSymbolsButton }

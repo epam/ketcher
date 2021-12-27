d86ff902e92596355273722a0ccb86b7921ffbe9
Aleksandr Gabrielov <aleksandr_gabrielov@epam.com>
2021-12-27 13:17:15 +0300

    Add active state for buttons

--- packages/ketcher-polymer-editor-react/src/components/rna/Switcher/Switcher.tsx
+++ packages/ketcher-polymer-editor-react/src/components/rna/Switcher/Switcher.tsx
@@ -3,18 +3,16 @@ import styled from '@emotion/styled'
 import LeftLink from './icons/leftLink.svg'
 import MiddleLink from './icons/middleLink.svg'
 import RightLink from './icons/rightLink.svg'
-// import { useState } from 'react'
+import { FC, useState } from 'react'
 
 const RAPButton = styled(Button)`
   color: rgba(0, 0, 0, 1);
   padding: 6px 12px;
   border-radius: 8px;
-  background-color: rgba(242, 242, 242, 1);
+  background-color: ${(props) =>
+    props.name ? 'rgba(0, 131, 143, 1)' : 'rgba(242, 242, 242, 1)'};
   min-width: 33px;
   :hover {
-    background-color: rgba(0, 131, 143, 1);
-  }
-  :active {
     background-color: rgba(0, 131, 143, 1);
   }
 `
@@ -28,21 +26,25 @@ const ButtonsContainer = styled('div')`
   display: flex;
   gap: 5px;
 `
-export const Switcher = () => {
-  // const [active, setActive] = useState('kek')
+const buttons = ['R', 'A', 'P']
+const svgLinks = [<LeftLink />, <MiddleLink />, <RightLink />]
+export const Switcher: FC = () => {
+  const [active, setActive] = useState('')
 
   return (
     <SwitcherContainer>
       <RAPButton>R(A)P</RAPButton>
+      <ButtonsContainer>{...svgLinks}</ButtonsContainer>
       <ButtonsContainer>
-        <LeftLink />
-        <MiddleLink />
-        <RightLink />
-      </ButtonsContainer>
-      <ButtonsContainer>
-        <RAPButton>R</RAPButton>
-        <RAPButton>A</RAPButton>
-        <RAPButton>P</RAPButton>
+        {buttons.map((button) => (
+          <RAPButton
+            key={button}
+            onClick={() => setActive(button)}
+            name={active === button ? button : ''}
+          >
+            {button}
+          </RAPButton>
+        ))}
       </ButtonsContainer>
     </SwitcherContainer>
   )

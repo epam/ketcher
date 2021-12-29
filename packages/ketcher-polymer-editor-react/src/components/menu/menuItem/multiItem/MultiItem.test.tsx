eae1e6b1010dfe2b08b8d804334b21e1b9af0d2b
ElenaOdnoshivkina <89905163+ElenaOdnoshivkina@users.noreply.github.com>
2021-12-29 13:29:40 +0300

    adding test

---
+++ packages/ketcher-polymer-editor-react/src/components/menu/menuItem/multiItem/MultiItem.test.tsx
@@ -0,0 +1,35 @@
+/****************************************************************************
+ * Copyright 2021 EPAM Systems
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ***************************************************************************/
+import { render } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import { MultiItem } from 'components/menu/menuItem/multiItem/MultiItem'
+
+const handleClick = jest.fn()
+describe('Test Menu Multi Item component', () => {
+  it('Test click Multi Item event', () => {
+    const { container } = render(
+      <MultiItem
+        options={['select-lasso']}
+        onClick={handleClick}
+        activeTool={'select-lasso'}
+      />
+    )
+    const element = container.querySelector('li')
+    // @ts-ignore
+    userEvent.click(element)
+    expect(handleClick.mock.calls.length).toEqual(1)
+  })
+})

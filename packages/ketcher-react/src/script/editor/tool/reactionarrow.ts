20d6f430cda14055e8a651d55019ff46b61d50fd
Viktoriia Fedotkina <viktoriia_fedotkina@epam.com>
2022-01-17 13:57:13 +0300

    Refactor Arrow tool

--- packages/ketcher-react/src/script/editor/tool/reactionarrow.ts
+++ packages/ketcher-react/src/script/editor/tool/reactionarrow.ts
@@ -15,119 +15,126 @@
  ***************************************************************************/
 
 import {
-  Vec2,
   fromArrowAddition,
   fromArrowDeletion,
   fromArrowResizing,
-  fromMultipleMove
+  fromMultipleMove,
+  Vec2
 } from 'ketcher-core'
+import Editor from '../Editor'
 
-function ReactionArrowTool(editor, mode) {
-  if (!(this instanceof ReactionArrowTool))
-    return new ReactionArrowTool(editor, mode)
+class ReactionArrowTool {
+  mode: any
+  editor: Editor
+  dragCtx: any
 
-  this.mode = mode
-  this.editor = editor
-  this.editor.selection(null)
-}
-
-ReactionArrowTool.prototype.mousedown = function (event) {
-  var rnd = this.editor.render
-  const p0 = rnd.page2obj(event)
-  this.dragCtx = { p0 }
-
-  var ci = this.editor.findItem(event, ['rxnArrows'])
-  if (ci && ci.map === 'rxnArrows') {
-    this.editor.hover(null)
-    this.editor.selection({ rxnArrows: [ci.id] })
-    this.dragCtx.ci = ci
-  } else {
-    this.dragCtx.isNew = true
+  constructor(editor, mode) {
+    this.mode = mode
+    this.editor = editor
     this.editor.selection(null)
   }
-}
 
-ReactionArrowTool.prototype.mousemove = function (event) {
-  var rnd = this.editor.render
-  if (this.dragCtx) {
-    const current = rnd.page2obj(event)
-    const diff = current.sub(this.dragCtx.p0)
-    this.dragCtx.previous = current
-    if (this.dragCtx.ci) {
-      if (this.dragCtx.action) this.dragCtx.action.perform(rnd.ctab)
-      if (!this.dragCtx.ci.ref) {
-        this.dragCtx.action = fromMultipleMove(
-          rnd.ctab,
-          this.editor.selection() || {},
-          diff
-        )
-      } else {
-        this.dragCtx.action = fromArrowResizing(
-          rnd.ctab,
-          this.dragCtx.ci.id,
-          diff,
-          current,
-          this.dragCtx.ci.ref
-        )
-      }
-      this.editor.update(this.dragCtx.action, true)
+  mousedown = (event) => {
+    const rnd = this.editor.render
+    const p0 = rnd.page2obj(event)
+    this.dragCtx = { p0 }
+
+    const ci = this.editor.findItem(event, ['rxnArrows'])
+    if (ci && ci.map === 'rxnArrows') {
+      this.editor.hover(null)
+      this.editor.selection({ rxnArrows: [ci.id] })
+      this.dragCtx.ci = ci
     } else {
-      if (!this.dragCtx.action) {
-        const action = fromArrowAddition(
-          rnd.ctab,
-          [this.dragCtx.p0, this.dragCtx.p0],
-          this.mode
-        )
-        // TODO: need to rework  actions/operations logic
-        const addOperation = action.operations[0]
-        const itemId = addOperation.data.id
-        this.dragCtx.itemId = itemId
-        this.dragCtx.action = action
+      this.dragCtx.isNew = true
+      this.editor.selection(null)
+    }
+  }
+
+  mousemove = (event) => {
+    const rnd = this.editor.render
+    if (this.dragCtx) {
+      const current = rnd.page2obj(event)
+      const diff = current.sub(this.dragCtx.p0)
+      this.dragCtx.previous = current
+      if (this.dragCtx.ci) {
+        if (this.dragCtx.action) this.dragCtx.action.perform(rnd.ctab)
+        if (!this.dragCtx.ci.ref) {
+          this.dragCtx.action = fromMultipleMove(
+            rnd.ctab,
+            this.editor.selection() || {},
+            diff
+          )
+        } else {
+          this.dragCtx.action = fromArrowResizing(
+            rnd.ctab,
+            this.dragCtx.ci.id,
+            diff,
+            current,
+            this.dragCtx.ci.ref
+          )
+        }
         this.editor.update(this.dragCtx.action, true)
       } else {
-        this.dragCtx.action.perform(rnd.ctab)
+        if (!this.dragCtx.action) {
+          const action = fromArrowAddition(
+            rnd.ctab,
+            [this.dragCtx.p0, this.dragCtx.p0],
+            this.mode
+          )
+          // TODO: need to rework  actions/operations logic
+          const addOperation = action.operations[0]
+          // @ts-ignore
+          this.dragCtx.itemId = addOperation.data.id
+          this.dragCtx.action = action
+          this.editor.update(this.dragCtx.action, true)
+        } else {
+          this.dragCtx.action.perform(rnd.ctab)
+        }
+
+        this.dragCtx.action = fromArrowResizing(
+          rnd.ctab,
+          this.dragCtx.itemId,
+          diff,
+          current,
+          null
+        )
+        this.editor.update(this.dragCtx.action, true)
       }
-
-      this.dragCtx.action = fromArrowResizing(
-        rnd.ctab,
-        this.dragCtx.itemId,
-        diff,
-        current,
-        null
-      )
-      this.editor.update(this.dragCtx.action, true)
+    } else {
+      const items = this.editor.findItem(event, ['rxnArrows'])
+      this.editor.hover(items)
     }
-  } else {
-    const items = this.editor.findItem(event, ['rxnArrows'])
-    this.editor.hover(items)
   }
-}
 
-ReactionArrowTool.prototype.mouseup = function (event) {
-  if (!this.dragCtx) return true
-  const rnd = this.editor.render
+  mouseup = () => {
+    if (!this.dragCtx) return true
+    const rnd = this.editor.render
 
-  const p0 = this.dragCtx.p0
-  const p1 = getDefaultLengthPos(p0, this.dragCtx.previous)
+    const p0 = this.dragCtx.p0
+    const p1 = getDefaultLengthPos(p0, this.dragCtx.previous)
 
-  if (this.dragCtx.action) {
-    if (this.dragCtx.isNew) {
-      this.editor.update(fromArrowDeletion(rnd.ctab, this.dragCtx.itemId), true)
-      this.dragCtx.action = fromArrowAddition(rnd.ctab, [p0, p1], this.mode)
+    if (this.dragCtx.action) {
+      if (this.dragCtx.isNew) {
+        this.editor.update(
+          fromArrowDeletion(rnd.ctab, this.dragCtx.itemId),
+          true
+        )
+        this.dragCtx.action = fromArrowAddition(rnd.ctab, [p0, p1], this.mode)
+      }
+      this.editor.update(this.dragCtx.action)
     }
-    this.editor.update(this.dragCtx.action)
+    delete this.dragCtx
+    return true
   }
-  delete this.dragCtx
-  return true
-}
 
-ReactionArrowTool.prototype.click = function (event) {
-  const rnd = this.editor.render
-  const ci = this.editor.findItem(event, ['rxnArrows'])
-  const p0 = rnd.page2obj(event)
-  if (!ci) {
-    const pos = [p0, getDefaultLengthPos(p0)]
-    this.editor.update(fromArrowAddition(rnd.ctab, pos, this.mode))
+  click = (event) => {
+    const rnd = this.editor.render
+    const ci = this.editor.findItem(event, ['rxnArrows'])
+    const p0 = rnd.page2obj(event)
+    if (!ci) {
+      const pos = [p0, getDefaultLengthPos(p0, null)]
+      this.editor.update(fromArrowAddition(rnd.ctab, pos, this.mode))
+    }
   }
 }
 

a93cc944363888b8949ec47067cf852f3b1255fe
Viktoriia Fedotkina <viktoriia_fedotkina@epam.com>
2022-01-17 13:48:25 +0300

    Refactor Paste tool

--- packages/ketcher-react/src/script/editor/tool/paste.ts
+++ packages/ketcher-react/src/script/editor/tool/paste.ts
@@ -19,133 +19,141 @@ import {
   fromPaste,
   FunctionalGroup,
   getHoverToFuse,
-  getItemsToFuse
+  getItemsToFuse,
+  Struct
 } from 'ketcher-core'
+import Editor from '../Editor'
 
-function PasteTool(editor, struct) {
-  if (!(this instanceof PasteTool)) return new PasteTool(editor, struct)
+class PasteTool {
+  editor: Editor
+  struct: Struct
+  action: any
+  mergeItems: any
 
-  this.editor = editor
-  this.editor.selection(null)
-  this.struct = struct
-  this.sgroups = editor.render.ctab.sgroups
-  this.molecule = editor.render.ctab.molecule
-  this.functionalGroups = this.molecule.functionalGroups
+  constructor(editor, struct) {
+    this.editor = editor
+    this.editor.selection(null)
+    this.struct = struct
 
-  const rnd = editor.render
-  const { clientHeight, clientWidth } = rnd.clientArea
-  const point = editor.lastEvent
-    ? rnd.page2obj(editor.lastEvent)
-    : rnd.page2obj({ pageX: clientWidth / 2, pageY: clientHeight / 2 })
+    const rnd = this.editor.render
+    const { clientHeight, clientWidth } = rnd.clientArea
+    const point = this.editor.lastEvent
+      ? rnd.page2obj(this.editor.lastEvent)
+      : rnd.page2obj({ pageX: clientWidth / 2, pageY: clientHeight / 2 })
 
-  const [action, pasteItems] = fromPaste(rnd.ctab, this.struct, point)
-  this.action = action
-  this.editor.update(this.action, true)
+    const [action, pasteItems] = fromPaste(rnd.ctab, this.struct, point)
+    this.action = action
+    this.editor.update(this.action, true)
 
-  this.mergeItems = getItemsToFuse(this.editor, pasteItems)
-  this.editor.hover(getHoverToFuse(this.mergeItems), this)
-}
-
-PasteTool.prototype.mousemove = function (event) {
-  const rnd = this.editor.render
-
-  if (this.action) this.action.perform(rnd.ctab)
-
-  const [action, pasteItems] = fromPaste(
-    rnd.ctab,
-    this.struct,
-    rnd.page2obj(event)
-  )
-  this.action = action
-  this.editor.update(this.action, true)
-
-  this.mergeItems = getItemsToFuse(this.editor, pasteItems)
-  this.editor.hover(getHoverToFuse(this.mergeItems))
-}
-
-PasteTool.prototype.mouseup = function () {
-  const atomsResult = []
-  const bondsResult = []
-  const result = []
-  if (
-    this.mergeItems &&
-    this.functionalGroups.size &&
-    this.mergeItems.atoms.size
-  ) {
-    for (const id of this.mergeItems.atoms.values()) {
-      const atomId = FunctionalGroup.atomsInFunctionalGroup(
-        this.functionalGroups,
-        id
-      )
-      if (atomId !== null) atomsResult.push(atomId)
-    }
+    this.mergeItems = getItemsToFuse(this.editor, pasteItems)
+    this.editor.hover(getHoverToFuse(this.mergeItems), this)
   }
-  if (
-    this.mergeItems &&
-    this.functionalGroups.size &&
-    this.mergeItems.bonds.size
-  ) {
-    for (const id of this.mergeItems.bonds.values()) {
-      const bondId = FunctionalGroup.atomsInFunctionalGroup(
-        this.functionalGroups,
-        id
-      )
-      if (bondId !== null) bondsResult.push(bondId)
-    }
+
+  mousemove = (event) => {
+    const rnd = this.editor.render
+
+    if (this.action) this.action.perform(rnd.ctab)
+
+    const [action, pasteItems] = fromPaste(
+      rnd.ctab,
+      this.struct,
+      rnd.page2obj(event)
+    )
+    this.action = action
+    this.editor.update(this.action, true)
+
+    this.mergeItems = getItemsToFuse(this.editor, pasteItems)
+    this.editor.hover(getHoverToFuse(this.mergeItems))
   }
-  if (atomsResult.length > 0) {
-    for (const id of atomsResult) {
-      const fgId = FunctionalGroup.findFunctionalGroupByAtom(
-        this.functionalGroups,
-        id
-      )
-      if (fgId !== null && !result.includes(fgId)) {
-        result.push(fgId)
+
+  mouseup = () => {
+    const struct = this.editor.render.ctab
+    const molecule = struct.molecule
+    const functionalGroups = molecule.functionalGroups
+    const atomsResult: Array<number> = []
+    const bondsResult: Array<number> = []
+    const result: Array<number> = []
+    if (
+      this.mergeItems &&
+      functionalGroups.size &&
+      this.mergeItems.atoms.size
+    ) {
+      for (const id of this.mergeItems.atoms.values()) {
+        const atomId = FunctionalGroup.atomsInFunctionalGroup(
+          functionalGroups,
+          id
+        )
+        if (atomId !== null) atomsResult.push(atomId)
       }
     }
-    this.editor.event.removeFG.dispatch({ fgIds: result })
-    return
-  } else if (bondsResult.length > 0) {
-    for (const id of bondsResult) {
-      const fgId = FunctionalGroup.findFunctionalGroupByBond(
-        this.molecule,
-        this.functionalGroups,
-        id
-      )
-      if (fgId !== null && !result.includes(fgId)) {
-        result.push(fgId)
+    if (
+      this.mergeItems &&
+      functionalGroups.size &&
+      this.mergeItems.bonds.size
+    ) {
+      for (const id of this.mergeItems.bonds.values()) {
+        const bondId = FunctionalGroup.atomsInFunctionalGroup(
+          functionalGroups,
+          id
+        )
+        if (bondId !== null) bondsResult.push(bondId)
       }
     }
-    this.editor.event.removeFG.dispatch({ fgIds: result })
-    return
+    if (atomsResult.length > 0) {
+      for (const id of atomsResult) {
+        const fgId = FunctionalGroup.findFunctionalGroupByAtom(
+          functionalGroups,
+          id
+        )
+        if (fgId !== null && !result.includes(fgId)) {
+          result.push(fgId)
+        }
+      }
+      this.editor.event.removeFG.dispatch({ fgIds: result })
+      return
+    } else if (bondsResult.length > 0) {
+      for (const id of bondsResult) {
+        const fgId = FunctionalGroup.findFunctionalGroupByBond(
+          molecule,
+          functionalGroups,
+          id
+        )
+        if (fgId !== null && !result.includes(fgId)) {
+          result.push(fgId)
+        }
+      }
+      this.editor.event.removeFG.dispatch({ fgIds: result })
+      return
+    }
+    const editor = this.editor
+    const restruct = editor.render.ctab
+
+    editor.selection(null)
+
+    this.action = this.action
+      ? fromItemsFuse(restruct, this.mergeItems).mergeWith(this.action)
+      : fromItemsFuse(restruct, this.mergeItems)
+
+    editor.hover(null)
+
+    if (this.action) {
+      const action = this.action
+      delete this.action
+      this.editor.update(action)
+    }
   }
-  const editor = this.editor
-  const restruct = editor.render.ctab
 
-  editor.selection(null)
-
-  this.action = this.action
-    ? fromItemsFuse(restruct, this.mergeItems).mergeWith(this.action)
-    : fromItemsFuse(restruct, this.mergeItems)
-
-  editor.hover(null)
-
-  if (this.action) {
-    const action = this.action
-    delete this.action
-    this.editor.update(action)
+  cancel = () => {
+    const rnd = this.editor.render
+    this.editor.hover(null)
+    if (this.action) {
+      this.action.perform(rnd.ctab) // revert the action
+      delete this.action
+      rnd.update()
+    }
   }
+
+  mouseleave = this.cancel
 }
-
-PasteTool.prototype.cancel = function () {
-  const rnd = this.editor.render
-  this.editor.hover(null)
-  if (this.action) {
-    this.action.perform(rnd.ctab) // revert the action
-    delete this.action
-    rnd.update()
-  }
-}
-PasteTool.prototype.mouseleave = PasteTool.prototype.cancel
 
 export default PasteTool

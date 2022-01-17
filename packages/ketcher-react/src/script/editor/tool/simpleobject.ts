a8c2692432e77e0c31f8615e1b2c2cf9c7d8ba09
Viktoriia Fedotkina <viktoriia_fedotkina@epam.com>
2022-01-17 15:08:51 +0300

    Refactor Simpleobject tool

--- packages/ketcher-react/src/script/editor/tool/simpleobject.ts
+++ packages/ketcher-react/src/script/editor/tool/simpleobject.ts
@@ -20,112 +20,117 @@ import {
   fromSimpleObjectDeletion,
   fromSimpleObjectResizing
 } from 'ketcher-core'
+import Editor from '../Editor'
 
-function SimpleObjectTool(editor, mode) {
-  if (!(this instanceof SimpleObjectTool))
-    return new SimpleObjectTool(editor, mode)
+class SimpleObjectTool {
+  mode: any
+  editor: Editor
+  dragCtx: any
 
-  this.mode = mode
-  this.editor = editor
-  this.editor.selection(null)
-}
-
-SimpleObjectTool.prototype.mousedown = function (event) {
-  var rnd = this.editor.render
-  const p0 = rnd.page2obj(event)
-  this.dragCtx = { p0 }
-
-  var ci = this.editor.findItem(event, ['simpleObjects'])
-  if (ci && ci.map === 'simpleObjects') {
-    this.editor.hover(null)
-    this.editor.selection({ simpleObjects: [ci.id] })
-    this.dragCtx.ci = ci
-  } else {
-    this.dragCtx.isNew = true
+  constructor(editor, mode) {
+    this.mode = mode
+    this.editor = editor
     this.editor.selection(null)
   }
-}
 
-SimpleObjectTool.prototype.mousemove = function (event) {
-  var rnd = this.editor.render
-  if (this.dragCtx) {
-    const current = rnd.page2obj(event)
-    const diff = current.sub(this.dragCtx.p0)
-    this.dragCtx.previous = current
-    if (this.dragCtx.ci) {
-      if (this.dragCtx.action) this.dragCtx.action.perform(rnd.ctab)
-      if (!this.dragCtx.ci.ref) {
-        this.dragCtx.action = fromMultipleMove(
-          rnd.ctab,
-          this.editor.selection() || {},
-          diff
-        )
+  mousedown = (event) => {
+    const rnd = this.editor.render
+    const p0 = rnd.page2obj(event)
+    this.dragCtx = { p0 }
+
+    const ci = this.editor.findItem(event, ['simpleObjects'])
+    if (ci && ci.map === 'simpleObjects') {
+      this.editor.hover(null)
+      this.editor.selection({ simpleObjects: [ci.id] })
+      this.dragCtx.ci = ci
+    } else {
+      this.dragCtx.isNew = true
+      this.editor.selection(null)
+    }
+  }
+
+  mousemove = (event) => {
+    const rnd = this.editor.render
+    if (this.dragCtx) {
+      const current = rnd.page2obj(event)
+      const diff = current.sub(this.dragCtx.p0)
+      this.dragCtx.previous = current
+      if (this.dragCtx.ci) {
+        if (this.dragCtx.action) this.dragCtx.action.perform(rnd.ctab)
+        if (!this.dragCtx.ci.ref) {
+          this.dragCtx.action = fromMultipleMove(
+            rnd.ctab,
+            this.editor.selection() || {},
+            diff
+          )
+        } else {
+          this.dragCtx.action = fromSimpleObjectResizing(
+            rnd.ctab,
+            this.dragCtx.ci.id,
+            diff,
+            current,
+            this.dragCtx.ci.ref,
+            event.shiftKey
+          )
+        }
+        this.editor.update(this.dragCtx.action, true)
       } else {
+        if (!this.dragCtx.action) {
+          const action = fromSimpleObjectAddition(
+            rnd.ctab,
+            [this.dragCtx.p0, this.dragCtx.p0],
+            this.mode,
+            false
+          )
+          // TODO: need to rework  actions/operations logic
+          const addOperation = action.operations[0]
+          // @ts-ignore
+          this.dragCtx.itemId = addOperation.data.id
+          this.dragCtx.action = action
+          this.editor.update(this.dragCtx.action, true)
+        } else {
+          this.dragCtx.action.perform(rnd.ctab)
+        }
+
         this.dragCtx.action = fromSimpleObjectResizing(
           rnd.ctab,
-          this.dragCtx.ci.id,
+          this.dragCtx.itemId,
           diff,
           current,
-          this.dragCtx.ci.ref,
+          null,
+          event.shiftKey
+        )
+        this.editor.update(this.dragCtx.action, true)
+      }
+    } else {
+      const items = this.editor.findItem(event, ['simpleObjects'])
+      this.editor.hover(items)
+    }
+  }
+
+  mouseup = (event) => {
+    if (!this.dragCtx) return true
+
+    if (this.dragCtx.action) {
+      if (this.dragCtx.isNew) {
+        const rnd = this.editor.render
+        this.editor.update(
+          fromSimpleObjectDeletion(rnd.ctab, this.dragCtx.itemId),
+          true
+        )
+        this.dragCtx.action = fromSimpleObjectAddition(
+          rnd.ctab,
+          [this.dragCtx.p0, this.dragCtx.previous],
+          this.mode,
           event.shiftKey
         )
       }
-      this.editor.update(this.dragCtx.action, true)
-    } else {
-      if (!this.dragCtx.action) {
-        const action = fromSimpleObjectAddition(
-          rnd.ctab,
-          [this.dragCtx.p0, this.dragCtx.p0],
-          this.mode
-        )
-        // TODO: need to rework  actions/operations logic
-        const addOperation = action.operations[0]
-        const itemId = addOperation.data.id
-        this.dragCtx.itemId = itemId
-        this.dragCtx.action = action
-        this.editor.update(this.dragCtx.action, true)
-      } else {
-        this.dragCtx.action.perform(rnd.ctab)
-      }
-
-      this.dragCtx.action = fromSimpleObjectResizing(
-        rnd.ctab,
-        this.dragCtx.itemId,
-        diff,
-        current,
-        null,
-        event.shiftKey
-      )
-      this.editor.update(this.dragCtx.action, true)
+      this.editor.update(this.dragCtx.action)
     }
-  } else {
-    const items = this.editor.findItem(event, ['simpleObjects'])
-    this.editor.hover(items)
+
+    delete this.dragCtx
+    return true
   }
-}
-
-SimpleObjectTool.prototype.mouseup = function (event) {
-  if (!this.dragCtx) return true
-
-  if (this.dragCtx.action) {
-    if (this.dragCtx.isNew) {
-      const rnd = this.editor.render
-      this.editor.update(
-        fromSimpleObjectDeletion(rnd.ctab, this.dragCtx.itemId),
-        true
-      )
-      this.dragCtx.action = fromSimpleObjectAddition(
-        rnd.ctab,
-        [this.dragCtx.p0, this.dragCtx.previous],
-        this.mode,
-        event.shiftKey
-      )
-    }
-    this.editor.update(this.dragCtx.action)
-  }
-
-  delete this.dragCtx
-  return true
 }
 
 export default SimpleObjectTool

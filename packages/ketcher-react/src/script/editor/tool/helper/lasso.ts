5da04a54fd32d13ea83050f86610425e586f59d5
Viktoriia Fedotkina <viktoriia_fedotkina@epam.com>
2022-01-17 17:57:24 +0300

    Refactor Lasso tool

--- packages/ketcher-react/src/script/editor/tool/helper/lasso.ts
+++ packages/ketcher-react/src/script/editor/tool/helper/lasso.ts
@@ -16,74 +16,93 @@
 
 import { Scale } from 'ketcher-core'
 import locate from './locate'
+import Editor from '../../Editor'
 
-function LassoHelper(mode, editor, fragment) {
-  this.mode = mode
-  this.fragment = fragment
-  this.editor = editor
-}
+class LassoHelper {
+  mode: any
+  editor: Editor
+  fragment: any
+  points: any
+  selection: any
 
-LassoHelper.prototype.getSelection = function () {
-  const rnd = this.editor.render
-
-  if (this.mode === 0) return locate.inPolygon(rnd.ctab, this.points)
-
-  if (this.mode === 1)
-    return locate.inRectangle(rnd.ctab, this.points[0], this.points[1])
-
-  throw new Error('Selector mode unknown') // eslint-disable-line no-else-return
-}
-
-LassoHelper.prototype.begin = function (event) {
-  const rnd = this.editor.render
-  this.points = [rnd.page2obj(event)]
-  if (this.mode === 1) this.points.push(this.points[0])
-}
-
-LassoHelper.prototype.running = function () {
-  return !!this.points
-}
-
-LassoHelper.prototype.addPoint = function (event) {
-  if (!this.points) return null
-
-  const rnd = this.editor.render
-
-  if (this.mode === 0) this.points.push(rnd.page2obj(event))
-  else if (this.mode === 1) this.points = [this.points[0], rnd.page2obj(event)]
-
-  this.update()
-  return this.getSelection()
-}
-
-LassoHelper.prototype.update = function () {
-  if (this.selection) {
-    this.selection.remove()
-    this.selection = null
+  constructor(mode, editor, fragment) {
+    this.mode = mode
+    this.fragment = fragment
+    this.editor = editor
   }
 
-  if (this.points && this.points.length > 1) {
+  getSelection = () => {
     const rnd = this.editor.render
-    const dp = this.points.map((p) =>
-      Scale.obj2scaled(p, rnd.options).add(rnd.options.offset)
-    )
-    this.selection =
-      this.mode === 0
-        ? rnd.selectionPolygon(dp)
-        : rnd.selectionRectangle(dp[0], dp[1])
+
+    if (this.mode === 0) {
+      return locate.inPolygon(rnd.ctab, this.points)
+    }
+
+    if (this.mode === 1) {
+      return locate.inRectangle(rnd.ctab, this.points[0], this.points[1])
+    }
+
+    throw new Error('Selector mode unknown') // eslint-disable-line no-else-return
   }
-}
 
-LassoHelper.prototype.end = function () {
-  const ret = this.getSelection()
-  this.points = null
-  this.update(null)
-  return ret
-}
+  begin = (event) => {
+    const rnd = this.editor.render
+    this.points = [rnd.page2obj(event)]
+    if (this.mode === 1) {
+      this.points.push(this.points[0])
+    }
+  }
 
-LassoHelper.prototype.cancel = function () {
-  this.points = null
-  this.update(null)
+  running = () => {
+    return !!this.points
+  }
+
+  addPoint = (event) => {
+    if (!this.points) {
+      return null
+    }
+
+    const rnd = this.editor.render
+
+    if (this.mode === 0) {
+      this.points.push(rnd.page2obj(event))
+    } else if (this.mode === 1) {
+      this.points = [this.points[0], rnd.page2obj(event)]
+    }
+
+    this.update()
+    return this.getSelection()
+  }
+
+  update = () => {
+    if (this.selection) {
+      this.selection.remove()
+      this.selection = null
+    }
+
+    if (this.points && this.points.length > 1) {
+      const rnd = this.editor.render
+      const dp = this.points.map((p) =>
+        Scale.obj2scaled(p, rnd.options).add(rnd.options.offset)
+      )
+      this.selection =
+        this.mode === 0
+          ? rnd.selectionPolygon(dp)
+          : rnd.selectionRectangle(dp[0], dp[1])
+    }
+  }
+
+  end = () => {
+    const ret = this.getSelection()
+    this.points = null
+    this.update()
+    return ret
+  }
+
+  cancel = () => {
+    this.points = null
+    this.update()
+  }
 }
 
 export default LassoHelper

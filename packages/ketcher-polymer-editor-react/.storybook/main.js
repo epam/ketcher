1d018cd74e0be543cac04b5ed6072650ff488b3f
Aleksandr Gabrielov <aleksandr_gabrielov@epam.com>
2022-01-18 13:27:29 +0300

    Remove unused packages

--- packages/ketcher-polymer-editor-react/.storybook/main.js
+++ packages/ketcher-polymer-editor-react/.storybook/main.js
@@ -4,7 +4,7 @@ const toPath = (_path) => path.join(process.cwd(), _path)
 
 module.exports = {
   stories: ['../src/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  addons: ['@storybook/addon-essentials'],
   framework: '@storybook/react',
   webpackFinal: async (config) => {
     return {
--- packages/ketcher-react/.storybook/main.js
+++ packages/ketcher-react/.storybook/main.js
@@ -1,5 +1,5 @@
 module.exports = {
   stories: ['../src/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  addons: ['@storybook/addon-essentials'],
   framework: '@storybook/react'
 }

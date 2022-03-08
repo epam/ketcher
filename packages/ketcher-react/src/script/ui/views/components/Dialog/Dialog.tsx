0ec899caded9ce7fbf9310847c91570eba1842bb
fedorseliankin <fedor_seliankin@epam.com>
2022-03-08 12:24:48 +0400

    add conditional for label edit popup

--- packages/ketcher-react/src/script/ui/views/components/Dialog/Dialog.tsx
+++ packages/ketcher-react/src/script/ui/views/components/Dialog/Dialog.tsx
@@ -63,7 +63,12 @@ const Dialog: FC<Props> = (props) => {
   const dialogRef = useRef<HTMLDivElement>(null)
 
   useLayoutEffect(() => {
-    ;(dialogRef.current as any).focus()
+    if ((dialogRef.current?.children[0] as any).innerText === 'LABEL EDIT'){
+      (dialogRef.current?.children[1] as any).focus()
+    }
+    else {
+      (dialogRef.current as any).focus()
+    }
     return () => {
       ;(
         dialogRef.current

68faec6279a7c00fcf92c65f212460d3b84461fe
Anastasiia Moskvina <77681522+AnaMoskvina@users.noreply.github.com>
2022-04-05 18:56:35 +0300

    #1356 structure check window add placeholder and block checkboxes (#1399)
    * change structure check
    
    * fix styles
    
    * remove comments
    
    Co-authored-by: Anastasiia Moskvina <anastasiia_moskvina@epam.com>

--- packages/ketcher-react/src/script/ui/views/components/Dialog/Dialog.tsx
+++ packages/ketcher-react/src/script/ui/views/components/Dialog/Dialog.tsx
@@ -117,7 +117,7 @@ const Dialog: FC<Props> = (props) => {
       {...rest}
     >
       <header>
-        {headerContent || <span className={styles.title}>{title}</span>}
+        {headerContent || <span>{title}</span>}
         <div className={styles.btnContainer}>
           {buttonsTop && buttonsTop.map((button) => button)}
           <button className={styles.buttonTop} onClick={() => exit('Cancel')}>

6f9364e298226adf7860e2194ed31eaa5c3633fe
fedorseliankin <fedor_seliankin@epam.com>
2022-04-18 07:02:12 +0300

    change styles

--- packages/ketcher-react/src/script/ui/views/toolbars/TopToolbar/ElementWithDropdown.tsx
+++ packages/ketcher-react/src/script/ui/views/toolbars/TopToolbar/ElementWithDropdown.tsx
@@ -14,75 +14,102 @@
  * limitations under the License.
  ***************************************************************************/
 
-import { useState } from 'react'
-import { ClickAwayListener, Collapse, IconButton } from '@mui/material'
-import styled from '@emotion/styled'
+ import { useState } from 'react'
+ import { ClickAwayListener, Collapse, IconButton } from '@mui/material'
+ import styled from '@emotion/styled'
+ 
+ import Icon from 'src/script/ui/component/view/icon'
+ 
+ const ElementAndDropdown = styled('div')`
+   position: relative;
+ `
+ 
+ const DropDownContent = styled('div')`
+   display: flex;
+   position: absolute;
+   left: 5px;
+   border-radius: 2px;
+   flex-direction: column;
+   z-index: 20;
+   white-space: nowrap;
+   word-break: keep-all;
+   padding: 2px;
+   background: white; // TODO use variable
+   border: 1px solid gray; // TODO use variable
+   border-radius: 5px;
+   box-shadow: 0 6px 12px rgb(0 0 0 / 18%);
+ `
+ 
+ const DropDownButton = styled(IconButton)`
+   position: absolute;
+   padding: 0;
+   right: 0;
+   bottom: 0;
 
-import Icon from 'src/script/ui/component/view/icon'
-
-const ElementAndDropdown = styled('div')`
-  position: relative;
-`
-
-const DropDownContent = styled('div')`
-  display: flex;
-  position: absolute;
-  left: 5px;
-  border-radius: 2px;
-  flex-direction: column;
-  z-index: 20;
-  white-space: nowrap;
-  word-break: keep-all;
-  padding: 2px;
-  background: white; // TODO use variable
-  border: 1px solid gray; // TODO use variable
-  border-radius: 5px;
-  box-shadow: 0 6px 12px rgb(0 0 0 / 18%);
-`
-
-const DropDownButton = styled(IconButton)`
-  position: absolute;
-  padding: 0;
-  right: 0;
-  bottom: 0;
-`
-
-const DropDownArrow = styled(Icon)`
-  display: block;
-  width: 7px;
-  height: 7px;
-`
-
-interface ElementWithDropdownProps {
-  topElement: JSX.Element
-  dropDownElements: JSX.Element | JSX.Element[]
-}
-
-export const ElementWithDropdown = ({
-  topElement,
-  dropDownElements
-}: ElementWithDropdownProps) => {
-  const [isExpanded, setIsExpanded] = useState<boolean>(false)
-
-  const collapse = () => {
-    setIsExpanded(false)
+   &.disabled {
+    cursor: not-allowed;
+    opacity: 0.5;
+    background-color: initial;
+    color: #333;
+    pointer-events: auto;
   }
-
-  const expand = () => {
-    setIsExpanded(true)
-  }
-
-  return (
-    <ElementAndDropdown>
-      {topElement}
-      <DropDownButton onClick={expand}>
-        <DropDownArrow name="dropdown" />
-      </DropDownButton>
-      <Collapse in={isExpanded} timeout="auto" onClick={collapse} unmountOnExit>
-        <ClickAwayListener onClickAway={collapse}>
-          <DropDownContent>{dropDownElements}</DropDownContent>
-        </ClickAwayListener>
-      </Collapse>
-    </ElementAndDropdown>
-  )
-}
+ `
+ 
+ const DropDownArrow = styled(Icon)`
+   display: block;
+   width: 7px;
+   height: 7px;
+   margin: 1px;
+ 
+   &:hover {
+     fill: #005662;;
+     transform: none;
+     box-shadow: none;
+     transition: none;
+     background: none;
+   }
+ 
+   &svg {
+     width: 100%;
+     height: 100%;
+   }
+ 
+   &svg path {
+     fill: inherit;
+   }
+ `
+ 
+ interface ElementWithDropdownProps {
+   topElement: JSX.Element
+   dropDownElements: JSX.Element | JSX.Element[]
+ }
+ 
+ export const ElementWithDropdown = ({
+   topElement,
+   dropDownElements
+ }: ElementWithDropdownProps) => {
+   const [isExpanded, setIsExpanded] = useState<boolean>(false)
+ 
+   const collapse = () => {
+     setIsExpanded(false)
+   }
+ 
+   const expand = () => {
+     setIsExpanded(true)
+   } 
+   console.log(topElement.props.disabled)
+   return (
+     <ElementAndDropdown>
+       {topElement}
+       <DropDownButton onClick={expand} className={`expanded ${topElement.props.disabled ? 'disabled': ''}`}>
+         {!isExpanded && <DropDownArrow name="dropdown"/>}
+       </DropDownButton>
+       <Collapse in={isExpanded} timeout="auto" onClick={collapse} unmountOnExit>
+         <ClickAwayListener onClickAway={collapse}>
+           <DropDownContent>{dropDownElements}</DropDownContent>
+         </ClickAwayListener>
+       </Collapse>
+     </ElementAndDropdown>
+   )
+ }
+ 

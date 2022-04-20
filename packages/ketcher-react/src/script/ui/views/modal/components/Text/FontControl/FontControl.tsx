e5e672957f73c6c3580f0c34aefe0413752e66a9
Zaim Beshich <zaim_beshich@epam.com>
2022-04-20 12:16:49 +0300

    change fontSize button on Select

--- packages/ketcher-react/src/script/ui/views/modal/components/Text/FontControl/FontControl.tsx
+++ packages/ketcher-react/src/script/ui/views/modal/components/Text/FontControl/FontControl.tsx
@@ -14,63 +14,92 @@
  * limitations under the License.
  ***************************************************************************/
 
-import { useEffect, useMemo, useState } from 'react'
+import { useState } from 'react'
+import { MenuItem, Select, SelectChangeEvent } from '@mui/material'
 
 import classes from './FontControl.module.less'
 
 import { range } from 'lodash/fp'
+// import { classes } from 'src/script/ui/views/toolbars/ToolbarGroupItem/ToolbarMultiToolItem/variants/DefaultMultiTool'
 
 export const FontControl = ({ editorState, setEditorState, styles }) => {
-  const defaultFontSize = '13px'
-  const [isShowingFontSizeMenu, setIsShowingFontSizeMenu] = useState(false)
-  const [currentFontSize, setCurrentFontSize] = useState(defaultFontSize)
-
-  const setFontSize = (e, value) => {
-    e.preventDefault()
-    setCurrentFontSize(value)
-    const newEditorState = styles.fontSize.remove(editorState)
-    setEditorState(styles.fontSize.add(newEditorState, value))
-    setIsShowingFontSizeMenu(false)
+  const defaultFontSize = 13
+  const menuProps = {
+    PaperProps: {
+      style: {
+        maxHeight: 150,
+        width: 69
+      }
+    }
   }
 
-  const currentStyle = styles.fontSize.current(editorState)
+  const [currentFontSize, setCurrentFontSize] = useState(defaultFontSize)
 
-  useEffect(() => {
-    setCurrentFontSize(currentStyle || defaultFontSize)
-  }, [currentStyle])
+  const setFontSize = (event: SelectChangeEvent<number>) => {
+    const {
+      target: { value }
+    } = event
+    setCurrentFontSize(typeof value === 'string' ? Number(value) : value)
+    const newEditorState = styles.fontSize.remove(editorState)
+    setEditorState(styles.fontSize.add(newEditorState, `${value}px`))
+  }
+  // const currentStyle = styles.fontSize.current(editorState)
 
   const MIN_FONT_SIZE = 4
   const MAX_FONT_SIZE = 144
   const fontSizes = range(MIN_FONT_SIZE, MAX_FONT_SIZE + 1)
 
-  const fontSizeOptions = useMemo(
-    () =>
-      fontSizes.map((fontSize) => (
-        <div
-          key={fontSize}
-          className={classes.fontSizeOption}
-          onMouseDown={(e) => setFontSize(e, `${fontSize}px`)}
-        >
-          {fontSize}
-        </div>
-      )),
-    [isShowingFontSizeMenu]
-  )
+  // useEffect(() => {
+  //   setCurrentFontSize(currentStyle || defaultFontSize)
+  // }, [currentStyle])
+
+  // const fontSizeOptions = useMemo(
+  //   () =>
+  //     fontSizes.map((fontSize) => (
+  //       <div
+  //         key={fontSize}
+  //         className={classes.fontSizeOption}
+  //         onMouseDown={(e) => setFontSize(e, `${fontSize}px`)}>
+  //         {fontSize}
+  //       </div>
+  //     )),
+  //   [isShowingFontSizeMenu]
+  // )
 
   return (
+    // <div>
+    //   <button
+    //     className={classes.fontBtn}
+    //     onMouseDown={(e) => {
+    //       e.preventDefault()
+    //       setIsShowingFontSizeMenu(!isShowingFontSizeMenu)
+    //     }}>
+    //     {currentFontSize}
+    //   </button>
+    //   {isShowingFontSizeMenu ? (
+    //     <div className={classes.fontSizeMenu}>{fontSizeOptions}</div>
+    //   ) : null}
+    // </div>
     <div>
-      <button
-        className={classes.fontBtn}
-        onMouseDown={(e) => {
-          e.preventDefault()
-          setIsShowingFontSizeMenu(!isShowingFontSizeMenu)
-        }}
-      >
-        {currentFontSize}
-      </button>
-      {isShowingFontSizeMenu ? (
-        <div className={classes.fontSizeMenu}>{fontSizeOptions}</div>
-      ) : null}
+      <label>
+        Font Size
+        <Select
+          sx={{ width: 70, fontSize: 14 }}
+          displayEmpty
+          className={classes.select}
+          multiple={false}
+          MenuProps={menuProps}
+          value={currentFontSize}
+          onChange={setFontSize}
+          defaultValue={defaultFontSize}
+          renderValue={(selected) => selected}>
+          {fontSizes.map((size) => (
+            <MenuItem key={size} value={size}>
+              {size}
+            </MenuItem>
+          ))}
+        </Select>
+      </label>
     </div>
   )
 }

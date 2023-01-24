ed2dca9b8261ae3b724956efccc9e7d71bf195e6
Nikita_Vozisov <Nikita_Vozisov@epam.com>
2023-01-24 13:29:54 +0300

    #1889 – added check for only bond selection

--- packages/ketcher-react/src/script/ui/action/flips.ts
+++ packages/ketcher-react/src/script/ui/action/flips.ts
@@ -1,4 +1,4 @@
-import { Bond, ReBond, ReStruct } from 'ketcher-core'
+import { ReBond, ReStruct, isAttachmentBond } from 'ketcher-core'
 
 export function isFlipDisabled(editor): boolean {
   const selection: { atoms: number[]; bonds: number[] } = editor.selection()
@@ -9,9 +9,6 @@ export function isFlipDisabled(editor): boolean {
   }
 
   const { bonds = [], atoms = [] } = selection
-  const isAttachmentBond = ({ begin, end }: Bond): boolean =>
-    (selection.atoms.includes(begin) && !selection.atoms.includes(end)) ||
-    (selection.atoms.includes(end) && !selection.atoms.includes(begin))
   const getBondIdsForAtom = (atomId: number) => {
     const result: number[] = []
     for (const [bondId, bond] of restruct.bonds.entries()) {
@@ -27,7 +24,7 @@ export function isFlipDisabled(editor): boolean {
       const atomBondIds = getBondIdsForAtom(atomId)
       for (const atomBondId of atomBondIds) {
         const bond: ReBond | undefined = restruct.bonds.get(atomBondId)
-        if (bond && isAttachmentBond(bond.b)) {
+        if (bond && isAttachmentBond(bond.b, selection)) {
           result.push(atomBondId)
         }
       }
@@ -39,7 +36,7 @@ export function isFlipDisabled(editor): boolean {
     const totalBondIds = new Set([...bonds, ...getBondIdsForAttachmentAtoms()])
     for (const bondId of totalBondIds) {
       const bond: ReBond | undefined = restruct.bonds.get(bondId)
-      if (bond && isAttachmentBond(bond.b)) {
+      if (bond && isAttachmentBond(bond.b, selection)) {
         amountOfAttachmentBonds++
       }
     }

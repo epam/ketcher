37470d78f30e65c311d1204de65d0eca88360ba6
Nikita_Vozisov <Nikita_Vozisov@epam.com>
2023-02-16 15:42:09 +0300

    #2228 – There are freezes in application when Chrome DevTools are opened

--- packages/ketcher-react/src/script/ui/state/functionalGroups/index.ts
+++ packages/ketcher-react/src/script/ui/state/functionalGroups/index.ts
@@ -23,6 +23,7 @@ import {
   Struct
 } from 'ketcher-core'
 import templatesRawData from '../../../../templates/fg.sdf'
+import _ from 'lodash'
 
 interface FGState {
   lib: []
@@ -58,9 +59,11 @@ const highlightFGroup = (group: any) => ({
   payload: group
 })
 
-export function highlightFG(dispatch, group: any) {
+function notThrottledHighlightFG(dispatch, group: any) {
   dispatch(highlightFGroup(group))
 }
+
+export const highlightFG = _.throttle(notThrottledHighlightFG, 100)
 
 export function initFGTemplates() {
   return async (dispatch) => {

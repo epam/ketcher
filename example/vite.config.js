daf3c28083855d9a072653263fe02ff85bac6035
Yulei Chen <yulei_chen@epam.com>
2023-04-17 17:57:32 +0800

    #2411 - Ignore warning which shouldn't be warned

--- example/vite.config.js
+++ example/vite.config.js
@@ -1,7 +1,7 @@
 import replace from '@rollup/plugin-replace'
 import react from '@vitejs/plugin-react'
 import { resolve } from 'path'
-import { defineConfig, loadEnv } from 'vite'
+import { createLogger, defineConfig, loadEnv } from 'vite'
 import { createHtmlPlugin } from 'vite-plugin-html'
 import vitePluginRaw from 'vite-plugin-raw'
 import svgr from 'vite-plugin-svgr'
@@ -67,6 +67,18 @@ const HtmlReplaceVitePlugin = () => {
         )
     }
   }
+}
+
+const logger = createLogger()
+const loggerWarn = logger.warn
+logger.warn = (msg, options) => {
+  if (
+    // This warning occurs when entry html is not at the root path
+    msg.includes('files in the public directory are served at the root path.')
+  ) {
+    return
+  }
+  loggerWarn(msg, options)
 }
 
 export default defineConfig({
@@ -173,5 +185,6 @@ export default defineConfig({
         replacement: './indigoWorker?worker'
       }
     ]
-  }
+  },
+  customLogger: logger
 })

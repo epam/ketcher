0de57623b6d3ba5ca1c8d88e352d7d29dbe44718
Nikita <Nikita_Vozisov@epam.com>
2023-05-02 14:26:41 +0300

    #2548 – If name of Superatom matches abbreviation in Custom Templates, then tooltip is shown on hover (#2563)

--- packages/ketcher-react/src/script/editor/utils/functionalGroupsTooltip.ts
+++ packages/ketcher-react/src/script/editor/utils/functionalGroupsTooltip.ts
@@ -1,8 +1,30 @@
-import { FunctionalGroup } from 'ketcher-core'
+import { Bond, Struct } from 'ketcher-core'
 
 let showTooltipTimer: ReturnType<typeof setTimeout> | null = null
 
 export const TOOLTIP_DELAY = 300
+
+function makeStruct(editor, sGroup) {
+  const existingStruct = editor.struct()
+  const struct = new Struct()
+  const atomsIdMapping = new Map()
+
+  sGroup.atoms.forEach((atomId) => {
+    const atomIdInTooltip = struct.atoms.add(existingStruct.atoms.get(atomId))
+    atomsIdMapping.set(atomId, atomIdInTooltip)
+  })
+  Array.from(existingStruct.bonds).forEach((value) => {
+    const [_, bond] = value as [number, Bond]
+    const clonedBond = bond.clone(atomsIdMapping)
+    const isInsideSGroup =
+      sGroup.atoms.includes(bond.begin) || sGroup.atoms.includes(bond.end)
+    if (isInsideSGroup) {
+      struct.bonds.add(clonedBond)
+    }
+  })
+
+  return struct
+}
 
 export function showTooltip(editor, infoPanelData) {
   editor.event.showInfo.dispatch(null)
@@ -28,7 +50,8 @@ export function showFunctionalGroupsTooltip(editor) {
     const sGroup = editor.struct()?.sgroups.get(closestCollapsibleStructures.id)
     if (sGroup && !sGroup.data.expanded && sGroup.hovering) {
       const groupName = sGroup.data.name
-      const groupStruct = FunctionalGroup.getFunctionalGroupByName(groupName)
+      const groupStruct = makeStruct(editor, sGroup)
+      groupStruct.name = groupName
       infoPanelData = {
         groupStruct,
         event,

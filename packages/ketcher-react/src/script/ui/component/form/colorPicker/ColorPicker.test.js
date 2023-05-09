dd2e2a0f467644727634c05d19f046354679a50c
gairon <gairon@mail.ru>
2023-05-09 12:13:08 +0300

    #2196 - the Save button is active for empty canvas (#2579)
    * #2196 - the Save button is active for empty canvas
    * Fix disabled prop for SaveButton
    * Add tests for disabled prop

--- packages/ketcher-react/src/script/ui/component/form/colorPicker/ColorPicker.test.js
+++ packages/ketcher-react/src/script/ui/component/form/colorPicker/ColorPicker.test.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import { fireEvent, render, screen, waitFor } from '@testing-library/react'
-import '@testing-library/jest-dom'
 import userEvent from '@testing-library/user-event'
 
 import ColorPicker from './ColorPicker'

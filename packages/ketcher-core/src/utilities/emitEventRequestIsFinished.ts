32b9b5affd6484e3e008a1166773ee1cd5cef668
Konstantin Zharich <kostantin_zharich@epam.com>
2023-05-16 17:21:33 +0200

    - moved emitEventRequestIsFinished into ketcher-core package

--- packages/ketcher-core/src/utilities/emitEventRequestIsFinished.ts
+++ packages/ketcher-core/src/utilities/emitEventRequestIsFinished.ts
@@ -1,16 +1,11 @@
-import { Ketcher } from 'ketcher-core'
-
 const REQUEST_IS_FINISHED = 'REQUEST_IS_FINISHED'
-
-declare global {
-  interface Window {
-    ketcher: Ketcher
-  }
-}
 
 /**
  * Emit event when request is finished
  * Used only for testers to prevent long delays in test cases
+ * Triggers on Add to canvas / Aromatize / Dearomatize
+ * Clean Up / Calculate CIP / Paste from clipboard
+ * maybe somewhere else
  */
 export function emitEventRequestIsFinished(): void {
   window?.ketcher?.eventBus.emit(REQUEST_IS_FINISHED)

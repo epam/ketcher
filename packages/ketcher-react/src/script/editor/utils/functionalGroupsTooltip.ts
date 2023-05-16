235fb388f4c6105ae956ccfb42b0e7604d572bb5
Maxim Novikov <74115008+Ziprion@users.noreply.github.com>
2023-05-16 19:14:08 +0300

    #2288 - tooltip appears after dragging abbreviation and stay on canvas until release click (#2535)
    Co-authored-by: Maxim Novikov <Maxim_Novikov@epam.com>
    Co-authored-by: Nikita_Vozisov <Nikita_Vozisov@epam.com>

--- packages/ketcher-react/src/script/editor/utils/functionalGroupsTooltip.ts
+++ packages/ketcher-react/src/script/editor/utils/functionalGroupsTooltip.ts
@@ -1,16 +1,23 @@
-import { Bond, Struct } from 'ketcher-core'
+import Editor from '../Editor'
+import { Bond, SGroup, Struct } from 'ketcher-core'
 
 let showTooltipTimer: ReturnType<typeof setTimeout> | null = null
 
 export const TOOLTIP_DELAY = 300
 
-function makeStruct(editor, sGroup) {
+type InfoPanelData = {
+  groupStruct: Struct
+  sGroup: SGroup
+  event: PointerEvent
+}
+
+function makeStruct(editor: Editor, sGroup: SGroup) {
   const existingStruct = editor.struct()
   const struct = new Struct()
   const atomsIdMapping = new Map()
 
   sGroup.atoms.forEach((atomId) => {
-    const atomIdInTooltip = struct.atoms.add(existingStruct.atoms.get(atomId))
+    const atomIdInTooltip = struct.atoms.add(existingStruct.atoms.get(atomId)!)
     atomsIdMapping.set(atomId, atomIdInTooltip)
   })
   Array.from(existingStruct.bonds).forEach((value) => {
@@ -26,27 +33,44 @@ function makeStruct(editor, sGroup) {
   return struct
 }
 
-export function showTooltip(editor, infoPanelData) {
+function hideTooltip(editor: Editor) {
   editor.event.showInfo.dispatch(null)
 
   if (showTooltipTimer) {
     clearTimeout(showTooltipTimer)
   }
-  if (infoPanelData) {
-    showTooltipTimer = setTimeout(() => {
-      editor.event.showInfo.dispatch(infoPanelData)
-    }, TOOLTIP_DELAY)
-  }
 }
 
-export function showFunctionalGroupsTooltip(editor) {
-  let infoPanelData: any = null
+function showTooltip(editor: Editor, infoPanelData: InfoPanelData | null) {
+  hideTooltip(editor)
+
+  showTooltipTimer = setTimeout(() => {
+    editor.event.showInfo.dispatch(infoPanelData)
+  }, TOOLTIP_DELAY)
+}
+
+export function setFunctionalGroupsTooltip({
+  editor,
+  event,
+  isShow
+}: {
+  editor: Editor
+  event?: PointerEvent
+  isShow: boolean
+}) {
+  if (!isShow) {
+    hideTooltip(editor)
+
+    return
+  }
+
+  let infoPanelData: null | InfoPanelData = null
   const checkFunctionGroupTypes = ['sgroups', 'functionalGroups']
   const closestCollapsibleStructures = editor.findItem(
     event,
     checkFunctionGroupTypes
   )
-  if (closestCollapsibleStructures) {
+  if (closestCollapsibleStructures && event) {
     const sGroup = editor.struct()?.sgroups.get(closestCollapsibleStructures.id)
     if (sGroup && !sGroup.data.expanded && sGroup.hovering) {
       const groupName = sGroup.data.name

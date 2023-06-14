c19101dbced2744ced03888937ebdf557f7dafb4
Dmitrii Vasilev <dmitrii.vasilev@clarivate.com>
2023-06-14 03:18:40 +0300

    #2626 - old IconButton component replaced with a new one from the components folder

--- packages/ketcher-react/src/script/ui/views/toolbars/TopToolbar/FileControls.tsx
+++ packages/ketcher-react/src/script/ui/views/toolbars/TopToolbar/FileControls.tsx
@@ -13,8 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  ***************************************************************************/
-
-import { IconButton } from './IconButton'
+import { IconButton } from 'components'
 
 interface FileControlsProps {
   onFileOpen: () => void

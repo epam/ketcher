126ece7650958923b23bbc1e54bf503496a16f6d
Nikita_Vozisov <Nikita_Vozisov@epam.com>
2023-06-22 19:19:37 +0300

    fixing waitFoLoad

--- ketcher-autotests/tests/utils/common/waitForLoad/waitForLoad.ts
+++ ketcher-autotests/tests/utils/common/waitForLoad/waitForLoad.ts
@@ -30,12 +30,10 @@ export const waitForLoad = async (page: Page, callback: Function) => {
   await page.waitForFunction(() => window.ketcher);
   // const promise = page.evaluate(evaluateCallback, REQUEST_IS_FINISHED);
   callback();
-
-  await page.waitForSelector('.loading-spinner', { state: 'attached' });
-  // Wait for the loading overlay to disappear
-  await page.waitForSelector('.loading-spinner', { state: 'detached' });
-
-  // await page.waitForSelector('.loading-spinner', { state: 'detached' });
+  await delay(DELAY_IN_SECONDS.ONE);
+  if (await page.locator('.loading-spinner').isVisible()) {
+    await page.waitForSelector('.loading-spinner', { state: 'detached' });
+  }
 
   // return promise;
 };

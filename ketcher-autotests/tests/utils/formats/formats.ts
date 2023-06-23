bf5b8794ddc2fc64ab7c21f71988d827665dae67
AnastasiiaPlyako <anastasia_plyako@epam.com>
2023-06-23 18:07:46 +0400

    log smile

--- ketcher-autotests/tests/utils/formats/formats.ts
+++ ketcher-autotests/tests/utils/formats/formats.ts
@@ -11,8 +11,8 @@ export async function getCml(page: Page) {
 
 export async function getSmiles(page: Page) {
   await page.waitForFunction(() => window.ketcher);
-  return await page.evaluate(() => {
-    window.ketcher.getSmiles();
+  return await page.evaluate(async () => {
+    return await window.ketcher.getSmiles();
   });
 }
 

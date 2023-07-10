54efecb8ae51d24625b01f6c1e7847487768ea0e
AnastasiiaPlyako <anastasia_plyako@epam.com>
2023-07-10 18:59:08 +0300

    delete ignored FileManagment, change function openFile

--- ketcher-autotests/playwright.config.ts
+++ ketcher-autotests/playwright.config.ts
@@ -15,7 +15,6 @@ dotenv.config();
 const ignoredTests = [
   'API/**',
   'Examples/**',
-  'File-Management/**',
   'Indigo-Tools/**',
   'Reagents/**',
   'Structure-Creating-&-Editing/**',
@@ -64,7 +63,7 @@ const config: PlaywrightTestConfig = {
   /* Retry on CI only */
   retries: process.env.CI ? MAX_NUMBER_OF_RETRIES : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  workers: process.env.CI ? 1 : 2,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
     [

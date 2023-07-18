2d9d789b2fb17a072cc28919428c072cf51f51c4
Kalugin Sergei <sergei_kalugin@epam.com>
2023-07-18 15:23:25 +0400

    #2765 - Introduce to .ket file specification attachment point information for super atoms (#2919)
    * #2765 - introduce to .ket file specification attachment point information for super atoms
    
    * #2765 - refactor import statements for sGroupAttachmentPoint
    
    * #2765 – Introduce to .ket file specification attachment point information for super atoms
    
    ---------
    
    Co-authored-by: Nikita_Vozisov <Nikita_Vozisov@epam.com>

--- packages/ketcher-core/src/domain/serializers/mol/__tests__/v2000.parseSGroup.SAP.test.ts
+++ packages/ketcher-core/src/domain/serializers/mol/__tests__/v2000.parseSGroup.SAP.test.ts
@@ -10,7 +10,7 @@ describe('parseSGroup', () => {
     expect(sGroupId).toBe(0);
     expect(attachmentPoints[0].atomId).toBe(1);
     expect(attachmentPoints[0].leaveAtomId).toBeUndefined();
-    expect(attachmentPoints[0].additionalData).toBe('  ');
+    expect(attachmentPoints[0].attachmentId).toBe('  ');
   });
 
   it('parseSGroupSAPLineV2000 should parse valid SAP line with two attachment points', () => {
@@ -23,6 +23,6 @@ describe('parseSGroup', () => {
     expect(attachmentPoints.length).toBe(2);
     expect(attachmentPoints[1].atomId).toBe(2);
     expect(attachmentPoints[1].leaveAtomId).toBe(0);
-    expect(attachmentPoints[1].additionalData).toBe('  ');
+    expect(attachmentPoints[1].attachmentId).toBe('  ');
   });
 });

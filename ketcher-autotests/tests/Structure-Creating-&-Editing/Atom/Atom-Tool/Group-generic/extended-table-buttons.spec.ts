bc1f4d0c5c5e5023290352568efae087f4921f8a
yholik <yahor_holik@epam.com>
2023-07-28 13:11:27 +0200

    #2981 - added tests for extended table

---
+++ ketcher-autotests/tests/Structure-Creating-&-Editing/Atom/Atom-Tool/Group-generic/extended-table-buttons.spec.ts
@@ -0,0 +1,19 @@
+import { test } from '@playwright/test';
+import { takeEditorScreenshot, AtomButton, selectAtomInToolbar } from '@utils';
+
+test.describe('Open Ketcher', () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto('');
+  });
+
+  test.afterEach(async ({ page }) => {
+    await takeEditorScreenshot(page);
+  });
+
+  test('Periodic table check', async ({ page }) => {
+    /* 
+      Test cases: EPMLSOPKET-16821, EPMLSOPKET-16825, EPMLSOPKET-16823, EPMLSOPKET-16827, EPMLSOPKET-16829
+      */
+    await selectAtomInToolbar(AtomButton.Extended, page);
+  });
+});

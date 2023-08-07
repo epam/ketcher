e34720a055e8f065c2081f06104b5b22245341bd
Starla Huang <starla_huang@epam.com>
2023-08-07 19:39:17 +0800

    #2912 modal example to reproduce memory leak issue

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -8,6 +8,7 @@ import {
   StructServiceProvider,
 } from 'ketcher-core';
 import { PolymerToggler } from './PolymerToggler';
+import Modal from 'react-modal';
 
 const getHiddenButtonsConfig = (): ButtonsConfig => {
   const searchParams = new URLSearchParams(window.location.search);
@@ -48,8 +49,9 @@ const App = () => {
   const hiddenButtonsConfig = getHiddenButtonsConfig();
   const [hasError, setHasError] = useState(false);
   const [errorMessage, setErrorMessage] = useState('');
+  const [isOpen, setIsOpen] = useState(false);
   const [showPolymerEditor, setShowPolymerEditor] = useState(false);
-
+  Modal.setAppElement('#root');
   return showPolymerEditor ? (
     <>
       <PolymerEditor />
@@ -57,38 +59,46 @@ const App = () => {
     </>
   ) : (
     <>
-      <Editor
-        errorHandler={(message: string) => {
-          setHasError(true);
-          setErrorMessage(message.toString());
-        }}
-        buttons={hiddenButtonsConfig}
-        staticResourcesUrl={process.env.PUBLIC_URL}
-        structServiceProvider={structServiceProvider}
-        onInit={(ketcher: Ketcher) => {
-          window.ketcher = ketcher;
-          window.parent.postMessage(
-            {
-              eventType: 'init',
-            },
-            '*',
-          );
-        }}
-      />
-      {enablePolymerEditor && <PolymerToggler toggle={setShowPolymerEditor} />}
-      {hasError && (
-        <InfoModal
-          message={errorMessage}
-          close={() => {
-            setHasError(false);
+      <button onClick={() => setIsOpen(true)}>open</button>
+      <Modal isOpen={isOpen}>
+        <div>
+          <button onClick={() => setIsOpen(false)}>close</button>
+          <Editor
+            errorHandler={(message: string) => {
+              setHasError(true);
+              setErrorMessage(message.toString());
+            }}
+            buttons={hiddenButtonsConfig}
+            staticResourcesUrl={process.env.PUBLIC_URL}
+            structServiceProvider={structServiceProvider}
+            onInit={(ketcher: Ketcher) => {
+              window.ketcher = ketcher;
+              window.parent.postMessage(
+                {
+                  eventType: 'init',
+                },
+                '*',
+              );
+            }}
+          />
+          {enablePolymerEditor && (
+            <PolymerToggler toggle={setShowPolymerEditor} />
+          )}
+          {hasError && (
+            <InfoModal
+              message={errorMessage}
+              close={() => {
+                setHasError(false);
 
-            // Focus on editor after modal is closed
-            const cliparea: HTMLElement | null =
-              document.querySelector('.cliparea');
-            cliparea?.focus();
-          }}
-        />
-      )}
+                // Focus on editor after modal is closed
+                const cliparea: HTMLElement | null =
+                  document.querySelector('.cliparea');
+                cliparea?.focus();
+              }}
+            />
+          )}
+        </div>
+      </Modal>
     </>
   );
 };

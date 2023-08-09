cb1ff67e6da6b6c255a4ff1e0bd64d068bd02afb
Sasha Graves <all0exa@gmail.com>
2023-08-09 14:21:08 +0200

    add peptides to canvas

---
+++ ketcher-autotests/tests/Macromolecule-editor/Peptides/peptides-add-to-canvas.spec.ts
@@ -0,0 +1,23 @@
+import { test } from '@playwright/test';
+import { clickInTheMiddleOfTheScreen } from '@utils';
+import { POLYMER_TOGGLER } from '../../../constants/testIdConstants';
+
+test('Select peptide and drag it to canvas', async ({ page }) => {
+  // Load "http://localhost:5173/"
+  await page.goto('');
+
+  // Click on POLYMER_TOGGLER
+  await page.getByTestId(POLYMER_TOGGLER).click();
+
+  // Click on <div> "A ★"
+  await page.click('[data-testid="A___Alanine"]');
+
+  // Click on <svg> #polymer-editor-canvas
+  await clickInTheMiddleOfTheScreen(page);
+
+  // Take full page screenshot
+  await page.screenshot({
+    path: 'tests/Macromolecule-editor/screenshots/peptides-add-to-canvas.png',
+    fullPage: true,
+  });
+});

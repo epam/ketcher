0f48a3038caf066cd9f9859870d3e8b77c7719ee
gayane_chilingaryan <gayane_chilingaryan@epam.com>
2023-08-14 10:42:18 +0400

    #2541 - removed filter method from unit test

--- packages/ketcher-react/src/script/ui/component/view/ToolActions.test.tsx
+++ packages/ketcher-react/src/script/ui/component/view/ToolActions.test.tsx
@@ -1,9 +1,7 @@
 import toolActions from '../../action/tools';
 
 describe('ToolActions', () => {
-  const toolsWithTitles = Object.values(toolActions).filter(
-    (tool) => tool.title !== undefined,
-  );
+  const toolsWithTitles = Object.values(toolActions);
 
   it('should have a "title" property for each tool', () => {
     toolsWithTitles.forEach((tool) => {

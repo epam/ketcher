74097c91c217d07ce044646e34a52bd8a6a2a546
Aleksei Proskurnov <aleksei_proskurnov@epam.com>
2023-08-14 17:58:45 +0300

    moved wait for loader to separate directory

--- ketcher-autotests/tests/utils/common/index.ts
+++ ketcher-autotests/tests/utils/common/index.ts
@@ -1,3 +1,1 @@
-export { waitForLoad } from '@utils/common/waitForLoad/waitForLoad';
-export { waitForKetcherInit } from '@utils/common/waitForKetcherInit/waitForKetcherInit';
-export { waitForSpinnerFinishedWork } from '@utils/common/waitForSpinnerFinishedWork/waitForSpinnerFinishedWork';
+export * from './loaders';

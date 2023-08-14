96f175661f17ac45dd7f84ec7a8636e3fa041370
Aleksei Proskurnov <aleksei_proskurnov@epam.com>
2023-08-14 11:54:39 +0300

    optimize API test execution

--- ketcher-autotests/tests/utils/common/index.ts
+++ ketcher-autotests/tests/utils/common/index.ts
@@ -1,1 +1,3 @@
 export { waitForLoad } from '@utils/common/waitForLoad/waitForLoad';
+export { waitForKetcherInit } from '@utils/common/waitForKetcherInit/waitForKetcherInit';
+export { waitForSpinnerFinishedWork } from '@utils/common/waitForSpinnerFinishedWork/waitForSpinnerFinishedWork';

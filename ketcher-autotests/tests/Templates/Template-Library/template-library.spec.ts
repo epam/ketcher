65fd631497960b87c5412cd39cd146b80a9c93c8
nanoblit <nanoblit@users.noreply.github.com>
2023-08-28 15:49:14 +0200

    #3019 - Add tests

--- ketcher-autotests/tests/Templates/Template-Library/template-library.spec.ts
+++ ketcher-autotests/tests/Templates/Template-Library/template-library.spec.ts
@@ -1,9 +1,40 @@
-import { test } from '@playwright/test';
+import { Page, test } from '@playwright/test';
 import {
+  getCoordinatesOfTheMiddleOfTheScreen,
+  pressButton,
+  resetCurrentTool,
   selectTopPanelButton,
+  STRUCTURE_LIBRARY_BUTTON_NAME,
   takeEditorScreenshot,
   TopPanelButton,
 } from '@utils';
+
+async function setDisplayStereoFlagsSettingToOn(page: Page) {
+  await selectTopPanelButton(TopPanelButton.Settings, page);
+  await page.getByText('Stereochemistry', { exact: true }).click();
+  await pressButton(page, 'IUPAC style');
+  await page.getByRole('option', { name: 'On' }).click();
+  await pressButton(page, 'Apply');
+}
+
+async function switchIgnoreChiralFlagSetting(page: Page) {
+  await selectTopPanelButton(TopPanelButton.Settings, page);
+  await page.getByText('Stereochemistry', { exact: true }).click();
+  await page.getByText('Ignore the chiral flag').click();
+  await pressButton(page, 'Apply');
+}
+
+async function placePhenylalanineMustard(page: Page, x: number, y: number) {
+  await pressButton(page, STRUCTURE_LIBRARY_BUTTON_NAME);
+  const phenylalanineLocator = page.locator(
+    `div[title*="Phenylalanine mustard"] > div`,
+  );
+  if ((await phenylalanineLocator.count()) === 0) {
+    await page.getByText('Aromatics').click();
+  }
+  await phenylalanineLocator.first().click();
+  await page.mouse.click(x, y);
+}
 
 test.describe('Templates - Template Library', () => {
   test.beforeEach(async ({ page }) => {
@@ -11,17 +42,22 @@ test.describe('Templates - Template Library', () => {
   });
 
   test.afterEach(async ({ page }) => {
+    await resetCurrentTool(page);
     await takeEditorScreenshot(page);
   });
 
-  test.only('Template with chiral flag 0 with ignoreChiralFlag enabled/disabled', async ({
+  test('Template with chiral flag 0 with ignoreChiralFlag enabled/disabled', async ({
     page,
   }) => {
-    await selectTopPanelButton(TopPanelButton.Settings, page);
-    await page.getByText('Stereochemistry', { exact: true }).click();
-    await page.getByLabel('Ignore the chiral flag').click();
-    // await pressButton(page, 'Labels Only');
-    // await page.getByRole('option', { name: color }).click();
-    // await pressButton(page, 'Apply');
+    const offsetX = 200;
+    const { x, y } = await getCoordinatesOfTheMiddleOfTheScreen(page);
+
+    await setDisplayStereoFlagsSettingToOn(page);
+
+    await switchIgnoreChiralFlagSetting(page);
+    await placePhenylalanineMustard(page, x - offsetX, y);
+
+    await switchIgnoreChiralFlagSetting(page);
+    await placePhenylalanineMustard(page, x + offsetX, y);
   });
 });

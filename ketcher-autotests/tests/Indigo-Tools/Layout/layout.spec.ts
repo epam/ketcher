2afee46c2d8e30bac0b7277fa5d6cc56dd1d3762
Gayane Chilingaryan <gayane_chilingaryan@epam.com>
2023-09-06 17:12:54 +0400

    #3208 - structure disappearance by pressing layout

--- ketcher-autotests/tests/Indigo-Tools/Layout/layout.spec.ts
+++ ketcher-autotests/tests/Indigo-Tools/Layout/layout.spec.ts
@@ -7,6 +7,7 @@ import {
   pressButton,
   waitForLoad,
   getCoordinatesOfTheMiddleOfTheScreen,
+  openFileAndAddToCanvas,
 } from '@utils';
 
 async function openFileWithShift(filename: string, page: Page) {
@@ -27,7 +28,7 @@ test.describe('Indigo Tools - Layout', () => {
 
   test('Center molecule after layout', async ({ page }) => {
     // Related Github issue: https://github.com/epam/ketcher/issues/2078
-    const anyStructure = 'benzene-rings.mol';
+    const anyStructure = 'Molfiles-V2000/benzene-rings.mol';
     await openFileWithShift(anyStructure, page);
     await selectTopPanelButton(TopPanelButton.Layout, page);
     await takeEditorScreenshot(page);
@@ -41,6 +42,18 @@ test.describe('Indigo Tools - Layout', () => {
     await openFileWithShift(structureWithStereoFlags, page);
     await selectTopPanelButton(TopPanelButton.Layout, page);
     await selectTopPanelButton(TopPanelButton.Layout, page);
+    await selectTopPanelButton(TopPanelButton.Layout, page);
+    await takeEditorScreenshot(page);
+  });
+
+  test('After applying Layout, the structure does not disappear and can be interacted with', async ({
+    page,
+  }) => {
+    // Related Github issue: https://github.com/epam/ketcher/issues/3208
+    await openFileAndAddToCanvas(
+      'Molfiles-V2000/chloro-ethylamino-dimethyl-propoxy-propan-ol.mol',
+      page,
+    );
     await selectTopPanelButton(TopPanelButton.Layout, page);
     await takeEditorScreenshot(page);
   });

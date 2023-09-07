519e67716f70ddec371f28dcc284c1176cfc9bef
Gayane Chilingaryan <gayane_chilingaryan@epam.com>
2023-09-07 14:07:25 +0400

    #3208 - simplify 'Stereo flag' test by using a loop and refactor 'Center molecule after layout' test

--- ketcher-autotests/tests/Indigo-Tools/Layout/layout.spec.ts
+++ ketcher-autotests/tests/Indigo-Tools/Layout/layout.spec.ts
@@ -28,8 +28,7 @@ test.describe('Indigo Tools - Layout', () => {
 
   test('Center molecule after layout', async ({ page }) => {
     // Related Github issue: https://github.com/epam/ketcher/issues/2078
-    const anyStructure = 'Molfiles-V2000/benzene-rings.mol';
-    await openFileWithShift(anyStructure, page);
+    await openFileAndAddToCanvas('Molfiles-V2000/benzene-rings.mol', page);
     await selectTopPanelButton(TopPanelButton.Layout, page);
     await takeEditorScreenshot(page);
   });
@@ -39,10 +38,11 @@ test.describe('Indigo Tools - Layout', () => {
   }) => {
     // Related Github issue: https://github.com/epam/ketcher/issues/3025
     const structureWithStereoFlags = 'KET/structure-with-stereo-flags.ket';
+    const numberOfIterations = 3;
     await openFileWithShift(structureWithStereoFlags, page);
-    await selectTopPanelButton(TopPanelButton.Layout, page);
-    await selectTopPanelButton(TopPanelButton.Layout, page);
-    await selectTopPanelButton(TopPanelButton.Layout, page);
+    for (let i = 0; i < numberOfIterations; i++) {
+      await selectTopPanelButton(TopPanelButton.Layout, page);
+    }
     await takeEditorScreenshot(page);
   });
 

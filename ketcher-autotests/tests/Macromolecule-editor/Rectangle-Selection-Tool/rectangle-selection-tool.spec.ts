4bf608a18c2d7a1fd35c42a730a8cfea6beb11ba
Olga Mazurina <olga_mazurina@epam.com>
2023-09-08 18:50:43 +0200

    #2367 - add e2e test

--- ketcher-autotests/tests/Macromolecule-editor/Rectangle-Selection-Tool/rectangle-selection-tool.spec.ts
+++ ketcher-autotests/tests/Macromolecule-editor/Rectangle-Selection-Tool/rectangle-selection-tool.spec.ts
@@ -1,6 +1,7 @@
 import { test } from '@playwright/test';
 import {
   addMonomerToCanvas,
+  dragMouseTo,
   selectEraseTool,
   selectRectangleArea,
   selectRectangleSelectionTool,
@@ -67,6 +68,54 @@ test.describe('Rectangle Selection Tool', () => {
 
     await page.screenshot({
       path: 'tests/Macromolecule-editor/screenshots/rectangle-selection-tool3.png',
+    });
+  });
+
+  test.skip('Move monomer bonded with another monomers', async ({ page }) => {
+    /* 
+    Test case: #2367 - move items on the canvas
+    Description: check ability to move items on the canvas
+    */
+
+    await turnOnMacromoleculesEditor(page);
+
+    // Choose peptide
+    await page.getByText('Tza').click();
+
+    // Create 4 peptides on canvas
+    await page.mouse.click(300, 300);
+    await page.mouse.click(400, 400);
+    await page.mouse.click(500, 500);
+    await page.mouse.click(600, 600);
+
+    // Get 4 peptides locators
+    const peptides = await page.getByText('Tza').locator('..');
+    const peptide1 = peptides.nth(0);
+    const peptide2 = peptides.nth(1);
+    const peptide3 = peptides.nth(2);
+    const peptide4 = peptides.nth(3);
+
+    // Select bond tool
+    await selectSingleBondTool(page);
+
+    // Create bonds between peptides
+    await bondTwoMonomers(page, peptide1, peptide2);
+    await bondTwoMonomers(page, peptide3, peptide2);
+    await bondTwoMonomers(page, peptide3, peptide4);
+
+    await page.screenshot({
+      path: 'tests/Macromolecule-editor/screenshots/move-tool.png',
+    });
+
+    // Move selected monomer
+    await selectRectangleSelectionTool(page);
+    await page.mouse.move(400, 400);
+    await dragMouseTo(500, 500, page);
+    await page.mouse.move(400, 400);
+    await dragMouseTo(200, 400, page);
+
+    await page.screenshot({
+      path: 'tests/Macromolecule-editor/screenshots/move-tool-expected.png',
     });
   });
 });

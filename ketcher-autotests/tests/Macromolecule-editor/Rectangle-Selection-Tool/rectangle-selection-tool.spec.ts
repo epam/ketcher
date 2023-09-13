ada37e4b7a1c88f37fb9d3f968bd9687ba7db2ec
Roman Rodionov <rrodionov91@mail.ru>
2023-09-13 13:42:19 +0200

    #2370 - erase tool for macromolecules editor (#3217)
    #2370 - "Erase" tool
    #2360 - "Select" tool
    
    - added select and erase tools
    - implemented DrawingEntitiesManager and RenderersManager which controls entities parameters changes and render

---
+++ ketcher-autotests/tests/Macromolecule-editor/Rectangle-Selection-Tool/rectangle-selection-tool.spec.ts
@@ -0,0 +1,72 @@
+import { test } from '@playwright/test';
+import {
+  addMonomerToCanvas,
+  selectEraseTool,
+  selectRectangleArea,
+  selectRectangleSelectionTool,
+  selectSingleBondTool,
+} from '@utils';
+import { turnOnMacromoleculesEditor } from '@utils/macromolecules';
+import { bondTwoMonomers } from '@utils/macromolecules/polymerBond';
+/* eslint-disable no-magic-numbers */
+
+test.describe('Rectangle Selection Tool', () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto('');
+    await turnOnMacromoleculesEditor(page);
+  });
+  test.skip('Select monomer and bonds and then erase', async ({ page }) => {
+    /* 
+    Test case: #2360 - "Select" tool for Macromolecules editor
+    Description: Rectangle Selection Tool
+    */
+
+    // Create 4 peptides on canvas
+    const MONOMER_NAME = 'Tza___3-thiazolylalanine';
+    const MONOMER_ALIAS = 'Tza';
+    await addMonomerToCanvas(page, MONOMER_NAME, 300, 300);
+    await addMonomerToCanvas(page, MONOMER_NAME, 400, 400);
+    await addMonomerToCanvas(page, MONOMER_NAME, 500, 500);
+    await addMonomerToCanvas(page, MONOMER_NAME, 500, 200);
+
+    // Get 4 peptides locators
+    const peptides = await page.getByText(MONOMER_ALIAS).locator('..');
+    const peptide1 = peptides.nth(0);
+    const peptide2 = peptides.nth(1);
+    const peptide3 = peptides.nth(2);
+    const peptide4 = peptides.nth(3);
+
+    // Select bond tool
+    await selectSingleBondTool(page);
+
+    // Create bonds between peptides
+    await bondTwoMonomers(page, peptide1, peptide2);
+    await bondTwoMonomers(page, peptide3, peptide2);
+    await bondTwoMonomers(page, peptide3, peptide4);
+
+    await page.screenshot({
+      path: 'tests/Macromolecule-editor/screenshots/rectangle-selection-tool.png',
+    });
+
+    await selectRectangleSelectionTool(page);
+
+    // Coordinates for rectangle selection
+    const startX = 100;
+    const startY = 100;
+    const endX = 500;
+    const endY = 500;
+
+    await selectRectangleArea(page, startX, startY, endX, endY);
+
+    await page.screenshot({
+      path: 'tests/Macromolecule-editor/screenshots/rectangle-selection-tool2.png',
+    });
+
+    // Erase selected elements
+    await selectEraseTool(page);
+
+    await page.screenshot({
+      path: 'tests/Macromolecule-editor/screenshots/rectangle-selection-tool3.png',
+    });
+  });
+});
---
+++ packages/ketcher-core/__tests__/application/editor/tools/SelectRectangleTool.test.ts
@@ -0,0 +1,72 @@
+import { CoreEditor } from 'application/editor';
+import { PeptideRenderer } from 'application/render/renderers/PeptideRenderer';
+import {
+  getFinishedPolymerBond,
+  peptideMonomerItem,
+  polymerEditorTheme,
+} from '../../../mock-data';
+import { createPolymerEditorCanvas } from '../../../helpers/dom';
+import { SelectRectangle } from 'application/editor/tools/SelectRectangle';
+
+jest.mock('d3', () => {
+  return {
+    brush() {
+      return {
+        on() {},
+      };
+    },
+    select() {
+      return {
+        call() {
+          return this;
+        },
+        insert() {
+          return this;
+        },
+        select() {
+          return this;
+        },
+        attr() {
+          return this;
+        },
+        style() {
+          return this;
+        },
+      };
+    },
+  };
+});
+
+describe('Select Rectangle Tool', () => {
+  it('should select drawing entity on mousedown', () => {
+    const polymerBond = getFinishedPolymerBond(0, 0, 10, 10);
+    const event = {
+      target: {
+        __data__: polymerBond.renderer,
+      },
+    };
+    const selectRectangleTool = new SelectRectangle(
+      new CoreEditor({
+        theme: polymerEditorTheme,
+        canvas: createPolymerEditorCanvas(),
+      }),
+    );
+
+    expect(polymerBond.selected).toBeFalsy();
+    selectRectangleTool.mousedown(event);
+    expect(polymerBond.selected).toBeTruthy();
+  });
+
+  it('should initiate the render of peptide mousedown', () => {
+    const canvas: SVGSVGElement = createPolymerEditorCanvas();
+    const editor: CoreEditor = new CoreEditor({
+      canvas,
+      theme: polymerEditorTheme,
+    });
+    const onShow = jest.fn();
+    jest.spyOn(PeptideRenderer.prototype, 'show').mockImplementation(onShow);
+    editor.events.selectMonomer.dispatch(peptideMonomerItem);
+    canvas.dispatchEvent(new Event('mouseover', { bubbles: true }));
+    expect(onShow).toHaveBeenCalled();
+  });
+});

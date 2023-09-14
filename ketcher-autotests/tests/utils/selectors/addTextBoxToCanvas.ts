63a12f165f9a24aecf00439cf62e50674e956b99
MartaWilliams <marta.lepecka@gmail.com>
2023-09-14 09:28:41 +0200

    - 6 testes - screenshots - 1 test data files

--- ketcher-autotests/tests/utils/selectors/addTextBoxToCanvas.ts
+++ ketcher-autotests/tests/utils/selectors/addTextBoxToCanvas.ts
@@ -1,6 +1,10 @@
 import { Page } from '@playwright/test';
 import { clickInTheMiddleOfTheScreen } from '@utils';
 
+/**
+ * Click in the Text Tool and put the textbox in center of canvas
+ **/
+
 export async function addTextBoxToCanvas(page: Page) {
   await page.getByTitle('Add text (Alt+T)').click();
   await clickInTheMiddleOfTheScreen(page);

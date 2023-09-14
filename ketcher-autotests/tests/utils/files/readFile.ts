5d588be5017f6ea0eba1e4c2bd39d361935e65c0
Nikita_Vozisov <Nikita_Vozisov@epam.com>
2023-09-14 14:00:17 +0300

    fixed waitForLoad

--- ketcher-autotests/tests/utils/files/readFile.ts
+++ ketcher-autotests/tests/utils/files/readFile.ts
@@ -9,7 +9,7 @@ import {
   waitForLoad,
   delay,
   takeEditorScreenshot,
-  waitForRender,
+  waitForSpinnerFinishedWork,
 } from '@utils';
 import { MolfileFormat } from 'ketcher-core';
 import { getSmiles, getInchi } from '@utils/formats';
@@ -41,9 +41,7 @@ export async function openFileAndAddToCanvas(filename: string, page: Page) {
   await waitForLoad(page, async () => {
     await pressButton(page, 'Add to Canvas');
   });
-  await waitForRender(page, async () => {
-    await clickInTheMiddleOfTheScreen(page);
-  });
+  await clickInTheMiddleOfTheScreen(page);
 }
 
 export async function pasteFromClipboardAndAddToCanvas(

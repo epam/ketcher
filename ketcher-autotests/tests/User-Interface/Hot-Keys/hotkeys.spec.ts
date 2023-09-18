d20c334e8a598b5063d03b9c2b0d7912c4ccaf1d
Starla Huang <starla_huang@epam.com>
2023-09-18 10:44:15 +0800

    #3221 enhancement

--- ketcher-autotests/tests/User-Interface/Hot-Keys/hotkeys.spec.ts
+++ ketcher-autotests/tests/User-Interface/Hot-Keys/hotkeys.spec.ts
@@ -1,18 +1,18 @@
 /* eslint-disable no-magic-numbers */
 import { expect, test } from '@playwright/test';
 import {
-  waitForIndigoToLoad,
   selectNestedTool,
   SelectTool,
   selectTool,
   LeftPanelButton,
   clickInTheMiddleOfTheScreen,
+  waitForPageInit,
 } from '@utils';
 
 test.describe('Hot keys', () => {
   test.beforeEach(async ({ page }) => {
     await page.goto('');
-    await waitForIndigoToLoad(page);
+    await waitForPageInit(page);
   });
 
   test.afterEach(async ({ page }) => {
@@ -25,13 +25,13 @@ test.describe('Hot keys', () => {
     await selectNestedTool(page, SelectTool.FRAGMENT_SELECTION);
     await selectTool(LeftPanelButton.AddText, page);
     await page.keyboard.press('Escape');
-    expect(page.getByTestId('select-fragment')).toBeTruthy();
+    expect(page.getByTestId('select-fragment')).toBeVisible();
   });
 
   test('Shift+Tab to switch selection tool', async ({ page }) => {
     await clickInTheMiddleOfTheScreen(page);
     await page.keyboard.press('Shift+Tab');
     await page.keyboard.press('Shift+Tab');
-    expect(page.getByTestId('select-fragment')).toBeTruthy();
+    expect(page.getByTestId('select-fragment')).toBeVisible();
   });
 });

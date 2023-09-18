cddbf7043d8c2a68693e197dac6da8f3ce830471
Starla Huang <starla_huang@epam.com>
2023-09-18 21:28:58 +0800

    #3221 add await to expect toBeVisible

--- ketcher-autotests/tests/User-Interface/Hot-Keys/hotkeys.spec.ts
+++ ketcher-autotests/tests/User-Interface/Hot-Keys/hotkeys.spec.ts
@@ -23,13 +23,13 @@ test.describe('Hot keys', () => {
     await selectNestedTool(page, SelectTool.FRAGMENT_SELECTION);
     await selectTool(LeftPanelButton.AddText, page);
     await page.keyboard.press('Escape');
-    expect(page.getByTestId('select-fragment')).toBeVisible();
+    await expect(page.getByTestId('select-fragment')).toBeVisible();
   });
 
   test('Shift+Tab to switch selection tool', async ({ page }) => {
     await clickInTheMiddleOfTheScreen(page);
     await page.keyboard.press('Shift+Tab');
     await page.keyboard.press('Shift+Tab');
-    expect(page.getByTestId('select-fragment')).toBeVisible();
+    await expect(page.getByTestId('select-fragment')).toBeVisible();
   });
 });

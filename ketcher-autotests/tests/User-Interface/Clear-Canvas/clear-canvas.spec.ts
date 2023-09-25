1e966bb3584e5cde5c5115cb9c61cdebbe729f32
MartaWilliams <marta.lepecka@gmail.com>
2023-09-25 21:49:11 +0200

    - Changes after PR review - New screenshots

--- ketcher-autotests/tests/User-Interface/Clear-Canvas/clear-canvas.spec.ts
+++ ketcher-autotests/tests/User-Interface/Clear-Canvas/clear-canvas.spec.ts
@@ -1,86 +1,85 @@
-import { test } from '@playwright/test';
+import { Page, test } from '@playwright/test';
 import {
+  TopPanelButton,
   takeEditorScreenshot,
   selectTopPanelButton,
-  delay,
-} from '@utils/canvas';
-import { clickInTheMiddleOfTheScreen, pressButton } from '@utils/clicks';
-import { waitForPageInit } from '@utils/common';
-import { openFileAndAddToCanvas } from '@utils/files';
-import { DELAY_IN_SECONDS } from '@utils/globals';
-import { RingButton, TopPanelButton, selectRing } from '@utils/selectors';
-import { addTextBoxToCanvas } from '@utils/selectors/addTextBoxToCanvas';
+  waitForPageInit,
+  clickInTheMiddleOfTheScreen,
+  openFileAndAddToCanvas,
+  selectRing,
+  RingButton,
+} from '@utils';
+// import { addTextBoxToCanvas } from '@utils/selectors/addTextBoxToCanvas';
 
 test.describe('Clear canvas', () => {
   test.beforeEach(async ({ page }) => {
     await waitForPageInit(page);
   });
 
+  test.afterEach(async ({ page }) => {
+    await takeEditorScreenshot(page);
+  });
+
   test('Clear Canvas - "Clear canvas" button', async ({ page }) => {
     // Test case:EPMLSOPKET-1702
     // Checking apperance of the tooltip and deleting object with Clear canvas button
-    // await selectTopPanelButton(TopPanelButton.Clear, page);
-    await page.getByRole('button', { name: 'Clear Canvas (Ctrl+Del)' }).hover();
-    await page.getByTitle('Clear Canvas (Ctrl+Del)').click();
-    await addTextBoxToCanvas(page);
-    await page.getByRole('dialog').getByRole('textbox').fill('12345');
-    await pressButton(page, 'Apply');
     await selectTopPanelButton(TopPanelButton.Clear, page);
-    await takeEditorScreenshot(page);
+    await MouseEvent;
+    const button = page.getByRole('button', {
+      name: 'Clear Canvas (Ctrl+Del)',
+    });
+    await expect(button).toHaveAttribute('title', 'Clear Canvas (Ctrl+Del)');
+  });
+
+  test('Clear Canvas - Deleting object with Clear canvas button', async ({
+    page,
+  }) => {
+    // Checking deleting object with Clear canvas button
+    await openFileAndAddToCanvas('KET/simple-chain.ket', page);
+    await selectTopPanelButton(TopPanelButton.Clear, page);
   });
 
   test('Clear Canvas - Undo/Redo', async ({ page }) => {
     // Test case: EPMLSOPKET-1704
-    // Checking deleting object with Clear canvas button with funcion of Undo/Redo buttons
+    // Checking deleting object with Clear canvas button with function of Undo/Redo buttons
     await openFileAndAddToCanvas('reaction-dif-prop.rxn', page);
-    await clickInTheMiddleOfTheScreen(page);
     await takeEditorScreenshot(page);
     await selectTopPanelButton(TopPanelButton.Clear, page);
     await clickInTheMiddleOfTheScreen(page);
     await selectTopPanelButton(TopPanelButton.Undo, page);
     await selectTopPanelButton(TopPanelButton.Redo, page);
     await selectTopPanelButton(TopPanelButton.Undo, page);
-    await takeEditorScreenshot(page);
   });
 
   test('Clear Canvas - Structure is opened from ket-file', async ({ page }) => {
     // Test case:EPMLSOPKET-1705
-    // Checking deleting object with Clear canvas button with funcion of hotkyes (Ctrl+ Del)
+    // Checking deleting object with Clear canvas button with funtcion of hotkyes (Ctrl+ Del)
     await openFileAndAddToCanvas('ketcher.ket', page);
-    await clickInTheMiddleOfTheScreen(page);
     await takeEditorScreenshot(page);
     await selectTopPanelButton(TopPanelButton.Clear, page);
-    await clickInTheMiddleOfTheScreen(page);
     await selectTopPanelButton(TopPanelButton.Undo, page);
     await page.keyboard.press('Control+Delete');
-    await takeEditorScreenshot(page);
   });
 
   test('Clear Canvas - Hotkeys', async ({ page }) => {
     // Test case:EPMLSOPKET-2864
-    // Checking deleting object with Clear canvas button with funcion of hotkyes (Ctrl+ Del)
-    await addTextBoxToCanvas(page);
-    await clickInTheMiddleOfTheScreen(page);
-    await page.getByRole('dialog').getByRole('textbox').fill('12345');
-    await pressButton(page, 'Apply');
+    // Checking deleting object with Clear canvas button with function of hotkyes (Ctrl+ Del)
+    await openFileAndAddToCanvas('KET/chain.ket', page);
     await selectRing(RingButton.Benzene, page);
     await page.getByTestId('canvas').click({ position: { x: 500, y: 250 } });
-    await delay(DELAY_IN_SECONDS.TWO);
     await takeEditorScreenshot(page);
     await page.keyboard.press('Control+Delete');
     await openFileAndAddToCanvas('ketcher.mol', page);
     await takeEditorScreenshot(page);
     await page.keyboard.press('Control+Delete');
-    await takeEditorScreenshot(page);
   });
 
   test('Clear Canvas - Structure is opened from smile-string', async ({
     page,
   }) => {
     // Test case: EPMLSOPKET-1706
-    // Checking deleting object with Clear canvas button with funcion of hotkyes (Ctrl+ Del) andf Undo/Redo button
+    // Checking deleting object with Clear canvas button with function of hotkyes (Ctrl+ Del) and Undo/Redo button
     await openFileAndAddToCanvas('chain-with-r-group.smi', page);
-    await clickInTheMiddleOfTheScreen(page);
     await selectTopPanelButton(TopPanelButton.Clear, page);
     await selectTopPanelButton(TopPanelButton.Undo, page);
     await page.keyboard.press('Control+Delete');

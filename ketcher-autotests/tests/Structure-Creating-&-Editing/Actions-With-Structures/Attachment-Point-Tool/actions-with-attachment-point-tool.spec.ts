6ce262dd456194dfc01fb12cc05132153668b156
MartaWilliams <marta.lepecka@gmail.com>
2023-09-29 12:38:17 +0200

    - changes after PR review

--- ketcher-autotests/tests/Structure-Creating-&-Editing/Actions-With-Structures/Attachment-Point-Tool/actions-with-attachment-point-tool.spec.ts
+++ ketcher-autotests/tests/Structure-Creating-&-Editing/Actions-With-Structures/Attachment-Point-Tool/actions-with-attachment-point-tool.spec.ts
@@ -19,7 +19,10 @@ test.describe('Attachment Point Tool', () => {
     page,
   }) => {
     // Test case: EPMLSOPKET-18024
-    await openFileAndAddToCanvas('s-group-with-attachment-points.mol', page);
+    await openFileAndAddToCanvas(
+      'Molfiles-V2000/s-group-with-attachment-points.mol',
+      page,
+    );
     await takeEditorScreenshot(page);
   });
 
@@ -27,7 +30,10 @@ test.describe('Attachment Point Tool', () => {
     page,
   }) => {
     // Test case: EPMLSOPKET-18026
-    await openFileAndAddToCanvas('s-group-with-attachment-points.ket', page);
+    await openFileAndAddToCanvas(
+      'KET/s-group-with-attachment-points.ket',
+      page,
+    );
     await takeEditorScreenshot(page);
   });
 
@@ -41,7 +47,7 @@ test.describe('Attachment Point Tool', () => {
     );
     const expectedFile = await getKet(page);
     await saveToFile(
-      'KET/S-Group structure with Attachment point-EPMLSOPKET-18025.ket',
+      'KET/S-Group-structure-with-Attachment-point.ket',
       expectedFile,
     );
 
@@ -49,12 +55,12 @@ test.describe('Attachment Point Tool', () => {
       await receiveFileComparisonData({
         page,
         expectedFileName:
-          'tests/test-data/KET/S-Group structure with Attachment point-EPMLSOPKET-18025.ket',
+          'tests/test-data/KET/S-Group-structure-with-Attachment-point.ket',
       });
     expect(ketFile).toEqual(ketFileExpected);
     await selectTopPanelButton(TopPanelButton.Clear, page);
     await openFileAndAddToCanvas(
-      'KET/S-Group structure with Attachment point-EPMLSOPKET-18025.ket',
+      'KET/S-Group-structure-with-Attachment-point.ket',
       page,
     );
     await page.keyboard.press('Control+a');

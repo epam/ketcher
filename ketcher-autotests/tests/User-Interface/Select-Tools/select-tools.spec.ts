d0360204a0ece057a752041c54b6eb07e5cadf13
Anastasiya Piatrova3 <Anastasiya_Piatrova3@epam.com>
2023-09-29 18:20:53 +0200

    added select-tools test

--- ketcher-autotests/tests/User-Interface/Select-Tools/select-tools.spec.ts
+++ ketcher-autotests/tests/User-Interface/Select-Tools/select-tools.spec.ts
@@ -7,12 +7,21 @@ import {
   LeftPanelButton,
   BondType,
   waitForPageInit,
+  takeEditorScreenshot,
+  drawBenzeneRing,
+  clickOnAtom,
+  selectDropdownTool,
 } from '@utils';
 import { getBondByIndex } from '@utils/canvas/bonds';
+// import { drawReactionWithTwoBenzeneRings } from '@utils/canvas/drawStructures';
 
 test.describe('Select tools tests', () => {
   test.beforeEach(async ({ page }) => {
     await waitForPageInit(page);
+  });
+
+  test.afterEach(async ({ page }) => {
+    await takeEditorScreenshot(page);
   });
 
   test('when add molecula and choose select tools and move cursor to edge it should show specific pointer', async ({
@@ -27,5 +36,19 @@ test.describe('Select tools tests', () => {
 
     const cursor = await page.getByTestId('canvas').getAttribute('cursor');
     expect(cursor).toBe('all-scroll');
+  });
+
+  test('When the structure is pasted or dragged', async ({ page }) => {
+    /*
+      Test case: EPMLSOPKET-8939
+      Select 'Benzene' from Templates
+      Paste it on canvas
+      Select 'Benzene' from Templates and paste over 'Benzene'
+      Place two 'Benzene' on the canvas and drag one onto the other
+    */
+    await drawBenzeneRing(page);
+    await selectDropdownTool(page, 'select-rectangle', 'select-fragment');
+    // eslint-disable-next-line no-magic-numbers
+    await clickOnAtom(page, 'C', 4);
   });
 });

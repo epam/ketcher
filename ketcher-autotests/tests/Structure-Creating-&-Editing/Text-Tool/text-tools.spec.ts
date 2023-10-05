fba333c634539c4f91e5dda5684aa2c8e3d607c4
MartaWilliams <marta.lepecka@gmail.com>
2023-10-05 14:54:34 +0200

    - adding test description

--- ketcher-autotests/tests/Structure-Creating-&-Editing/Text-Tool/text-tools.spec.ts
+++ ketcher-autotests/tests/Structure-Creating-&-Editing/Text-Tool/text-tools.spec.ts
@@ -14,7 +14,7 @@ test.describe('Text tools test cases', () => {
     await waitForPageInit(page);
   });
 
-  test('Clear Canvas - checking button tooltip', async ({ page }) => {
+  test('Text tool - Button and tooltip: verification', async ({ page }) => {
     // Test case: EPMLSOPKET-2225
     await selectLeftPanelButton(LeftPanelButton.AddText, page);
     const button = page.getByTestId('text');
@@ -23,14 +23,15 @@ test.describe('Text tools test cases', () => {
 
   test('Text tool - UI', async ({ page }) => {
     // Test case: EPMLSOPKET-2226
+    // Verify if the text box displayed properly all elements
     await page.getByTestId('text').click();
     await clickInTheMiddleOfTheScreen(page);
-
     await takeEditorScreenshot(page);
   });
 
   test('Text tool - Create a single text object', async ({ page }) => {
     // Test case: EPMLSOPKET-2227
+    // Verify action of adding text object to canvas
     await addTextBoxToCanvas(page);
     await page.getByRole('dialog').getByRole('textbox').fill('Ketcher');
     await pressButton(page, 'Cancel');

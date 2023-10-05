4a0220e4c4225f332dea84de69c986ae920fdb83
Olga Mazurina <olga_mazurina@epam.com>
2023-10-05 13:38:41 +0200

    #2869 - add test

--- ketcher-autotests/tests/Macromolecule-editor/Peptides/peptides-add-to-canvas.spec.ts
+++ ketcher-autotests/tests/Macromolecule-editor/Peptides/peptides-add-to-canvas.spec.ts
@@ -11,12 +11,29 @@ import { turnOnMacromoleculesEditor } from '@utils/macromolecules';
 Test case: #3063 - Add e2e tests for Macromolecule editor
 */
 
-test('Select peptide and drag it to canvas', async ({ page }) => {
-  await waitForPageInit(page);
-  await turnOnMacromoleculesEditor(page);
+test.describe('Peptide', () => {
+  test.beforeEach(async ({ page }) => {
+    await waitForPageInit(page);
+    await turnOnMacromoleculesEditor(page);
+  });
 
-  await page.click(ALANINE);
-  await clickInTheMiddleOfTheScreen(page);
+  test.afterEach(async ({ page }) => {
+    await takePageScreenshot(page);
+  });
 
-  await takePageScreenshot(page);
+  test('Select peptide and drag it to canvas', async ({ page }) => {
+    const coords = { x: 100, y: 100 };
+    await page.click(ALANINE);
+    await clickInTheMiddleOfTheScreen(page);
+    await page.mouse.move(coords.x, coords.y);
+  });
+
+  test('Add monomer preview on canvas', async ({ page }) => {
+    /* 
+    Test case: #2869 - Preview of monomer structures on canvas
+    Description: Add monomer preview on canvas
+    */
+    await page.click(ALANINE);
+    await clickInTheMiddleOfTheScreen(page);
+  });
 });

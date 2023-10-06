18aaf8d835ebe289f54cd76836e19bbec3b8e668
MartaWilliams <marta.lepecka@gmail.com>
2023-10-06 09:55:58 +0200

    - fix tests according to comments

--- ketcher-autotests/tests/User-Interface/Clear-Canvas/clear-canvas.spec.ts
+++ ketcher-autotests/tests/User-Interface/Clear-Canvas/clear-canvas.spec.ts
@@ -24,7 +24,6 @@ test.describe('Clear canvas', () => {
 
   test('Clear Canvas - checking button tooltip', async ({ page }) => {
     // Test case: EPMLSOPKET-1702
-    await selectTopPanelButton(TopPanelButton.Clear, page);
     const button = page.getByTestId('clear-canvas-button');
     await expect(button).toHaveAttribute('title', 'Clear Canvas (Ctrl+Del)');
   });

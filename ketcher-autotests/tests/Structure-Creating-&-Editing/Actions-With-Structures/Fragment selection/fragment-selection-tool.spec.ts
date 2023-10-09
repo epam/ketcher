7f1b1d146d9f42dea155bf7c84f7b029c04fa055
y-holik <yahor_holik@epam.com>
2023-10-09 14:38:57 +0200

    added updates for new function, tests&scrshots

--- ketcher-autotests/tests/Structure-Creating-&-Editing/Actions-With-Structures/Fragment selection/fragment-selection-tool.spec.ts
+++ ketcher-autotests/tests/Structure-Creating-&-Editing/Actions-With-Structures/Fragment selection/fragment-selection-tool.spec.ts
@@ -9,6 +9,7 @@ import {
   getControlModifier,
   getCoordinatesOfTheMiddleOfTheScreen,
   getCoordinatesTopAtomOfBenzeneRing,
+  openDropdown,
   openFileAndAddToCanvas,
   selectDropdownTool,
   selectFragmentSelectionTool,
@@ -40,8 +41,8 @@ test.describe('Fragment selection tool', () => {
     await takeEditorScreenshot(page);
   });
 
-  test('Structure selection with fragment selection tool', async ({ page }) => {
-    // Test case: EPMLSOPKET-1347
+  test('Molecule selection', async ({ page }) => {
+    // Test case: EPMLSOPKET-1355
     await openFileAndAddToCanvas('glutamine.mol', page);
     await selectDropdownTool(page, 'select-rectangle', 'select-fragment');
     // await clickCanvas(page);
@@ -49,11 +50,32 @@ test.describe('Fragment selection tool', () => {
   });
 
   test('Reaction component selection', async ({ page }) => {
-    //  Test case: EPMLSOPKET-1349
+    //  Test case: EPMLSOPKET-1356
     await openFileAndAddToCanvas('RXN-V2000/reaction_4.rxn', page);
-    // await clickOnPlus(page, 1);
-    // await takeEditorScreenshot(page);
-    await clickOnArrow(page, 0);
+    await selectDropdownTool(page, 'select-rectangle', 'select-fragment');
+    await clickOnPlus(page, 1);
     await takeEditorScreenshot(page);
+    await clickOnArrow(page, 0);
+  });
+
+  test('Fuse atoms together', async ({ page }) => {
+    //  Test case: EPMLSOPKET-1358
+    const atomNumber = 4;
+    const atomLabel = 9;
+    await openFileAndAddToCanvas('KET/two-benzene-with-atoms.ket', page);
+    await selectDropdownTool(page, 'select-rectangle', 'select-fragment');
+    await clickOnAtom(page, 'C', atomNumber);
+    const atomPoint = await getAtomByIndex(page, { label: 'C' }, atomLabel);
+    await dragMouseTo(atomPoint.x, atomPoint.y, page);
+  });
+
+  test('Deleting molecule', async ({ page }) => {
+    //  Test case: EPMLSOPKET-1359
+    await openFileAndAddToCanvas('Rxn-V2000/reaction_4.rxn', page);
+    await selectDropdownTool(page, 'select-rectangle', 'select-fragment');
+    await clickOnAtom(page, 'Br', 0);
+    await page.keyboard.press('Delete');
+    // await clickOnAtom(page, 'C', atomNumber);
+    // await page.keyboard.press('Delete');
   });
 });
--- ketcher-autotests/tests/utils/canvas/arrow-signes/getArrow.ts
+++ ketcher-autotests/tests/utils/canvas/arrow-signes/getArrow.ts
@@ -1,39 +1,44 @@
-import { Page } from '@playwright/test';
-import { getLeftTopBarSize } from '../common/getLeftTopBarSize';
-import { sortItems } from '../common/sortItems';
-import { NO_STRUCTURE_AT_THE_CANVAS_ERROR } from '../constants';
-import { ArrowXy, SORT_TYPE } from '../types';
-
-export async function getArrowsByIndex(
-  page: Page,
-  index: number,
-  sortBy: SORT_TYPE = SORT_TYPE.ASC_X,
-): Promise<ArrowXy> {
-  const { arrows, scale, offset } = await page.evaluate(() => {
-    return {
-      // eslint-disable-next-line no-unsafe-optional-chaining
-      arrows: [...window.ketcher?.editor?.struct()?.rxnArrows?.values()],
-      scale: window.ketcher?.editor?.options()?.scale,
-      offset: window.ketcher?.editor?.options()?.offset,
-    };
-  });
-
-  if (arrows.length === 0) {
-    throw new Error(NO_STRUCTURE_AT_THE_CANVAS_ERROR);
-  }
-  const { leftBarWidth, topBarHeight } = await getLeftTopBarSize(page);
-  const coords = arrows.map((arrow) => {
-    return {
-      ...arrow,
-      x: arrow.pp.x * scale + offset.x + leftBarWidth,
-      y: arrow.pp.y * scale + offset.y + topBarHeight,
-    };
-  });
-
-  return sortItems(sortBy, coords)[index];
-}
+import { Page } from '@playwright/test/types/test';
+import { EntityType, getDrawingEntitiesByIndex } from '../getDrawingEntities';
 
 export async function clickOnArrow(page: Page, index: number) {
-  const { x, y } = await getArrowsByIndex(page, index);
+  const { x, y } = await getDrawingEntitiesByIndex(
+    page,
+    index,
+    EntityType.arrow,
+  );
   await page.mouse.click(x, y);
 }
+// import { getLeftTopBarSize } from '../common/getLeftTopBarSize';
+// import { sortItems } from '../common/sortItems';
+// import { NO_STRUCTURE_AT_THE_CANVAS_ERROR } from '../constants';
+// import { ArrowXy, SORT_TYPE } from '../types';
+
+// export async function getArrowsByIndex(
+//   page: Page,
+//   index: number,
+//   sortBy: SORT_TYPE = SORT_TYPE.ASC_X,
+// ): Promise<ArrowXy> {
+//   const { arrows, scale, offset } = await page.evaluate(() => {
+//     return {
+//       // eslint-disable-next-line no-unsafe-optional-chaining
+//       arrows: [...window.ketcher?.editor?.struct()?.rxnArrows?.values()],
+//       scale: window.ketcher?.editor?.options()?.scale,
+//       offset: window.ketcher?.editor?.options()?.offset,
+//     };
+//   });
+
+//   if (arrows.length === 0) {
+//     throw new Error(NO_STRUCTURE_AT_THE_CANVAS_ERROR);
+//   }
+//   const { leftBarWidth, topBarHeight } = await getLeftTopBarSize(page);
+//   const coords = arrows.map((arrow) => {
+//     return {
+//       ...arrow,
+//       x: arrow.pp.x * scale + offset.x + leftBarWidth,
+//       y: arrow.pp.y * scale + offset.y + topBarHeight,
+//     };
+//   });
+
+//   return sortItems(sortBy, coords)[index];
+// }

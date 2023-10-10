632ca2a8424dee17ba190a349dc6b11b742c9413
Gayane Chilingaryan <gayane_chilingaryan@epam.com>
2023-10-10 19:39:32 +0400

    #3251 - by selecting rna elements add presets button is enabled

--- packages/ketcher-polymer-editor-react/src/components/monomerLibrary/RnaBuilder/RnaEditor/RnaEditorExpanded/RnaEditorExpanded.tsx
+++ packages/ketcher-polymer-editor-react/src/components/monomerLibrary/RnaBuilder/RnaEditor/RnaEditorExpanded/RnaEditorExpanded.tsx
@@ -1,19 +1,3 @@
-/****************************************************************************
- * Copyright 2021 EPAM Systems
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- ***************************************************************************/
-
 import { MonomerGroups } from 'src/constants';
 import { GroupBlock } from './GroupBlock';
 import {
@@ -36,6 +20,7 @@ import {
   selectActiveRnaBuilderItem,
   selectIsPresetReadyToSave,
   selectPresetFullName,
+  setActivePresetName,
   setActiveRnaBuilderItem,
 } from 'state/rna-builder';
 import { useAppSelector } from 'hooks';
@@ -77,21 +62,26 @@ export const RnaEditorExpanded = ({
 
   const activeMonomerGroup = useAppSelector(selectActiveRnaBuilderItem);
   const activePresetFullName = selectPresetFullName(activePreset);
-  const [presetName, setPresetName] = useState('');
+  const [presetName, setPresetName] = useState(name || '');
   const [editedPresetName, setEditedPresetName] = useState(false);
-
   useEffect(() => {
     if (
       activeMonomerGroup !== RnaBuilderPresetsItem.Presets &&
-      !editedPresetName
+      !editedPresetName &&
+      !presetName.trim()
     ) {
-      setPresetName(activePresetFullName);
+      setPresetName((currentPresetName) => {
+        const newName = activePresetFullName || currentPresetName;
+        dispatch(setActivePresetName(newName));
+        return newName;
+      });
     }
-  }, [activeMonomerGroup, activePresetFullName, editedPresetName]);
+  }, [activeMonomerGroup, activePresetFullName, editedPresetName, presetName]);
 
   onChangeName = (event: ChangeEvent<HTMLInputElement>) => {
     if (isEditMode) {
       const newPresetName = event.target.value;
+      dispatch(setActivePresetName(newPresetName));
       setPresetName(newPresetName);
       setEditedPresetName(newPresetName.trim() !== '');
     }

624cf90187e736e572f588874a23d8aef84238b9
Olga Mazurina <olga_mazurina@epam.com>
2023-10-10 10:36:04 +0200

    #2869 - fix tests

--- ketcher-autotests/tests/Macromolecule-editor/Polymer-Bond-Tool/attachment-point-rotation.spec.ts
+++ ketcher-autotests/tests/Macromolecule-editor/Polymer-Bond-Tool/attachment-point-rotation.spec.ts
@@ -26,17 +26,39 @@ test.describe('Check attachment point rotation', () => {
     // Create 4 peptides on canvas
     const MONOMER_NAME = 'Tza___3-thiazolylalanine';
     const MONOMER_ALIAS = 'Tza';
-    await addMonomerToCanvas(page, MONOMER_NAME, 300, 300);
-    await addMonomerToCanvas(page, MONOMER_NAME, 400, 400);
-    await addMonomerToCanvas(page, MONOMER_NAME, 500, 500);
-    await addMonomerToCanvas(page, MONOMER_NAME, 500, 200);
 
     // Get 4 peptides locators
-    const peptides = await page.getByText(MONOMER_ALIAS).locator('..');
-    const peptide1 = peptides.nth(0);
-    const peptide2 = peptides.nth(1);
-    const peptide3 = peptides.nth(2);
-    const peptide4 = peptides.nth(3);
+    const peptide1 = await addMonomerToCanvas(
+      page,
+      MONOMER_NAME,
+      MONOMER_ALIAS,
+      300,
+      300,
+    );
+
+    const peptide2 = await addMonomerToCanvas(
+      page,
+      MONOMER_NAME,
+      MONOMER_ALIAS,
+      400,
+      400,
+    );
+
+    const peptide3 = await addMonomerToCanvas(
+      page,
+      MONOMER_NAME,
+      MONOMER_ALIAS,
+      500,
+      500,
+    );
+
+    const peptide4 = await addMonomerToCanvas(
+      page,
+      MONOMER_NAME,
+      MONOMER_ALIAS,
+      500,
+      200,
+    );
 
     // Select bond tool
     await selectSingleBondTool(page);
@@ -66,14 +88,23 @@ test.describe('Check attachment point rotation', () => {
     */
 
     const MONOMER_NAME = 'Tza___3-thiazolylalanine';
-
-    await addMonomerToCanvas(page, MONOMER_NAME, 300, 300);
-    await addMonomerToCanvas(page, MONOMER_NAME, 400, 400);
+    const MONOMER_ALIAS = 'Tza';
 
     // Get 4 peptides locators
-    const peptides = await page.getByText('Tza').locator('..');
-    const peptide1 = peptides.nth(0);
-    const peptide2 = peptides.nth(1);
+    const peptide1 = await addMonomerToCanvas(
+      page,
+      MONOMER_NAME,
+      MONOMER_ALIAS,
+      300,
+      300,
+    );
+    const peptide2 = await addMonomerToCanvas(
+      page,
+      MONOMER_NAME,
+      MONOMER_ALIAS,
+      400,
+      400,
+    );
 
     // Select bond tool
     await selectSingleBondTool(page);

1f8403edc3d064e8cbae76402a11037606233789
Urszula Gawlowska <urszula.gawlowska@epam.com>
2023-10-16 12:20:13 +0200

    adding tests for more atom query specific attributes

--- ketcher-autotests/tests/Structure-Creating-&-Editing/Query-attributes-SMARTS/query-attributes-smarts.spec.ts
+++ ketcher-autotests/tests/Structure-Creating-&-Editing/Query-attributes-SMARTS/query-attributes-smarts.spec.ts
@@ -9,7 +9,18 @@ import {
   waitForAtomPropsModal,
   waitForPageInit,
 } from '@utils';
-import { checkSmartsValue, setHCount, setRingBondCount } from './utils';
+import {
+  checkSmartsValue,
+  setAromaticity,
+  setChirality,
+  setConnectivity,
+  setHCount,
+  setImplicitHCount,
+  setRingBondCount,
+  setRingMembership,
+  setRingSize,
+  setSubstitutionCount,
+} from './utils';
 
 async function drawStructure(page: Page) {
   await selectBond(BondTypeName.Single, page);
@@ -29,16 +40,84 @@ test.describe('Query features', () => {
   });
 
   test('Setting ring bond count', async ({ page }) => {
-    await setRingBondCount(page, '3');
+    await setRingBondCount(page, '2');
     await pressButton(page, 'Apply');
     await takeEditorScreenshot(page);
-    await checkSmartsValue(page, '[#6](-[#6])(-[#6;x3])-[#6]');
+    await checkSmartsValue(page, '[#6](-[#6])(-[#6;x2])-[#6]');
   });
 
   test('Setting H count', async ({ page }) => {
-    await setHCount(page, '2');
+    await setHCount(page, '3');
     await pressButton(page, 'Apply');
     await takeEditorScreenshot(page);
-    await checkSmartsValue(page, '[#6](-[#6])(-[#6;H2])-[#6]');
+    await checkSmartsValue(page, '[#6](-[#6])(-[#6;H3])-[#6]');
+  });
+
+  test('Setting substitution count', async ({ page }) => {
+    await setSubstitutionCount(page, '4');
+    await pressButton(page, 'Apply');
+    await takeEditorScreenshot(page);
+    await checkSmartsValue(page, '[#6](-[#6])(-[#6;D4])-[#6]');
+  });
+
+  test('Setting aromacity - aromatic', async ({ page }) => {
+    /**
+     * TODO: add expected SMARTS representative when https://github.com/epam/Indigo/issues/1329 would be fixed
+     */
+    await setAromaticity(page, 'aromatic');
+    await pressButton(page, 'Apply');
+    await takeEditorScreenshot(page);
+    await checkSmartsValue(page, '');
+  });
+
+  test('Setting aromacity - aliphatic', async ({ page }) => {
+    /**
+     * TODO: add expected SMARTS representative when https://github.com/epam/Indigo/issues/1329 would be fixed
+     */
+    await setAromaticity(page, 'aliphatic');
+    await pressButton(page, 'Apply');
+    await takeEditorScreenshot(page);
+    await checkSmartsValue(page, '');
+  });
+
+  test('Setting implicit H count', async ({ page }) => {
+    /**
+     * TODO: add expected SMARTS representative when https://github.com/epam/Indigo/issues/1330 would be fixed
+     */
+    await setImplicitHCount(page, '5');
+    await pressButton(page, 'Apply');
+    await takeEditorScreenshot(page);
+    await checkSmartsValue(page, '');
+  });
+
+  test('Setting ring membership', async ({ page }) => {
+    await setRingMembership(page, '6');
+    await pressButton(page, 'Apply');
+    await takeEditorScreenshot(page);
+    await checkSmartsValue(page, '[#6](-[#6])(-[#6;R6])-[#6]');
+  });
+
+  test('Setting ring size', async ({ page }) => {
+    await setRingSize(page, '7');
+    await pressButton(page, 'Apply');
+    await takeEditorScreenshot(page);
+    await checkSmartsValue(page, '[#6](-[#6])(-[#6;r7])-[#6]');
+  });
+
+  test('Setting connectivity', async ({ page }) => {
+    await setConnectivity(page, '8');
+    await pressButton(page, 'Apply');
+    await takeEditorScreenshot(page);
+    await checkSmartsValue(page, '[#6](-[#6])(-[#6;X8])-[#6]');
+  });
+
+  test('Setting chirality', async ({ page }) => {
+    /**
+     * TODO: add expected SMARTS representative when https://github.com/epam/Indigo/issues/1328 would be fixed
+     */
+    await setChirality(page, 'anticlockwise');
+    await pressButton(page, 'Apply');
+    await takeEditorScreenshot(page);
+    await checkSmartsValue(page, '');
   });
 });

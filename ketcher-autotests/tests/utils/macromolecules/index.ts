cbc3b95d49e65867ffc35604b7fe0a224c6a24ee
Olga Mazurina <olga_mazurina@epam.com>
2023-10-17 20:23:29 +0200

    #2987 - update screenshots for Macromolecules

--- ketcher-autotests/tests/utils/macromolecules/index.ts
+++ ketcher-autotests/tests/utils/macromolecules/index.ts
@@ -1,9 +1,14 @@
 import { expect, Page } from '@playwright/test';
-import { POLYMER_TOGGLER } from '@constants/testIdConstants';
+import {
+  MACROMOLECULES_MODE,
+  POLYMER_TOGGLER,
+} from '@constants/testIdConstants';
 
 export async function turnOnMacromoleculesEditor(page: Page) {
   await expect(page.getByTestId(POLYMER_TOGGLER)).toBeVisible();
   await page.getByTestId(POLYMER_TOGGLER).click();
+  await expect(page.getByTestId(MACROMOLECULES_MODE)).toBeVisible();
+  await page.getByTestId(MACROMOLECULES_MODE).click();
 }
 
 export async function hideMonomerPreview(page: Page) {

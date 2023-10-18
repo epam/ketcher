a5f440fc18127799304c05551e01ec00fd0dfdd6
SashaGraves <60065704+SashaGraves@users.noreply.github.com>
2023-10-18 13:23:25 +0200

    Macro: #3373 - Rotate Attachment Point Unused (#3396)
    * rotate attachment points unused
    
    * remove snapshots
    
    * add snapshots
    
    * added types after review
    
    * fix type
    
    * fix type issue after merge
    
    * test updated
    
    * snapshots updated
    
    * - in progress
    
    ---------
    
    Co-authored-by: Roman Rodionov <roman_rodionov@epam.com>

--- packages/ketcher-core/src/domain/types/monomers.ts
+++ packages/ketcher-core/src/domain/types/monomers.ts
@@ -22,4 +22,12 @@ export type MonomerItemType = {
   };
 };
 
-export type AttachmentPointName = 'R1' | 'R2' | 'R3' | 'R4';
+export type AttachmentPointName =
+  | 'R1'
+  | 'R2'
+  | 'R3'
+  | 'R4'
+  | 'R5'
+  | 'R6'
+  | 'R7'
+  | 'R8';

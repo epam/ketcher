bc814f11b30ac2fcf29b1c66549850877795f8dc
Yulei Chen <yulei_chen@epam.com>
2023-10-20 19:25:07 +0800

    #3203 - Structure shifting to upper left corner during zoom operations (#3412)
    * #3203 - Change zoom origin to the center of canvas
    
    * #3203 - Do not allow paper size to be changed
    
    * #3203 - Move viewBox by mouse wheel
    
    * #3203 - Fix structures to be drawn under cursor
    
    * #3203 - Add document for `viewBoxToProto`
    
    * #3203 - Fix coordinates of cursor-following atom
    
    * #3203 - Fix wrong coordinates when scrolling up/left
    
    * #3203 - Update hand tool to move viewBox
    
    * #3203 - Support scrolling by touchpad
    
    * #3203 - Revert viewBox setting in structure library
    
    * #3203 - Refactor: rename Scale methods, add new CoordinateTransformation methods
    
    * #3203 - Fix wrong position of InfoPanel
    
    * #3203 - Fix wrong position of S-Group Data tooltip
    
    * #3203 - Fix position of floating buttons to change when scrolling
    
    * #3203 - Fix wrong positions after zooming, including InfoPanel, HoverIcon, hand move delta,
    
    * #3203 - Fix eslint (remove param `resizeCanvas`)
    
    * #3203 - Zoom on mouse position by mouse wheel, and zoom on the center of canvas by the buttons
    
    * #3203 - Draw scrollbar
    
    * #3203 - Refactor scrollbar to abstract class
    
    * #3203 - Allow to drag scrollbar
    
    * #3203 - Fix eslint
    
    * #3203 - Fix viewBox and canvas to resize when the media size is changing
    
    * #3203 - Fix hand tool to be smooth
    
    * #3203 - Fix errors when dragging structure beyond canvas
    
    * #3203 - Zoom by hotkeys
    
    * #3203 - Update `centerStructure` based on viewBox
    
    * #3203 - Fix editor in template modal to center structure
    
    * #3203 - Fix scrollbar showing in template modal
    
    * #3203 - Fix size of InfoPanel
    
    * #3203 - Fix viewport to move when using hotkeys to move structures beyond the canvas
    
    * Rebase master:  #3203 - Fix playwright tests (most of them failed due to position nuances)
    
    * #3203 - Fix eslint
    
    * #3203 - Add playwright tests for scrolling
    
    * #3203 - Add unit tests for CoordinateTransformation
    
    * #3203 - Fix playwright tests
    
    * #3203 - Fix playwright tests
    
    * #3203 - Update document and comments
    
    * Merge master: Rename reference  `scaled2obj` with `canvasToProto`
    
    * #3203 - Fix wrong positions of previews after zooming
    
    * #3203 - Fix playwright tests
    
    * #3203 - Rename `proto` to `model`, `viewBox` to `view`
    
    * #3203 - Rename `pageEvent` to `page`
    
    * #3203 - Add document to coordinate systems
    
    * #3203 - Update playwright screenshots
    
    * #3203 - Rename `proto` to `model` in class `Scale`

--- packages/ketcher-core/src/application/render/raphaelRender.ts
+++ packages/ketcher-core/src/application/render/raphaelRender.ts
@@ -22,9 +22,11 @@ import { ReStruct } from './restruct';
 import { Scale } from 'domain/helpers';
 import defaultOptions from './options';
 import draw from './draw';
-import { RenderOptions } from './render.types';
+import { RenderOptions, ViewBox } from './render.types';
 import _ from 'lodash';
 import { KetcherLogger } from 'utilities';
+import { CoordinateTransformation } from './coordinateTransformation';
+import { ScrollbarContainer } from './scrollbar';
 
 const notifyRenderComplete = _.debounce(() => {
   const event = new Event('renderComplete');
@@ -40,22 +42,47 @@ export class Render {
   // TODO https://github.com/epam/ketcher/issues/2630
   public ctab: ReStruct;
   public options: RenderOptions;
+  public viewBox!: ViewBox;
   private readonly userOpts: RenderOptions;
   private oldCb: Box2Abs | null = null;
+  private scrollbar: ScrollbarContainer;
+  private resizeObserver: ResizeObserver | null = null;
 
   constructor(clientArea: HTMLElement, options: RenderOptions) {
-    let renderWidth = options.width || clientArea.clientWidth - 10;
-    let renderHeight = options.height || clientArea.clientHeight - 10;
-    renderWidth = renderWidth > 0 ? renderWidth : 0;
-    renderHeight = renderHeight > 0 ? renderHeight : 0;
-
     this.userOpts = options;
     this.clientArea = clientArea;
-    this.paper = new Raphael(clientArea, renderWidth, renderHeight);
-    this.sz = Vec2.ZERO;
+    this.paper = new Raphael(
+      clientArea,
+      options.width || '100%',
+      options.height || '100%',
+    );
+    this.sz = this.getCanvasSizeVector();
     this.ctab = new ReStruct(new Struct(), this);
     this.options = defaultOptions(this.userOpts);
+    this.scrollbar = new ScrollbarContainer(this);
+    this.setViewBox({
+      minX: 0,
+      minY: 0,
+      width: this.sz.x,
+      height: this.sz.y,
+    });
   }
+
+  observeCanvasResize = () => {
+    this.resizeObserver = new ResizeObserver(() => {
+      this.sz = new Vec2(
+        this.clientArea.clientWidth,
+        this.clientArea.clientHeight,
+      );
+      this.resizeViewBox();
+    });
+
+    this.resizeObserver.observe(this.paper.canvas);
+  };
+
+  unobserveCanvasResize = () => {
+    this.resizeObserver?.unobserve(this.paper.canvas);
+  };
 
   updateOptions(opts: string) {
     try {
@@ -83,106 +110,79 @@ export class Render {
     return draw.selectionRectangle(this.paper, point0, point1, this.options);
   }
 
-  view2obj(point: Vec2, isRelative?: boolean) {
-    let scroll = this.scrollPos();
-
-    point = point.scaled(1 / this.options.zoom);
-    scroll = scroll.scaled(1 / this.options.zoom);
-
-    point = isRelative ? point : point.add(scroll).sub(this.options.offset);
-
-    return Scale.scaled2obj(point, this.options);
-  }
-
-  obj2view(vector: Vec2, isRelative?: boolean) {
-    let p = Scale.obj2scaled(vector, this.options);
-    p = isRelative
-      ? p
-      : p
-          .add(this.options.offset)
-          .sub(this.scrollPos().scaled(1 / this.options.zoom));
-    p = p.scaled(this.options.zoom);
-
-    return p;
-  }
-
-  scrollPos() {
-    return new Vec2(this.clientArea.scrollLeft, this.clientArea.scrollTop);
-  }
-
+  /** @deprecated recommend using `CoordinateTransformation.pageToModel` instead */
   page2obj(event: MouseEvent | { clientX: number; clientY: number }) {
-    const clientArea = this.clientArea;
-
-    const { top: offsetTop, left: offsetLeft } =
-      clientArea.getBoundingClientRect();
-
-    const pp = new Vec2(event.clientX - offsetLeft, event.clientY - offsetTop);
-    return this.view2obj(pp);
+    return CoordinateTransformation.pageToModel(event, this);
   }
 
-  setPaperSize(newSize: Vec2) {
-    this.sz = newSize;
-    this.paper.setSize(
-      newSize.x * this.options.zoom,
-      newSize.y * this.options.zoom,
-    );
-    this.setViewBox();
-  }
+  setZoom(zoom: number, event?: WheelEvent) {
+    const zoomedWidth = this.sz.x / zoom;
+    const zoomedHeight = this.sz.y / zoom;
+    const [viewBoxX, viewBoxY] = event
+      ? this.zoomOnMouse(event, zoomedWidth, zoomedHeight)
+      : this.zoomOnCanvasCenter(zoomedWidth, zoomedHeight);
+    this.setViewBox({
+      minX: viewBoxX,
+      minY: viewBoxY,
+      width: zoomedWidth,
+      height: zoomedHeight,
+    });
 
-  setOffset(newOffset: Vec2): void {
-    const delta = new Vec2(
-      newOffset.x - this.options.offset.x,
-      newOffset.y - this.options.offset.y,
-    );
-    this.clientArea.scrollLeft += delta.x;
-    this.clientArea.scrollTop += delta.y;
-    this.options.offset = newOffset;
-  }
-
-  setZoom(zoom: number) {
-    // when scaling the canvas down it may happen that the scaled canvas is smaller than the view window
-    // don't forget to call setScrollOffset after zooming (or use extendCanvas directly)
     this.options.zoom = zoom;
-    this.paper.setSize(this.sz.x * zoom, this.sz.y * zoom);
-    this.setViewBox();
   }
 
-  setScrollOffset(x: number, y: number) {
-    const clientArea = this.clientArea;
-    const cx = clientArea.clientWidth;
-    const cy = clientArea.clientHeight;
-    const e = calcExtend(
-      this.sz.scaled(this.options.zoom),
-      x,
-      y,
-      cx + Math.abs(x),
-      cy + Math.abs(y),
-    ).scaled(1 / this.options.zoom);
-    if (e.x > 0 || e.y > 0) {
-      this.setPaperSize(this.sz.add(e));
-      const d = new Vec2(x < 0 ? -x : 0, y < 0 ? -y : 0).scaled(
-        1 / this.options.zoom,
-      );
-      if (d.x > 0 || d.y > 0) {
-        this.ctab.translate(d);
-        this.setOffset(this.options.offset.add(d));
-      }
-    }
-    // clientArea.scrollLeft = x
-    // clientArea.scrollTop = y
-    clientArea.scrollLeft = x * this.options.scale;
-    clientArea.scrollTop = y * this.options.scale;
-    // TODO: store drag position in scaled systems
-    // scrollLeft = clientArea.scrollLeft;
-    // scrollTop = clientArea.scrollTop;
-    this.update(false);
+  private getCanvasSizeVector() {
+    return new Vec2(this.clientArea.clientWidth, this.clientArea.clientHeight);
   }
 
-  setViewBox() {
+  resizeViewBox() {
+    this.sz = this.getCanvasSizeVector();
+    const newWidth = this.sz.x / this.options.zoom;
+    const newHeight = this.sz.y / this.options.zoom;
+    this.setViewBox((prev) => ({
+      ...prev,
+      width: newWidth,
+      height: newHeight,
+    }));
+  }
+
+  private zoomOnCanvasCenter(zoomedWidth: number, zoomedHeight: number) {
+    const fixedPoint = new Vec2(
+      this.viewBox.minX + this.viewBox.width / 2,
+      this.viewBox.minY + this.viewBox.height / 2,
+    );
+    const viewBoxX = fixedPoint.x - zoomedWidth / 2;
+    const viewBoxY = fixedPoint.y - zoomedHeight / 2;
+    return [viewBoxX, viewBoxY];
+  }
+
+  private zoomOnMouse(
+    event: WheelEvent,
+    zoomedWidth: number,
+    zoomedHeight: number,
+  ) {
+    const fixedPoint = CoordinateTransformation.pageToCanvas(event, this);
+    const widthRatio = (fixedPoint.x - this.viewBox.minX) / this.viewBox.width;
+    const heightRatio =
+      (fixedPoint.y - this.viewBox.minY) / this.viewBox.height;
+    const viewBoxX = fixedPoint.x - zoomedWidth * widthRatio;
+    const viewBoxY = fixedPoint.y - zoomedHeight * heightRatio;
+    return [viewBoxX, viewBoxY];
+  }
+
+  /**
+   * @see https://developer.mozilla.org/zh-CN/docs/Web/SVG/Attribute/viewBox
+   */
+  setViewBox(func: (viewBox: ViewBox) => ViewBox): void;
+  setViewBox(viewBox: ViewBox): void;
+  setViewBox(arg: ViewBox | ((viewBox: ViewBox) => ViewBox)): void {
+    const newViewBox = typeof arg === 'function' ? arg(this.viewBox) : arg;
+    this.viewBox = newViewBox;
     this.paper.canvas.setAttribute(
       'viewBox',
-      '0 0 ' + this.sz.x + ' ' + this.sz.y,
+      `${newViewBox.minX} ${newViewBox.minY} ${newViewBox.width} ${newViewBox.height}`,
     );
+    this.scrollbar.update();
   }
 
   setMolecule(struct: Struct) {
@@ -192,13 +192,7 @@ export class Render {
     this.update(false);
   }
 
-  update(
-    force = false,
-    viewSz: Vec2 | null = null,
-    options = {
-      resizeCanvas: true,
-    },
-  ) {
+  update(force = false, viewSz: Vec2 | null = null) {
     // eslint-disable-line max-statements
     viewSz =
       viewSz ||
@@ -210,10 +204,9 @@ export class Render {
     const changes = this.ctab.update(force);
     this.ctab.setSelection(); // [MK] redraw the selection bits where necessary
     if (changes) {
-      const scale = this.options.scale;
       const bb = this.ctab
         .getVBoxObj()
-        .transform(Scale.obj2scaled, this.options)
+        .transform(Scale.modelToCanvas, this.options)
         .translate(this.options.offset || new Vec2());
 
       if (this.options.downScale) {
@@ -222,31 +215,9 @@ export class Render {
 
       const isAutoScale = this.options.autoScale || this.options.downScale;
       if (!isAutoScale) {
-        const ext = Vec2.UNIT.scaled(scale);
-        const eb = bb.sz().length() > 0 ? bb.extend(ext, ext) : bb;
-        const vb = new Box2Abs(
-          this.scrollPos(),
-          viewSz.scaled(1 / this.options.zoom).sub(Vec2.UNIT.scaled(20)),
-        );
-        const cb = Box2Abs.union(vb, eb);
         if (!this.oldCb) this.oldCb = new Box2Abs();
-
-        const sz = cb.sz().floor();
-        const delta = this.oldCb.p0.sub(cb.p0).ceil();
-        const shouldResizeCanvas =
-          (!this.sz || sz.x !== this.sz.x || sz.y !== this.sz.y) &&
-          options.resizeCanvas;
-        if (shouldResizeCanvas) {
-          this.setPaperSize(sz);
-        }
-
+        this.scrollbar.update();
         this.options.offset = this.options.offset || new Vec2();
-        const shouldScrollCanvas =
-          (delta.x !== 0 || delta.y !== 0) && options.resizeCanvas;
-        if (shouldScrollCanvas) {
-          this.setOffset(this.options.offset.add(delta));
-          this.ctab.translate(delta);
-        }
       } else {
         const sz1 = bb.sz();
         const marg = this.options.autoScaleMargin;
@@ -265,37 +236,15 @@ export class Render {
           rescale = 1;
         }
         const sz2 = sz1.add(mv.scaled(2 * rescale));
-        /* eslint-disable no-mixed-operators */
         this.paper.setViewBox(
           bb.pos().x - marg * rescale - (csz.x * rescale - sz2.x) / 2,
           bb.pos().y - marg * rescale - (csz.y * rescale - sz2.y) / 2,
           csz.x * rescale,
           csz.y * rescale,
         );
-        /* eslint-enable no-mixed-operators */
       }
 
       notifyRenderComplete();
     }
   }
-}
-
-function calcExtend(
-  canvasSize: Vec2,
-  x0: number,
-  y0: number,
-  newXSize: number,
-  newYSize: number,
-): Vec2 {
-  // eslint-disable-line max-params
-  let ex = x0 < 0 ? -x0 : 0;
-  let ey = y0 < 0 ? -y0 : 0;
-
-  if (canvasSize.x < newXSize) {
-    ex += newXSize - canvasSize.x;
-  }
-  if (canvasSize.y < newYSize) {
-    ey += newYSize - canvasSize.y;
-  }
-  return new Vec2(ex, ey);
 }

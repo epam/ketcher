5ad937c3278aaf79b53c3c2245d102c6724819fa
Roman Rodionov <roman_rodionov@epam.com>
2023-10-30 10:30:10 +0100

    - in progress

--- packages/ketcher-core/src/application/editor/operations/monomer/monomerFactory.ts
+++ packages/ketcher-core/src/application/editor/operations/monomer/monomerFactory.ts
@@ -20,6 +20,7 @@ export const MONOMER_CONST = {
   PEPTIDE: 'PEPTIDE',
   CHEM: 'CHEM',
   RNA: 'RNA',
+  DNA: 'DNA',
   R: 'R', // states for Ribose
   P: 'P', // states for Phosphate
   SUGAR: 'SUGAR',
@@ -38,7 +39,10 @@ export const monomerFactory = (
   let MonomerRenderer;
   let monomerClass: monomerClass;
 
-  if (monomer.props.MonomerType === MONOMER_CONST.CHEM) {
+  if (
+    monomer.props.MonomerType === MONOMER_CONST.CHEM ||
+    monomer.props.MonomerType === MONOMER_CONST.DNA
+  ) {
     Monomer = Chem;
     MonomerRenderer = ChemRenderer;
     monomerClass = 'CHEM';

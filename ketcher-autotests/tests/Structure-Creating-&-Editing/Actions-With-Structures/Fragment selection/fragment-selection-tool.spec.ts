9f87dd968c0eb3e8ba4ab75a312ac4852532f885
Marta Williams <marta_williams@epam.com>
2023-11-07 15:47:12 +0100

    - 2 testes cases - screenshots

--- ketcher-autotests/tests/Structure-Creating-&-Editing/Actions-With-Structures/Fragment selection/fragment-selection-tool.spec.ts
+++ ketcher-autotests/tests/Structure-Creating-&-Editing/Actions-With-Structures/Fragment selection/fragment-selection-tool.spec.ts
@@ -1,19 +1,42 @@
-import { test } from '@playwright/test';
+import { Page, test } from '@playwright/test';
 import {
-  clickOnAtom,
+  clickInTheMiddleOfTheScreen,
+    clickOnAtom,
   doubleClickOnAtom,
   dragMouseTo,
   openFileAndAddToCanvas,
+  screenshotBetweenUndoRedo,
   selectDropdownTool,
-  takeEditorScreenshot,
+    takeEditorScreenshot,
   waitForPageInit,
+  waitForRender,
 } from '@utils';
 import { clickOnArrow } from '@utils/canvas/arrow-signes/getArrow';
-import { getAtomByIndex } from '@utils/canvas/atoms';
+import { getAtomByIndex, getRightAtomByAttributes } from '@utils/canvas/atoms';
 import { clickOnPlus } from '@utils/canvas/plus-signes/getPluses';
+
 
 const xMark = 300;
 const yMark = 200;
+
+async function selectObjects (page: Page) {
+  await page.keyboard.down('Shift');
+  await clickOnPlus(page, 1);
+  await clickOnArrow(page, 0);
+  const atomToClick = 7;
+  await doubleClickOnAtom(page, 'C', atomToClick);
+}
+
+async function selectSomeObjects (page: Page) {
+  await waitForRender(page, async () => {
+    await page.keyboard.down('Shift');
+    await clickOnPlus(page, 1);
+    await clickOnArrow(page, 0);
+    await clickOnPlus(page,0);
+    await page.mouse.down();
+  });
+}
+
 
 test.describe('Fragment selection tool', () => {
   test.beforeEach(async ({ page }) => {
@@ -42,16 +65,9 @@ test.describe('Fragment selection tool', () => {
 
   test('Select and drag reaction components', async ({ page }) => {
     //  Test case: EPMLSOPKET-1357
-    async function selectObjects() {
-      await page.keyboard.down('Shift');
-      await clickOnPlus(page, 1);
-      await clickOnArrow(page, 0);
-      const atomToClick = 7;
-      await doubleClickOnAtom(page, 'C', atomToClick);
-    }
     await openFileAndAddToCanvas('RXN-V2000/reaction_4.rxn', page);
     await selectDropdownTool(page, 'select-rectangle', 'select-fragment');
-    await selectObjects();
+    await selectObjects(page);
     await dragMouseTo(xMark, yMark, page);
   });
 
@@ -72,5 +88,25 @@ test.describe('Fragment selection tool', () => {
     await selectDropdownTool(page, 'select-rectangle', 'select-fragment');
     await clickOnAtom(page, 'Br', 0);
     await page.keyboard.press('Delete');
+  });
+
+  test('Undo - Redo moving of structures', async ({ page }) => {
+    // Test case: EPMLSOPKET-1360
+    // Move some parts off structure - plus and arrow - then use Undo?redo
+    await openFileAndAddToCanvas('Rxn-V2000/reaction_4.rxn', page);
+    await selectDropdownTool(page, 'select-rectangle', 'select-fragment');
+    await selectSomeObjects(page);
+    await dragMouseTo(xMark, yMark, page);
+    await screenshotBetweenUndoRedo(page);
+  });
+
+  test('Drawing selection contours correctly for hovered structures', async ({ page }) => {
+    // Test case: EPMLSOPKET-17664
+    // Verify the bond contours are not intersected with atom contours
+    await openFileAndAddToCanvas('glutamine.mol', page);
+    await selectDropdownTool(page, 'select-rectangle', 'select-fragment');
+    const point = await getRightAtomByAttributes(page, { label: 'N' });
+    await page.mouse.click(point.x, point.y);
+    await page.mouse.move(point.x, point.y);
   });
 });

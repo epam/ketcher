46a6ac6582d0305844f13e5971401c3aaf0fd90e
chgayane <chgayane@EPAMYERW006D.localdomain>
2023-11-08 19:33:47 +0400

    #3475 - fixed code styles

--- packages/ketcher-core/src/domain/entities/Peptide.ts
+++ packages/ketcher-core/src/domain/entities/Peptide.ts
@@ -1,7 +1,6 @@
 import { BaseMonomer } from './BaseMonomer';
 
 export class Peptide extends BaseMonomer {
-
   public getValidSourcePoint(monomer: BaseMonomer) {
     if (
       monomer.isAttachmentPointExistAndFree('R1') &&

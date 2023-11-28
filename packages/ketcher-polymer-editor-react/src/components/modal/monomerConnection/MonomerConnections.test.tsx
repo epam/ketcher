9b6fdd80dc0f2125c8f4c1d5b1b24d2c3b2a412c
Ivan Arkhipov <ivan_arkhipov@epam.com>
2023-11-28 14:14:26 +0300

    Fixed unrelated TS issue

--- packages/ketcher-polymer-editor-react/src/components/modal/monomerConnection/MonomerConnections.test.tsx
+++ packages/ketcher-polymer-editor-react/src/components/modal/monomerConnection/MonomerConnections.test.tsx
@@ -16,7 +16,6 @@ const monomerData = {
     MonomerNaturalAnalogCode: 'A',
     MonomerType: 'MONOMER',
     BranchMonomer: '',
-    MonomerCaps: '',
     MonomerCode: '',
     MonomerName: '',
     Name: 'First peptide',
@@ -30,9 +29,9 @@ const secondPeptide = new Peptide({
 });
 
 firstPeptide.attachmentPointsToBonds = { R1: null, R2: null };
-firstPeptide.monomerItem.props.MonomerCaps = '[R1]H,[R2]H';
+firstPeptide.monomerItem.props.MonomerCaps = { R1: 'H', R2: 'H' };
 secondPeptide.attachmentPointsToBonds = { R1: null, R2: null };
-secondPeptide.monomerItem.props.MonomerCaps = '[R1]O,[R2]O';
+secondPeptide.monomerItem.props.MonomerCaps = { R1: 'O', R2: 'O' };
 
 const mockProps = {
   onClose: jest.fn(),

85b64a60556a6637b6d2cc95d488fd90fbd91c8b
Nikita <Nikita_Vozisov@epam.com>
2024-01-05 15:42:05 +0300

    Autotests: #3436 - Autotests: query attributes for SMARTS (#3506) (#3838)
    * Autotests: #3436 - Autotests: query attributes for SMARTS (#3506)
    
    * examples for testing query features
    
    * adding tests and utils at new folder
    
    * adding tests for more atom query specific attributes
    
    * creating test suite for bond attributes, changign structure of tests files
    
    * chaning data-testId to data-testid
    
    * duplicated data-testid removed
    
    * adding 4 tests for custom query and logical operators, removing unnecessary files, updating data-testid for bonds properties
    
    * refactor, added tests for bond attributes
    
    * fixing data-testid for some elements at atom properties
    
    * updating snapshot
    
    * adding tests for general atom properties, adding and changing data-testid identifiers for some properties
    
    * adding tests for reaction mapping tool, passing default file name to the function for checking smarts
    
    * updating snapshot for new identifier
    
    * updating informations about existing bugs, marking failing tests
    
    * removing comments about bugs which are fixed, marking test as failing
    
    * fixing few tests, updating screenshots
    
    * updating screenshots, fixing one test
    
    * refactor
    
    * refactor
    
    * extracting repeated code into function
    
    * refactor
    
    ---------
    
    Co-authored-by: Nikita_Vozisov <Nikita_Vozisov@epam.com>
    Co-authored-by: Urszula Gawlowska <urszula.gawlowska@epam.com>
    
    * - copy bond-attrib
    
    * -copy drawStructures
    
    * - copy query-specific
    
    * - copy custom-query
    
    * - copy reaction-queries
    
    * - copy atom-properties
    
    * - copy utils
    
    * - code refactor
    
    * - code refactoring;
    
    * - updated test file
    
    ---------
    
    Co-authored-by: Urszula Gawlowska <urszula.gawlowska@epam.com>
    Co-authored-by: Zhirnoff <mikhail_zhirnov@epam.com>

--- ketcher-autotests/tests/Structure-Creating-&-Editing/SMARTS-attributes/Bond-attributes/bond-attributes.spec.ts
+++ ketcher-autotests/tests/Structure-Creating-&-Editing/SMARTS-attributes/Bond-attributes/bond-attributes.spec.ts
@@ -1,11 +1,10 @@
 import { Page, test } from '@playwright/test';
 import {
   BondType,
-  BondTypeName,
-  clickInTheMiddleOfTheScreen,
   doubleClickOnBond,
   pressButton,
-  selectBond,
+  receiveFileComparisonData,
+  saveToFile,
   takeEditorScreenshot,
   waitForBondPropsModal,
   waitForPageInit,
@@ -13,26 +12,11 @@ import {
   checkSmartsWarnings,
   setBondTopology,
   setBondType,
-  setCustomQuery,
-} from '@utils';
-
-async function drawStructure(page: Page) {
-  await selectBond(BondTypeName.Single, page);
-  await clickInTheMiddleOfTheScreen(page);
-  await clickInTheMiddleOfTheScreen(page);
-  await clickInTheMiddleOfTheScreen(page);
-}
-
-async function setAndCheckBondType(
-  page: Page,
-  bondType: BondTypeName | string,
-  expectedSmarts: string,
-) {
-  await setBondType(page, bondType);
-  await pressButton(page, 'Apply');
-  await takeEditorScreenshot(page);
-  await checkSmartsValue(page, expectedSmarts);
-}
+  setCustomQueryForBond,
+  setReactingCenter,
+} from '../utils';
+import { getKet } from '@utils/formats';
+import { drawStructure } from '@utils/canvas/drawStructures';
 
 async function setAndCheckBondProperties(
   page: Page,
@@ -46,14 +30,36 @@ async function setAndCheckBondProperties(
   await checkSmartsValue(page, expectedSmarts);
 }
 
+async function waitForBondPropsModal(page: Page) {
+  await expect(page.getByTestId('bondProps-dialog')).toBeVisible();
+}
+
+async function drawStructureAndDoubleClickOnBond(
+  page: Page,
+  bondType: BondType,
+  numberOfBond: number,
+) {
+  await waitForPageInit(page);
+  await drawStructure(page);
+  await page.keyboard.press('Escape');
+  await doubleClickOnBond(page, bondType, numberOfBond);
+  await waitForBondPropsModal(page);
+}
+
+async function setCustomQueryAndCheckValue(page: Page, expectedValue: string) {
+  await page.getByTestId('custom-query-checkbox').check();
+  const customQueryInput = page.getByTestId('custom-query-value');
+  await expect(customQueryInput).toHaveValue(expectedValue);
+}
+
 test.describe('Checking bond attributes in SMARTS format', () => {
   test.beforeEach(async ({ page }) => {
     const numberOfBond = 2;
-    await waitForPageInit(page);
-    await drawStructure(page);
-    await page.keyboard.press('Escape');
-    await doubleClickOnBond(page, BondType.SINGLE, numberOfBond);
-    await waitForBondPropsModal(page);
+    await drawStructureAndDoubleClickOnBond(
+      page,
+      BondType.SINGLE,
+      numberOfBond,
+    );
   });
 
   // Tests for bond type:
@@ -63,23 +69,30 @@ test.describe('Checking bond attributes in SMARTS format', () => {
   });
 
   test('Setting bond type - single up', async ({ page }) => {
-    await setAndCheckBondType(
+    test.fail();
+    await setAndCheckBondProperties(
       page,
-      BondTypeName.SingleUp,
-      '[#6](-[#6])(-[#6])/[#6]',
+      setBondType,
+      'Single Up-option',
+      '[#6@](-[#6])(-[#6])/[#6]',
     );
   });
 
   test('Setting bond type - single down', async ({ page }) => {
-    await setAndCheckBondType(
+    test.fail();
+    await setAndCheckBondProperties(
       page,
-      BondTypeName.SingleDown,
-      '[#6](-[#6])(-[#6])\\[#6]',
+      setBondType,
+      'Single Down-option',
+      '[#6@@](-[#6])(-[#6])\\[#6]',
     );
   });
 
   test('Setting bond type - single up/down', async ({ page }) => {
-    await setAndCheckBondType(
+    /**
+     * This test will fail until https://github.com/epam/Indigo/issues/1371 is fixed
+     */
+    await setAndCheckBondProperties(
       page,
       BondTypeName.SingleUpDown,
       '[#6](-[#6])(-[#6])-[#6]',
@@ -91,7 +104,10 @@ test.describe('Checking bond attributes in SMARTS format', () => {
   });
 
   test('Setting bond type - double cis/trans', async ({ page }) => {
-    await setAndCheckBondType(
+    /**
+     * This test will fail until https://github.com/epam/Indigo/issues/1371 is fixed
+     */
+    await setAndCheckBondProperties(
       page,
       BondTypeName.DoubleCisTrans,
       '[#6](-[#6])(-[#6])=[#6]',
@@ -184,12 +200,188 @@ test.describe('Checking bond attributes in SMARTS format', () => {
     );
   });
 
-  test('Setting custom query - any OR double', async ({ page }) => {
+  test('Setting reacting center', async ({ page }) => {
+    /**
+     * Test case: https://github.com/epam/ketcher/issues/3431
+     * Description: setting reacting center option should have no impact on SMARTS output but warning should be displayed
+     */
     await setAndCheckBondProperties(
       page,
-      setCustomQuery,
+      setReactingCenter,
+      'Center-option',
+      '[#6](-[#6])(-[#6])-[#6]',
+    );
+    await checkSmartsWarnings(page);
+  });
+
+  // Custom query for bond
+
+  test('Setting custom query - any OR double', async ({ page }) => {
+    /**
+     * This test will fail until https://github.com/epam/Indigo/issues/1372 is fixed
+     */
+    await setAndCheckBondProperties(
+      page,
+      setCustomQueryForBond,
       '~,=',
       '[#6](-[#6])(-[#6])~,=[#6]',
     );
+  });
+
+  test('Setting custom query - directional bond "up or unspecified"', async ({
+    page,
+  }) => {
+    /**
+     * Test case: https://github.com/epam/Indigo/issues/1281
+     * Description: "or unspecified"("?") quantifier should be supported
+     */
+    await setAndCheckBondProperties(
+      page,
+      setCustomQueryForBond,
+      '/?',
+      '[#6](-[#6])(-[#6])/?[#6]',
+    );
+  });
+});
+
+test.describe('Checking converting bond attributes to custom query', () => {
+  test.beforeEach(async ({ page }) => {
+    const numberOfBond = 2;
+    await drawStructureAndDoubleClickOnBond(
+      page,
+      BondType.SINGLE,
+      numberOfBond,
+    );
+  });
+
+  test('Converting Topology = "Either" and Type = "Single" to custom query', async ({
+    page,
+  }) => {
+    /**
+     * Test case: https://github.com/epam/ketcher/issues/3372
+     * Description: If topology = "Either" then customQuery should be empty e.g. Type=Single, Topology=Either => customQuery=-
+     */
+    const expectedValue = '-';
+    await setBondType(page, 'Single-option');
+    await setBondTopology(page, 'Either-option');
+    await setCustomQueryAndCheckValue(page, expectedValue);
+    await takeEditorScreenshot(page);
+  });
+
+  test('Converting Topology = "Ring" and Type = "Double" to custom query', async ({
+    page,
+  }) => {
+    /**
+     * Test case: https://github.com/epam/ketcher/issues/3372
+     * Description: If topology = "Ring" then customQuery should be @, e.g. Type=Double, Topology=Ring=>customQuery==;@
+     */
+    const expectedValue = '=;@';
+    await setBondType(page, 'Double-option');
+    await setBondTopology(page, 'Ring-option');
+    await setCustomQueryAndCheckValue(page, expectedValue);
+  });
+
+  test('Converting Topology = "Chain" and Type = "Triple" to custom query', async ({
+    page,
+  }) => {
+    /**
+     * Test case: https://github.com/epam/ketcher/issues/3372
+     * Description: If topology = "Chain" then customQuery should be !@, e.g. Type=Triple, Topology=Chain=>customQuery=#;!@
+     */
+    const expectedValue = '#;!@';
+    await setBondType(page, 'Triple-option');
+    await setBondTopology(page, 'Chain-option');
+    await setCustomQueryAndCheckValue(page, expectedValue);
+  });
+
+  test('Converting Type = "Aromatic" and Reacting Center = "Center" to custom query', async ({
+    page,
+  }) => {
+    /**
+     * Test case: https://github.com/epam/ketcher/issues/3328
+     * Description: aromatic bond should be converted to custom query as: :
+     * Reacting Center option should be ingored at custom query
+     */
+    const expectedValue = ':';
+    await setBondType(page, 'Aromatic-option');
+    await setReactingCenter(page, 'Center-option');
+    await setCustomQueryAndCheckValue(page, expectedValue);
+  });
+
+  test('Converting Type = "Any" and Reacting Center = "Made/broken" to custom query', async ({
+    page,
+  }) => {
+    /**
+     * Test case: https://github.com/epam/ketcher/issues/3328
+     * Description: any bond should be converted to custom query as: ~
+     * Reacting Center option should be ingored at custom query
+     */
+    const expectedValue = '~';
+    await setBondType(page, 'Any-option');
+    await setReactingCenter(page, 'Made/broken-option');
+    await setCustomQueryAndCheckValue(page, expectedValue);
+  });
+
+  test('Converting Type = "Single up" and Reacting Center = "Order changes" to custom query', async ({
+    page,
+  }) => {
+    /**
+     * Test case: https://github.com/epam/ketcher/issues/3328
+     * Description: Single up bond should be converted to custom query as: /
+     * Reacting Center option should be ingored at custom query
+     */
+    const expectedValue = '/';
+    await setBondType(page, 'Single Up-option');
+    await setReactingCenter(page, 'Order changes-option');
+    await setCustomQueryAndCheckValue(page, expectedValue);
+  });
+
+  test('Converting Type = "Single down" to custom query', async ({ page }) => {
+    /**
+     * Test case: https://github.com/epam/ketcher/issues/3328
+     * Description: Single down bond should be converted to custom query as: \
+     */
+    const expectedValue = '\\';
+    await setBondType(page, 'Single Down-option');
+    await setCustomQueryAndCheckValue(page, expectedValue);
+  });
+});
+
+test.describe('Checking saving attributes to .ket file', () => {
+  test.beforeEach(async ({ page }) => {
+    const numberOfBond = 2;
+    await drawStructureAndDoubleClickOnBond(
+      page,
+      BondType.SINGLE,
+      numberOfBond,
+    );
+  });
+
+  test('Save *.ket file with custom query for bond attribute', async ({
+    page,
+  }) => {
+    /**
+     * Test case: https://github.com/epam/ketcher/issues/3328
+     * Description: In KET format customQuery should be saved into the bond object without other properties
+     */
+    await setBondType(page, 'Double-option');
+    await setBondTopology(page, 'Ring-option');
+    await page.getByTestId('custom-query-checkbox').check();
+    await pressButton(page, 'Apply');
+
+    const expectedFile = await getKet(page);
+    await saveToFile(
+      'KET/three-bond-structure-with-custom-query-to-compare.ket',
+      expectedFile,
+    );
+
+    const { fileExpected: ketFileExpected, file: ketFile } =
+      await receiveFileComparisonData({
+        page,
+        expectedFileName:
+          'tests/test-data/KET/three-bond-structure-with-custom-query-to-compare.ket',
+      });
+
+    expect(ketFile).toEqual(ketFileExpected);
   });
 });

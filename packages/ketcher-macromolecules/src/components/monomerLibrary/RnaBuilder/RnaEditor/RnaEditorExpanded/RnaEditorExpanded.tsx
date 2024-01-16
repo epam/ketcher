2fb24c2402c20d17147d6645fd3c57c69bfa7890
chgayane <chgayane08@student.rau.am>
2024-01-16 16:33:29 +0400

    #3900 - Updated name change handler to allow for complete deletion of text

--- packages/ketcher-macromolecules/src/components/monomerLibrary/RnaBuilder/RnaEditor/RnaEditorExpanded/RnaEditorExpanded.tsx
+++ packages/ketcher-macromolecules/src/components/monomerLibrary/RnaBuilder/RnaEditor/RnaEditorExpanded/RnaEditorExpanded.tsx
@@ -146,10 +146,16 @@ export const RnaEditorExpanded = ({
   const onChangeName = (event: ChangeEvent<HTMLInputElement>) => {
     if (isEditMode) {
       const newPresetName = event.target.value;
-      if (newPresetName.trim() !== '') {
+      if (newPresetName !== '') {
         setNewPrest({
           ...newPreset,
           name: newPresetName.trim(),
+          editedName: true,
+        });
+      } else {
+        setNewPrest({
+          ...newPreset,
+          name: '',
           editedName: true,
         });
       }

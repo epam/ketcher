3e7e9c5bdf2c1302e6e1b5228ebd52092d8bd342
Zhirnoff <mikhail_zhirnov@epam.com>
2024-02-23 15:41:34 +0300

    - added tests; - added 2 test files; - added expected snapshots

--- ketcher-autotests/tests/Macromolecule-editor/Sequence-Mode/sequence-mode.spec.ts
+++ ketcher-autotests/tests/Macromolecule-editor/Sequence-Mode/sequence-mode.spec.ts
@@ -8,6 +8,7 @@ import {
   scrollDown,
   selectSnakeLayoutModeTool,
   selectFlexLayoutModeTool,
+  clickUndo,
 } from '@utils';
 import { turnOnMacromoleculesEditor } from '@utils/macromolecules';
 
@@ -107,6 +108,86 @@ test.describe('Sequence Mode', () => {
     according to the canvas size at 100% zoom rate, and symbols are transferred to next line in tens.
     */
     await openFileAndAddToCanvas('KET/50-peptides-and-2-chems.ket', page);
+    await selectSequenceLayoutModeTool(page);
+    await takeEditorScreenshot(page);
+  });
+
+  test('Confirm that if system encounters Peptide it is appears as letter for natural analog', async ({
+    page,
+  }) => {
+    /* 
+    Test case: #3648
+    Description: Peptide appears as letter for natural analog
+    */
+    const ZOOM_OUT_VALUE = 800;
+    const SCROLL_DOWN_VALUE = 150;
+    await openFileAndAddToCanvas('KET/natural-analog-peptides.ket', page);
+    await selectSequenceLayoutModeTool(page);
+    await zoomWithMouseWheel(page, ZOOM_OUT_VALUE);
+    await scrollDown(page, SCROLL_DOWN_VALUE);
+    await takeEditorScreenshot(page);
+  });
+
+  test('Confirm that if system encounters Sugar, Base or CHEM it is appears as @ symbol', async ({
+    page,
+  }) => {
+    /* 
+    Test case: #3648
+    Description: Sugar, Base or CHEM appears as @ symbol
+    */
+    await openFileAndAddToCanvas('KET/sugar-base-chem-not-connected.ket', page);
+    await selectSequenceLayoutModeTool(page);
+    await takeEditorScreenshot(page);
+  });
+
+  test('Open RNA in sequence mode, switch to flex mode and confirm that RNA chain layout is left-to-right', async ({
+    page,
+  }) => {
+    /* 
+    Test case: #3648
+    Description: RNA opened in sequence mode and RNA chain layout is left-to-right.
+    */
+    await selectSequenceLayoutModeTool(page);
+    await openFileAndAddToCanvas('Molfiles-V3000/rna.mol', page);
+    await takeEditorScreenshot(page);
+    await selectFlexLayoutModeTool(page);
+    await takeEditorScreenshot(page);
+  });
+
+  test('Press Undo button and verify that layout returns to unarranged state', async ({
+    page,
+  }) => {
+    /* 
+    Test case: #3648
+    Description: After press 'Undo' button layout returns to unarranged state.
+    */
+    await openFileAndAddToCanvas('Molfiles-V3000/rna.mol', page);
+    await selectSequenceLayoutModeTool(page);
+    await clickUndo(page);
+    await takeEditorScreenshot(page);
+  });
+
+  test('Press CTRL+Z hotkey button and verify that layout returns to unarranged state', async ({
+    page,
+  }) => {
+    /* 
+    Test case: #3648
+    Description: After press CTRL+Z hotkey layout returns to unarranged state.
+    */
+    await openFileAndAddToCanvas('Molfiles-V3000/rna.mol', page);
+    await selectSequenceLayoutModeTool(page);
+    await page.keyboard.press('Control+z');
+    await takeEditorScreenshot(page);
+  });
+
+  test('Confirm that length of each row is limited to 30 nucleotides', async ({
+    page,
+  }) => {
+    /* 
+    Test case: #3648
+    Description: Length of each row is limited to 30 nucleotides after switch to sequence mode.
+    */
+    await openFileAndAddToCanvas('Molfiles-V3000/dna-long.mol', page);
     await selectSequenceLayoutModeTool(page);
     await takeEditorScreenshot(page);
   });

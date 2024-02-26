35e96c89608f1e157597bb86f290ca6783531345
Zhirnoff <mikhail_zhirnov@epam.com>
2024-02-26 18:40:40 +0300

    - added 'test.fail()' label

--- ketcher-autotests/tests/Templates/Functional-Groups/Functional-Group-Tools/functional-group-tools.spec.ts
+++ ketcher-autotests/tests/Templates/Functional-Groups/Functional-Group-Tools/functional-group-tools.spec.ts
@@ -813,6 +813,7 @@ test.describe('Templates - Functional Group Tools3', () => {
     Description: Can attach copied Functional Group to atoms of structure
     Test not working proberly right now. Bug https://github.com/epam/ketcher/issues/2660
    */
+    test.fail();
     const anyAtom = 4;
     await openFileAndAddToCanvas(
       'Molfiles-V2000/functional-group-and-benzene.mol',
@@ -837,6 +838,7 @@ test.describe('Templates - Functional Group Tools3', () => {
     Description: Can attach cutted Functional Group to atoms of structure
     Test not working proberly right now. Bug https://github.com/epam/ketcher/issues/2660
    */
+    test.fail();
     const anyAtom = 4;
     await openFileAndAddToCanvas(
       'Molfiles-V2000/functional-group-and-benzene.mol',

eabc4c30476306147b0f49c42b3e1b33de85211e
Starla Huang <starla_huang@epam.com>
2024-03-01 20:59:20 +0800

    #3819 fix test

--- ketcher-autotests/tests/Templates/Functional-Groups/Functional-Group-Tools/functional-group-tools.spec.ts
+++ ketcher-autotests/tests/Templates/Functional-Groups/Functional-Group-Tools/functional-group-tools.spec.ts
@@ -813,7 +813,6 @@ test.describe('Templates - Functional Group Tools3', () => {
     Description: Can attach copied Functional Group to atoms of structure
     Test not working proberly right now. Bug https://github.com/epam/ketcher/issues/2660
    */
-    test.fail();
     const anyAtom = 4;
     await openFileAndAddToCanvas(
       'Molfiles-V2000/functional-group-and-benzene.mol',

bbfe56a35a65c62c0963b0b49d698fed50cb30eb
Zhirnoff <mikhail_zhirnov@epam.com>
2024-03-11 16:41:50 +0300

    - added tests to 'zoom-tool'; - added function screenshotBetweenUndoRedoInMacro; - added expected snapshots

--- ketcher-autotests/tests/Macromolecule-editor/Zoom-Tool/zoom.spec.ts
+++ ketcher-autotests/tests/Macromolecule-editor/Zoom-Tool/zoom.spec.ts
@@ -7,8 +7,14 @@ import {
   waitForRender,
   takeLeftToolbarMacromoleculeScreenshot,
   selectSnakeLayoutModeTool,
+  clickInTheMiddleOfTheScreen,
+  screenshotBetweenUndoRedoInMacro,
+  moveMouseToTheMiddleOfTheScreen,
+  selectSingleBondTool,
+  takePageScreenshot,
 } from '@utils';
 import { turnOnMacromoleculesEditor } from '@utils/macromolecules';
+import { connectMonomersWithBonds } from '@utils/macromolecules/monomer';
 
 async function zoomWithMouseScrollAndTakeScreenshot(page: Page) {
   const zoomLevelDelta = 600;
@@ -218,14 +224,142 @@ test.describe('Zoom Tool', () => {
       }
     });
     for (let i = 0; i < 30; i++) {
+      await page.keyboard.press('Control+=');
+    }
+    for (let i = 0; i < 30; i++) {
+      await page.keyboard.press('Control+-');
+    }
+  });
+
+  test('Check if you create a peptide chain, zoom in and add new elements to chain then zoom out and add another elements', async ({
+    page,
+  }) => {
+    /* 
+    Test case: Zoom Tool
+    Description: After create a peptide chain, zoom in and add new elements to chain then zoom out and add 
+    another elements, then after back to zoom 100% all structure is create properly.
+    */
+    const x = 800;
+    const y = 350;
+    const x1 = 650;
+    const y1 = 150;
+    await page.getByTestId('Bal___beta-Alanine').click();
+    await clickInTheMiddleOfTheScreen(page);
+    for (let i = 0; i < 3; i++) {
       await waitForRender(page, async () => {
         await page.keyboard.press('Control+=');
       });
     }
-    for (let i = 0; i < 30; i++) {
+    await page.getByTestId('Edc___S-ethylthiocysteine').click();
+    await page.mouse.click(x, y);
+    await connectMonomersWithBonds(page, ['Bal', 'Edc']);
+    for (let i = 0; i < 5; i++) {
       await waitForRender(page, async () => {
         await page.keyboard.press('Control+-');
       });
     }
+    await page.getByTestId('meD___N-Methyl-Aspartic acid').click();
+    await page.mouse.click(x1, y1);
+    await connectMonomersWithBonds(page, ['Edc', 'meD']);
+    await takeEditorScreenshot(page);
+  });
+
+  test('Undo not undoing zoom', async ({ page }) => {
+    /* 
+    Test case: Zoom Tool
+    Description: After creating structure on canvas and then 
+    click zoom in, and then Undo, Redo the structure is the same size as before.
+    */
+    const x = 800;
+    const y = 350;
+    await page.getByTestId('Bal___beta-Alanine').click();
+    await clickInTheMiddleOfTheScreen(page);
+    await page.getByTestId('Edc___S-ethylthiocysteine').click();
+    await page.mouse.click(x, y);
+    await connectMonomersWithBonds(page, ['Bal', 'Edc']);
+    await takeEditorScreenshot(page);
+    for (let i = 0; i < 5; i++) {
+      await waitForRender(page, async () => {
+        await page.keyboard.press('Control+=');
+      });
+    }
+    await screenshotBetweenUndoRedoInMacro(page);
+    await takeEditorScreenshot(page);
+  });
+
+  test('After zooming out to maximum canvas zoom level, preview of monomer entities remains same as before zoom out', async ({
+    page,
+  }) => {
+    /* 
+    Test case: Zoom Tool
+    Description: After zooming out to maximum canvas 
+    zoom level, preview of monomer entities under mouse cursor remains same as before zoom out.
+    */
+    for (let i = 0; i < 8; i++) {
+      await waitForRender(page, async () => {
+        await page.keyboard.press('Control+-');
+      });
+    }
+    await page.getByTestId('Bal___beta-Alanine').click();
+    await moveMouseToTheMiddleOfTheScreen(page);
+    await takeEditorScreenshot(page);
+    await page.getByTestId('RNA-TAB').click();
+    await page.getByTestId('C_C_R_P').click();
+    await moveMouseToTheMiddleOfTheScreen(page);
+    await takeEditorScreenshot(page);
+    await page.getByTestId('CHEM-TAB').click();
+    await page.getByTestId('SMPEG2___SM(PEG)2 linker from Pierce').click();
+    await moveMouseToTheMiddleOfTheScreen(page);
+    await takeEditorScreenshot(page);
+  });
+
+  test('After zooming in to maximum on monomer, connection points looks undistorted', async ({
+    page,
+  }) => {
+    /* 
+    Test case: Zoom Tool
+    Description: After zooming in to maximum on monomer, connection points looks undistorted.
+    */
+    await openFileAndAddToCanvas(
+      `KET/Peptide-Templates/15 - (R1,R2,R3,R4,R5).ket`,
+      page,
+    );
+    for (let i = 0; i < 30; i++) {
+      await waitForRender(page, async () => {
+        await page.keyboard.press('Control+=');
+      });
+    }
+    await selectSingleBondTool(page);
+    await page.getByText('(R').locator('..').first().hover();
+    await takeEditorScreenshot(page);
+  });
+
+  test('When zoomed to 175% and more, buttons in Paste from Clipboard window not change their position and not overlap each other', async ({
+    page,
+    browser,
+  }) => {
+    /* 
+    Test case: Zoom Tool
+    Description: When zoomed to 175% and more, buttons in 
+    Paste from Clipboard window not change their position and not overlap each other.
+    After fix bug https://github.com/epam/ketcher/issues/4174 need to update snapshot.
+    */
+    await page.getByTestId('open-button').click();
+    await page.getByTestId('paste-from-clipboard-button').click();
+    await browser.newContext({ deviceScaleFactor: 2.5 });
+    await page.setViewportSize({ width: 435, height: 291 });
+    await takePageScreenshot(page);
+  });
+
+  test('Maximum browser zoom out', async ({ page, browser }) => {
+    /* 
+    Test case: Zoom Tool
+    Description: When zoomed out to 25%, buttons and toolbars have the correct appearance
+    */
+    await page.getByTestId('open-button').click();
+    await page.getByTestId('paste-from-clipboard-button').click();
+    await browser.newContext({ deviceScaleFactor: 0.2 });
+    await page.setViewportSize({ width: 4358, height: 2918 });
+    await takePageScreenshot(page);
   });
 });

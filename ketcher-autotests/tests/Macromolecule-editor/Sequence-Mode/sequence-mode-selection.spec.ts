d409ba7e89d03803599c21b6e4d5c9859cb4a7df
Starla Huang <starla_huang@epam.com>
2024-03-13 17:34:06 +0800

    #3819 update autotests

---
+++ ketcher-autotests/tests/Macromolecule-editor/Sequence-Mode/sequence-mode-selection.spec.ts
@@ -0,0 +1,109 @@
+import { test } from '@playwright/test';
+import {
+  takeEditorScreenshot,
+  waitForPageInit,
+  openFileAndAddToCanvas,
+  selectSequenceLayoutModeTool,
+  zoomWithMouseWheel,
+  scrollDown,
+  selectRectangleArea,
+  selectFlexLayoutModeTool,
+  dragMouseTo,
+} from '@utils';
+import { turnOnMacromoleculesEditor } from '@utils/macromolecules';
+
+test.describe('Sequence mode selection for view mode', () => {
+  test.beforeEach(async ({ page }) => {
+    await waitForPageInit(page);
+    await turnOnMacromoleculesEditor(page);
+    const ZOOM_OUT_VALUE = 400;
+    const SCROLL_DOWN_VALUE = 250;
+
+    await openFileAndAddToCanvas('KET/monomers-chains.ket', page);
+    await selectSequenceLayoutModeTool(page);
+    await zoomWithMouseWheel(page, ZOOM_OUT_VALUE);
+    await scrollDown(page, SCROLL_DOWN_VALUE);
+  });
+
+  test('Select letters with rectangular selection tool', async ({ page }) => {
+    // Coordinates for rectangle selection
+    const startX = 100;
+    const startY = 100;
+    const endX = 500;
+    const endY = 500;
+
+    await selectRectangleArea(page, startX, startY, endX, endY);
+    await takeEditorScreenshot(page);
+  });
+
+  test('Select letters with Shift+Lclick', async ({ page }) => {
+    await page.keyboard.down('Shift');
+    await page.getByText('G').first().click();
+    await page.getByText('T').first().click();
+    await page.keyboard.up('Shift');
+    await takeEditorScreenshot(page);
+  });
+
+  test('Select entire chain with Ctrl+Lclick', async ({ page }) => {
+    await page.keyboard.down('Control');
+    await page.getByText('G').first().click();
+    await page.keyboard.up('Control');
+    await takeEditorScreenshot(page);
+  });
+});
+
+test.describe('Sequence mode selection for edit mode', () => {
+  test.beforeEach(async ({ page }) => {
+    await waitForPageInit(page);
+    await turnOnMacromoleculesEditor(page);
+    const ZOOM_OUT_VALUE = 400;
+    const SCROLL_DOWN_VALUE = 250;
+
+    await openFileAndAddToCanvas('KET/monomers-chains.ket', page);
+    await selectSequenceLayoutModeTool(page);
+    await zoomWithMouseWheel(page, ZOOM_OUT_VALUE);
+    await scrollDown(page, SCROLL_DOWN_VALUE);
+    await page.getByText('G').first().click({ button: 'right' });
+    await page.getByTestId('edit_sequence').click();
+  });
+
+  test('Select letters with LClick+drag', async ({ page }) => {
+    await page.getByText('G').first().hover();
+    await page.mouse.down();
+    const number = 5;
+    await page.getByText('G').nth(number).hover();
+    await page.mouse.up();
+    await takeEditorScreenshot(page);
+
+    const blankAreaAxis = { x: 200, y: 200 };
+    await page.mouse.click(blankAreaAxis.x, blankAreaAxis.y);
+    await takeEditorScreenshot(page);
+  });
+
+  test('Select letters with Shift + ArrowRight button', async ({ page }) => {
+    const arrowCount = 10;
+    await page.keyboard.down('Shift');
+    for (let i = 0; i < arrowCount; i++) {
+      await page.keyboard.press('ArrowRight');
+    }
+    await page.keyboard.up('Shift');
+    await takeEditorScreenshot(page);
+
+    await selectFlexLayoutModeTool(page);
+    await selectSequenceLayoutModeTool(page);
+    await takeEditorScreenshot(page);
+  });
+
+  test('Select letters with Shift + ArrowLeft', async ({ page }) => {
+    const arrowCount = 10;
+    await page.keyboard.down('Shift');
+    for (let i = 0; i < arrowCount; i++) {
+      await page.keyboard.press('ArrowLeft');
+    }
+    await page.keyboard.up('Shift');
+    await takeEditorScreenshot(page);
+
+    await page.keyboard.press('Escape');
+    await takeEditorScreenshot(page);
+  });
+});

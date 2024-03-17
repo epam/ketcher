8803e285f4c058db3453b84db0182f4c5aec9dd2
Alexey Girin <agirin@mail.ru>
2024-03-17 23:30:16 +0300

    Adding tests, ket files and screenshots for #4258 - 2

--- ketcher-autotests/tests/Indigo-Tools/Toggle-Explicit-Hydrogens-With-Respect-To-Selected-Atoms/toggle-explicit-hydrogens-with-respect-to-selected-atoms.spec.ts
+++ ketcher-autotests/tests/Indigo-Tools/Toggle-Explicit-Hydrogens-With-Respect-To-Selected-Atoms/toggle-explicit-hydrogens-with-respect-to-selected-atoms.spec.ts
@@ -1,5 +1,5 @@
-/* eslint-disable no-magic-numbers */
 /* eslint-disable prettier/prettier */
+/* eslint-disable no-magic-numbers */
 import { test } from '@playwright/test';
 import {
   selectTopPanelButton,

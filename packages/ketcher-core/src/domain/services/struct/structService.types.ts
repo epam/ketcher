e0e3a0d41ec9beac1b226de46d5f888fa317d37a
Roman Rodionov <roman_rodionov@epam.com>
2024-03-21 13:50:24 +0100

    #4295 - Cannot save FASTA using the Remote mode

--- packages/ketcher-core/src/domain/services/struct/structService.types.ts
+++ packages/ketcher-core/src/domain/services/struct/structService.types.ts
@@ -29,7 +29,7 @@ export enum ChemicalMimeType {
   KET = 'chemical/x-indigo-ket',
   UNKNOWN = 'chemical/x-unknown',
   SDF = 'chemical/x-sdf',
-  FASTA = 'fasta',
+  FASTA = 'chemical/x-fasta',
   SEQUENCE = 'chemical/x-sequence',
 }
 

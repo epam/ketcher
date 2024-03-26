bbda410cdb6aad9262464c8ad07f54f5d7fd3b74
Zhirnoff <mikhail_zhirnov@epam.com>
2024-03-26 16:27:44 +0300

    - added tests for 'erase-tool'; - added 2 test files; - added expected screenshots

--- ketcher-autotests/tests/Macromolecule-editor/Erase-Tool/erase-tool.spec.ts
+++ ketcher-autotests/tests/Macromolecule-editor/Erase-Tool/erase-tool.spec.ts
@@ -1,8 +1,13 @@
 import { test } from '@playwright/test';
 import {
   addSingleMonomerToCanvas,
+  clickInTheMiddleOfTheScreen,
+  moveMouseAway,
+  openFileAndAddToCanvasAsNewProject,
   selectEraseTool,
+  selectPartOfMolecules,
   selectSingleBondTool,
+  selectSnakeLayoutModeTool,
   takeEditorScreenshot,
   waitForPageInit,
 } from '@utils';
@@ -80,6 +85,157 @@ test.describe('Erase Tool', () => {
     // Get rid of flakiness because of preview
     await hideMonomerPreview(page);
 
+    await takeEditorScreenshot(page);
+  });
+
+  test('Check that RNA and its bonds are deleted when deleting monomer from RNA chain using Erase Tool', async ({
+    page,
+  }) => {
+    /* 
+    Test case: Erase Tool
+    Description: RNA and its bonds are deleted when deleting monomer from RNA chain using Erase Tool.
+    */
+    await openFileAndAddToCanvasAsNewProject(
+      `KET/rna-with-fmoe-sugar.ket`,
+      page,
+    );
+    await selectEraseTool(page);
+    await page.getByText('FMOE').locator('..').first().click();
+    await takeEditorScreenshot(page);
+  });
+
+  test('Delete placed CHEM on canvas using Erase Tool', async ({ page }) => {
+    /* 
+    Test case: Erase Tool
+    Description: CHEM is deleted.
+    */
+    await page.getByTestId('CHEM-TAB').click();
+    await page.getByTestId('Test-6-Ch___Test-6-AP-Chem').click();
+    await clickInTheMiddleOfTheScreen(page);
+    await moveMouseAway(page);
+    await takeEditorScreenshot(page);
+    await selectEraseTool(page);
+    await page.getByText('Test-6-Ch').locator('..').first().click();
+    await takeEditorScreenshot(page);
+  });
+
+  test('Delete bond between two CHEMs on canvas using Erase Tool', async ({
+    page,
+  }) => {
+    /* 
+    Test case: Erase Tool
+    Description: Bond between two CHEMs are deleted.
+    */
+    const bondLine = await page.locator('g[pointer-events="stroke"]').first();
+    await openFileAndAddToCanvasAsNewProject(
+      `KET/two-chems-connected.ket`,
+      page,
+    );
+    await takeEditorScreenshot(page);
+    await selectEraseTool(page);
+    await bondLine.locator('..').click();
+    await takeEditorScreenshot(page);
+  });
+
+  test('Delete bond between two CHEMs on canvas using Erase Tool and check that it restored after pressing Undo ', async ({
+    page,
+  }) => {
+    /* 
+    Test case: Erase Tool
+    Description: Bond between two CHEMs are deleted and restored.
+    */
+    const bondLine = await page.locator('g[pointer-events="stroke"]').first();
+    await openFileAndAddToCanvasAsNewProject(
+      `KET/two-chems-connected.ket`,
+      page,
+    );
+    await selectEraseTool(page);
+    await bondLine.locator('..').click();
+    await takeEditorScreenshot(page);
+    await page.getByTestId('undo').click();
+    await takeEditorScreenshot(page);
+  });
+
+  test('Delete structure by clicking Erase tool button after selecting with short key "Control + A"', async ({
+    page,
+  }) => {
+    /* 
+    Test case: Erase Tool
+    Description: Structure deleted.
+    */
+    await openFileAndAddToCanvasAsNewProject(
+      `KET/rna-with-fmoe-sugar.ket`,
+      page,
+    );
+    await takeEditorScreenshot(page);
+    await page.keyboard.press('Control+a');
+    await selectEraseTool(page);
+    await takeEditorScreenshot(page);
+  });
+
+  test('Check that CHEM and its bonds are deleted when deleting using Erase Tool', async ({
+    page,
+  }) => {
+    /* 
+    Test case: Erase Tool
+    Description: Bonds and CHEN are deleted.
+    */
+    await openFileAndAddToCanvasAsNewProject(`KET/chems-connected.ket`, page);
+    await takeEditorScreenshot(page);
+    await selectEraseTool(page);
+    await page.getByText('A6OH').locator('..').first().click();
+    await takeEditorScreenshot(page);
+  });
+
+  test('Check that deleted entity from the middle of chain restored after pressing Undo', async ({
+    page,
+  }) => {
+    /* 
+    Test case: Erase Tool
+    Description: Deleted entity from the middle of chain restored after pressing Undo.
+    */
+    await openFileAndAddToCanvasAsNewProject(`KET/chems-connected.ket`, page);
+    await selectEraseTool(page);
+    await page.getByText('A6OH').locator('..').first().click();
+    await takeEditorScreenshot(page);
+    await page.getByTestId('undo').click();
+    await takeEditorScreenshot(page);
+  });
+
+  test('Check if you delete one part of chain and click Undo button elements will return to the same place', async ({
+    page,
+  }) => {
+    /* 
+    Test case: Snake Mode
+    Description: Elements return to the same place.
+    */
+    await openFileAndAddToCanvasAsNewProject(
+      `KET/peptides-flex-chain.ket`,
+      page,
+    );
+    await selectPartOfMolecules(page);
+    await selectEraseTool(page);
+    await takeEditorScreenshot(page);
+    await page.getByTestId('undo').click();
+    await takeEditorScreenshot(page);
+  });
+
+  test('Check if you delete one part of RNA chain and click Undo button elements will return to the same place', async ({
+    page,
+  }) => {
+    /* 
+    Test case: Snake Mode
+    Description: Elements of RNA chain returns to the same place.
+    */
+    await openFileAndAddToCanvasAsNewProject(
+      `Molfiles-V3000/rna-modified-sugars.mol`,
+      page,
+    );
+    await selectSnakeLayoutModeTool(page);
+    await selectPartOfMolecules(page);
+    await selectEraseTool(page);
+    await takeEditorScreenshot(page);
+    await page.getByTestId('undo').click();
     await takeEditorScreenshot(page);
   });
 });

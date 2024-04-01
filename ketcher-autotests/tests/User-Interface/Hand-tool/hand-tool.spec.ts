d9d0c5e4acbf375a28573c1de56d65f4dc1f75bd
Zhirnoff <mikhail_zhirnov@epam.com>
2024-04-01 17:56:52 +0300

    - updated tests; - updated snapshots

--- ketcher-autotests/tests/User-Interface/Hand-tool/hand-tool.spec.ts
+++ ketcher-autotests/tests/User-Interface/Hand-tool/hand-tool.spec.ts
@@ -1,6 +1,5 @@
 import { Page, test, expect } from '@playwright/test';
 import {
-  
   LeftPanelButton,
   STRUCTURE_LIBRARY_BUTTON_NAME,
   moveMouseToTheMiddleOfTheScreen,
@@ -11,9 +10,8 @@ import {
   waitForPageInit,
 } from '@utils';
 
-
-async function mouseMovement(page: Page, endPoint: { x: number, y: number }) {
-  const startPoint = {x: 300, y:300};
+async function mouseMovement(page: Page, endPoint: { x: number; y: number }) {
+  const startPoint = { x: 300, y: 300 };
   await page.mouse.move(startPoint.x, startPoint.y);
   await page.mouse.down();
   await page.mouse.move(endPoint.x, endPoint.y);
@@ -24,51 +22,56 @@ test.describe('Hand tool', () => {
     await waitForPageInit(page);
   });
 
-    test.afterEach(async ({ page }) => {
-      await takeEditorScreenshot(page);
-    });
-
-  test('Hand tool icon interaction', async ({ page }) => {
+  test('Hand tool icon tooltip', async ({ page }) => {
     // Test case: EPMLSOPKET-4240
-    const button = page.getByTestId('hand');
-    await expect(button).toHaveAttribute('title', 'Hand tool (Ctrl+Alt+H)');
-    await button.click();
-    await moveMouseToTheMiddleOfTheScreen(page);
-  
+    const icon = {
+      testId: 'hand',
+      title: 'Hand tool (Ctrl+Alt+H)',
+    };
+    const iconButton = page.getByTestId(icon.testId);
+    await expect(iconButton).toHaveAttribute('title', icon.title);
+    await iconButton.hover();
+    expect(icon.title).toBeTruthy();
   });
 
   test('Moving canvas', async ({ page }) => {
-    //Test case: EPMLSOPKET-4241
+    // Test case: EPMLSOPKET-4241
     // Verify if canvas is captured and move with Hand Tool
-   await openFileAndAddToCanvas('KET/chain-with-atoms.ket', page);
-   await takeEditorScreenshot(page);
-   await selectLeftPanelButton(LeftPanelButton.HandTool, page);
-   await mouseMovement(page,{x:700, y:300});
+    await openFileAndAddToCanvas('KET/chain-with-atoms.ket', page);
+    await takeEditorScreenshot(page);
+    await selectLeftPanelButton(LeftPanelButton.HandTool, page);
+    await mouseMovement(page, { x: 700, y: 300 });
+    await takeEditorScreenshot(page);
   });
 
   test('Shortcut Ctrl+Alt+H/Cmd+H', async ({ page }) => {
     // Test case: EPMLSOPKET-4243
     // Verify if hot keys changed to Active Hand Tool cursor
-    await openFileAndAddToCanvas('KET/chain-with-atoms.ket', page);  
+    await openFileAndAddToCanvas('KET/chain-with-atoms.ket', page);
     await page.keyboard.press('Control+Alt+h');
-    await mouseMovement(page,{x:700, y:300});
+    await mouseMovement(page, { x: 700, y: 300 });
+    await takeEditorScreenshot(page);
   });
 
-  test('The hand tool is not following the cursor when moving outside the canvas', async ({page}) => {
-    // test case: EPMLSOPKET-8937 
+  test('The hand tool is not following the cursor when moving outside the canvas', async ({
+    page,
+  }) => {
+    // test case: EPMLSOPKET-8937
     // Verify if hand is not following coursor outside the canvas
-    const point = {x: 45, y:148};
+    const point = { x: 45, y: 148 };
     await selectLeftPanelButton(LeftPanelButton.HandTool, page);
     await pressButton(page, STRUCTURE_LIBRARY_BUTTON_NAME);
     await page.mouse.move(point.x, point.y);
+    await takeEditorScreenshot(page);
   });
 
   test('Able to scroll canvas down and to the right', async ({ page }) => {
-    //Test case: EPMLSOPKET-8937
-    // Verify posibility to move cnvas down and to the right 
-    await openFileAndAddToCanvas('KET/chain-with-atoms.ket', page);  
+    // Test case: EPMLSOPKET-8937
+    // Verify posibility to move cnvas down and to the right
+    await openFileAndAddToCanvas('KET/chain-with-atoms.ket', page);
     await selectLeftPanelButton(LeftPanelButton.HandTool, page);
-    await mouseMovement(page, {x:300, y:50});
-    await mouseMovement(page, {x:60, y:100});
+    await mouseMovement(page, { x: 300, y: 50 });
+    await mouseMovement(page, { x: 60, y: 100 });
+    await takeEditorScreenshot(page);
   });
 });

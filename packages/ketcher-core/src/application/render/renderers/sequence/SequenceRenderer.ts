21c0b2df673749e4201af8ccb03e0c5248b0f110
Roman Rodionov <roman_rodionov@epam.com>
2024-05-10 15:08:55 +0200

    #4548 - Cyclic chains disappear from canvas when try to Edit in the sequence mode - used lastNonEmptyNode instead of lastNode for cyclic chain end bond render

--- packages/ketcher-core/src/application/render/renderers/sequence/SequenceRenderer.ts
+++ packages/ketcher-core/src/application/render/renderers/sequence/SequenceRenderer.ts
@@ -218,7 +218,7 @@ export class SequenceRenderer {
         const bondRenderer = new PolymerBondSequenceRenderer(
           polymerBond,
           chain.firstNode,
-          chain.lastNode,
+          chain.lastNonEmptyNode,
         );
         bondRenderer.show();
         polymerBond.setRenderer(bondRenderer);

bf462b400a98afa6df9073b6078f90d184c0876b
Roman Rodionov <roman_rodionov@epam.com>
2024-05-15 14:39:44 +0200

    #4405 - Macro: Switching to Sequence mode causes page freeze (#4616)
    - refactored snake layout to work with ChainsCollection
    - fixed chains sorting to use Y coordinate more than X

--- packages/ketcher-core/src/domain/entities/RNABase.ts
+++ packages/ketcher-core/src/domain/entities/RNABase.ts
@@ -1,5 +1,8 @@
 import { BaseMonomer } from 'domain/entities/BaseMonomer';
 import { ChemSubChain } from 'domain/entities/monomer-chains/ChemSubChain';
+import { PolymerBond } from 'domain/entities/PolymerBond';
+import { AttachmentPointName } from 'domain/types';
+import { getSugarFromRnaBase } from 'domain/helpers/monomers';
 
 export class RNABase extends BaseMonomer {
   public getValidSourcePoint() {
@@ -18,5 +21,19 @@ export class RNABase extends BaseMonomer {
 
   public get SubChainConstructor() {
     return ChemSubChain;
+  }
+
+  public override get sideConnections() {
+    const sideConnections: PolymerBond[] = [];
+    this.forEachBond((polymerBond, attachmentPointName) => {
+      if (
+        attachmentPointName !== AttachmentPointName.R1 ||
+        !getSugarFromRnaBase(this)
+      ) {
+        sideConnections.push(polymerBond);
+      }
+    });
+
+    return sideConnections;
   }
 }

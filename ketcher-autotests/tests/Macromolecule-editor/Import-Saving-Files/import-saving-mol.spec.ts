77c989c220bd56b6160f7a20b9d071f52e6ce9cb
Zhirnoff <mikhail_zhirnov@epam.com>
2024-05-23 20:33:58 +0300

    - commented 2 tests

--- ketcher-autotests/tests/Macromolecule-editor/Import-Saving-Files/import-saving-mol.spec.ts
+++ ketcher-autotests/tests/Macromolecule-editor/Import-Saving-Files/import-saving-mol.spec.ts
@@ -465,23 +465,15 @@ test.describe('Import modified .mol files from external editor', () => {
     'dna-peptide-conj-example.mol',
     'dna-peptideSS-conj-example.mol',
     'insulin-2-peptides-connected-with-SS.mol',
-    'peptide-modified-2aa-example.mol',
-    'peptide-modified-aa-example.mol',
+    // 'peptide-modified-2aa-example.mol',
+    // 'peptide-modified-aa-example.mol',
     'rna-mod-phosphate-example.mol',
     'rna-mod-phosphate-mod-base-example.mol',
     'rna-modified.mol',
   ];
 
-  const filesToFail = new Set([
-    'peptide-modified-2aa-example.mol',
-    'peptide-modified-aa-example.mol',
-  ]);
-
   for (const fileName of fileNames) {
     test(`for ${fileName}`, async () => {
-      if (filesToFail.has(fileName)) {
-        test.fail(true, `This test is expected to fail for file: ${fileName}`);
-      }
       await openFileAndAddToCanvasMacro(`Molfiles-V3000/${fileName}`, page);
       const numberOfPressZoomOut = 4;
       for (let i = 0; i < numberOfPressZoomOut; i++) {

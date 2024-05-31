31c9105085f18e0b4af0372c4391accaacf3360d
Roman Rodionov <roman_rodionov@epam.com>
2024-05-31 13:54:37 +0200

    - added test

---
+++ ketcher-autotests/tests/utils/canvas/atoms/superatomAttachmentPoints.ts
@@ -0,0 +1,13 @@
+import { Page } from '@playwright/test';
+import { clickOnAtom } from '@utils';
+import { AtomLabelType } from '@utils/clicks/types';
+
+export async function addSuperatomAttachmentPoint(
+  page: Page,
+  atomLabel: AtomLabelType,
+  atomIndex: number,
+) {
+  await page.keyboard.press('Escape');
+  await clickOnAtom(page, atomLabel, atomIndex, 'right');
+  await page.getByText('Add attachment point').click();
+}

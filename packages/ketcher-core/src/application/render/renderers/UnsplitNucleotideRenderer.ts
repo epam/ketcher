3260f90511505ff572976a94e709cf736b281782
Nikita Chistousov <nikita_chistousov@epam.com>
2024-07-01 20:34:59 +0200

    #4930 – Fix missing IDT aliases and label positioning for unsplit nucleotides (#4953)

--- packages/ketcher-core/src/application/render/renderers/UnsplitNucleotideRenderer.ts
+++ packages/ketcher-core/src/application/render/renderers/UnsplitNucleotideRenderer.ts
@@ -1,6 +1,7 @@
 import { Selection } from 'd3';
 import { BaseMonomerRenderer } from 'application/render/renderers/BaseMonomerRenderer';
 import { UnsplitNucleotide } from 'domain/entities';
+import { D3SvgElementSelection } from 'application/render/types';
 
 const NUCLEOTIDE_SELECTED_ELEMENT_ID = '#nucleotide-selection';
 const NUCLEOTIDE_HOVERED_ELEMENT_ID = '#nucleotide-hover';
@@ -31,6 +32,29 @@ export class UnsplitNucleotideRenderer extends BaseMonomerRenderer {
   show(theme) {
     super.show(theme);
     this.appendEnumeration();
+  }
+
+  protected appendLabel(rootElement: D3SvgElementSelection<SVGGElement, void>) {
+    const fontSize = 6;
+    rootElement
+      .append('foreignObject')
+      .attr('width', this.width)
+      .attr('height', this.height)
+      .html(
+        `
+        <div style="padding: 0 4px; text-align: center; color: ${this.textColor}">
+          ${this.monomer.label}
+        </div>
+      `,
+      )
+      .attr('font-size', `${fontSize}px`)
+      .attr('line-height', `${fontSize}px`)
+      .attr('font-weight', '700')
+      .style('cursor', 'pointer')
+      .style('user-select', 'none')
+      .attr('pointer-events', 'none')
+      .attr('x', '4px')
+      .attr('y', this.height / 2);
   }
 
   protected get enumerationElementPosition() {

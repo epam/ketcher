87e89c6da3a8b9def9568654f6c72a237c5852ae
Roman Rodionov <roman_rodionov@epam.com>
2024-07-02 18:20:37 +0200

    - updated screenshots - skipped unstable test

--- ketcher-autotests/tests/Macromolecule-editor/Macro-Micro-Switcher/macro-micro-switcher.spec.ts
+++ ketcher-autotests/tests/Macromolecule-editor/Macro-Micro-Switcher/macro-micro-switcher.spec.ts
@@ -729,7 +729,7 @@ test.describe('Macro-Micro-Switcher', () => {
     await takeEditorScreenshot(page);
   });
 
-  test('Make full screen mode in micro mode and switch to macro mode.', async ({
+  test.skip('Make full screen mode in micro mode and switch to macro mode.', async ({
     page,
   }) => {
     /* 

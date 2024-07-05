9e7a0ac454ec43cdc24ad5a8c12a136f13d95933
Roman Rodionov <roman_rodionov@epam.com>
2024-07-05 19:06:56 +0200

    #4889 - After using "Clear Canvas" in the Sequence mode and entering new sequence, extra structures are shown in Flex and Snake modes
    - fixed clear tool for sequence mode

--- packages/ketcher-core/src/application/editor/modes/types.ts
+++ packages/ketcher-core/src/application/editor/modes/types.ts
@@ -1,6 +1,13 @@
+import { BaseMode } from 'application/editor/modes/BaseMode';
+import { SequenceMode } from 'application/editor';
+
 export type LayoutMode =
   | 'flex-layout-mode'
   | 'snake-layout-mode'
   | 'sequence-layout-mode';
 
 export const DEFAULT_LAYOUT_MODE: LayoutMode = 'sequence-layout-mode';
+
+export function isSequenceMode(mode: BaseMode): mode is SequenceMode {
+  return mode instanceof SequenceMode;
+}

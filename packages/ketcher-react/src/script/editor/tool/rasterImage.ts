7be55a4ef355a3e67848a95323fc835d7b2c27cf
Daniil Sloboda <daniil_sloboda@epam.com>
2024-07-12 12:18:06 +0400

    #4902 - image validation and selection fixes

--- packages/ketcher-react/src/script/editor/tool/rasterImage.ts
+++ packages/ketcher-react/src/script/editor/tool/rasterImage.ts
@@ -4,8 +4,13 @@ import {
   Vec2,
   fromRasterImageCreation,
   Editor,
+  KetcherLogger,
 } from 'ketcher-core';
 import { Tool } from './Tool';
+
+const TAG = 'tool/rasterImage.ts';
+const allowList = /image\/(png|jpeg|bmp|webp|x-icon|svg\+xml)/;
+const MIN_DIMENSION_SIZE = 16;
 
 export class RasterImageTool implements Tool {
   static readonly INPUT_ID = 'image-upload';
@@ -30,16 +35,43 @@ export class RasterImageTool implements Tool {
   }
 
   onFileUpload(clickPosition: Vec2): void {
+    const errorHandler = this.editor.errorHandler;
     this.element.onchange = null;
     if (this.element.files && this.element.files[0]) {
+      const file = this.element.files[0];
       const image = new Image();
       const reader = new FileReader();
+
+      if (!file.type.match(allowList)) {
+        const errorMesssage = `Wrong mime type: ${file.type}`;
+        KetcherLogger.error(`${TAG}:onFileUpload`, errorMesssage);
+        if (errorHandler) {
+          errorHandler(errorMesssage);
+        }
+
+        this.resetElementValue();
+        return;
+      }
 
       reader.addEventListener('load', () => {
         image.src = reader.result as string;
       });
 
       image.onload = () => {
+        this.resetElementValue();
+        if (
+          image.width < MIN_DIMENSION_SIZE ||
+          image.height < MIN_DIMENSION_SIZE
+        ) {
+          const errorMessage =
+            'Image should have be at least 16px wide and 16px tall';
+          KetcherLogger.error(`${TAG}:onLoad`, errorMessage);
+          if (errorHandler) {
+            errorHandler(errorMessage);
+          }
+          return;
+        }
+
         const halfSize = Scale.canvasToModel(
           new Vec2(image.width / 2, image.height / 2),
           this.editor.render.options,
@@ -53,7 +85,15 @@ export class RasterImageTool implements Tool {
             halfSize,
           ),
         );
+      };
+
+      image.onerror = (e) => {
         this.resetElementValue();
+        const errorMessage = 'Cannot load image';
+        KetcherLogger.error(`${TAG}:onerror`, errorMessage, e);
+        if (errorHandler) {
+          errorHandler(errorMessage);
+        }
       };
 
       reader.readAsDataURL(this.element.files[0]);

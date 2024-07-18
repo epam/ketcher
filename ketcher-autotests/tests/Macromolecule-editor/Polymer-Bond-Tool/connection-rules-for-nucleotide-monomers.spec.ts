78f78e7a1509bdde0eacdbf73c610891b4ca1fb9
Alexey Girin <agirin@mail.ru>
2024-07-18 16:43:17 +0300

    5122 - Case 11, 12

--- ketcher-autotests/tests/Macromolecule-editor/Polymer-Bond-Tool/connection-rules-for-nucleotide-monomers.spec.ts
+++ ketcher-autotests/tests/Macromolecule-editor/Polymer-Bond-Tool/connection-rules-for-nucleotide-monomers.spec.ts
@@ -1333,4 +1333,222 @@ test.describe('Connection rules for Nucleotide monomers: ', () => {
       });
     });
   });
+
+  const baseMonomers: { [monomerName: string]: IMonomer } = {
+    '(R1) - Left only': {
+      monomerType: 'base',
+      fileName: 'KET/Base-Templates/01 - (R1) - Left only.ket',
+      alias: '(R1)_-_Left_only',
+      connectionPoints: {
+        R1: 'R1',
+      },
+    },
+    '(R2) - Right only': {
+      monomerType: 'base',
+      fileName: 'KET/Base-Templates/02 - (R2) - Right only.ket',
+      alias: '(R2)_-_Right_only',
+      connectionPoints: {
+        R2: 'R2',
+      },
+    },
+    '(R3) - Side only': {
+      monomerType: 'base',
+      fileName: 'KET/Base-Templates/03 - (R3) - Side only.ket',
+      alias: '(R3)_-_Side_only',
+      connectionPoints: {
+        R3: 'R3',
+      },
+    },
+    '(R1,R2) - R3 gap': {
+      monomerType: 'base',
+      fileName: 'KET/Base-Templates/04 - (R1,R2) - R3 gap.ket',
+      alias: '(R1,R2)_-_R3_gap',
+      connectionPoints: {
+        R1: 'R1',
+        R2: 'R2',
+      },
+    },
+    '(R1,R3) - R2 gap': {
+      monomerType: 'base',
+      fileName: 'KET/Base-Templates/05 - (R1,R3) - R2 gap.ket',
+      alias: '(R1,R3)_-_R2_gap',
+      connectionPoints: {
+        R1: 'R1',
+        R3: 'R3',
+      },
+    },
+    '(R2,R3) - R1 gap': {
+      monomerType: 'base',
+      fileName: 'KET/Base-Templates/06 - (R2,R3) - R1 gap.ket',
+      alias: '(R2,R3)_-_R1_gap',
+      connectionPoints: {
+        R2: 'R2',
+        R3: 'R3',
+      },
+    },
+    // '(R3,R4)': {
+    //        monomerType: 'base',
+    //   fileName: 'KET/Base-Templates/07 - (R3,R4).ket',
+    //   alias: '(R3,R4)',
+    //   connectionPoints: {
+    //     R3: 'R3',
+    //     R4: 'R4',
+    //   },
+    // },
+    '(R1,R2,R3)': {
+      monomerType: 'base',
+      fileName: 'KET/Base-Templates/08 - (R1,R2,R3).ket',
+      alias: '(R1,R2,R3)',
+      connectionPoints: {
+        R1: 'R1',
+        R2: 'R2',
+        R3: 'R3',
+      },
+    },
+    // '(R1,R3,R4)': {
+    // monomerType: 'base',
+    //   fileName: 'KET/Base-Templates/09 - (R1,R3,R4).ket',
+    //   alias: '(R1,R3,R4)',
+    //   connectionPoints: {
+    //     R1: 'R1',
+    //     R3: 'R3',
+    //     R4: 'R4',
+    //   },
+    // },
+    // '(R2,R3,R4)': {
+    // monomerType: 'base',
+    //   fileName: 'KET/Base-Templates/10 - (R2,R3,R4).ket',
+    //   alias: '(R2,R3,R4)',
+    //   connectionPoints: {
+    //     R2: 'R2',
+    //     R3: 'R3',
+    //     R4: 'R4',
+    //   },
+    // },
+    // '(R3,R4,R5)': {
+    // monomerType: 'base',
+    //   fileName: 'KET/Base-Templates/11 - (R3,R4,R5).ket',
+    //   alias: '(R3,R4,R5)',
+    //   connectionPoints: {
+    //     R3: 'R3',
+    //     R4: 'R4',
+    //     R5: 'R5',
+    //   },
+    // },
+    // '(R1,R2,R3,R4)': {
+    // monomerType: 'base',
+    //   fileName: 'KET/Base-Templates/12 - (R1,R2,R3,R4).ket',
+    //   alias: '(R1,R2,R3,R4)',
+    //   connectionPoints: {
+    //     R1: 'R1',
+    //     R2: 'R2',
+    //     R3: 'R3',
+    //     R4: 'R4',
+    //   },
+    // },
+    // '(R1,R3,R4,R5)': {
+    // monomerType: 'base',
+    //   fileName: 'KET/Base-Templates/13 - (R1,R3,R4,R5).ket',
+    //   alias: '(R1,R3,R4,R5)',
+    //   connectionPoints: {
+    //     R1: 'R1',
+    //     R3: 'R3',
+    //     R4: 'R4',
+    //     R5: 'R5',
+    //   },
+    // },
+    // '(R2,R3,R4,R5)': {
+    // monomerType: 'base',
+    //   fileName: 'KET/Base-Templates/14 - (R2,R3,R4,R5).ket',
+    //   alias: '(R2,R3,R4,R5)',
+    //   connectionPoints: {
+    //     R2: 'R2',
+    //     R3: 'R3',
+    //     R4: 'R4',
+    //     R5: 'R5',
+    //   },
+    // },
+    // '(R1,R2,R3,R4,R5)': {
+    // monomerType: 'base',
+    //   fileName: 'KET/Base-Templates/15 - (R1,R2,R3,R4,R5).ket',
+    //   alias: '(R1,R2,R3,R4,R5)',
+    //   connectionPoints: {
+    //     R1: 'R1',
+    //     R2: 'R2',
+    //     R3: 'R3',
+    //     R4: 'R4',
+    //     R5: 'R5',
+    //   },
+    // },
+  };
+
+  Object.values(nucleotideMonomers).forEach((leftNucleotide) => {
+    Object.values(baseMonomers).forEach((rightBase) => {
+      Object.values(leftNucleotide.connectionPoints).forEach(
+        (leftNucleotideConnectionPoint) => {
+          Object.values(rightBase.connectionPoints).forEach(
+            (rightBaseConnectionPoint) => {
+              /*
+               *  Test case: https://github.com/epam/ketcher/issues/5122 - Case 11 (Nucleotide - Base)
+               *  Description: Validate that unsplit nucleotide could be connected with base (with every attachment point of unsplit nucleotide)
+               * For each %nucleotideType% from the library (nucleotideMonomers)
+               *   For each %baseType% from the library (baseMonomers)
+               *      For each %ConnectionPoint% (avaliable connections of %nucleotideType%)
+               *         For each %ConnectionPoint2% (avaliable connections of %baseType%) do:
+               *  1. Clear canvas
+               *  2. Load %nucleotideType% and %baseType% and put them on the canvas
+               *  3. Establish connection between %snucleotideType%(%ConnectionPoint%) and %baseType%(%ConnectionPoint2%)
+               *  4. Validate canvas (connection should appear)
+               */
+              test(`Case11: Cnnct ${leftNucleotideConnectionPoint} to ${rightBaseConnectionPoint} of Nuc(${leftNucleotide.alias}) and Base(${rightBase.alias})`, async () => {
+                test.setTimeout(20000);
+
+                await loadTwoMonomers(page, leftNucleotide, rightBase);
+
+                await bondTwoMonomersByPointToPoint(
+                  page,
+                  leftNucleotide,
+                  rightBase,
+                  leftNucleotideConnectionPoint,
+                  rightBaseConnectionPoint,
+                );
+
+                await zoomWithMouseWheel(page, -600);
+                await hoverOverConnectionLine(page);
+
+                await takeEditorScreenshot(page);
+              });
+            },
+          );
+        },
+      );
+    });
+  });
+
+  Object.values(nucleotideMonomers).forEach((leftNucleotide) => {
+    Object.values(baseMonomers).forEach((rightBase) => {
+      /*
+       *  Test case: https://github.com/epam/ketcher/issues/5122 - Case 12 (Nucleotide - Base)
+       *  Description: Validate that unsplit nucleotide could be connected with base (center-to-center way)
+       * For each %nucleotideType% from the library (nucleotideMonomers)
+       *   For each %baseType% from the library (baseMonomers)
+       *  1. Clear canvas
+       *  2. Load %nucleotideType% and %baseType% and put them on the canvas
+       *  3. Establish connection between %snucleotideType%(center) and %baseType%(center)
+       *  4. Validate canvas (connection should appear)
+       */
+      test(`Case12: Cnnct Center to Center of Nucleotide(${leftNucleotide.alias}) and Base(${rightBase.alias})`, async () => {
+        test.setTimeout(20000);
+
+        await loadTwoMonomers(page, leftNucleotide, rightBase);
+
+        await bondTwoMonomersByCenterToCenter(page, leftNucleotide, rightBase);
+
+        await zoomWithMouseWheel(page, -600);
+        await hoverOverConnectionLine(page);
+
+        await takeEditorScreenshot(page);
+      });
+    });
+  });
 });

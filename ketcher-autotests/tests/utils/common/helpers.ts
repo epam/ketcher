76cd928362cb820e95980526cb9b05fa80893d79
Alexey Girin <agirin@mail.ru>
2024-07-24 10:16:29 +0300

    5150 - Side chain connection is done (#5158)

--- ketcher-autotests/tests/utils/common/helpers.ts
+++ ketcher-autotests/tests/utils/common/helpers.ts
@@ -1,2 +1,17 @@
+import { Page } from '@playwright/test';
+import { turnOnMacromoleculesEditor } from '@utils/macromolecules';
+import { waitForKetcherInit, waitForIndigoToLoad } from './loaders';
+
 // eslint-disable-next-line @typescript-eslint/no-empty-function
 export async function emptyFunction() {}
+
+export async function pageReload(page: Page) {
+  /* In order to fix problem with label renderer (one pixel shift) 
+        we have to try to reload page
+    */
+  await page.reload();
+  await page.goto('', { waitUntil: 'domcontentloaded' });
+  await waitForKetcherInit(page);
+  await waitForIndigoToLoad(page);
+  await turnOnMacromoleculesEditor(page);
+}

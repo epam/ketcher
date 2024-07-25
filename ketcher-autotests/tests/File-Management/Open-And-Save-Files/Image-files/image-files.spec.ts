b1d2a237158281aa7797f519e11c0f9b9002324f
Zhirnoff <mikhail_zhirnov@epam.com>
2024-07-25 11:23:03 +0300

    - added tests; - added test file; - added expected snapshots

--- ketcher-autotests/tests/File-Management/Open-And-Save-Files/Image-files/image-files.spec.ts
+++ ketcher-autotests/tests/File-Management/Open-And-Save-Files/Image-files/image-files.spec.ts
@@ -1,10 +1,14 @@
 /* eslint-disable no-magic-numbers */
 import { test, expect, Page } from '@playwright/test';
 import {
+  clickInTheMiddleOfTheScreen,
   getKet,
   openFileAndAddToCanvas,
   openFileAndAddToCanvasAsNewProject,
   openImageAndAddToCanvas,
+  openPasteFromClipboard,
+  pressButton,
+  readFileContents,
   receiveFileComparisonData,
   saveToFile,
   takeEditorScreenshot,
@@ -38,6 +42,10 @@ test.describe('Image files', () => {
   test('Verify that single image of SVG format can be saved to KET file and load', async ({
     page,
   }) => {
+    /**
+     * Test case: #4911
+     * Description: Single image of SVG format can be saved to KET file and load
+     */
     await openImageAndAddToCanvas('Images/image-svg-demo.svg', page);
     await takeEditorScreenshot(page);
     await verifyFile(
@@ -50,6 +58,10 @@ test.describe('Image files', () => {
   test('Verify that single image of PNG format can be saved to KET file and load', async ({
     page,
   }) => {
+    /**
+     * Test case: #4911
+     * Description: Single image of PNG format can be saved to KET file and load
+     */
     await openImageAndAddToCanvas('Images/image-png.png', page);
     await takeEditorScreenshot(page);
     await verifyFile(
@@ -62,6 +74,10 @@ test.describe('Image files', () => {
   test('Verify that images of SVG and PNG format can be saved to KET file and load', async ({
     page,
   }) => {
+    /**
+     * Test case: #4911
+     * Description: Images of SVG and PNG format can be saved to KET file and load
+     */
     await openImageAndAddToCanvas('Images/image-svg-demo.svg', page);
     await openImageAndAddToCanvas('Images/image-png.png', page, 200, 200);
     await takeEditorScreenshot(page);
@@ -75,6 +91,11 @@ test.describe('Image files', () => {
   test('Verify that images of SVG and PNG format can be saved to KET file and added to canvas with correct positions and layer levels (last added image is on top)', async ({
     page,
   }) => {
+    /**
+     * Test case: #4911
+     * Description: Images of SVG and PNG format can be saved to KET file and added to canvas
+     * with correct positions and layer levels (last added image is on top)
+     */
     await openFileAndAddToCanvasAsNewProject(
       'KET/four-images-svg-and-png.ket',
       page,
@@ -90,6 +111,10 @@ test.describe('Image files', () => {
   test('Verify that images of SVG and PNG format can be saved to KET file and added to canvas with structures', async ({
     page,
   }) => {
+    /**
+     * Test case: #4911
+     * Description: Images of SVG and PNG format can be saved to KET file and added to canvas with structures
+     */
     await openFileAndAddToCanvasAsNewProject(
       'KET/images-with-benzene-ring-and-arrow.ket',
       page,
@@ -105,6 +130,10 @@ test.describe('Image files', () => {
   test('Verify that images of SVG and PNG format with Structure library elements can be saved to KET file and added to canvas', async ({
     page,
   }) => {
+    /**
+     * Test case: #4911
+     * Description: Images of SVG and PNG format with Structure library elements can be saved to KET file and added to canvas
+     */
     await openFileAndAddToCanvasAsNewProject(
       'KET/images-png-svg-with-elements.ket',
       page,
@@ -120,6 +149,10 @@ test.describe('Image files', () => {
   test('Verify that images of SVG and PNG format with 30 structure elements can be saved to KET file and added to canvas', async ({
     page,
   }) => {
+    /**
+     * Test case: #4911
+     * Description: Images of SVG and PNG format with 30 structure elements can be saved to KET file and added to canvas
+     */
     await openFileAndAddToCanvasAsNewProject(
       'KET/images-png-svg-30-with-30-elements.ket',
       page,
@@ -135,6 +168,10 @@ test.describe('Image files', () => {
   test('Verify that images of SVG and PNG format can be added from two different KET files saved and opened', async ({
     page,
   }) => {
+    /**
+     * Test case: #4911
+     * Description: Images of SVG and PNG format can be added from two different KET files saved and opened
+     */
     await openFileAndAddToCanvas('KET/images-png-svg-with-elements.ket', page);
     await openFileAndAddToCanvas(
       'KET/images-with-benzene-ring-and-arrow.ket',
@@ -148,5 +185,36 @@ test.describe('Image files', () => {
       'KET/two-images-with-many-elements-expected.ket',
       'tests/test-data/KET/two-images-with-many-elements-expected.ket',
     );
+  });
+
+  test('Verify that images of (PNG, SVG) are copied from .ket format and added to canvas using "PASTE FROM CLIPBOARD - Add to Canvas"', async ({
+    page,
+  }) => {
+    /**
+     * Test case: #4911
+     * Description: Images of (PNG, SVG) are copied from .ket format and added to canvas using "PASTE FROM CLIPBOARD - Add to Canvas"
+     */
+    const fileContent = await readFileContents(
+      'tests/test-data/KET/images-png-svg.ket',
+    );
+    await openPasteFromClipboard(page, fileContent);
+    await pressButton(page, 'Add to Canvas');
+    await clickInTheMiddleOfTheScreen(page);
+    await takeEditorScreenshot(page);
+  });
+
+  test('Verify that images of (PNG, SVG) are copied from .ket format and added to canvas using "PASTE FROM CLIPBOARD - Open as New Project"', async ({
+    page,
+  }) => {
+    /**
+     * Test case: #4911
+     * Description: Images of (PNG, SVG) are copied from .ket format and added to canvas using "PASTE FROM CLIPBOARD - Open as New Project"
+     */
+    const fileContent = await readFileContents(
+      'tests/test-data/KET/images-png-svg.ket',
+    );
+    await openPasteFromClipboard(page, fileContent);
+    await pressButton(page, 'Open as New Project');
+    await takeEditorScreenshot(page);
   });
 });

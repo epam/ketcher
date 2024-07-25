5c2964ac5499c53112b6589da7d26889408610cd
Zhirnoff <mikhail_zhirnov@epam.com>
2024-07-25 12:11:36 +0300

    - updated tests

--- ketcher-autotests/tests/File-Management/Open-And-Save-Files/Image-files/image-files.spec.ts
+++ ketcher-autotests/tests/File-Management/Open-And-Save-Files/Image-files/image-files.spec.ts
@@ -46,12 +46,12 @@ test.describe('Image files', () => {
      * Test case: #4911
      * Description: Single image of SVG format can be saved to KET file and load
      */
-    await openImageAndAddToCanvas('Images/image-svg-demo.svg', page);
+    await openImageAndAddToCanvas('Images/image-svg.svg', page);
     await takeEditorScreenshot(page);
     await verifyFile(
       page,
-      'KET/image-svg-demo-expected.ket',
-      'tests/test-data/KET/image-svg-demo-expected.ket',
+      'KET/image-svg-expected.ket',
+      'tests/test-data/KET/image-svg-expected.ket',
     );
   });
 
@@ -78,7 +78,7 @@ test.describe('Image files', () => {
      * Test case: #4911
      * Description: Images of SVG and PNG format can be saved to KET file and load
      */
-    await openImageAndAddToCanvas('Images/image-svg-demo.svg', page);
+    await openImageAndAddToCanvas('Images/image-svg.svg', page);
     await openImageAndAddToCanvas('Images/image-png.png', page, 200, 200);
     await takeEditorScreenshot(page);
     await verifyFile(

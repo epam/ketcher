f7ee479fa34fc1746f22978763e2a4409e3719d2
Dmitrii Polovnev <dmitrii_polovnev@epam.com>
2024-09-12 03:21:46 +0300

    #5497 – Refactor (Connection.ts): Specify the types (amend)

--- packages/ketcher-core/src/domain/entities/canvas-matrix/Connection.ts
+++ packages/ketcher-core/src/domain/entities/canvas-matrix/Connection.ts
@@ -1,10 +1,13 @@
 import { PolymerBond } from 'domain/entities/PolymerBond';
 import { SubChainNode } from 'domain/entities';
 
+type DirectionInDegrees = 0 | 90 | 180 | 270;
+type DirectionOfLastCell = { readonly x: number; readonly y: number };
+
 export class Connection {
   constructor(
     public readonly connectedNode: SubChainNode | null,
-    public readonly direction: 0 | 90 | 180 | 270 | { x: number; y: number },
+    public readonly direction: DirectionInDegrees | DirectionOfLastCell,
     public readonly isVertical: boolean,
     public readonly polymerBond: PolymerBond,
     public xOffset: number,

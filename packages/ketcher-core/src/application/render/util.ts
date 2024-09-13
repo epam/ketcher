1812c8478f8f9a958ee3f3a4158277a8d67343a5
Roman Rodionov <roman_rodionov@epam.com>
2024-09-13 17:38:13 +0200

    - reverted implementation for atoms

--- packages/ketcher-core/src/application/render/util.ts
+++ packages/ketcher-core/src/application/render/util.ts
@@ -181,7 +181,14 @@ function drawCIPLabel({
     options,
   });
 
+  if (atomOrBond instanceof Atom) {
+    const box = relBox(cipValuePath.path.getBBox());
+
+    cipValuePath.path.translateAbs(0.5 * box.width, -0.5 * box.height);
+  }
+
   path.push(cipValuePath.path.toFront());
+
   restruct.addReObjectPath(LayerMap.additionalInfo, visel, path, null, true);
 
   return cipValuePath;

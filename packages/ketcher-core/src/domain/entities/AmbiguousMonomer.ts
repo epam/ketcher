55881deb7eb2c9af9b913f6c3d56eff92286e5f6
Roman Rodionov <roman_rodionov@epam.com>
2024-09-17 14:10:50 +0200

    #5519 - Alias labels for mixed bases loaded from IDT are wrong

--- packages/ketcher-core/src/domain/entities/AmbiguousMonomer.ts
+++ packages/ketcher-core/src/domain/entities/AmbiguousMonomer.ts
@@ -4,10 +4,14 @@ import { SubChainNode } from 'domain/entities/monomer-chains/types';
 import { Vec2 } from 'domain/entities/vec2';
 import { Struct } from 'domain/entities/struct';
 import { monomerFactory } from 'application/editor/operations/monomer/monomerFactory';
-import { KetMonomerClass } from 'application/formatters';
+import {
+  KetAmbiguousMonomerTemplateSubType,
+  KetMonomerClass,
+} from 'application/formatters';
 import { MONOMER_CLASS_TO_CONSTRUCTOR } from 'domain/constants/monomers';
 import { IVariantMonomer } from 'domain/entities/types';
 import { AmbiguousMonomerType, AttachmentPointName } from 'domain/types';
+
 export const DEFAULT_VARIANT_MONOMER_LABEL = '%';
 
 export class AmbiguousMonomer extends BaseMonomer implements IVariantMonomer {
@@ -19,7 +23,10 @@ export class AmbiguousMonomer extends BaseMonomer implements IVariantMonomer {
     generateId = true,
   ) {
     const variantMonomerLabel =
-      variantMonomerItem.label || DEFAULT_VARIANT_MONOMER_LABEL;
+      variantMonomerItem.subtype === KetAmbiguousMonomerTemplateSubType.MIXTURE
+        ? DEFAULT_VARIANT_MONOMER_LABEL
+        : variantMonomerItem.label || DEFAULT_VARIANT_MONOMER_LABEL;
+
     super(
       {
         label: variantMonomerLabel,

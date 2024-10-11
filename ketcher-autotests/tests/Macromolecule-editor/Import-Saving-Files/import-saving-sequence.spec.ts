23bc694a17b17bc9c1371844dac19537d2e01275
Alexey Girin <agirin@mail.ru>
2024-10-11 10:58:46 +0300

    Autotests: #5558 - add ambiguous monomers to the library and allow their addition to the canvas (#5611)
    * Autotests: #5558 - Add ambiguous monomers to the library and allow their addition to the canvas - Cases 1-6
    
    * Autotests: #5558 - Add ambiguous monomers to the library and allow their addition to the canvas - Case 7
    
    * Autotests: #5558 - Add ambiguous monomers to the library and allow their addition to the canvas - Case 8
    
    * Autotests: #5558 - Add ambiguous monomers to the library and allow their addition to the canvas - Case 9 - Bases
    
    * Autotests: #5558 - Add ambiguous monomers to the library and allow their addition to the canvas - Case 9 - Nucleotides
    
    * Autotests: #5558 - Add ambiguous monomers to the library and allow their addition to the canvas - Case 9 - RNA
    
    * Autotests: #5558 - Add ambiguous monomers to the library and allow their addition to the canvas - Case 9 - CHEMs
    
    * Autotests: #5558 - Add ambiguous monomers to the library and allow their addition to the canvas - Case 9 - Peptides
    
    * Autotests: #5558 - Add ambiguous monomers to the library and allow their addition to the canvas - Case 9 - phosphate
    
    * Autotests: #5558 - Add ambiguous monomers to the library and allow their addition to the canvas - Case 9 - sugars
    
    * Autotests: #5558 - Add ambiguous monomers to the library and allow their addition to the canvas - Case  10-11
    
    * Autotests: #5558 - Add ambiguous monomers to the library and allow their addition to the canvas - Case  12
    
    * Autotests: #5558 - Add ambiguous monomers to the library and allow their addition to the canvas - Case  12(2)
    
    * Autotests: #5558 - Add ambiguous monomers to the library and allow their addition to the canvas - Case  13
    
    * Autotests: #5558 - Add ambiguous monomers to the library and allow their addition to the canvas - Case  15(1-5)
    
    * Autotests: #5558 - Add ambiguous monomers to the library and allow their addition to the canvas - Case  16(1-5)
    
    * Autotests: #5558 - Add ambiguous monomers to the library and allow their addition to the canvas - Case  16(1-5) - update
    
    * Autotests: #5558 - Add ambiguous monomers to the library and allow their addition to the canvas - Case  17(1-5)
    
    * Autotests: #5558 - Add ambiguous monomers to the library and allow their addition to the canvas - Case  17(1-5) - 2
    
    * Sequence tests update
    
    * FASTA tests update
    
    * Autotests: #5558 - Add ambiguous monomers to the library and allow their addition to the canvas - Case  17(1-10) - Export
    
    * Autotests: #5558 - Add ambiguous monomers to the library and allow their addition to the canvas - Case  17(11-12) and more
    
    * Autotests: #5558 - Add ambiguous monomers to the library and allow their addition to the canvas - Import ambiguous monomers from FASTA
    
    * Autotests: #5558 - Export to SVG of ambiguous monomers
    
    * Autotests: #5558 - Export to SVG of ambiguous monomers 2
    
    * Some fixes
    
    * More fixes
    
    * Autotests: #5558 - Add ambiguous monomers to the library and allow their addition to the canvas - Bug fixes
    
    * last fix
    
    * Autotests: #5558 - add ambiguous monomers to the library and allow their addition to the canvas
    
    * Last fix

--- ketcher-autotests/tests/Macromolecule-editor/Import-Saving-Files/import-saving-sequence.spec.ts
+++ ketcher-autotests/tests/Macromolecule-editor/Import-Saving-Files/import-saving-sequence.spec.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-magic-numbers */
 import { test, expect } from '@playwright/test';
 import {
   TopPanelButton,
@@ -15,19 +16,26 @@ import {
   readFileContents,
   getSequence,
   moveMouseAway,
+  openFileAndAddToCanvasAsNewProjectMacro,
+  TypeDropdownOptions,
 } from '@utils';
-import { turnOnMacromoleculesEditor } from '@utils/macromolecules';
+import { closeErrorMessage, pageReload } from '@utils/common/helpers';
+import {
+  turnOnMacromoleculesEditor,
+  zoomWithMouseWheel,
+} from '@utils/macromolecules';
+
+test.beforeEach(async ({ page }) => {
+  await waitForPageInit(page);
+  await turnOnMacromoleculesEditor(page);
+});
 
 test.describe('Import-Saving .seq Files', () => {
-  test.beforeEach(async ({ page }) => {
-    await waitForPageInit(page);
-    await turnOnMacromoleculesEditor(page);
-  });
-
   const sequenceFileTypes = ['DNA', 'RNA', 'Peptide'] as const;
 
   for (const fileType of sequenceFileTypes) {
     test(`Import .seq ${fileType} file`, async ({ page }) => {
+      await pageReload(page);
       await openFileAndAddToCanvasMacro(
         `Sequence/sequence-${fileType.toLowerCase()}.seq`,
         page,
@@ -238,20 +246,430 @@ test.describe('Import-Saving .seq Files', () => {
     },
   );
 
-  test(
-    'RNA and DNA structures not overlay each other on canvas, when adding them through the “Open as file”',
-    { tag: ['@IncorrectResultBecauseOfBug'] },
-    async ({ page }) => {
-      /*
+  test('RNA and DNA structures not overlay each other on canvas, when adding them through the “Open as file”', async ({
+    page,
+  }) => {
+    /*
     Test case: #4175
     Description: RNA and DNA structures not overlay each other on canvas, when adding them through the "Paste from Clipboard".
     The test doesn't work as it should because we have a bug https://github.com/epam/ketcher/issues/4175 For now structures overlap each other.
     When fix is made, you need to update screenshot.
     */
-      await openFileAndAddToCanvasMacro('Sequence/sequence-acgtu.seq', page);
-      // Need open twice
-      await openFileAndAddToCanvasMacro('Sequence/sequence-acgtu.seq', page);
+    await openFileAndAddToCanvasMacro('Sequence/sequence-acgtu.seq', page);
+    // Need open twice
+    await openFileAndAddToCanvasMacro('Sequence/sequence-acgtu.seq', page);
+    await takeEditorScreenshot(page);
+  });
+
+  test('Saving ambiguous peptides (with mapping, alternatives) in Sequence format', async ({
+    page,
+  }) => {
+    /*
+    Test task: https://github.com/epam/ketcher/issues/5558
+    Description: 15.1 Verify saving ambiguous peptides (with mapping, alternatives) in Sequence format (macro mode)
+    Case: 1. Load ambiguous peptides (that have mapping to library) from KET 
+          2. Take screenshot to make sure monomers on the canvas
+          3. Open Save dialog and choose Sequence option
+          4. Take screenshot to make sure export is correct
+    */
+    await openFileAndAddToCanvasAsNewProjectMacro(
+      'KET/Ambiguous-monomers/Peptides (that have mapping to library, alternatives).ket',
+      page,
+    );
+
+    await zoomWithMouseWheel(page, -600);
+    await moveMouseAway(page);
+    await takeEditorScreenshot(page);
+
+    await selectTopPanelButton(TopPanelButton.Save, page);
+    await chooseFileFormat(page, 'Sequence');
+    await takeEditorScreenshot(page);
+
+    await pressButton(page, 'Cancel');
+    await zoomWithMouseWheel(page, 600);
+  });
+
+  test(
+    'Saving ambiguous peptides (with mapping, mixed) in Sequence format',
+    { tag: ['@IncorrectResultBecauseOfBug'] },
+    async ({ page }) => {
+      /*
+    Test task: https://github.com/epam/ketcher/issues/5558
+    Description: 15.2 Verify saving ambiguous peptides (with mapping, mixed) in Sequence format (macro mode)
+    Case: 1. Load ambiguous peptides (that have mapping to library) from KET 
+          2. Take screenshot to make sure monomers on the canvas
+          3. Open Save dialog and choose Sequence option
+          4. Take screenshot to make sure export is correct
+    */
+      await openFileAndAddToCanvasAsNewProjectMacro(
+        'KET/Ambiguous-monomers/Peptides (that have mapping to library, mixed).ket',
+        page,
+      );
+
+      await zoomWithMouseWheel(page, -600);
+      await moveMouseAway(page);
       await takeEditorScreenshot(page);
+
+      await selectTopPanelButton(TopPanelButton.Save, page);
+      await chooseFileFormat(page, 'Sequence');
+      await takeEditorScreenshot(page);
+
+      test.fixme(
+        true,
+        `That test fails because of https://github.com/epam/Indigo/issues/2435 issue.`,
+      );
+
+      await closeErrorMessage(page);
+
+      await pressButton(page, 'Cancel');
+      await zoomWithMouseWheel(page, 600);
     },
   );
+
+  test('Saving ambiguous peptides (without mapping, alternatives) in Sequence format', async ({
+    page,
+  }) => {
+    /*
+    Test task: https://github.com/epam/ketcher/issues/5558
+    Description: 15.3 Verify saving ambiguous peptides (without mapping, alternatives) in Sequence format (macro mode)
+    Case: 1. Load ambiguous peptides (that have mapping to library) from KET 
+          2. Take screenshot to make sure monomers on the canvas
+          3. Open Save dialog and choose Sequence option
+            (Error should occure)
+          4. Take screenshot to make sure export is correct
+    */
+    await openFileAndAddToCanvasAsNewProjectMacro(
+      'KET/Ambiguous-monomers/Peptides (that have no mapping to library, alternatives).ket',
+      page,
+    );
+
+    await zoomWithMouseWheel(page, -200);
+    await moveMouseAway(page);
+    await takeEditorScreenshot(page);
+
+    await selectTopPanelButton(TopPanelButton.Save, page);
+    await chooseFileFormat(page, 'Sequence');
+    await takeEditorScreenshot(page);
+
+    await closeErrorMessage(page);
+
+    await pressButton(page, 'Cancel');
+    await zoomWithMouseWheel(page, 200);
+  });
+
+  test(
+    'Saving ambiguous peptides (without mapping, mixed) in Sequence format',
+    { tag: ['@IncorrectResultBecauseOfBug'] },
+    async ({ page }) => {
+      /*
+    Test task: https://github.com/epam/ketcher/issues/5558
+    Description: 15.4 Verify saving ambiguous peptides (without mapping, mixed) in Sequence format (macro mode)
+    Case: 1. Load ambiguous peptides (that have mapping to library, mixed) from KET 
+          2. Take screenshot to make sure monomers on the canvas
+          3. Open Save dialog and choose Sequence option
+            (Error should occure)
+          4. Take screenshot to make sure export is correct
+    */
+      await openFileAndAddToCanvasAsNewProjectMacro(
+        'KET/Ambiguous-monomers/Peptides (that have no mapping to library, mixed).ket',
+        page,
+      );
+
+      await zoomWithMouseWheel(page, -200);
+      await moveMouseAway(page);
+      await takeEditorScreenshot(page);
+
+      await selectTopPanelButton(TopPanelButton.Save, page);
+      await chooseFileFormat(page, 'Sequence');
+      await takeEditorScreenshot(page);
+
+      await closeErrorMessage(page);
+
+      await pressButton(page, 'Cancel');
+      await zoomWithMouseWheel(page, 200);
+      test.fixme(
+        true,
+        `That test fails because of https://github.com/epam/Indigo/issues/2435, https://github.com/epam/Indigo/issues/2436 issue.`,
+      );
+    },
+  );
+
+  test('Saving ambiguous DNA bases (with mapping, alternatives) in Sequence format', async ({
+    page,
+  }) => {
+    /*
+    Test task: https://github.com/epam/ketcher/issues/5558
+    Description: 15.5 Verify saving ambiguous DNA bases (with mapping, alternatives) in Sequence format (macro mode)
+    Case: 1. Load ambiguous bases (that have mapping to library) from KET 
+          2. Take screenshot to make sure monomers on the canvas
+          3. Open Save dialog and choose Sequence option
+          4. Take screenshot to make sure export is correct
+    */
+    await openFileAndAddToCanvasAsNewProjectMacro(
+      'KET/Ambiguous-monomers/Ambiguous DNA Bases (alternatives).ket',
+      page,
+    );
+
+    await zoomWithMouseWheel(page, -100);
+    await moveMouseAway(page);
+    await takeEditorScreenshot(page);
+
+    await selectTopPanelButton(TopPanelButton.Save, page);
+    await chooseFileFormat(page, 'Sequence');
+    await takeEditorScreenshot(page);
+
+    await pressButton(page, 'Cancel');
+    await zoomWithMouseWheel(page, 100);
+  });
+
+  test(
+    'Saving ambiguous DNA bases (with mapping, mixed) in Sequence format',
+    { tag: ['@IncorrectResultBecauseOfBug'] },
+    async ({ page }) => {
+      /*
+    Test task: https://github.com/epam/ketcher/issues/5558
+    Description: 15.6 Verify saving ambiguous DNA bases (with mapping, mixed) in Sequence format (macro mode)
+    Case: 1. Load ambiguous bases (that have mapping to library) from KET 
+          2. Take screenshot to make sure monomers on the canvas
+          3. Open Save dialog and choose Sequence option
+          4. Take screenshot to make sure export is correct
+    */
+      await openFileAndAddToCanvasAsNewProjectMacro(
+        'KET/Ambiguous-monomers/Ambiguous DNA Bases (mixed).ket',
+        page,
+      );
+
+      await zoomWithMouseWheel(page, -100);
+      await moveMouseAway(page);
+      await takeEditorScreenshot(page);
+
+      await selectTopPanelButton(TopPanelButton.Save, page);
+      await chooseFileFormat(page, 'Sequence');
+      await takeEditorScreenshot(page);
+
+      test.fixme(
+        true,
+        `That test fails because of https://github.com/epam/Indigo/issues/2435 issue.`,
+      );
+
+      await closeErrorMessage(page);
+
+      await pressButton(page, 'Cancel');
+      await zoomWithMouseWheel(page, 100);
+    },
+  );
+
+  test('Saving ambiguous RNA bases (with mapping, alternatives) in Sequence format', async ({
+    page,
+  }) => {
+    /*
+    Test task: https://github.com/epam/ketcher/issues/5558
+    Description: 15.7 Verify saving ambiguous RNA bases (with mapping, alternatives) in Sequence format (macro mode)
+    Case: 1. Load ambiguous bases (that have mapping to library) from KET 
+          2. Take screenshot to make sure monomers on the canvas
+          3. Open Save dialog and choose Sequence option
+          4. Take screenshot to make sure export is correct
+    */
+    await openFileAndAddToCanvasAsNewProjectMacro(
+      'KET/Ambiguous-monomers/Ambiguous RNA Bases (alternatives).ket',
+      page,
+    );
+
+    await zoomWithMouseWheel(page, -100);
+    await moveMouseAway(page);
+    await takeEditorScreenshot(page);
+
+    await selectTopPanelButton(TopPanelButton.Save, page);
+    await chooseFileFormat(page, 'Sequence');
+    await takeEditorScreenshot(page);
+
+    await pressButton(page, 'Cancel');
+    await zoomWithMouseWheel(page, 100);
+  });
+
+  test(
+    'Saving ambiguous RNA bases (with mapping, mixed) in Sequence format',
+    { tag: ['@IncorrectResultBecauseOfBug'] },
+    async ({ page }) => {
+      /*
+    Test task: https://github.com/epam/ketcher/issues/5558
+    Description: 15.8 Verify saving ambiguous RNA bases (with mapping, mixed) in Sequence format (macro mode)
+    Case: 1. Load ambiguous bases (that have mapping to library) from KET 
+          2. Take screenshot to make sure monomers on the canvas
+          3. Open Save dialog and choose Sequence option
+          4. Take screenshot to make sure export is correct
+    */
+      await openFileAndAddToCanvasAsNewProjectMacro(
+        'KET/Ambiguous-monomers/Ambiguous RNA Bases (mixed).ket',
+        page,
+      );
+
+      await zoomWithMouseWheel(page, -100);
+      await moveMouseAway(page);
+      await takeEditorScreenshot(page);
+
+      await selectTopPanelButton(TopPanelButton.Save, page);
+      await chooseFileFormat(page, 'Sequence');
+      await takeEditorScreenshot(page);
+
+      test.fixme(
+        true,
+        `That test fails because of https://github.com/epam/Indigo/issues/2435 issue.`,
+      );
+
+      await closeErrorMessage(page);
+
+      await pressButton(page, 'Cancel');
+      await zoomWithMouseWheel(page, 100);
+    },
+  );
+
+  test('Saving ambiguous (common) bases (with mapping, alternatives) in Sequence format', async ({
+    page,
+  }) => {
+    /*
+    Test task: https://github.com/epam/ketcher/issues/5558
+    Description: 15.9 Verify saving ambiguous (common) bases (with mapping, alternatives) in Sequence format (macro mode)
+    Case: 1. Load ambiguous bases (that have mapping to library) from KET 
+          2. Take screenshot to make sure monomers on the canvas
+          3. Open Save dialog and choose Sequence option
+          4. Take screenshot to make sure export is correct
+    */
+    await openFileAndAddToCanvasAsNewProjectMacro(
+      'KET/Ambiguous-monomers/Ambiguous (common) Bases (alternatives).ket',
+      page,
+    );
+
+    await zoomWithMouseWheel(page, -200);
+    await moveMouseAway(page);
+    await takeEditorScreenshot(page);
+
+    await selectTopPanelButton(TopPanelButton.Save, page);
+    await chooseFileFormat(page, 'Sequence');
+    await takeEditorScreenshot(page);
+
+    await pressButton(page, 'Cancel');
+    await zoomWithMouseWheel(page, 200);
+  });
+
+  test(
+    'Saving ambiguous (common) bases (with mapping, mixed) in Sequence format',
+    { tag: ['@IncorrectResultBecauseOfBug'] },
+    async ({ page }) => {
+      /*
+    Test task: https://github.com/epam/ketcher/issues/5558
+    Description: 15.10 Verify saving ambiguous (common) bases (with mapping, mixed) in Sequence format (macro mode)
+    Case: 1. Load ambiguous bases (that have mapping to library) from KET 
+          2. Take screenshot to make sure monomers on the canvas
+          3. Open Save dialog and choose Sequence option
+          4. Take screenshot to make sure export is correct
+    */
+      await openFileAndAddToCanvasAsNewProjectMacro(
+        'KET/Ambiguous-monomers/Ambiguous (common) Bases (mixed).ket',
+        page,
+      );
+
+      await zoomWithMouseWheel(page, -200);
+      await moveMouseAway(page);
+      await takeEditorScreenshot(page);
+
+      await selectTopPanelButton(TopPanelButton.Save, page);
+      await chooseFileFormat(page, 'Sequence');
+      await takeEditorScreenshot(page);
+
+      test.fixme(
+        true,
+        `That test fails because of https://github.com/epam/Indigo/issues/2435 issue.`,
+      );
+
+      await closeErrorMessage(page);
+
+      await pressButton(page, 'Cancel');
+      await zoomWithMouseWheel(page, 200);
+    },
+  );
+});
+
+test.describe('Import correct Sequence file: ', () => {
+  interface ISequenceFile {
+    SequenceDescription: string;
+    SequenceFileName: string;
+    SequenceType: TypeDropdownOptions;
+    // Set shouldFail to true if you expect test to fail because of existed bug and put issues link to issueNumber
+    shouldFail?: boolean;
+    // issueNumber is mandatory if shouldFail === true
+    issueNumber?: string;
+    // set pageReloadNeeded to true if you need to restart ketcher before test (f.ex. to restart font renderer)
+    pageReloadNeeded?: boolean;
+  }
+
+  const correctSequenceFiles: ISequenceFile[] = [
+    {
+      // Test task: https://github.com/epam/ketcher/issues/5558
+      SequenceDescription: '1. Ambiguous (common) Bases with DNA sugar',
+      SequenceFileName:
+        'Sequence/Ambiguous-Monomers/Ambiguous (common) Bases.seq',
+      SequenceType: 'DNA',
+      shouldFail: true,
+      issueNumber: 'wrong labels screenshots',
+    },
+    {
+      // Test task: https://github.com/epam/ketcher/issues/5558
+      SequenceDescription: '2. Ambiguous (common) Bases with RNA sugar',
+      SequenceFileName:
+        'Sequence/Ambiguous-Monomers/Ambiguous (common) Bases.seq',
+      SequenceType: 'RNA',
+      shouldFail: true,
+      issueNumber: "can't load in rc2",
+    },
+    {
+      // Test task: https://github.com/epam/ketcher/issues/5558
+      SequenceDescription: '3. Ambiguous DNA Bases',
+      SequenceFileName: 'Sequence/Ambiguous-Monomers/Ambiguous DNA Bases.seq',
+      SequenceType: 'DNA',
+      shouldFail: true,
+      issueNumber: 'wrong labels screenshots',
+    },
+    {
+      // Test task: https://github.com/epam/ketcher/issues/5558
+      SequenceDescription: '4. Ambiguous RNA Bases',
+      SequenceFileName: 'Sequence/Ambiguous-Monomers/Ambiguous RNA Bases.seq',
+      SequenceType: 'RNA',
+      shouldFail: true,
+      issueNumber: 'wrong labels screenshots',
+    },
+    {
+      // Test task: https://github.com/epam/ketcher/issues/5558
+      SequenceDescription: '5. Peptides (that have mapping to library)',
+      SequenceFileName:
+        'Sequence/Ambiguous-Monomers/Peptides (that have mapping to library).seq',
+      SequenceType: 'Peptide',
+      shouldFail: true,
+      issueNumber: 'wrong labels screenshots',
+    },
+  ];
+
+  for (const correctSequenceFile of correctSequenceFiles) {
+    test(`${correctSequenceFile.SequenceDescription}`, async ({ page }) => {
+      /*
+      Description: Verify import of Sequence files works correct
+      Case: 1. Load Sequence file 
+            2. Take screenshot to make sure import works correct
+      */
+      if (correctSequenceFile.pageReloadNeeded) await pageReload(page);
+      // Test should be skipped if related bug exists
+      test.fixme(
+        correctSequenceFile.shouldFail === true,
+        `That test fails because of ${correctSequenceFile.issueNumber} issue.`,
+      );
+
+      await openFileAndAddToCanvasAsNewProjectMacro(
+        correctSequenceFile.SequenceFileName,
+        page,
+        correctSequenceFile.SequenceType,
+      );
+
+      await takeEditorScreenshot(page);
+    });
+  }
 });

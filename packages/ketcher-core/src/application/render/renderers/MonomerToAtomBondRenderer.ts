1a727fea040f1a5815e2dc9fa3f6594344d37f85
Roman Rodionov <roman_rodionov@epam.com>
2024-10-16 18:15:32 +0200

    #5686 - Bonds between micro and macro structures couldn't be selected and deleted in Macro mode (#5768)

--- packages/ketcher-core/src/application/render/renderers/MonomerToAtomBondRenderer.ts
+++ packages/ketcher-core/src/application/render/renderers/MonomerToAtomBondRenderer.ts
@@ -4,6 +4,10 @@ import { Scale } from 'domain/helpers';
 import { MonomerToAtomBond } from 'domain/entities/MonomerToAtomBond';
 
 export class MonomerToAtomBondRenderer extends BaseRenderer {
+  private selectionElement:
+    | D3SvgElementSelection<SVGLineElement, void>
+    | undefined;
+
   constructor(public monomerToAtomBond: MonomerToAtomBond) {
     super(monomerToAtomBond);
     this.monomerToAtomBond.setRenderer(this);
@@ -29,10 +33,11 @@ export class MonomerToAtomBondRenderer extends BaseRenderer {
   show() {
     this.rootElement = this.canvas
       .insert('g', `.monomer`)
+      .data([this])
       .attr(
         'transform',
         `translate(${this.scaledPosition.startPosition.x}, ${this.scaledPosition.startPosition.y})`,
-      );
+      ) as never as D3SvgElementSelection<SVGGElement, void>;
 
     this.rootElement
       ?.append('line')
@@ -48,15 +53,60 @@ export class MonomerToAtomBondRenderer extends BaseRenderer {
       )
       .attr('stroke', '#333333')
       .attr('stroke-width', 1);
+    this.appendHover();
   }
 
   protected appendHover(): D3SvgElementSelection<SVGUseElement, void> | void {
-    return undefined;
+    this.rootElement
+      ?.append('line')
+      .attr('x1', 0)
+      .attr('y1', 0)
+      .attr(
+        'x2',
+        this.scaledPosition.endPosition.x - this.scaledPosition.startPosition.x,
+      )
+      .attr(
+        'y2',
+        this.scaledPosition.endPosition.y - this.scaledPosition.startPosition.y,
+      )
+      .attr('stroke', 'transparent')
+      .attr('stroke-width', 10);
   }
 
   protected appendHoverAreaElement(): void {}
 
-  drawSelection(): void {}
+  public drawSelection() {
+    if (!this.rootElement) {
+      return;
+    }
+    if (this.monomerToAtomBond.selected) {
+      this.appendSelection();
+    } else {
+      this.removeSelection();
+    }
+  }
+
+  public appendSelection(): void {
+    this.selectionElement = this.rootElement
+      ?.insert('line', ':first-child')
+      .attr('x1', 0)
+      .attr('y1', 0)
+      .attr(
+        'x2',
+        this.scaledPosition.endPosition.x - this.scaledPosition.startPosition.x,
+      )
+      .attr(
+        'y2',
+        this.scaledPosition.endPosition.y - this.scaledPosition.startPosition.y,
+      )
+      .attr('stroke', '#57ff8f')
+      .attr('stroke-width', 10);
+  }
+
+  public removeSelection() {
+    this.selectionElement?.remove();
+    this.selectionElement = undefined;
+  }
 
   public move() {
     if (!this.rootElement) {
@@ -67,7 +117,10 @@ export class MonomerToAtomBondRenderer extends BaseRenderer {
     this.show();
   }
 
-  protected removeHover(): void {}
+  protected removeHover(): void {
+    this.hoverElement?.remove();
+    this.hoverElement = undefined;
+  }
 
   public moveSelection(): void {}
 }

4df07f04b3e77fb5b2d7961a8e33bc426cfaf94a
Nikita Chistousov <nikita_chistousov@epam.com>
2024-10-18 03:43:48 +0200

    #5579 – Introduce Hand tool for macromolecules mode

---
+++ packages/ketcher-core/src/application/editor/tools/Hand.ts
@@ -0,0 +1,38 @@
+import { BaseTool, CoreEditor, ZoomTool } from 'application/editor';
+import {
+  HandBase64Image,
+  HandDraggingBase64Image,
+} from 'application/editor/constants';
+import { drag, DragBehavior, select } from 'd3';
+
+export class Hand implements BaseTool {
+  private readonly dragBehavior: DragBehavior<SVGSVGElement, unknown, unknown>;
+
+  constructor(private editor: CoreEditor) {
+    this.editor.canvas.style.cursor = `url(${HandBase64Image}), auto`;
+
+    this.dragBehavior = drag<SVGSVGElement, unknown, unknown>()
+      .on('start', this.handleDragStart.bind(this))
+      .on('drag', this.handleDragging.bind(this))
+      .on('end', this.handleDragEnd.bind(this));
+
+    select(this.editor.canvas).call(this.dragBehavior);
+  }
+
+  private handleDragStart() {
+    this.editor.canvas.style.cursor = `url(${HandDraggingBase64Image}), auto`;
+  }
+
+  private handleDragging(event: any) {
+    ZoomTool.instance.scrollBy(event.dx, event.dy);
+  }
+
+  private handleDragEnd() {
+    this.editor.canvas.style.cursor = `url(${HandBase64Image}), auto`;
+  }
+
+  destroy() {
+    this.editor.canvas.style.cursor = 'auto';
+    select(this.editor.canvas).on('.drag', null);
+  }
+}

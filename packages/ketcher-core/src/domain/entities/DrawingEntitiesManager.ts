169f10c8282e7c2a0ae1f4d1f0b2320571bed5cc
Roman Rodionov <roman_rodionov@epam.com>
2024-10-21 12:29:16 +0200

    #5686 - Bonds between micro and macro structures couldn't be selected and deleted in Macro mode (#5785)

--- packages/ketcher-core/src/domain/entities/DrawingEntitiesManager.ts
+++ packages/ketcher-core/src/domain/entities/DrawingEntitiesManager.ts
@@ -2237,6 +2237,7 @@ export class DrawingEntitiesManager {
   ) {
     if (_monomerToAtomBond) {
       this.monomerToAtomBonds.set(_monomerToAtomBond.id, _monomerToAtomBond);
+      monomer.setBond(attachmentPoint, _monomerToAtomBond);
 
       return _monomerToAtomBond;
     }

8b798361c3230f7a99d9c5b993e73a873d9acb0e
Andrey Menshikov <andrey.menshikov@bostongene.com>
2024-11-11 14:45:10 +0400

    Revert specific files to previous state

--- ketcher-autotests/tests/Structure-Creating-&-Editing/Actions-With-Structures/Selection-Tool/bond-properties.spec.ts
+++ ketcher-autotests/tests/Structure-Creating-&-Editing/Actions-With-Structures/Selection-Tool/bond-properties.spec.ts
@@ -52,15 +52,15 @@ test.describe('Bond Properties', () => {
   }) => {
     /*
         Test case: EPMLSOPKET-1458(1)
-        Description: 'Type' field contain the correct bond type: if you click the single bond,
-        the field should contain 'Single',
+        Description: 'Type' field contain the correct bond type: if you click the single bond, 
+        the field should contain 'Single', 
         if you click the double bond, the field should contain 'Double', etc.
-        The drop-down list contains all possible bond types (simple bonds, stereobonds, query bonds):
-        Single, Single Up, Single Down, Single Up/Down, Double, Double Cis/Trans, Triple, Aromatic,
+        The drop-down list contains all possible bond types (simple bonds, stereobonds, query bonds): 
+        Single, Single Up, Single Down, Single Up/Down, Double, Double Cis/Trans, Triple, Aromatic, 
         Any, Single/Double, Single/Aromatic, Double/Aromatic.
         User is able to select any bond type.
         It is possible to navigate through the list with the Arrow Down / Up keyboard buttons.
-        User is able to select the Type by typing the A, D, S, T letters
+        User is able to select the Type by typing the A, D, S, T letters 
         (first letters of the bond types in the list).
       */
     await openFileAndAddToCanvas('Molfiles-V2000/benzene.mol', page);
@@ -82,15 +82,15 @@ test.describe('Bond Properties', () => {
   test('`Type` field: verification - 2/3 navigate with s', async ({ page }) => {
     /*
         Test case: EPMLSOPKET-1458(2)
-        Description: 'Type' field contain the correct bond type: if you click the single bond,
-        the field should contain 'Single',
+        Description: 'Type' field contain the correct bond type: if you click the single bond, 
+        the field should contain 'Single', 
         if you click the double bond, the field should contain 'Double', etc.
-        The drop-down list contains all possible bond types (simple bonds, stereobonds, query bonds):
-        Single, Single Up, Single Down, Single Up/Down, Double, Double Cis/Trans, Triple, Aromatic,
+        The drop-down list contains all possible bond types (simple bonds, stereobonds, query bonds): 
+        Single, Single Up, Single Down, Single Up/Down, Double, Double Cis/Trans, Triple, Aromatic, 
         Any, Single/Double, Single/Aromatic, Double/Aromatic.
         User is able to select any bond type.
         It is possible to navigate through the list with the Arrow Down / Up keyboard buttons.
-        User is able to select the Type by typing the A, D, S, T letters
+        User is able to select the Type by typing the A, D, S, T letters 
         (first letters of the bond types in the list).
       */
     await openFileAndAddToCanvas('Molfiles-V2000/benzene.mol', page);
@@ -107,15 +107,15 @@ test.describe('Bond Properties', () => {
   test('`Type` field: verification - 3/3 navigate with t', async ({ page }) => {
     /*
         Test case: EPMLSOPKET-1458(2)
-        Description: 'Type' field contain the correct bond type: if you click the single bond,
-        the field should contain 'Single',
+        Description: 'Type' field contain the correct bond type: if you click the single bond, 
+        the field should contain 'Single', 
         if you click the double bond, the field should contain 'Double', etc.
-        The drop-down list contains all possible bond types (simple bonds, stereobonds, query bonds):
-        Single, Single Up, Single Down, Single Up/Down, Double, Double Cis/Trans, Triple, Aromatic,
+        The drop-down list contains all possible bond types (simple bonds, stereobonds, query bonds): 
+        Single, Single Up, Single Down, Single Up/Down, Double, Double Cis/Trans, Triple, Aromatic, 
         Any, Single/Double, Single/Aromatic, Double/Aromatic.
         User is able to select any bond type.
         It is possible to navigate through the list with the Arrow Down / Up keyboard buttons.
-        User is able to select the Type by typing the A, D, S, T letters
+        User is able to select the Type by typing the A, D, S, T letters 
         (first letters of the bond types in the list).
       */
     await openFileAndAddToCanvas('Molfiles-V2000/benzene.mol', page);
@@ -256,9 +256,9 @@ test.describe('Bond Properties', () => {
           Test case: EPMLSOPKET-1461(1)
           Description: Any bond can be marked with any bond topology mark selected in the 'Topology' list.
           The 'rng' or 'chn' mark appears on the selected bond.
-          If the 'Either' parameter is selected in the 'Topology' list,
+          If the 'Either' parameter is selected in the 'Topology' list, 
           no any mark
-           should appear on the unmarked bond or topology mark should be removed
+           should appear on the unmarked bond or topology mark should be removed 
           from the marked with 'rng' or 'chn' bond.
           *.mol file should be correctly opened, applied bond property should be correctly represented.
         */
@@ -374,12 +374,12 @@ test.describe('Bond Properties', () => {
           Made/broken - two lines (//);
           Order changes - one line ( / );
           Made/broken and changes - three lines (///).
-          If the 'Unmarked' parameter is selected in the 'Reacting Center' list,
+          If the 'Unmarked' parameter is selected in the 'Reacting Center' list, 
           no any mark appears on the unmarked bond or mark for reacting center is removed from the marked bond.
           *.rxn file is correctly opened in Ketcher, applied bond property is correctly represented.
           Note: in some applications the 'No center' parameter has the mark - dot, so you see that mark!
 
-          Now it doesn`t work because if we try to save bond with 'Not center' option,
+          Now it doesn`t work because if we try to save bond with 'Not center' option, 
           we can`t  open the file
         */
 
@@ -439,12 +439,12 @@ test.describe('Bond Properties', () => {
     /*
         Test case: EPMLSOPKET-2926
         Description: All structure is highlighted green color.
-        'Bond properties' window is opened.
+        'Bond properties' window is opened.  
         All selected bonds are changed with 'Double' bond.
-        'Bond properties' window is opened.
+        'Bond properties' window is opened. 
         All selected bonds are changed. Bonds that were not selected are not changed.
         Selected bonds are highlighted green color.
-        'Bond properties' window is opened.
+        'Bond properties' window is opened. 
         All selected bonds are changed. Bonds that were not selected are not changed.
         Selected bonds are highlighted green color.
  */
@@ -526,53 +526,54 @@ test.describe('Bond Properties', () => {
     await takeEditorScreenshot(page);
   });
 
-  test(`Different combinations - 2/3 open the saved .mol file`, async ({
-    page,
-  }) => {
-    /*
-     * IMPORTANT: Test fails because we have bug https://github.com/epam/Indigo/issues/2495
-     * Test case: EPMLSOPKET-1465
-     * Description: User is able to change the bond properties at a time.
-     * All selected properties are assigned to the selected bond.
-     * *.mol and *.rxn files correctly opened, applied atom property correctly represented.
-     */
+  test.fail(
+    `Different combinations - 2/3 open the saved .mol file`,
+    async ({ page }) => {
+      /*
+       * IMPORTANT: Test fails because we have bug https://github.com/epam/Indigo/issues/2495
+       * Test case: EPMLSOPKET-1465
+       * Description: User is able to change the bond properties at a time.
+       * All selected properties are assigned to the selected bond.
+       * *.mol and *.rxn files correctly opened, applied atom property correctly represented.
+       */
 
-    await openFileAndAddToCanvas(
-      'Molfiles-V2000/mol_1465_to_open-expected.mol',
-      page,
-    );
-    await doubleClickOnBond(page, BondType.SINGLE, 5);
-    await selectOption(page, 'Single', 'Single Up');
-    await selectOption(page, 'Either', 'Chain');
-    await selectOption(page, 'Unmarked', 'No change');
-    await pressButton(page, 'Apply');
-
-    await selectLeftPanelButton(LeftPanelButton.ArrowOpenAngleTool, page);
-    const { x, y } = await getCoordinatesOfTheMiddleOfTheScreen(page);
-    await page.mouse.move(x, y + 30);
-    dragMouseTo(x + 100, y + 100, page);
-    await selectLeftPanelButton(LeftPanelButton.RectangleSelection, page);
-
-    await selectRingButton(RingButton.Benzene, page);
-    await page.mouse.click(x + 150, y + 150);
-
-    const expectedFile = await getRxn(page, 'v2000');
-    await saveToFile('Rxn-V2000/rxn-1465-to-open-expected.rxn', expectedFile);
-
-    const METADATA_STRING_INDEX = [2, 7, 34];
-
-    const { fileExpected: rxnFileExpected, file: rxnFile } =
-      await receiveFileComparisonData({
+      await openFileAndAddToCanvas(
+        'Molfiles-V2000/mol_1465_to_open-expected.mol',
         page,
-        expectedFileName:
-          'tests/test-data/Rxn-V2000/rxn-1465-to-open-expected.rxn',
-        metaDataIndexes: METADATA_STRING_INDEX,
-        fileFormat: 'v2000',
-      });
+      );
+      await doubleClickOnBond(page, BondType.SINGLE, 5);
+      await selectOption(page, 'Single', 'Single Up');
+      await selectOption(page, 'Either', 'Chain');
+      await selectOption(page, 'Unmarked', 'No change');
+      await pressButton(page, 'Apply');
 
-    expect(rxnFile).toEqual(rxnFileExpected);
-    await takeEditorScreenshot(page);
-  });
+      await selectLeftPanelButton(LeftPanelButton.ArrowOpenAngleTool, page);
+      const { x, y } = await getCoordinatesOfTheMiddleOfTheScreen(page);
+      await page.mouse.move(x, y + 30);
+      dragMouseTo(x + 100, y + 100, page);
+      await selectLeftPanelButton(LeftPanelButton.RectangleSelection, page);
+
+      await selectRingButton(RingButton.Benzene, page);
+      await page.mouse.click(x + 150, y + 150);
+
+      const expectedFile = await getRxn(page, 'v2000');
+      await saveToFile('Rxn-V2000/rxn-1465-to-open-expected.rxn', expectedFile);
+
+      const METADATA_STRING_INDEX = [2, 7, 34];
+
+      const { fileExpected: rxnFileExpected, file: rxnFile } =
+        await receiveFileComparisonData({
+          page,
+          expectedFileName:
+            'tests/test-data/Rxn-V2000/rxn-1465-to-open-expected.rxn',
+          metaDataIndexes: METADATA_STRING_INDEX,
+          fileFormat: 'v2000',
+        });
+
+      expect(rxnFile).toEqual(rxnFileExpected);
+      await takeEditorScreenshot(page);
+    },
+  );
 
   test(`Different combinations - 3/3 open the saved *.rxn and edit it`, async ({
     page,

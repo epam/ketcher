2ec52164924d1f6225ffd7a5f037ca5e10624175
Alexey Girin <agirin@mail.ru>
2024-11-26 13:43:09 +0300

    Autotests: #6003 - autotests allow import and export of sequences with three letter amino acid codes (#6010)
    * First draft
    
    * First draft
    
    * Verify import with valid three-letter codes in the sequence
    
    * Verify that spaces separate different amino acid sequences in import
    
    * Verify ignoring of line breaks during import
    
    * 4. Verify error message for unsupported symbols in import
    5. Verify error for incorrect formatting in import (uppercase/lowercase mix)
    6. Verify error when a three-letter sequence cannot be matched to amino acids(e.g. "Alx")
    
    * 9. Verify export with non-standard ambiguous amino acids
    10. Verify that non pure peptide sequences are prevented from exporting
    
    * 11. Verify natural analogue conversion on export
    12. Verify specific export format for "Any amino acid
    13. Verify export format for ambiguous amino acids
    14. Verify that multiple sequences are separated by space in exported file
    
    * Bug fix
    
    * Fixes

--- ketcher-autotests/tests/utils/canvas/types.ts
+++ ketcher-autotests/tests/utils/canvas/types.ts
@@ -86,3 +86,17 @@ export enum SequenceType {
   DNA = 'DNA',
   PEPTIDE = 'PEPTIDE',
 }
+
+export enum PeptideType {
+  oneLetterCode = '1-letter code',
+  threeLetterCode = '3-letter code',
+}
+
+export enum MacroFileType {
+  Ket = 'Ket',
+  MOLv3000 = 'MDL Molfile V3000',
+  Sequence = 'Sequence',
+  FASTA = 'FASTA',
+  IDT = 'IDT',
+  HELM = 'HELM',
+}

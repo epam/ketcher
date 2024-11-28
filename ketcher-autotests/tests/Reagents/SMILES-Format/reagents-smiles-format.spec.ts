24bf4db984fc046155101f9d99443221a370e83b
Alexey Girin <agirin@mail.ru>
2024-11-28 10:50:53 +0300

    more reagents-smiles-format.spec.ts updates

--- ketcher-autotests/tests/Reagents/SMILES-Format/reagents-smiles-format.spec.ts
+++ ketcher-autotests/tests/Reagents/SMILES-Format/reagents-smiles-format.spec.ts
@@ -90,6 +90,7 @@ test.describe('Reagents SMILES format', () => {
     expect(smiFile).toEqual(smiFileExpected);
 
     await getPreviewForSmiles(page, 'Daylight SMILES');
+    await moveMouseAway(page);
     await takeEditorScreenshot(page);
   });
 

67636e3a64ee3291fe861928896ff1d40a602398
Alexey Girin <agirin@mail.ru>
2024-12-02 23:02:15 +0300

    lasso-tool.spec.ts updated 2

--- ketcher-autotests/tests/Structure-Creating-&-Editing/Actions-With-Structures/Selection-Tool/Lasso-Selection-Tool/lasso-tool.spec.ts
+++ ketcher-autotests/tests/Structure-Creating-&-Editing/Actions-With-Structures/Selection-Tool/Lasso-Selection-Tool/lasso-tool.spec.ts
@@ -218,7 +218,7 @@ test.describe('Lasso Selection tool', () => {
      * Description: Undo/Redo should work correctly for the actions for the selected objects.
      */
     const randomCoords = { x: 20, y: 20 };
-    const shiftCoords = { x: 50, y: 50 };
+    const shiftCoords = { x: 70, y: 50 };
     const centerPoint = await getCoordinatesOfTheMiddleOfTheScreen(page);
     await openFileAndAddToCanvas('Rxn-V2000/benzene-chain-reaction.rxn', page);
     await selectNestedTool(page, SelectTool.LASSO_SELECTION);

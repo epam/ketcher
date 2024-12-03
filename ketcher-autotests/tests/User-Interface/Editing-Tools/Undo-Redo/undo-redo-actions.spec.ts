0fa3963a8b16d26bc418f452ae9021e79c79c6c3
Alexey Girin <agirin@mail.ru>
2024-12-03 18:05:19 +0300

    First Draft

--- ketcher-autotests/tests/User-Interface/Editing-Tools/Undo-Redo/undo-redo-actions.spec.ts
+++ ketcher-autotests/tests/User-Interface/Editing-Tools/Undo-Redo/undo-redo-actions.spec.ts
@@ -37,6 +37,8 @@ import {
   pasteFromClipboardByKeyboard,
   selectAllStructuresOnCanvas,
   clickOnCanvas,
+  selectUndoByKeyboard,
+  selectRedoByKeyboard,
 } from '@utils';
 import { getAtomByIndex } from '@utils/canvas/atoms';
 import {
@@ -632,11 +634,11 @@ test.describe('Undo/Redo Actions', () => {
     await page.getByLabel(AttachmentPoint.SECONDARY).check();
     await pressButton(page, 'Apply');
     for (let i = 0; i < 2; i++) {
-      await page.keyboard.press('Control+z');
+      await selectUndoByKeyboard(page);
     }
     await takeEditorScreenshot(page);
     for (let i = 0; i < 2; i++) {
-      await page.keyboard.press('Control+Shift+z');
+      await selectRedoByKeyboard(page);
     }
     await takeEditorScreenshot(page);
   });
@@ -656,11 +658,11 @@ test.describe('Undo/Redo Actions', () => {
       await page.keyboard.press('Control+_');
     }
     for (let i = 0; i < 2; i++) {
-      await page.keyboard.press('Control+z');
+      await selectUndoByKeyboard(page);
     }
     await takeEditorScreenshot(page);
     for (let i = 0; i < 2; i++) {
-      await page.keyboard.press('Control+Shift+z');
+      await selectRedoByKeyboard(page);
     }
     await takeEditorScreenshot(page);
     for (let i = 0; i < 5; i++) {
@@ -734,7 +736,7 @@ test.describe('Undo/Redo Actions', () => {
     await clickOnAtom(page, 'C', 2);
     await page.getByTestId('canvas').hover();
     await takeEditorScreenshot(page);
-    await page.keyboard.press('Control+z');
+    await selectUndoByKeyboard(page);
     await takeEditorScreenshot(page);
   });
 
@@ -757,7 +759,7 @@ test.describe('Undo/Redo Actions', () => {
     await selectAllStructuresOnCanvas(page);
     await copyToClipboardByKeyboard(page);
     await pasteFromClipboardByKeyboard(page);
-    await page.keyboard.press('Control+z');
-    await page.keyboard.press('Control+z');
+    await selectUndoByKeyboard(page);
+    await selectUndoByKeyboard(page);
   });
 });

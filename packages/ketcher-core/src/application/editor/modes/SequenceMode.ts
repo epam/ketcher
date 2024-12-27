e90ccc1a7ad055201e71f0f0357c5a5a08eef373
Igor Kostrubin <kostrubin@bk.ru>
2024-12-27 19:53:34 +0100

    #4526 - In the Text-editing mode, the canvas is not moved to make the newly added sequence visible

--- packages/ketcher-core/src/application/editor/modes/SequenceMode.ts
+++ packages/ketcher-core/src/application/editor/modes/SequenceMode.ts
@@ -128,6 +128,16 @@ export class SequenceMode extends BaseMode {
       zoom.scrollTo(firstMonomerPosition);
     }
 
+    const drawnStructuresElement = document.querySelector('.drawn-structures');
+    const isScrollToTheBottomNeeded =
+      drawnStructuresElement &&
+      drawnStructuresElement.getBoundingClientRect().bottom >
+        window.innerHeight;
+
+    if (isScrollToTheBottomNeeded) {
+      zoom.scrollToVerticalBottom();
+    }
+
     modelChanges.merge(command);
 
     return modelChanges;

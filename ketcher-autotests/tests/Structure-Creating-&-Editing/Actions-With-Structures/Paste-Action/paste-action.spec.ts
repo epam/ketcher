caf34d812f3c283dbeed55360137665ccbf44e79
Mikhail Zhirnov <62464241+Zhirnoff@users.noreply.github.com>
2025-02-05 09:38:06 +0300

    Autotests: #6363 - replace file comparison for rxn files only operations with valid helper functions verifyfileexport (#6414)
    * Refactor autotests to use verifyFileExport helper and update expected output files for RXN format
    
    * Add test for pasting canvas as MOL format in Micro mode

--- ketcher-autotests/tests/Structure-Creating-&-Editing/Actions-With-Structures/Paste-Action/paste-action.spec.ts
+++ ketcher-autotests/tests/Structure-Creating-&-Editing/Actions-With-Structures/Paste-Action/paste-action.spec.ts
@@ -5,6 +5,10 @@ import {
   selectAction,
   waitForPageInit,
   selectAllStructuresOnCanvas,
+  drawBenzeneRing,
+  copyToClipboardByKeyboard,
+  selectTopPanelButton,
+  pasteFromClipboardByKeyboard,
 } from '@utils';
 import { TopPanelButton } from '@utils/selectors';
 
@@ -20,6 +24,27 @@ test.describe('Paste Tool', () => {
     await selectAction(TopPanelButton.Copy, page);
     await selectAction(TopPanelButton.Paste, page);
     await page.getByTestId('infoModal-shortcut-for-paste').first().isVisible();
+    await takeEditorScreenshot(page);
+  });
+
+  test('Canvas in Micro mode copied as MOL', async ({ page }) => {
+    /*
+    Test case: https://github.com/epam/ketcher/issues/6411
+    Description: Canvas copied as MOL
+    Case:
+    1. Add Benzene ring to the canvas
+    2. Copy by CTRL+C
+    3. Open Paste from Canvas tool and paste by CTRL+V
+    4. Check that the structure of file is MOL
+    For now test working not as expected, because of the issue https://github.com/epam/ketcher/issues/6411
+    After fixing the issue, the test should be updated.
+    */
+    await drawBenzeneRing(page);
+    await selectAllStructuresOnCanvas(page);
+    await copyToClipboardByKeyboard(page);
+    await selectTopPanelButton(TopPanelButton.Open, page);
+    await page.getByText('Paste from clipboard').click();
+    await pasteFromClipboardByKeyboard(page);
     await takeEditorScreenshot(page);
   });
 });

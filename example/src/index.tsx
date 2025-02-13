67ba2ac57f3f0da47db4ebf68b6ec79f3241f26f
Aleksandr Tanas <tanas80@gmail.com>
2025-02-13 14:21:44 +0400

    Fix example enabling routes for popup and duo for vite.

--- example/src/index.tsx
+++ example/src/index.tsx
@@ -3,8 +3,20 @@ import 'react-app-polyfill/stable';
 import 'url-search-params-polyfill';
 import './index.css';
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 import App from './App';
+import DuoApp from './DuoApp';
+import PopupApp from './PopupApp';
 
 const container = document.getElementById('root');
 const root = createRoot(container as HTMLElement);
-root.render(<App />);
+root.render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/duo" element={<DuoApp />} />
+      <Route path="/popup" element={<PopupApp />} />
+      <Route path="*" element={<App />} />
+    </Routes>
+  </BrowserRouter>,
+);

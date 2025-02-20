7ae9a72d1dc6a80368c433d5a98362e19c9707f9
Nikita Chistousov <nikita_chistousov@epam.com>
2025-02-20 22:06:31 +0100

    #6215 – Transient view and manager implemented

--- packages/ketcher-core/src/application/render/renderers/TransientDrawingView.ts
+++ packages/ketcher-core/src/application/render/renderers/TransientDrawingView.ts
@@ -1,11 +1,55 @@
 import { BondSnapView } from 'application/render/renderers/BondSnapView';
 import { PolymerBond } from 'domain/entities';
+import { D3SvgElementSelection } from 'application/render/types';
+import { ZoomTool } from 'application/editor';
+import { select } from 'd3';
+import { drawnStructuresSelector } from 'application/editor/constants';
+
+type ViewData<P> = {
+  show: (layer: D3SvgElementSelection<SVGGElement, void>, params: P) => void;
+  params: P;
+};
 
 export class TransientDrawingView {
-  constructor() {}
+  private views: Map<string, ViewData<unknown>> = new Map();
 
-  public showBondSnapping(bond: PolymerBond) {
-    const bondSnap = new BondSnapView(bond);
-    bondSnap.show();
+  private readonly layer: D3SvgElementSelection<SVGGElement, void>;
+
+  constructor() {
+    const canvas = ZoomTool.instance?.canvas || select(drawnStructuresSelector);
+    this.layer = canvas.append('g');
+  }
+
+  private addView<P>(viewName, viewData: ViewData<P>) {
+    this.views.set(viewName, viewData);
+  }
+
+  private removeView(viewName: string) {
+    this.views.delete(viewName);
+    this.update();
+  }
+
+  public showBondSnap(bond: PolymerBond) {
+    this.addView(BondSnapView.viewName, {
+      show: BondSnapView.show,
+      params: bond,
+    });
+  }
+
+  public hideBondSnap() {
+    this.removeView(BondSnapView.viewName);
+  }
+
+  public clear() {
+    this.views.clear();
+    this.update();
+  }
+
+  public update() {
+    this.layer.selectAll('*').remove();
+    this.views.forEach((viewData) => {
+      viewData.show(this.layer, viewData.params);
+    });
+    this.layer.raise();
   }
 }
--- packages/ketcher-core/src/application/render/renderers/TransientView.ts
+++ packages/ketcher-core/src/application/render/renderers/TransientView.ts
@@ -1,20 +1,12 @@
 import { D3SvgElementSelection } from 'application/render/types';
-import ZoomTool from '../../editor/tools/Zoom';
-import { select } from 'd3';
-import { drawnStructuresSelector } from 'application/editor/constants';
 
 export abstract class TransientView {
-  protected rootElement?: D3SvgElementSelection<SVGGElement, void>;
+  public static viewName: string;
 
-  protected canvas: D3SvgElementSelection<SVGSVGElement, void>;
-
-  constructor() {
-    this.canvas = ZoomTool.instance?.canvas || select(drawnStructuresSelector);
-  }
-
-  public abstract show(): void;
-  public remove() {
-    this.rootElement?.remove();
-    this.rootElement = undefined;
+  public static show<P>(
+    _transientLayer: D3SvgElementSelection<SVGGElement, void>,
+    _params: P,
+  ) {
+    throw new Error('Method is not implemented for an abstract class');
   }
 }

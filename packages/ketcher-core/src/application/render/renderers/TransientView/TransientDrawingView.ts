4c58753b091b840758c002f691016904e2b8d4db
Nikita Chistousov <nikita_chistousov@epam.com>
2025-02-25 20:12:01 +0100

    #6215 – Finalized UX

--- packages/ketcher-core/src/application/render/renderers/TransientView/TransientDrawingView.ts
+++ packages/ketcher-core/src/application/render/renderers/TransientView/TransientDrawingView.ts
@@ -2,23 +2,33 @@ import { select } from 'd3';
 import { D3SvgElementSelection } from 'application/render/types';
 import { ZoomTool } from 'application/editor';
 import { drawnStructuresSelector } from 'application/editor/constants';
-import { PolymerBond, Vec2 } from 'domain/entities';
+import { PolymerBond } from 'domain/entities';
 import { BondSnapView } from './BondSnapView';
-import { AngleSnapView } from 'application/render/renderers/TransientView/AngleSnapView';
+import { AngleSnapView, AngleSnapViewParams } from './AngleSnapView';
+import { BaseMonomerRenderer } from 'application/render';
 
 type ViewData<P> = {
   show: (layer: D3SvgElementSelection<SVGGElement, void>, params: P) => void;
   params: P;
+  onShow?: VoidFunction;
+  onHide?: VoidFunction;
+  topLayer?: boolean;
 };
 
 export class TransientDrawingView {
   private views: Map<string, ViewData<unknown>> = new Map();
 
-  private readonly layer: D3SvgElementSelection<SVGGElement, void>;
+  private readonly topLayer: D3SvgElementSelection<SVGGElement, void>;
+  private readonly defaultLayer: D3SvgElementSelection<SVGGElement, void>;
 
   constructor() {
     const canvas = ZoomTool.instance?.canvas || select(drawnStructuresSelector);
-    this.layer = canvas.append('g');
+    this.topLayer = canvas
+      .append('g')
+      .attr('class', 'transient-views-top-layer');
+    this.defaultLayer = canvas
+      .append('g')
+      .attr('class', 'transient-views-layer');
   }
 
   private addView<P>(viewName, viewData: ViewData<P>) {
@@ -28,14 +38,45 @@ export class TransientDrawingView {
   }
 
   private removeView(viewName: string) {
+    const viewData = this.views.get(viewName);
+    if (viewData?.onHide) {
+      viewData.onHide();
+    }
+
     this.views.delete(viewName);
-    this.update();
   }
 
   public showBondSnap(bond: PolymerBond) {
     this.addView(BondSnapView.viewName, {
       show: BondSnapView.show,
       params: bond,
+      topLayer: true,
+      onShow: () => {
+        bond.renderer?.setVisibility(false);
+        if (bond.firstMonomer.renderer) {
+          (
+            bond.firstMonomer.renderer as BaseMonomerRenderer
+          ).setLabelVisibility(false);
+        }
+        if (bond.secondMonomer?.renderer) {
+          (
+            bond.secondMonomer.renderer as BaseMonomerRenderer
+          ).setLabelVisibility(false);
+        }
+      },
+      onHide: () => {
+        bond.renderer?.setVisibility(false);
+        if (bond.firstMonomer.renderer) {
+          (
+            bond.firstMonomer.renderer as BaseMonomerRenderer
+          ).setLabelVisibility(true);
+        }
+        if (bond.secondMonomer?.renderer) {
+          (
+            bond.secondMonomer.renderer as BaseMonomerRenderer
+          ).setLabelVisibility(true);
+        }
+      },
     });
   }
 
@@ -43,10 +84,18 @@ export class TransientDrawingView {
     this.removeView(BondSnapView.viewName);
   }
 
-  public showAngleSnap(connectedPosition: Vec2, movingPosition: Vec2) {
+  public showAngleSnap(params: AngleSnapViewParams) {
+    const { polymerBond } = params;
+
     this.addView(AngleSnapView.viewName, {
       show: AngleSnapView.show,
-      params: { connectedPosition, movingPosition },
+      params,
+      onShow: () => {
+        polymerBond.renderer?.setVisibility(false);
+      },
+      onHide: () => {
+        polymerBond.renderer?.setVisibility(true);
+      },
     });
   }
 
@@ -55,15 +104,22 @@ export class TransientDrawingView {
   }
 
   public clear() {
-    this.views.clear();
+    this.views.forEach((_, viewName) => this.removeView(viewName));
     this.update();
   }
 
   public update() {
-    this.layer.selectAll('*').remove();
+    this.topLayer.selectAll('*').remove();
+    this.defaultLayer.selectAll('*').remove();
+
     this.views.forEach((viewData) => {
-      viewData.show(this.layer, viewData.params);
+      viewData.show(
+        viewData.topLayer ? this.topLayer : this.defaultLayer,
+        viewData.params,
+      );
+      viewData?.onShow?.();
     });
-    this.layer.raise();
+
+    this.topLayer.raise();
   }
 }

72edac12c26c75a9bec0cfc798d7946a33ad4c74
Alexey Girin <agirin@mail.ru>
2025-03-13 10:55:10 +0300

    Done 2

--- ketcher-autotests/playwright.config.ts
+++ ketcher-autotests/playwright.config.ts
@@ -77,6 +77,7 @@ const config: PlaywrightTestConfig = {
     {
       // former chromium-ReactMUI857x648
       name: 'chromium-popup',
+      grep: /chromium-popup/,
       use: {
         ...devices['Desktop Chrome'],
         baseURL: baseURLPopup(),

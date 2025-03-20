c36c88a6e3be6a3734714bdaee614a9f49a7bbf6
Nikita Chistousov <nikita_chistousov@epam.com>
2025-03-20 16:58:47 +0100

    #6519 – Add fader on monomers list scrolling, fix unit tests, add test-id for new tabs

--- packages/ketcher-macromolecules/src/components/shared/Tabs/Tabs.types.ts
+++ packages/ketcher-macromolecules/src/components/shared/Tabs/Tabs.types.ts
@@ -3,6 +3,7 @@ import { FC } from 'react';
 export type TabPanelData = {
   caption: string;
   tooltip?: string;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   component: FC<any>;
   testId: string;
   props?: Record<string, unknown>;

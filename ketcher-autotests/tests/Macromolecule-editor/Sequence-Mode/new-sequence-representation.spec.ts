4e72ef76f6c27bd69777fe5eca391c1dc494ce71
Alexey Girin <agirin@mail.ru>
2025-03-23 19:52:31 +0300

    Check that "Establish Hydrogen Bonds" is disabled if any of the symbols (the one that was clicked on, or the one opposite it) is the @ symbol representing multiple monomers ( Requirement: 1.1 )

--- ketcher-autotests/tests/Macromolecule-editor/Sequence-Mode/new-sequence-representation.spec.ts
+++ ketcher-autotests/tests/Macromolecule-editor/Sequence-Mode/new-sequence-representation.spec.ts
@@ -18,7 +18,6 @@ import {
   SequenceModeType,
   SymbolType,
   takeEditorScreenshot,
-  takeElementScreenshot,
   waitForPageInit,
   waitForRender,
 } from '@utils';
@@ -2219,6 +2218,16 @@ const emptySequence: IMonomerForHydrogenBondTest = {
   Description: 'Empty sequence',
 };
 
+const symbolRepresentingMultipleMonomers: IMonomerForHydrogenBondTest = {
+  Id: 1,
+  ContentType: MacroFileType.HELM,
+  SenseForm:
+    'RNA1{[Sm6fhn]([c7io7n])}|CHEM1{[4aPEGMal]}|CHEM2{[4FB]}$RNA1,CHEM1,1:R2-1:R1|CHEM1,CHEM2,1:R2-1:R1$$$V2.0',
+  AntiSenseForm:
+    'RNA1{[Sm6fhn]([c7io7n])}|CHEM1{[4aPEGMal]}|CHEM2{[4FB]}$RNA1,CHEM1,1:R1-1:R2|CHEM1,CHEM2,1:R1-1:R2$$$V2.0',
+  Description: 'Empty sequence',
+};
+
 async function setupSenseAndAntiSenseSequences(
   page: Page,
   senseSequence: IMonomerForHydrogenBondTest,
@@ -2393,7 +2402,7 @@ for (const senseSequence of sequencesForHydrogenBondTests) {
   for (const antisenseSequence of sequencesForHydrogenBondTests) {
     test(`Case 12-${senseSequence.Id}-${antisenseSequence.Id}. [${senseSequence.Description}]---[${antisenseSequence.Description}] Establish/Delete Hydrogen Bonds checks for multipal monomers`, async () => {
       /*
-       * Test case: https://github.com/epam/ketcher/issues/6722 - Test case 1
+       * Test case: https://github.com/epam/ketcher/issues/6722 - Test case 2
        * Description: 1. Check if multiple monomers/symbols are selected and at least one of them does not have hydrogen bonds established with the monomer/symbol above/bellow it the option "Establish Hydrogen Bonds" available from the r-click menu ( Requirement: 1.3 )
        *              2. Check if multiple monomers/symbols are selected and all symbols have H-bonds established with the monomer/symbol above/bellow it the option "Establish Hydrogen Bonds" disabled from the r-click menu ( Requirement: 1.3 )
        *              3. Check If multiple monomers are selected, and any of them have hydrogen bonds, the option "Delete Hydrogen Bonds" available in the r-click menu ( Requirement: 1.4 )
@@ -2497,23 +2506,59 @@ for (const senseSequence of sequencesForHydrogenBondTests) {
 }
 
 for (const senseSequence of sequencesForHydrogenBondTests) {
-  test(`Case 13-${senseSequence.Id}. ${senseSequence.Description} Check if there is no monomer opposite it, the option "Establish Hydrogen Bonds" disabled`, async () => {
+  test(`Case 13-${senseSequence.Id}. ${senseSequence.Description} - Check if there is no monomer opposite it, the option "Establish Hydrogen Bonds" disabled`, async () => {
     /*
      * Test case: https://github.com/epam/ketcher/issues/6722 - Test case 3
      * Description: Check if there is no monomer opposite it, the option "Establish Hydrogen Bonds" disabled ( Requirement: 1.1 )
      * Scenario:
-     * 1. Load sense and antisense sequences (and connect their c7io7n bases with hydrogen bond to make sence/antisense sequence)
+     * 1. Load sense and empty antisense sequences (and connect their c7io7n bases with hydrogen bond to make sence/antisense sequence)
      * 2. Switch to Sequence mode
      * 3. Getting sense and antisense symbol ids and locators
-     * 4. Check that context menu has "Establish Hydrogen Bonds" option available and enabled ("Delete Hydrogen Bonds" option should be disabled)
-     * 5. Click "Establish Hydrogen Bonds" and check that hydrogen bond was established between sense and antisense symbols
-     * 6. If sense symbol is a RNA or DNA, check that all bases have hydrogen connection
-     * 7. Right click on the sense symbol and check that "Establish Hydrogen Bonds" option is disabled
-     * 8. Click on "Delete Hydrogen Bonds"
-     * 9. Check that hydrogen bond was removed
+     * 4. Open context menu for sense symbol and check that context menu has "Establish Hydrogen Bonds" option disabled
      */
 
     await setupSenseAndAntiSenseSequences(page, senseSequence, emptySequence);
+
+    await selectSequenceLayoutModeTool(page);
+
+    const senseSymbolId = await getSymbolLocator(page, {
+      hydrogenConnectionNumber: 0,
+      isAntisense: false,
+    })
+      .first()
+      .getAttribute('data-symbol-id');
+
+    const senseSymbol = await getSymbolLocator(page, {
+      symbolId: senseSymbolId || '',
+    }).first();
+
+    const establishHydrogenBondsOption = page
+      .getByTestId('establish_hydrogen_bond')
+      .first();
+
+    // Check if there is no monomer opposite it, the option "Establish Hydrogen Bonds" disabled ( Requirement: 1.1 )
+    await senseSymbol.click({ button: 'right', force: true });
+    await expect(establishHydrogenBondsOption).toBeDisabled();
+  });
+}
+
+for (const senseSequence of sequencesForHydrogenBondTests) {
+  test(`Case 14-${senseSequence.Id}. ${senseSequence.Description} - Check that "Establish Hydrogen Bonds" is disabled if any of the symbols (the one that was clicked on, or the one opposite it) is the @ symbol representing multiple monomers`, async () => {
+    /*
+     * Test case: https://github.com/epam/ketcher/issues/6722 - Test case 4
+     * Description: Check that "Establish Hydrogen Bonds" is disabled if any of the symbols (the one that was clicked on, or the one opposite it) is the @ symbol representing multiple monomers ( Requirement: 1.1 )
+     * Scenario:
+     * 1. Load sense and empty antisense sequences (and connect their c7io7n bases with hydrogen bond to make sence/antisense sequence)
+     * 2. Switch to Sequence mode
+     * 3. Getting sense and antisense symbol ids and locators
+     * 4. Open context menu for sense symbol and check that context menu has "Establish Hydrogen Bonds" option disabled
+     */
+
+    await setupSenseAndAntiSenseSequences(
+      page,
+      senseSequence,
+      symbolRepresentingMultipleMonomers,
+    );
 
     await selectSequenceLayoutModeTool(page);
 

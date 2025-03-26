4db13a537c01f14db786822e76cbdb92f9115b99
Nikita Chistousov <nikita_chistousov@epam.com>
2025-03-26 10:19:47 +0100

    #6519 – Adjust useIsCompact view return value

--- packages/ketcher-macromolecules/src/hooks/useIsCompactView.ts
+++ packages/ketcher-macromolecules/src/hooks/useIsCompactView.ts
@@ -2,7 +2,7 @@ import { useContext } from 'react';
 import { RootSizeContext } from '../contexts';
 
 export const useIsCompactView = () => {
-  const { width, height } = useContext(RootSizeContext);
+  const { height } = useContext(RootSizeContext);
 
-  return height < 768 || width < 1024;
+  return height < 720;
 };

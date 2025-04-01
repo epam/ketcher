4de4e118fcebda844dc5476e55ee5e5246321312
Alexey Girin <agirin@mail.ru>
2025-04-01 21:11:37 +0300

    First try

--- ketcher-autotests/tests/utils/files/readFile.ts
+++ ketcher-autotests/tests/utils/files/readFile.ts
@@ -16,6 +16,7 @@ import {
 } from '@utils';
 import { waitForLoad } from '@utils/common';
 import { getSmiles, getInchi } from '@utils/formats';
+import { selectOpenFileTool } from '@tests/pages/common/TopLeftToolbar';
 
 import { MolfileFormat } from 'ketcher-core';
 import { selectOpenFileTool } from '@tests/pages/common/TopLeftToolbar';

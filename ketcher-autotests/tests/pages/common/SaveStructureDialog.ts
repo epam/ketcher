00a814b3236b3e90337b0b02a0b467c691ce783f
Alexey Girin <agirin@mail.ru>
2025-04-28 11:04:25 +0300

    f6

--- ketcher-autotests/tests/pages/common/SaveStructureDialog.ts
+++ ketcher-autotests/tests/pages/common/SaveStructureDialog.ts
@@ -1,4 +1,4 @@
-import { type Page, expect } from '@playwright/test';
+import { type Page } from '@playwright/test';
 import { MoleculesFileFormatType } from '../constants/fileFormats/microFileFormats';
 import { MacromoleculesFileFormatType } from '../constants/fileFormats/macroFileFormats';
 import { delay } from '@utils/canvas';

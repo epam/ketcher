f85d006b86f5399405d1d8353a1f59f966c8559e
Roman Rodionov <roman_rodionov@epam.com>
2025-04-29 22:29:15 +0200

    - made nat analogue 3 letters field optional

--- packages/ketcher-core/src/domain/types/monomers.ts
+++ packages/ketcher-core/src/domain/types/monomers.ts
@@ -47,7 +47,7 @@ export type MonomerItemType = MonomerItemBase & {
   struct: Struct;
   props: {
     id?: string;
-    MonomerNaturalAnalogThreeLettersCode: string;
+    MonomerNaturalAnalogThreeLettersCode?: string;
     MonomerNaturalAnalogCode: string;
     MonomerName: string;
     MonomerFullName?: string;

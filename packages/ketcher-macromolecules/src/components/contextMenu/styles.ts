69b4577686807cdb708988dbf0e0e7cc30811e91
KristinaKasyanovskaya <kristina_kasyanovskaya@epam.com>
2025-06-13 11:54:01 +0500

    fix: fix styles

--- packages/ketcher-macromolecules/src/components/contextMenu/styles.ts
+++ packages/ketcher-macromolecules/src/components/contextMenu/styles.ts
@@ -8,6 +8,8 @@ export const StyledMenu = styled(Menu)`
   .contexify_itemContent {
     font-family: ${({ theme }) => theme.ketcher.font.family.inter};
     font-size: ${({ theme }) => theme.ketcher.font.size.regular};
+    display: flex;
+    align-items: center;
   }
   .contexify_separator {
     color: rgba(225, 229, 234, 1);
@@ -16,7 +18,10 @@ export const StyledMenu = styled(Menu)`
     opacity: 1;
     font-weight: bold;
   }
-
+  .context_menu-icon {
+    display: flex;
+    align-items: center;
+  }
   .context_menu-text {
     padding-top: 1px;
   }

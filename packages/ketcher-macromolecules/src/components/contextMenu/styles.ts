86509d44f0699e0404ed3ecd4dbf7d91fd176734
Roman Rodionov <roman_rodionov@epam.com>
2025-06-16 17:11:34 +0200

    - fixed styles

--- packages/ketcher-macromolecules/src/components/contextMenu/styles.ts
+++ packages/ketcher-macromolecules/src/components/contextMenu/styles.ts
@@ -5,29 +5,33 @@ export const StyledMenu = styled(Menu)`
   --contexify-activeItem-bgColor: rgba(243, 245, 247, 1);
   --contexify-menu-minWidth: 140px;
   --contexify-activeItem-color: rgba(51, 51, 51, 1);
+  --contexify-menu-padding: 4px;
+  --contexify-itemContent-padding: 6px 8px;
+  --contexify-separator-margin: 4px 0;
+  --contexify-separator-color: #e1e5ea;
   .contexify_itemContent {
     font-family: ${({ theme }) => theme.ketcher.font.family.inter};
     font-size: ${({ theme }) => theme.ketcher.font.size.regular};
-  }
-  .contexify_separator {
-    color: rgba(225, 229, 234, 1);
+    height: 28px;
   }
   .contexify_item-title {
     opacity: 1;
     font-weight: bold;
+    background: #e1e5ea;
+    margin: -4px 0 4px -4px;
+    width: calc(100% + 8px);
+    border-radius: 4px 4px 0 0;
   }
   .context_menu-icon {
-    display: inline-flex;
+    width: 16px;
+    height: 16px;
+    display: flex;
     align-items: center;
-    vertical-align: middle;
-  }
-  .context_menu-icon svg {
-    vertical-align: middle;
+    margin-right: 4px;
   }
   .context_menu-text {
-    display: inline-flex;
+    display: flex;
     align-items: center;
-    padding-top: 1px;
-    vertical-align: middle;
+    line-height: ${({ theme }) => theme.ketcher.font.size.regular};
   }
 `;

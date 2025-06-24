b434bcfe0d4e7634254b6ec2709da3366f516162
Kira Andrianova <andrianovakira@gmail.com>
2025-06-24 15:38:41 +0200

    Create AttachmentPointsDialog page object

--- ketcher-autotests/tests/pages/molecules/canvas/AttachmentPointsDialog.ts
+++ ketcher-autotests/tests/pages/molecules/canvas/AttachmentPointsDialog.ts
@@ -1,1 +1,48 @@
-export {};
+import { Page, Locator } from '@playwright/test';
+
+type AttachmentPointsDialogLocators = {
+  closeWindowButton: Locator;
+  window: Locator;
+  primaryAttachmentPointCheckbox: Locator;
+  secondaryAttachmentPointCheckbox: Locator;
+  applyButton: Locator;
+  cancelButton: Locator;
+};
+
+export const AttachmentPointsDialog = (page: Page) => {
+  const locators: AttachmentPointsDialogLocators = {
+    closeWindowButton: page.getByTestId('close-window-button'),
+    window: page.getByTestId('attachmentPoints-dialog'),
+    primaryAttachmentPointCheckbox: page.getByTestId('primary-input'),
+    secondaryAttachmentPointCheckbox: page.getByTestId('secondary-input'),
+    applyButton: page.getByTestId('OK'),
+    cancelButton: page.getByTestId('Cancel'),
+  };
+  return {
+    ...locators,
+
+    async close() {
+      await locators.closeWindowButton.click();
+    },
+
+    async clickPrimaryAttachment() {
+      await locators.primaryAttachmentPointCheckbox.click();
+    },
+
+    async clickSecondaryAttachment() {
+      await locators.secondaryAttachmentPointCheckbox.click();
+    },
+
+    async apply() {
+      await locators.applyButton.click();
+    },
+
+    async cancel() {
+      await locators.cancelButton.click();
+    },
+  };
+};
+
+export type AttachmentPointsDialogType = ReturnType<
+  typeof AttachmentPointsDialog
+>;

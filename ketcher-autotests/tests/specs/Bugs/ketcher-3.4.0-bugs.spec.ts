f08e2b1050ecc90c9af3f8e0a485f022236e8256
Mikhail Zhirnov <zhirnoff85@gmail.com>
2025-06-25 16:58:42 +0300

    - add tests; - add test files; - add screenshots

--- ketcher-autotests/tests/specs/Bugs/ketcher-3.4.0-bugs.spec.ts
+++ ketcher-autotests/tests/specs/Bugs/ketcher-3.4.0-bugs.spec.ts
@@ -16,6 +16,9 @@ import {
   takeLeftToolbarMacromoleculeScreenshot,
   takeTopToolbarScreenshot,
   SdfFileFormat,
+  selectSequenceLayoutModeTool,
+  clickInTheMiddleOfTheScreen,
+  takePageScreenshot,
 } from '@utils';
 import { waitForPageInit } from '@utils/common';
 import { getSymbolLocator } from '@utils/macromolecules/monomer';
@@ -32,7 +35,7 @@ import {
 } from '@utils/files/receiveFileComparisonData';
 import { Library } from '@tests/pages/macromolecules/Library';
 import { ContextMenu } from '@tests/pages/common/ContextMenu';
-import { expandMonomers } from '@utils/canvas/monomer/helpers';
+import { expandMonomer, expandMonomers } from '@utils/canvas/monomer/helpers';
 import { Presets } from '@constants/monomers/Presets';
 import {
   switchToPeptideMode,
@@ -40,6 +43,11 @@ import {
 } from '@utils/macromolecules/sequence';
 import { SaveStructureDialog } from '@tests/pages/common/SaveStructureDialog';
 import { MacromoleculesFileFormatType } from '@tests/pages/constants/fileFormats/macroFileFormats';
+import {
+  COORDINATES_TO_PERFORM_ROTATION,
+  rotateToCoordinates,
+} from '../Structure-Creating-&-Editing/Actions-With-Structures/Rotation/utils';
+import { MoleculesFileFormatType } from '@tests/pages/constants/fileFormats/microFileFormats';
 
 let page: Page;
 
@@ -144,6 +152,7 @@ test.describe('Ketcher bugs in 3.4.0', () => {
      * 3. Select any Preset (U in my case)
      * 4. Press Enter key
      */
+    await selectSequenceLayoutModeTool(page);
     await Library(page).selectMonomer(Presets.U);
     await keyboardPressOnCanvas(page, 'Enter');
     await takeEditorScreenshot(page, {
@@ -315,5 +324,183 @@ test.describe('Ketcher bugs in 3.4.0', () => {
       hideMacromoleculeEditorScrollBars: true,
     });
     await SaveStructureDialog(page).cancel();
+  });
+
+  test('Case 12: DNA/RNA sequences should NOT accept * symbols', async () => {
+    /*
+     * Test case: https://github.com/epam/ketcher/issues/7243
+     * Bug: https://github.com/epam/ketcher/issues/4358
+     * Description: DNA/RNA sequences should NOT accept * symbols.
+     * Error message should appear: "*" symbol is not allowed for DNA sequence.
+     * Scenario:
+     * 1. Open Macro mode
+     * 2. Load by Paste from Clipboard as FASTA - RNA/DNA
+     */
+    await pasteFromClipboardAndAddToMacromoleculesCanvas(
+      page,
+      MacroFileType.FASTA,
+      'AAAA*AAAA',
+      true,
+    );
+    await takeEditorScreenshot(page, {
+      hideMonomerPreview: true,
+      hideMacromoleculeEditorScrollBars: true,
+    });
+  });
+
+  test('Case 13: System not replaces "Salts and Solvents" molecules with CH4 while loading if no mouse move and some other molecules present on the canvas', async () => {
+    /*
+     * Test case: https://github.com/epam/ketcher/issues/7243
+     * Bug: https://github.com/epam/ketcher/issues/3878
+     * Description: System not replaces "Salts and Solvents" molecules with CH4 while loading if no mouse move and some other molecules present on the canvas.
+     * Scenario:
+     * 1. Go to Micro mode
+     * 2. Load from file
+     */
+    await CommonTopRightToolbar(page).turnOnMicromoleculesEditor();
+    await openFileAndAddToCanvasAsNewProject(
+      page,
+      'Molfiles-V2000/Bugs/Source.mol',
+    );
+    await takeEditorScreenshot(page);
+  });
+
+  test('Case 14: When opening a macro file with 50 or more monomers in micro mode, application not freezes', async () => {
+    /*
+     * Test case: https://github.com/epam/ketcher/issues/7243
+     * Bug: https://github.com/epam/ketcher/issues/3907
+     * Description: When opening a macro file with 50 or more monomers in micro mode, application not freezes.
+     * Scenario:
+     * 1. Go to Micro mode
+     * 2. Load from file
+     * 3. Swtch to Macromolecules mode
+     * 4. Take screenshot
+     */
+    await CommonTopRightToolbar(page).turnOnMicromoleculesEditor();
+    await openFileAndAddToCanvasAsNewProject(
+      page,
+      'KET/Bugs/fifty-monomers.ket',
+    );
+    await takeEditorScreenshot(page);
+    await CommonTopRightToolbar(page).turnOnMacromoleculesEditor();
+    await takeEditorScreenshot(page, {
+      hideMonomerPreview: true,
+      hideMacromoleculeEditorScrollBars: true,
+    });
+  });
+
+  test('Case 15: When opening a macro file with 100 or more monomers in micro mode, application not freezes', async () => {
+    /*
+     * Test case: https://github.com/epam/ketcher/issues/7243
+     * Bug: https://github.com/epam/ketcher/issues/3907
+     * Description: When opening a macro file with 100 or more monomers in micro mode, application not freezes.
+     * Scenario:
+     * 1. Go to Micro mode
+     * 2. Load from file
+     * 3. Swtch to Macromolecules mode
+     * 4. Take screenshot
+     */
+    await CommonTopRightToolbar(page).turnOnMicromoleculesEditor();
+    await openFileAndAddToCanvasAsNewProject(
+      page,
+      'KET/Bugs/hundred-monomers.ket',
+    );
+    await takeEditorScreenshot(page);
+    await CommonTopRightToolbar(page).turnOnMacromoleculesEditor();
+    await takeEditorScreenshot(page, {
+      hideMonomerPreview: true,
+      hideMacromoleculeEditorScrollBars: true,
+    });
+  });
+
+  test('Case 16: It is not possible to expand ambiguous monomers on micromolecules canvas', async () => {
+    /*
+     * Test case: https://github.com/epam/ketcher/issues/7243
+     * Bug: https://github.com/epam/ketcher/issues/5789
+     * Description: It is not possible to expand ambiguous monomers on micromolecules canvas
+     * Scenario:
+     * 1. Open file in Micro mode
+     * 2. Select all structure
+     * 3. Expand it
+     */
+    await CommonTopRightToolbar(page).turnOnMicromoleculesEditor();
+    await openFileAndAddToCanvasAsNewProject(
+      page,
+      'KET/Bugs/1. Peptide X (ambiguouse, alternatives, from library).ket',
+    );
+    await takeEditorScreenshot(page);
+    const point = page.getByText('X');
+    await ContextMenu(page, point).open();
+    await takeEditorScreenshot(page);
+  });
+
+  test('Case 17: It is not possible to expand unknown nucleotide on micromolecules canvas', async () => {
+    /*
+     * Test case: https://github.com/epam/ketcher/issues/7243
+     * Bug: https://github.com/epam/ketcher/issues/5791
+     * Description: It is not possible to expand unknown nucleotide on micromolecules canvas
+     * Scenario:
+     * 1. Open file in Micro mode
+     * 2. Select all structure
+     * 3. Expand it
+     */
+    await CommonTopRightToolbar(page).turnOnMicromoleculesEditor();
+    await openFileAndAddToCanvasAsNewProject(
+      page,
+      'KET/Bugs/17. Unknown nucleotide.ket',
+    );
+    await takeEditorScreenshot(page);
+    const point = page.getByText('Unknown');
+    await ContextMenu(page, point).open();
+    await takeEditorScreenshot(page);
+  });
+
+  test('Case 18: Rotation is correct upon exporting transformed monomer to SVG or PNG', async () => {
+    /*
+     * Test case: https://github.com/epam/ketcher/issues/7243
+     * Bug: https://github.com/epam/ketcher/issues/7062
+     * Description: Rotation is correct upon exporting transformed monomer to SVG or PNG
+     * Scenario:
+     * 1. Open file in Micro mode
+     * 2. Select all structure
+     * 3. Rotate it
+     * 4. Export to SVG
+     * 5. Take screenshot
+     */
+    await CommonTopRightToolbar(page).turnOnMicromoleculesEditor();
+    await openFileAndAddToCanvasAsNewProject(
+      page,
+      'KET/Bugs/two-monomers-connected.ket',
+    );
+    await expandMonomer(page, page.getByText('Edc'));
+    await clickInTheMiddleOfTheScreen(page);
+    await selectAllStructuresOnCanvas(page);
+    await takeEditorScreenshot(page);
+    await rotateToCoordinates(page, COORDINATES_TO_PERFORM_ROTATION);
+    await takeEditorScreenshot(page);
+    await CommonTopLeftToolbar(page).saveFile();
+    await SaveStructureDialog(page).chooseFileFormat(
+      MoleculesFileFormatType.SVGDocument,
+    );
+    await takeEditorScreenshot(page);
+  });
+
+  test('Case 19: Unipositive ions default value is shown in mM for double-stranded sequence selection', async () => {
+    /*
+     * Test case: https://github.com/epam/ketcher/issues/7042
+     * Bug: https://github.com/epam/ketcher/issues/7034
+     * Description: Unipositive ions default value is shown in mM for double-stranded sequence selection.
+     * Scenario:
+     * 1. Go to Macro
+     * 2. Load from HELM
+     * 3. Open the "Calculate Properties" window
+     */
+    await pasteFromClipboardAndAddToMacromoleculesCanvas(
+      page,
+      MacroFileType.HELM,
+      'RNA1{[dR](A)P.[dR](A)P.[dR](A)P.[dR](A)P.[dR](A)P.[dR](A)P.[dR](A)P.[dR](A)P.[dR](A)P.[dR](A)P.[dR](A)P.[dR](A)P.[dR](A)}|RNA2{[dR](T)P.[dR](T)P.[dR](T)P.[dR](T)P.[dR](T)P.[dR](T)P.[dR](T)P.[dR](T)P.[dR](T)P.[dR](T)P.[dR](T)P.[dR](T)P.[dR](T)}$RNA1,RNA2,38:pair-2:pair|RNA1,RNA2,35:pair-5:pair|RNA1,RNA2,32:pair-8:pair|RNA1,RNA2,29:pair-11:pair|RNA1,RNA2,26:pair-14:pair|RNA1,RNA2,23:pair-17:pair|RNA1,RNA2,20:pair-20:pair|RNA1,RNA2,17:pair-23:pair|RNA1,RNA2,14:pair-26:pair|RNA1,RNA2,11:pair-29:pair|RNA1,RNA2,8:pair-32:pair|RNA1,RNA2,5:pair-35:pair|RNA1,RNA2,2:pair-38:pair$$$V2.0',
+    );
+    await CommonTopLeftToolbar(page).calculateProperties();
+    await takePageScreenshot(page);
   });
 });

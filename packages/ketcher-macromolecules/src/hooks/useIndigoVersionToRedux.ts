9a177cf7ec8757409b3b59748131d50ba88a43e3
KristinaKasyanovskaya <kristina_kasyanovskaya@epam.com>
2025-07-17 13:53:32 +0500

    refactor: move useIndigoVersionToRedux to hooks and use useAppSelector for consistency

--- packages/ketcher-macromolecules/src/hooks/useIndigoVersionToRedux.ts
+++ packages/ketcher-macromolecules/src/hooks/useIndigoVersionToRedux.ts
@@ -1,12 +1,12 @@
 import { useEffect } from 'react';
 import { IndigoProvider } from 'ketcher-react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
+import { useAppSelector } from './stateHooks';
 import { setAppMeta } from 'state/common/editorSlice';
-import type { RootState } from 'state';
 
 export function useIndigoVersionToRedux() {
   const dispatch = useDispatch();
-  const app = useSelector((state: RootState) => state.editor?.app || {});
+  const app = useAppSelector((state) => state.editor?.app || {});
 
   useEffect(() => {
     async function fetchIndigoInfo() {

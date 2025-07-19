a392a6269cbccc9889cfb489c9c1f9013613329b
KukushkinDmi1riy <777Dmitrij@gmail.com>
2025-07-19 13:03:06 +0500

    #7083 Delete operation on Sequence mode causes hanging bonds and undeletable monomers

--- packages/ketcher-core/src/application/editor/modes/SequenceMode.ts
+++ packages/ketcher-core/src/application/editor/modes/SequenceMode.ts
@@ -1237,20 +1237,23 @@ export class SequenceMode extends BaseMode {
       }
     };
 
+    const deleteHandler = (direction: Direction) => {
+      if (this.isEditInRNABuilderMode) return;
+      if (SequenceRenderer.selections.length > 0) {
+        this.deleteSelection();
+      } else {
+        deleteNode(direction);
+      }
+    };
+
     return {
       delete: {
         shortcut: ['Delete'],
-        handler: () => {
-          if (this.isEditInRNABuilderMode) return;
-          deleteNode(Direction.Right);
-        },
+        handler: () => deleteHandler(Direction.Right),
       },
       backspace: {
         shortcut: ['Backspace'],
-        handler: () => {
-          if (this.isEditInRNABuilderMode) return;
-          deleteNode(Direction.Left);
-        },
+        handler: () => deleteHandler(Direction.Left),
       },
       'turn-off-edit-mode': {
         shortcut: ['Escape'],

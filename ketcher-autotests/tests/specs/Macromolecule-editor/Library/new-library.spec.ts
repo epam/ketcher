f2bdc1e6cf5749956390be865cf314c656c98e18
Alexey Girin <agirin@mail.ru>
2025-07-21 11:49:24 +0300

    18

--- ketcher-autotests/tests/specs/Macromolecule-editor/Library/new-library.spec.ts
+++ ketcher-autotests/tests/specs/Macromolecule-editor/Library/new-library.spec.ts
@@ -53,6 +53,10 @@ import { SaveStructureDialog } from '@tests/pages/common/SaveStructureDialog';
 import { MoleculesFileFormatType } from '@tests/pages/constants/fileFormats/microFileFormats';
 import { MacromoleculesTopToolbar } from '@tests/pages/macromolecules/MacromoleculesTopToolbar';
 import { LayoutMode } from '@tests/pages/constants/macromoleculesTopToolbar/Constants';
+import {
+  getCoordinatesOfTheMiddleOfTheScreen,
+  moveMouseToTheMiddleOfTheScreen,
+} from '@utils/clicks';
 
 let page: Page;
 
@@ -1713,5 +1717,156 @@ for (const monomer of monomerToDrag) {
     await MacromoleculesTopToolbar(page).calculateProperties();
     await takePageScreenshot(page);
     await MacromoleculesTopToolbar(page).calculateProperties();
+  });
+}
+
+for (const monomer of monomerToDrag) {
+  test(`34. Check that drag and drop of ${monomer.alias} not working in Sequence mode and when user try to use it we have no errors and app crash`, async () => {
+    /*
+     *
+     * Test task: https://github.com/epam/ketcher/issues/7419
+     * Description: Check that drag and drop not working in Sequence mode and when user try to use it we have no errors and app crash
+     *
+     * Case:
+     * 1. Open Ketcher and turn on Macromolecules editor
+     * 2. Subscribe to console and page errors
+     * 3. Go to Sequence mode
+     * 4. Grab target monomer from library and drop it on the canvas
+     * 5. Validate that no errors in console and page errors
+     *
+     * Version 3.6
+     */
+    await MacromoleculesTopToolbar(page).selectLayoutModeTool(
+      LayoutMode.Sequence,
+    );
+    const consoleErrors: string[] = [];
+    page.on('console', (msg) => {
+      if (msg.type() === 'error') {
+        consoleErrors.push(msg.text());
+      }
+    });
+
+    const pageErrors: string[] = [];
+    page.on('pageerror', (error) => {
+      pageErrors.push(error.message);
+    });
+
+    await Library(page).dragMonomerOnCanvas(monomer, { x: 100, y: 100 });
+
+    const allErrors = [...consoleErrors, ...pageErrors];
+
+    test.fail(
+      allErrors.length > 0,
+      `Errors were caught during the test: ${allErrors.join(', ')}`,
+    );
+    expect(allErrors.length).toEqual(0);
+  });
+}
+
+for (const monomer of monomerToDrag) {
+  test(`35.1 Verify behavior when dragging of ${monomer.alias} outside the canvas area (Flex mode)`, async () => {
+    /*
+     *
+     * Test task: https://github.com/epam/ketcher/issues/7419
+     * Description: Verify behavior when dragging an element (Favoutites, RNA/DNA, Peptides, CHEM, Presets) outside the canvas area (Flex mode)
+     *
+     * Case:
+     * 1. Open Ketcher and turn on Macromolecules editor
+     * 2. Subscribe to console and page errors
+     * 3. Go to Flex mode
+     * 4. Grab target monomer from library and move it outside the canvas area
+     * 5. Validate that no errors in console and page errors
+     *
+     * Version 3.6
+     */
+    await MacromoleculesTopToolbar(page).selectLayoutModeTool(LayoutMode.Flex);
+    const consoleErrors: string[] = [];
+    page.on('console', (msg) => {
+      if (msg.type() === 'error') {
+        consoleErrors.push(msg.text());
+      }
+    });
+
+    const pageErrors: string[] = [];
+    page.on('pageerror', (error) => {
+      pageErrors.push(error.message);
+    });
+
+    const viewport = page.viewportSize();
+    if (!viewport) {
+      throw new Error('Viewport size is not set');
+    }
+    const { width, height } = viewport;
+    await Library(page).hoverMonomer(monomer);
+    await page.mouse.down();
+    await page.mouse.move(width / 2, 0);
+    await page.mouse.move(width / 2, 9999);
+    await page.mouse.move(0, height / 2);
+    await page.mouse.move(9999, height / 2);
+    await page.mouse.up();
+
+    await takeEditorScreenshot(page);
+
+    const allErrors = [...consoleErrors, ...pageErrors];
+
+    test.fail(
+      allErrors.length > 0,
+      `Errors were caught during the test: ${allErrors.join(', ')}`,
+    );
+    expect(allErrors.length).toEqual(0);
+  });
+}
+
+for (const monomer of monomerToDrag) {
+  test(`35.2 Verify behavior when dragging of ${monomer.alias} outside the canvas area (Snake mode)`, async () => {
+    /*
+     *
+     * Test task: https://github.com/epam/ketcher/issues/7419
+     * Description: Verify behavior when dragging an element (Favoutites, RNA/DNA, Peptides, CHEM, Presets) outside the canvas area (Snake mode)
+     *
+     * Case:
+     * 1. Open Ketcher and turn on Macromolecules editor
+     * 2. Subscribe to console and page errors
+     * 3. Go to Snake mode
+     * 4. Grab target monomer from library and move it outside the canvas area
+     * 5. Validate that no errors in console and page errors
+     *
+     * Version 3.6
+     */
+    await MacromoleculesTopToolbar(page).selectLayoutModeTool(LayoutMode.Snake);
+    const consoleErrors: string[] = [];
+    page.on('console', (msg) => {
+      if (msg.type() === 'error') {
+        consoleErrors.push(msg.text());
+      }
+    });
+
+    const pageErrors: string[] = [];
+    page.on('pageerror', (error) => {
+      pageErrors.push(error.message);
+    });
+
+    const viewport = page.viewportSize();
+    if (!viewport) {
+      throw new Error('Viewport size is not set');
+    }
+    const { width, height } = viewport;
+    await Library(page).hoverMonomer(monomer);
+    await page.mouse.down();
+    await page.mouse.move(width / 2, 0);
+    await page.mouse.move(width / 2, 9999);
+    await page.mouse.move(0, height / 2);
+    await page.mouse.move(9999, height / 2);
+    await page.mouse.up();
+
+    await takeEditorScreenshot(page);
+
+    const allErrors = [...consoleErrors, ...pageErrors];
+
+    test.fail(
+      allErrors.length > 0,
+      `Errors were caught during the test: ${allErrors.join(', ')}`,
+    );
+    expect(allErrors.length).toEqual(0);
   });
 }

228551992f2e05a8c13a4310903e30fd821f311b
Kristina <kristina_kasyanovskaya@epam.com>
2025-07-21 11:19:03 +0500

    #6270 - Add "About" (i) and "Help" (?) buttons to the main toolbar in macromolecules mode (#7420)
    * feat: Add About (i) and Help (?) buttons to the main toolbar in macromolecules mode
    
    * fix: remove data test id
    
    * test: update snapshots
    
    * refactor: move useIndigoVersionToRedux to editorSlice, update imports, and minor UI improvements
    
    * refactor: move useIndigoVersionToRedux to hooks and use useAppSelector for consistency
    
    * refactor: move useIndigoVersionToRedux to hooks and use useAppSelector for consistency
    
    * refactor: useAppSelector, selectAppMeta and useAppDispatch for consistent typed redux hooks
    
    * test: update snapshots
    
    * test:update snapshot
    
    * test:update snapshot
    
    * test: update snapshots
    
    * fix: small fix
    
    * test: update snapshot

---
+++ packages/ketcher-macromolecules/src/components/modal/About/About.styles.ts
@@ -0,0 +1,110 @@
+import styled from '@emotion/styled';
+
+export const About = styled('div')(({ theme }) => ({
+  width: '420px',
+  height: '260px',
+  padding: '0 22px',
+  borderRadius: '6px',
+  fontSize: '12px',
+  color: theme.ketcher.color.text.primary,
+  fontWeight: theme.ketcher?.font?.weight?.regular,
+
+  a: {
+    color: '#167782',
+  },
+
+  '.body': {
+    height: '174px',
+    borderRadius: '6px',
+    padding: '2px 72px',
+    overflowY: 'auto',
+    overflowX: 'hidden',
+
+    '.versionName': {
+      fontWeight: 400,
+      marginBottom: '2px',
+    },
+
+    '.firstline': {
+      display: 'inline-block',
+    },
+
+    '.links': {
+      textAlign: 'right',
+    },
+
+    '.indigoVersion': {
+      marginTop: '20px',
+      display: 'flex',
+      gap: '2px',
+    },
+  },
+
+  '.headerContent': {
+    display: 'flex',
+    justifyContent: 'space-between',
+    padding: '0 40px',
+
+    a: {
+      display: 'flex',
+      alignItems: 'center',
+      gap: '10px',
+      textDecoration: 'none',
+      color: theme.ketcher?.color?.text?.primary,
+    },
+
+    '.title': {
+      fontSize: '20px',
+      lineHeight: '22px',
+    },
+  },
+
+  '.indigoFirstLine': {
+    display: 'inline-block',
+  },
+
+  dd: {
+    margin: 0,
+    marginBottom: '0.2em',
+  },
+
+  dt: {
+    marginTop: '3px',
+  },
+
+  '.okButton': {
+    border: '1px solid #333333',
+    backgroundColor: '#FFFFFF',
+    color: '#333333',
+    display: 'inline-flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    outline: 'none',
+    minWidth: '70px',
+    lineHeight: '14px',
+    height: '24px',
+    borderRadius: '4px',
+    fontSize: '12px',
+    '&:hover': {
+      color: '#333333',
+      border: '1px solid  #333333',
+    },
+    '&:active': {
+      color: '#333333',
+      border: '1px solid #333333',
+    },
+    '&:disabled': {
+      color: 'rgba(51, 51, 51, 0.7)',
+      border: '1px solid rgba(51, 51, 51, 0.7)',
+    },
+  },
+
+  '.aboutFooter': {
+    borderTop: '1px solid #e1e5ea',
+    margin: 0,
+    padding: '20px',
+    display: 'flex',
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+}));

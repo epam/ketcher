ddd3b5170dc4131dc9c51b5d1e57daaa11e7bddb
Mikhail Zhirnov <zhirnoff85@gmail.com>
2025-07-30 15:51:12 +0300

    - add tests; - add test files; - add screenshots

--- ketcher-autotests/tests/specs/Bugs/ketcher-3.6.0-bugs.spec.ts
+++ ketcher-autotests/tests/specs/Bugs/ketcher-3.6.0-bugs.spec.ts
@@ -20,6 +20,7 @@ import {
   readFileContent,
   clickOnCanvas,
   MolFileFormat,
+  takePageScreenshot,
 } from '@utils';
 import { waitForPageInit, waitForSpinnerFinishedWork } from '@utils/common';
 import {
@@ -30,7 +31,6 @@ import { processResetToDefaultState } from '@utils/testAnnotations/resetToDefaul
 import { CommonTopLeftToolbar } from '@tests/pages/common/CommonTopLeftToolbar';
 import { CommonTopRightToolbar } from '@tests/pages/common/CommonTopRightToolbar';
 import { MacromoleculesTopToolbar } from '@tests/pages/macromolecules/MacromoleculesTopToolbar';
-import { LayoutMode } from '@tests/pages/constants/macromoleculesTopToolbar/Constants';
 import { CommonLeftToolbar } from '@tests/pages/common/CommonLeftToolbar';
 import { MacroBondType } from '@tests/pages/constants/bondSelectionTool/Constants';
 import { ContextMenu } from '@tests/pages/common/ContextMenu';
@@ -46,6 +46,10 @@ import {
 } from '@utils/files/receiveFileComparisonData';
 import { CalculateVariablesPanel } from '@tests/pages/macromolecules/CalculateVariablesPanel';
 import { OpenPPTXFileDialog } from '@tests/pages/molecules/OpenPPTXFileDialog';
+import { closeErrorAndInfoModals } from '@utils/common/helpers';
+import { SaveStructureDialog } from '@tests/pages/common/SaveStructureDialog';
+import { MacromoleculesFileFormatType } from '@tests/pages/constants/fileFormats/macroFileFormats';
+import { MolecularMassUnit } from '@tests/pages/constants/calculateVariablesPanel/Constants';
 
 async function connectMonomerToAtom(page: Page) {
   await getMonomerLocator(page, Peptides.A).hover();
@@ -163,10 +167,9 @@ test.describe('Ketcher bugs in 3.6.0', () => {
      * 4. Connect the bond to the micro molecule
      */
     await CommonTopRightToolbar(page).turnOnMacromoleculesEditor({
-      enableFlexMode: false,
+      enableFlexMode: true,
       goToPeptides: false,
     });
-    await MacromoleculesTopToolbar(page).selectLayoutModeTool(LayoutMode.Flex);
     await openFileAndAddToCanvasAsNewProjectMacro(
       page,
       'KET/Bugs/Unable to connect monomer to molecule in snake mode.ket',
@@ -190,10 +193,9 @@ test.describe('Ketcher bugs in 3.6.0', () => {
      * 3. Open the context menu for the monomer
      */
     await CommonTopRightToolbar(page).turnOnMacromoleculesEditor({
-      enableFlexMode: false,
+      enableFlexMode: true,
       goToPeptides: false,
     });
-    await MacromoleculesTopToolbar(page).selectLayoutModeTool(LayoutMode.Flex);
     await pasteFromClipboardAndAddToMacromoleculesCanvas(
       page,
       MacroFileType.HELM,
@@ -219,10 +221,9 @@ test.describe('Ketcher bugs in 3.6.0', () => {
      * 3. Establish a hydrogen bond between two monomers
      */
     await CommonTopRightToolbar(page).turnOnMacromoleculesEditor({
-      enableFlexMode: false,
+      enableFlexMode: true,
       goToPeptides: false,
     });
-    await MacromoleculesTopToolbar(page).selectLayoutModeTool(LayoutMode.Flex);
     await openFileAndAddToCanvasAsNewProjectMacro(
       page,
       'KET/Bugs/Unable to create bond Uncaught RangeError Maximum call stack size exceeded.ket',
@@ -279,10 +280,9 @@ test.describe('Ketcher bugs in 3.6.0', () => {
      * 5. Move the mouse cursor away from the monomer
      */
     await CommonTopRightToolbar(page).turnOnMacromoleculesEditor({
-      enableFlexMode: false,
+      enableFlexMode: true,
       goToPeptides: false,
     });
-    await MacromoleculesTopToolbar(page).selectLayoutModeTool(LayoutMode.Flex);
     await pasteFromClipboardAndAddToMacromoleculesCanvas(
       page,
       MacroFileType.HELM,
@@ -566,7 +566,6 @@ test.describe('Ketcher bugs in 3.6.0', () => {
     const fileContent = await readFileContent(
       'Molfiles-V3000/Bugs/monomer-with-unique-structure.mol',
     );
-    await MacromoleculesTopToolbar(page).selectLayoutModeTool(LayoutMode.Flex);
     await pasteFromClipboardAndAddToMacromoleculesCanvas(
       page,
       MacroFileType.MOLv3000,
@@ -591,5 +590,235 @@ test.describe('Ketcher bugs in 3.6.0', () => {
     await getMonomerLocator(page, Peptides.Cys_Bn).nth(1).hover();
     await waitForMonomerPreview(page);
     await takeEditorScreenshot(page);
+  });
+
+  test('Case 16: System calculate melting temperature for mix of nucleotides/nucleosides and unsplit nucleotides/unsplit nucleosides', async () => {
+    /*
+     * Test case: https://github.com/epam/ketcher/issues/7454
+     * Bug: https://github.com/epam/Indigo/issues/2936
+     * Description: System calculate melting temperature for mix of nucleotides/nucleosides and unsplit nucleotides/unsplit nucleosides.
+     * Scenario:
+     * 1. Go to Macro
+     * 2. Load from HELM
+     * 3. Open the "Calculate Properties" window
+     * 4. Check Melting Temperature
+     */
+    await CommonTopRightToolbar(page).turnOnMacromoleculesEditor({
+      enableFlexMode: true,
+      goToPeptides: false,
+    });
+    await pasteFromClipboardAndAddToMacromoleculesCanvas(
+      page,
+      MacroFileType.HELM,
+      'RNA1{[2-damdA]}|RNA2{R(G)P}|RNA3{[5Br-dU].R(C)}$RNA2,RNA1,3:R2-1:R1|RNA1,RNA3,1:pair-1:pair|RNA2,RNA3,2:pair-3:pair$$$V2.0',
+    );
+    await MacromoleculesTopToolbar(page).calculateProperties();
+    expect(
+      await CalculateVariablesPanel(page).getMeltingTemperatureValue(),
+    ).toEqual('-1.3');
+  });
+
+  test('Case 17: System not consider closing bracket as part of name', async () => {
+    /*
+     * Test case: https://github.com/epam/ketcher/issues/7454
+     * Bug: https://github.com/epam/Indigo/issues/2993
+     * Description: System not consider closing bracket as part of name.
+     * System throws error message: Convert error! Given string could not be loaded as
+     * (query or plain) molecule or reaction, see the error messages: 'SEQUENCE loader: emsc'
+     * Scenario:
+     * 1. Go to Macro
+     * 2. Load from HELM
+     * 3. Take screenshot
+     */
+    const helmCases = [
+      'PEPTIDE1{1Nal]}$$$$V2.0',
+      'RNA1{5S6Rm5](mo6pur)sP-}$$$$V2.0',
+      'RNA1{5S6Rm5(mo6pur])sP-}$$$$V2.0',
+      'RNA1{5S6Rm5(mo6pur)sP-]}$$$$V2.0',
+      'RNA1{2-damdA]}$$$$V2.0',
+      'CHEM1{4aPEGMal]}$$$$V2.0',
+    ];
+    for (const helm of helmCases) {
+      await CommonTopRightToolbar(page).turnOnMacromoleculesEditor({
+        enableFlexMode: true,
+        goToPeptides: false,
+      });
+      await pasteFromClipboardAndAddToMacromoleculesCanvas(
+        page,
+        MacroFileType.HELM,
+        helm,
+      );
+      await takeEditorScreenshot(page, {
+        hideMonomerPreview: true,
+        hideMacromoleculeEditorScrollBars: true,
+      });
+      await closeErrorAndInfoModals(page);
+    }
+  });
+
+  test('Case 18: Input fields for ion concentration and oligonucleotides not become inactive after entering excessively long number', async () => {
+    /*
+     * Test case: https://github.com/epam/ketcher/issues/7454
+     * Bug: https://github.com/epam/Indigo/issues/2998
+     * Description: Input fields for ion concentration and oligonucleotides not become inactive after entering excessively long number.
+     * Scenario:
+     * 1. Go to Macro
+     * 2. Load from HELM
+     * 3. Open the "Calculate Properties" window
+     * 4. Enter excessively long number in the ion concentration field
+     * 5. Enter excessively long number in the oligonucleotides field
+     * 6. Verify that the input fields
+     */
+    await CommonTopRightToolbar(page).turnOnMacromoleculesEditor({
+      enableFlexMode: true,
+      goToPeptides: false,
+    });
+    await pasteFromClipboardAndAddToMacromoleculesCanvas(
+      page,
+      MacroFileType.HELM,
+      'RNA1{[dR](A)P.[dR](A)P.[dR](A)P.[dR](A)P.[dR](A)}|RNA2{[dR](T)P.[dR](T)P.[dR](T)P.[dR](T)P.[dR](T)}$RNA1,RNA2,11:pair-5:pair|RNA1,RNA2,8:pair-8:pair|RNA1,RNA2,5:pair-11:pair|RNA1,RNA2,2:pair-14:pair|RNA1,RNA2,14:pair-2:pair$$$V2.0',
+    );
+    await MacromoleculesTopToolbar(page).calculateProperties();
+    await CalculateVariablesPanel(page).setUnipositiveIonsValue(
+      '99999999999999999999999999999999999999999999999999',
+    );
+    await takePageScreenshot(page);
+    await keyboardPressOnCanvas(page, 'Backspace');
+    await CalculateVariablesPanel(page).setOligonucleotidesValue(
+      '99999999999999999999999999999999999999999999999999',
+    );
+    await takePageScreenshot(page);
+  });
+
+  test('Case 19: System not allow to export molecules to 3-letter sequence format', async () => {
+    /*
+     * Test case: https://github.com/epam/ketcher/issues/7454
+     * Bug: https://github.com/epam/Indigo/issues/2965
+     * Description: System not allow to export molecules to 3-letter sequence format.
+     * System throws an error: Convert error! Error during sequence type recognition(RNA, DNA or Peptide)
+     * Scenario:
+     * 1. Go to Macro mode -> Flex mode
+     * 2. Open from KET file
+     * 3. Press Save button and select 3-letter sequence format
+     * 4. Verify that the error message appears
+     */
+    await CommonTopRightToolbar(page).turnOnMacromoleculesEditor({
+      enableFlexMode: true,
+      goToPeptides: false,
+    });
+    await openFileAndAddToCanvasAsNewProjectMacro(
+      page,
+      'KET/Bugs/System allow to export molecules to 3-letter sequence format.ket',
+    );
+    await CommonTopLeftToolbar(page).saveFile();
+    await SaveStructureDialog(page).chooseFileFormat(
+      MacromoleculesFileFormatType.Sequence3LetterCode,
+    );
+    await takeEditorScreenshot(page, {
+      hideMonomerPreview: true,
+      hideMacromoleculeEditorScrollBars: true,
+    });
+    await closeErrorAndInfoModals(page);
+  });
+
+  test('Case 20: Atom weights in indigo updated according to last IUPAC data', async () => {
+    /*
+     * Test case: https://github.com/epam/ketcher/issues/7454
+     * Bug: https://github.com/epam/Indigo/issues/2926
+     * Description: Atom weights in indigo updated according to last IUPAC data. Molecular mass is 4113,641
+     * Scenario:
+     * 1. Go to Macro
+     * 2. Load from HELM
+     * 3. Open the "Calculate Properties" window
+     */
+    await CommonTopRightToolbar(page).turnOnMacromoleculesEditor({
+      enableFlexMode: true,
+      goToPeptides: false,
+    });
+    await pasteFromClipboardAndAddToMacromoleculesCanvas(
+      page,
+      MacroFileType.HELM,
+      'CHEM1{[CCC(C(NC(C(NC(C(NC(C(NC(C(NC(C(NC(C(NC(CNC(C(NC(C(NC(C(NC(C(NC(C(NC(C(NC(C(NC(C(NC(C(NC(C(NC(C(NC(CNC(C(NC(C(NC(C(N)CC1NC=NC=1)=O)(C)C)=O)CCC(O)=O)=O)=O)C(O)C)=O)CC1C=CC=CC=1)=O)C(O)C)=O)CO)=O)CC(O)=O)=O)C(C)C)=O)CO)=O)CO)=O)CC1C=CC(O)=CC=1)=O)CC(C)C)=O)CCC(O)=O)=O)=O)CCC(N)=O)=O)C)=O)C)=O)CCCCNC(COCCOCCNC(COCCOCCNC(CCC(NC(CCCCCCCCCCCCCCCCC(O)=O)=O)C(O)=O)=O)=O)=O)=O)CCC(O)=O)=O)CC1C=CC=CC=1)=O)C(NC(C(NC(C(NC(C(NC(C(NC(C(NCC(NC(C(NCC(O)=O)=O)CCCNC(N)=N)=O)=O)CCCNC(N)=N)=O)C(C)C)=O)CC(C)C)=O)CC1C2C(=CC=CC=2)NC=1)=O)C)=O)C]}$$$$V2.0',
+    );
+    await MacromoleculesTopToolbar(page).calculateProperties();
+    await CalculateVariablesPanel(page).setMolecularMassUnits(
+      MolecularMassUnit.Da,
+    );
+    expect(await CalculateVariablesPanel(page).getMolecularMassValue()).toEqual(
+      '4113.641',
+    );
+  });
+
+  test('Case 21: Select central monomer of chain of three', async () => {
+    /*
+     * Test case: https://github.com/epam/ketcher/issues/7454
+     * Bug: https://github.com/epam/Indigo/issues/2926
+     * Description: Mass should be 103.139 Da for peptide C.
+     * Scenario:
+     * 1. Go to Macro
+     * 2. Load from HELM
+     * 3. Select central monomer of chain of three C monomers
+     * 4. Open the "Calculate Properties" window
+     * 5. Verify that the mass is 103.139 Da
+     */
+    await CommonTopRightToolbar(page).turnOnMacromoleculesEditor({
+      enableFlexMode: true,
+      goToPeptides: false,
+    });
+    await pasteFromClipboardAndAddToMacromoleculesCanvas(
+      page,
+      MacroFileType.HELM,
+      'PEPTIDE1{C.C.C}$$$$V2.0',
+    );
+    await getMonomerLocator(page, Peptides.C).nth(1).click();
+    await MacromoleculesTopToolbar(page).calculateProperties();
+    await CalculateVariablesPanel(page).setMolecularMassUnits(
+      MolecularMassUnit.Da,
+    );
+    expect(await CalculateVariablesPanel(page).getMolecularMassValue()).toEqual(
+      '103.139',
+    );
+    await MacromoleculesTopToolbar(page).calculateProperties();
+  });
+
+  test('Case 22: Export (and import) of sequence of nucleosides to HELM works', async () => {
+    /*
+     * Test case: https://github.com/epam/ketcher/issues/7454
+     * Bug: https://github.com/epam/Indigo/issues/2989
+     * Description: Export (and import) of sequence of nucleosides to HELM works
+     * Scenario:
+     * 1. Go to Macro mode -> Flex mode
+     * 2. Open from KET file
+     * 3. Export canvas to HELM
+     * 4. Import HELM file to canvas
+     * 5. Take screenshot
+     */
+    await CommonTopRightToolbar(page).turnOnMacromoleculesEditor({
+      enableFlexMode: true,
+      goToPeptides: false,
+    });
+    await openFileAndAddToCanvasAsNewProjectMacro(
+      page,
+      'KET/Bugs/Export (and import) of sequence of nucleosides to HELM works wrong.ket',
+    );
+    await CommonTopLeftToolbar(page).saveFile();
+    await SaveStructureDialog(page).chooseFileFormat(
+      MacromoleculesFileFormatType.HELM,
+    );
+    await takeEditorScreenshot(page, {
+      hideMonomerPreview: true,
+      hideMacromoleculeEditorScrollBars: true,
+    });
+    await closeErrorAndInfoModals(page);
+    await CommonTopLeftToolbar(page).clearCanvas();
+    await pasteFromClipboardAndAddToMacromoleculesCanvas(
+      page,
+      MacroFileType.HELM,
+      'RNA1{[5R6Rm5](A).[5R6Rm5](A).[5R6Rm5](A)}$$$$V2.0',
+    );
+    await takeEditorScreenshot(page, {
+      hideMonomerPreview: true,
+      hideMacromoleculeEditorScrollBars: true,
+    });
   });
 });

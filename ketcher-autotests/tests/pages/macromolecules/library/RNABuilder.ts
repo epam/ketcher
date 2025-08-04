9207c246895819cfa4e8ea9e1e23b6917ceec4f1
Alexey Girin <agirin@mail.ru>
2025-08-04 17:28:13 +0300

    final

--- ketcher-autotests/tests/pages/macromolecules/library/RNABuilder.ts
+++ ketcher-autotests/tests/pages/macromolecules/library/RNABuilder.ts
@@ -1,4 +1,4 @@
-import { Page, Locator } from '@playwright/test';
+import { Page, Locator, expect } from '@playwright/test';
 import { moveMouseAway } from '@utils/moveMouseAway';
 
 type RNABuilderLocators = {

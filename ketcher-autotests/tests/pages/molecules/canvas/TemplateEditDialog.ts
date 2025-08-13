3177dd3ea7e34061edcfb877cb86a6cfc41f07f5
ViktoriaTkacheva <139227567+ViktoriaTkacheva@users.noreply.github.com>
2025-08-13 16:36:08 +0300

    Autotests: #7437 - autotests create page object for template edit window and replace with it necessary code (#7561)
    * Added page object for Template Edit window
    Added 3 data-testId for Edit and Cancel buttons, and "Selected attachment points" field
    
    * Added page object
    
    * Changed data-testID name
    
    * Added selectAtomById and selectBondById functions
    
    * Add page object
    
    * Change function name
    
    * Added a few more functions to the page object
    Adjusted autotests
    
    * Remoted an extra function
    
    * Changed the autotest's name

---
+++ ketcher-autotests/tests/pages/molecules/canvas/TemplateEditDialog.ts
@@ -0,0 +1,62 @@
+/* eslint-disable prettier/prettier */
+/* eslint-disable no-magic-numbers */
+import { Page, Locator } from '@playwright/test';
+import { waitForRender } from '@utils/common';
+
+type TemplateEditDialogLocators = {
+  dialog: Locator;
+  closeWindowButton: Locator;
+  moleculeNameInput: Locator;
+  selectedAttachmentPoints: Locator;
+  cancelButton: Locator;
+  editButton: Locator;
+  saveButton: Locator;
+  canvas: Locator;
+};
+export const TemplateEditDialog = (page: Page) => {
+  const locators: TemplateEditDialogLocators = {
+    dialog: page.getByTestId('attach-dialog'),
+    closeWindowButton: page.getByTestId('close-window-button'),
+    moleculeNameInput: page.getByTestId('name-input'),
+    selectedAttachmentPoints: page.getByTestId('attach-output'),
+    cancelButton: page.getByTestId('template-cancel-button'),
+    editButton: page.getByTestId('template-edit-button'),
+    saveButton: page.getByTestId('template-save-button'),
+    canvas: page.getByTestId('canvas'),
+  };
+
+  return {
+    ...locators,
+
+    async close() {
+      await locators.closeWindowButton.click();
+    },
+    async edit() {
+      await waitForRender(page, async () => {
+        await locators.editButton.click({ force: true });
+      });
+    },
+    async save() {
+      await waitForRender(page, async () => {
+        await locators.saveButton.click({ force: true });
+      });
+    },
+    async cancel() {
+      await locators.cancelButton.click({ force: true });
+    },
+    async setMoleculeName(name: string) {
+      await locators.moleculeNameInput.fill(name);
+    },
+    async getMoleculeName(): Promise<string> {
+      return await locators.moleculeNameInput.inputValue();
+    },
+    async clickMoleculeName() {
+      await locators.moleculeNameInput.click({ force: true });
+    },
+    async clickCanvas() {
+      await page.getByTestId('attach-dialog').getByTestId('canvas').click();
+    },
+  };
+};
+
+export type TemplateEditDialogType = ReturnType<typeof TemplateEditDialog>;

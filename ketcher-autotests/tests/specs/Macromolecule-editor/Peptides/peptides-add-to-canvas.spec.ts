e7ce69824959dcf5a8c4cb1f3ab05a02e88a2df7
Alexey Girin <agirin@mail.ru>
2025-08-17 18:10:35 +0300

    f4

--- ketcher-autotests/tests/specs/Macromolecule-editor/Peptides/peptides-add-to-canvas.spec.ts
+++ ketcher-autotests/tests/specs/Macromolecule-editor/Peptides/peptides-add-to-canvas.spec.ts
@@ -13,6 +13,8 @@ import { CommonLeftToolbar } from '@tests/pages/common/CommonLeftToolbar';
 import { MacroBondType } from '@tests/pages/constants/bondSelectionTool/Constants';
 import { CommonTopRightToolbar } from '@tests/pages/common/CommonTopRightToolbar';
 import { Library } from '@tests/pages/macromolecules/Library';
+import { get } from 'http';
+import { getMonomerLocator } from '@utils/macromolecules/monomer';
 
 /* 
 Test case: #3063 - Add e2e tests for Macromolecule editor
@@ -40,12 +42,12 @@ test.describe('Peptide', () => {
     Description: Add monomer preview on canvas
     */
     await Library(page).dragMonomerOnCanvas(Peptides.A, {
-      x: -10,
-      y: -10,
+      x: 0,
+      y: 0,
       fromCenter: true,
     });
     await CommonLeftToolbar(page).selectBondTool(MacroBondType.Single);
-    await moveMouseToTheMiddleOfTheScreen(page);
+    await getMonomerLocator(page, Peptides.A).hover();
     await waitForMonomerPreview(page);
     await takeEditorScreenshot(page);
   });
--- ketcher-autotests/tests/specs/Macromolecule-editor/Snake-Mode/snake-bond-tool.spec.ts
+++ ketcher-autotests/tests/specs/Macromolecule-editor/Snake-Mode/snake-bond-tool.spec.ts
@@ -443,57 +443,50 @@ test.describe('Snake Bond Tool', () => {
 
   test('Create snake bond for chain with side chains', async () => {
     await MacromoleculesTopToolbar(page).selectLayoutModeTool(LayoutMode.Flex);
-    await Library(page).switchToRNATab();
-    await Library(page).openRNASection(RNASection.Presets);
-
     const { phosphate } = await addRnaPresetOnCanvas(
       page,
       Presets.C,
-      200,
-      200,
+      50,
+      50,
       0,
       0,
     );
     const { sugar: sugar1, phosphate: phosphate1 } = await addRnaPresetOnCanvas(
       page,
       Presets.G,
-      500,
-      300,
+      350,
+      150,
       1,
       1,
     );
     const { sugar: sugar2, phosphate: phosphate2 } = await addRnaPresetOnCanvas(
       page,
       Presets.T,
-      700,
-      300,
+      550,
+      150,
       2,
       2,
     );
     await addRnaPresetOnCanvas(page, Presets.U, 900, 300, 3, 3);
-    await Library(page).openRNASection(RNASection.Sugars);
     const sugarOfNucleoside = await addSingleMonomerToCanvas(
       page,
       Sugars.R,
-      350,
-      350,
+      200,
+      200,
       4,
     );
-    await Library(page).openRNASection(RNASection.Bases);
     const baseOfNucleoside = await addSingleMonomerToCanvas(
       page,
       Bases.nC6n8A,
+      200,
       350,
-      500,
       0,
     );
-
-    await Library(page).switchToPeptidesTab();
     const [peptide] = await addBondedMonomersToCanvas(
       page,
       Peptides.A,
-      500,
-      500,
+      350,
+      350,
       50,
       50,
       3,
@@ -502,8 +495,8 @@ test.describe('Snake Bond Tool', () => {
     const [hcyPeptide, hcyPeptide1] = await addBondedMonomersToCanvas(
       page,
       Peptides.Hcy,
-      600,
-      500,
+      350,
+      250,
       50,
       0,
       2,
@@ -512,8 +505,8 @@ test.describe('Snake Bond Tool', () => {
     const [balPeptide] = await addBondedMonomersToCanvas(
       page,
       Peptides.bAla,
-      700,
-      700,
+      500,
+      500,
       50,
       0,
       2,
@@ -521,12 +514,11 @@ test.describe('Snake Bond Tool', () => {
     const balPeptide1 = await addSingleMonomerToCanvas(
       page,
       Peptides.bAla,
-      850,
-      650,
+      520,
+      500,
       2,
     );
 
-    await CommonLeftToolbar(page).selectBondTool(MacroBondType.Single);
     await bondTwoMonomers(page, sugarOfNucleoside, baseOfNucleoside);
     await bondTwoMonomers(
       page,
@@ -1013,6 +1005,7 @@ test.describe('Snake Bond Tool', () => {
     Description: Open chain with 4000 peptides items. Turn on snake mode. Snake mode is applied on structure 
     and maximum call stack size exceeded error not appears during snake layout.
     */
+    test.slow();
     page.on('console', (msg) => {
       if (msg.type() === 'error') {
         test.fail(

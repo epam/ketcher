73dc344fc85020db31fba6c53e22fc185c4ea261
Roman Rodionov <roman_rodionov@epam.com>
2025-08-22 18:13:30 +0200

    - fixed tests

--- ketcher-autotests/tests/specs/Chromium-popup/Bugs/ketcher-2.26.0-bugs.spec.ts
+++ ketcher-autotests/tests/specs/Chromium-popup/Bugs/ketcher-2.26.0-bugs.spec.ts
@@ -64,7 +64,6 @@ import {
   getSymbolLocator,
 } from '@utils/macromolecules/monomer';
 import { Peptide } from '@tests/pages/constants/monomers/Peptides';
-import { Phosphate } from '@tests/pages/constants/monomers/Phosphates';
 import { Sugar } from '@tests/pages/constants/monomers/Sugars';
 import { LeftToolbar } from '@tests/pages/molecules/LeftToolbar';
 import { ArrowType } from '@tests/pages/constants/arrowSelectionTool/Constants';
@@ -714,17 +713,13 @@ test.describe('Ketcher bugs in 2.26.0', () => {
     await takeEditorScreenshot(page);
     await moveMouseAway(page);
 
-    const _25mo3rSugar = getMonomerLocator(page, Sugar._25mo3r);
-    await _25mo3rSugar.hover();
-    await waitForMonomerPreview(page);
-    await takeEditorScreenshot(page);
-    await moveMouseAway(page);
-
-    const mspPhosphate = getMonomerLocator(page, Phosphate.msp);
-    await mspPhosphate.hover();
-    await waitForMonomerPreview(page);
-    await takeEditorScreenshot(page);
-  });
+      const _25mo3rSugar = getMonomerLocator(page, Sugar._25mo3r);
+      await _25mo3rSugar.hover();
+      await waitForMonomerPreview(page);
+      await takeEditorScreenshot(page);
+      await moveMouseAway(page);
+    },
+  );
 
   test('Case 25: The tail of Multi-Tailed Arrow is added to the proper place on the Spine after the Redo action of removing the tail if the length of the spine were changed', async () => {
     /*

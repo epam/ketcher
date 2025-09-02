ada3c84396f1f78cf50c4882a44908bcd9ec2cbd
Alexey Girin <agirin@mail.ru>
2025-09-02 15:09:52 +0300

    11

--- ketcher-autotests/tests/specs/Chromium-popup/Monomer-creation/monomer-creation.spec.ts
+++ ketcher-autotests/tests/specs/Chromium-popup/Monomer-creation/monomer-creation.spec.ts
@@ -19,6 +19,7 @@ import {
   clickOnCanvas,
   dragTo,
   MolFileFormat,
+  SdfFileFormat,
   waitForMonomerPreview,
 } from '@utils/index';
 import {
@@ -1590,6 +1591,7 @@ for (const monomerToCreate of monomersToCreate) {
 
 for (const monomerToCreate of monomersToCreate) {
   test(`28. Check that created ${monomerToCreate.description} monomer (expanded) can be saved/opened to/from MOL V3000 in Micro mode`, async () => {
+    // Screenshots are wrong because of bug: https://github.com/epam/ketcher/issues/7764
     /*
      * Test task: https://github.com/epam/ketcher/issues/7657
      * Description: Check that created ${monomerToCreate.description} monomer (expanded) can be saved/opened to/from MOL V3000 in Micro mode
@@ -1628,6 +1630,52 @@ for (const monomerToCreate of monomersToCreate) {
     await openFileAndAddToCanvasAsNewProject(
       page,
       `Molfiles-V3000/Chromium-popup/Create-monomer/${monomerToCreate.description}-expected.mol`,
+    );
+    await takeEditorScreenshot(page);
+  });
+}
+
+for (const monomerToCreate of monomersToCreate) {
+  test(`29. Check that created ${monomerToCreate.description} monomer (expanded) can be saved/opened to/from SDF V2000 in Micro mode`, async () => {
+    // Screenshots are wrong because of bug: https://github.com/epam/ketcher/issues/7764
+    /*
+     * Test task: https://github.com/epam/ketcher/issues/7657
+     * Description: Check that created ${monomerToCreate.description} monomer (expanded) can be saved/opened to/from SDF V2000 in Micro mode
+     *
+     * Case:
+     *      1. Open Molecules canvas
+     *      2. Load molecule on canvas
+     *      3. Select whole molecule and deselect atoms/bonds that not needed for monomer
+     *      4. Create monomer with given attributes
+     *      5. Save it to SDF V2000 and validate the result
+     *      6. Load saved monomer from SDF V2000 as New Project
+     *      7. Take screenshot to validate monomer got loaded
+     *
+     * Version 3.7
+     */
+    await pasteFromClipboardAndOpenAsNewProject(
+      page,
+      eligableMolecules[0].MoleculeSMARTS,
+    );
+    await prepareMoleculeForMonomerCreation(
+      page,
+      eligableMolecules[0].AtomIDsToExclude,
+      eligableMolecules[0].BondIDsToExclude,
+    );
+
+    await createMonomer(page, {
+      ...monomerToCreate,
+    });
+
+    await verifyFileExport(
+      page,
+      `SDF-V2000/Chromium-popup/Create-monomer/${monomerToCreate.description}-expected.sdf`,
+      FileType.SDF,
+      SdfFileFormat.v2000,
+    );
+    await openFileAndAddToCanvasAsNewProject(
+      page,
+      `SDF-V2000/Chromium-popup/Create-monomer/${monomerToCreate.description}-expected.sdf`,
     );
     await takeEditorScreenshot(page);
   });

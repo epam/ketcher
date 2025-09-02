3cd27d7041dcfb64a027a60e8d857ccfd7015469
Mikhail Zhirnov <zhirnoff85@gmail.com>
2025-09-02 14:51:12 +0300

    - delete tag

--- ketcher-autotests/tests/specs/Chromium-popup/Molecules-editor/loading-of-many-monomers.spec.ts
+++ ketcher-autotests/tests/specs/Chromium-popup/Molecules-editor/loading-of-many-monomers.spec.ts
@@ -19,142 +19,114 @@ test.describe('Loading of many monomers to Micro', () => {
     await closePage();
   });
 
-  test(
-    'Case 1: Check that we can open a chain of 500 monomers in Micro mode',
-    { tag: ['@chromium-popup'] },
-    async () => {
-      /*
-       * Version 3.7
-       * Test case: https://github.com/epam/ketcher/issues/7351
-       * Description: Check that we can open a chain of 500 monomers in Micro mode
-       * Scenario:
-       * 1. Go to Micro mode
-       * 2. Open file with a chain of 500 monomers
-       */
-      const expected = 500;
-      await openFileAndAddToCanvasAsNewProject(page, 'KET/500-peptides.ket');
-      const actual = await countByDataSgroupName(page);
-      expect(actual).toBe(expected);
-    },
-  );
+  test('Case 1: Check that we can open a chain of 500 monomers in Micro mode', async () => {
+    /*
+     * Version 3.7
+     * Test case: https://github.com/epam/ketcher/issues/7351
+     * Description: Check that we can open a chain of 500 monomers in Micro mode
+     * Scenario:
+     * 1. Go to Micro mode
+     * 2. Open file with a chain of 500 monomers
+     */
+    const expected = 500;
+    await openFileAndAddToCanvasAsNewProject(page, 'KET/500-peptides.ket');
+    const actual = await countByDataSgroupName(page);
+    expect(actual).toBe(expected);
+  });
 
-  test(
-    'Case 2: Check that we can open a chain of 1000 monomers in Micro mode',
-    { tag: ['@chromium-popup'] },
-    async () => {
-      /*
-       * Version 3.7
-       * Test case: https://github.com/epam/ketcher/issues/7351
-       * Description: Check that we can open a chain of 1000 monomers in Micro mode
-       * Scenario:
-       * 1. Go to Micro mode
-       * 2. Open file with a chain of 1000 monomers
-       */
-      const expected = 1000;
-      test.slow();
-      await openFileAndAddToCanvasAsNewProject(page, 'KET/1000-peptides.ket');
-      const actual = await countByDataSgroupName(page);
-      expect(actual).toBe(expected);
-    },
-  );
+  test('Case 2: Check that we can open a chain of 1000 monomers in Micro mode', async () => {
+    /*
+     * Version 3.7
+     * Test case: https://github.com/epam/ketcher/issues/7351
+     * Description: Check that we can open a chain of 1000 monomers in Micro mode
+     * Scenario:
+     * 1. Go to Micro mode
+     * 2. Open file with a chain of 1000 monomers
+     */
+    const expected = 1000;
+    test.slow();
+    await openFileAndAddToCanvasAsNewProject(page, 'KET/1000-peptides.ket');
+    const actual = await countByDataSgroupName(page);
+    expect(actual).toBe(expected);
+  });
 
-  test(
-    'Case 3: Check that we can open a chain of 1500 monomers in Micro mode',
-    { tag: ['@chromium-popup'] },
-    async () => {
-      /*
-       * Version 3.7
-       * Test case: https://github.com/epam/ketcher/issues/7351
-       * Description: Check that we can open a chain of 1500 monomers in Micro mode
-       * Scenario:
-       * 1. Go to Micro mode
-       * 2. Open file with a chain of 1500 monomers
-       */
-      const expected = 1500;
-      test.slow();
-      await openFileAndAddToCanvasAsNewProject(page, 'KET/1500-peptides.ket');
-      const actual = await countByDataSgroupName(page);
-      expect(actual).toBe(expected);
-    },
-  );
+  test('Case 3: Check that we can open a chain of 1500 monomers in Micro mode', async () => {
+    /*
+     * Version 3.7
+     * Test case: https://github.com/epam/ketcher/issues/7351
+     * Description: Check that we can open a chain of 1500 monomers in Micro mode
+     * Scenario:
+     * 1. Go to Micro mode
+     * 2. Open file with a chain of 1500 monomers
+     */
+    const expected = 1500;
+    test.slow();
+    await openFileAndAddToCanvasAsNewProject(page, 'KET/1500-peptides.ket');
+    const actual = await countByDataSgroupName(page);
+    expect(actual).toBe(expected);
+  });
 
-  test(
-    'Case 4: Check that we can open a chain of 2000 monomers in Micro mode',
-    { tag: ['@chromium-popup'] },
-    async () => {
-      /*
-       * Version 3.7
-       * Test case: https://github.com/epam/ketcher/issues/7351
-       * Description: Check that we can open a chain of 2000 monomers in Micro mode
-       * Scenario:
-       * 1. Go to Micro mode
-       * 2. Open file with a chain of 2000 monomers
-       */
-      const expected = 2000;
-      test.slow();
-      await openFileAndAddToCanvasAsNewProject(page, 'KET/2000-peptides.ket');
-      const actual = await countByDataSgroupName(page);
-      expect(actual).toBe(expected);
-    },
-  );
+  test('Case 4: Check that we can open a chain of 2000 monomers in Micro mode', async () => {
+    /*
+     * Version 3.7
+     * Test case: https://github.com/epam/ketcher/issues/7351
+     * Description: Check that we can open a chain of 2000 monomers in Micro mode
+     * Scenario:
+     * 1. Go to Micro mode
+     * 2. Open file with a chain of 2000 monomers
+     */
+    const expected = 2000;
+    test.slow();
+    await openFileAndAddToCanvasAsNewProject(page, 'KET/2000-peptides.ket');
+    const actual = await countByDataSgroupName(page);
+    expect(actual).toBe(expected);
+  });
 
-  test(
-    'Case 5: Check that we can open a chain of 2500 monomers in Micro mode',
-    { tag: ['@chromium-popup'] },
-    async () => {
-      /*
-       * Version 3.7
-       * Test case: https://github.com/epam/ketcher/issues/7351
-       * Description: Check that we can open a chain of 2500 monomers in Micro mode
-       * Scenario:
-       * 1. Go to Micro mode
-       * 2. Open file with a chain of 2500 monomers
-       */
-      const expected = 2500;
-      test.slow();
-      await openFileAndAddToCanvasAsNewProject(page, 'KET/2500-peptides.ket');
-      const actual = await countByDataSgroupName(page);
-      expect(actual).toBe(expected);
-    },
-  );
+  test('Case 5: Check that we can open a chain of 2500 monomers in Micro mode', async () => {
+    /*
+     * Version 3.7
+     * Test case: https://github.com/epam/ketcher/issues/7351
+     * Description: Check that we can open a chain of 2500 monomers in Micro mode
+     * Scenario:
+     * 1. Go to Micro mode
+     * 2. Open file with a chain of 2500 monomers
+     */
+    const expected = 2500;
+    test.slow();
+    await openFileAndAddToCanvasAsNewProject(page, 'KET/2500-peptides.ket');
+    const actual = await countByDataSgroupName(page);
+    expect(actual).toBe(expected);
+  });
 
-  test(
-    'Case 6: Check that we can open a chain of 3000 monomers in Micro mode',
-    { tag: ['@chromium-popup'] },
-    async () => {
-      /*
-       * Version 3.7
-       * Test case: https://github.com/epam/ketcher/issues/7351
-       * Description: Check that we can open a chain of 3000 monomers in Micro mode
-       * Scenario:
-       * 1. Go to Micro mode
-       * 2. Open file with a chain of 3000 monomers
-       */
-      const expected = 3000;
-      test.slow();
-      await openFileAndAddToCanvasAsNewProject(page, 'KET/3000-peptides.ket');
-      const actual = await countByDataSgroupName(page);
-      expect(actual).toBe(expected);
-    },
-  );
+  test('Case 6: Check that we can open a chain of 3000 monomers in Micro mode', async () => {
+    /*
+     * Version 3.7
+     * Test case: https://github.com/epam/ketcher/issues/7351
+     * Description: Check that we can open a chain of 3000 monomers in Micro mode
+     * Scenario:
+     * 1. Go to Micro mode
+     * 2. Open file with a chain of 3000 monomers
+     */
+    const expected = 3000;
+    test.slow();
+    await openFileAndAddToCanvasAsNewProject(page, 'KET/3000-peptides.ket');
+    const actual = await countByDataSgroupName(page);
+    expect(actual).toBe(expected);
+  });
 
-  test(
-    'Case 7: Check that we can open a chain of 3500 monomers in Micro mode',
-    { tag: ['@chromium-popup'] },
-    async () => {
-      /*
-       * Version 3.7
-       * Test case: https://github.com/epam/ketcher/issues/7351
-       * Description: Check that we can open a chain of 3500 monomers in Micro mode
-       * Scenario:
-       * 1. Go to Micro mode
-       * 2. Open file with a chain of 3500 monomers
-       */
-      const expected = 3500;
-      test.slow();
-      await openFileAndAddToCanvasAsNewProject(page, 'KET/3500-peptides.ket');
-      const actual = await countByDataSgroupName(page);
-      expect(actual).toBe(expected);
-    },
-  );
+  test('Case 7: Check that we can open a chain of 3500 monomers in Micro mode', async () => {
+    /*
+     * Version 3.7
+     * Test case: https://github.com/epam/ketcher/issues/7351
+     * Description: Check that we can open a chain of 3500 monomers in Micro mode
+     * Scenario:
+     * 1. Go to Micro mode
+     * 2. Open file with a chain of 3500 monomers
+     */
+    const expected = 3500;
+    test.slow();
+    await openFileAndAddToCanvasAsNewProject(page, 'KET/3500-peptides.ket');
+    const actual = await countByDataSgroupName(page);
+    expect(actual).toBe(expected);
+  });
 });

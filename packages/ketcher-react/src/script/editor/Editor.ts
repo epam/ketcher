f9ffbbdf5593fc5f15d24e73a7567a97c92c9395
Roman Rodionov <roman_rodionov@epam.com>
2025-09-15 22:40:59 +0200

    #7688 - Attached groups are lost when saving and reopening monomer created via Monomer Wizard in KET format (#7870)
    - added recalculation of fragments after monomer creation
    - skipped connected component search for monomers

--- packages/ketcher-react/src/script/editor/Editor.ts
+++ packages/ketcher-react/src/script/editor/Editor.ts
@@ -1102,6 +1102,9 @@ class Editor implements KetcherEditor {
       monomer,
     );
 
+    this.render.ctab.molecule.clearFragments();
+    this.render.ctab.molecule.markFragments();
+
     this.update(action);
 
     const { root: templateRoot, ...templateData } = monomerTemplate;

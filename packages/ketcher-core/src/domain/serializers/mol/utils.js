0390191135a93ef2d50e58bff2e51e33dc0dc134
Alexey Girin <agirin@mail.ru>
2025-10-07 08:48:55 +0300

    refactor: replace var declarations in serializers

--- packages/ketcher-core/src/domain/serializers/mol/utils.js
+++ packages/ketcher-core/src/domain/serializers/mol/utils.js
@@ -24,9 +24,11 @@ import {
 } from 'domain/entities';
 
 function paddedNum(number, width, precision) {
-  number = parseFloat(number);
+  const parsedNumber = parseFloat(number);
 
-  const numStr = number.toFixed(precision || 0).replace(',', '.'); // Really need to replace?
+  const numStr = parsedNumber
+    .toFixed(precision || 0)
+    .replace(',', '.'); // Really need to replace?
   if (numStr.length > width) throw new Error('number does not fit');
 
   return numStr.padStart(width);
@@ -151,10 +153,11 @@ function rxnMerge(
   const molReact = [];
   const molAgent = [];
   const molProd = [];
+  let mol;
   let j;
   const bondLengthData = { cnt: 0, totalLength: 0 };
   for (j = 0; j < mols.length; ++j) {
-    var mol = mols[j];
+    mol = mols[j];
     const bondLengthDataMol = mol.getBondLengthData();
     bondLengthData.cnt += bondLengthDataMol.cnt;
     bondLengthData.totalLength += bondLengthDataMol.totalLength;
@@ -176,7 +179,7 @@ function rxnMerge(
     const bb = mol.getCoordBoundingBoxObj();
     if (!bb) continue; // eslint-disable-line no-continue
 
-    var fragmentType =
+    const fragmentType =
       j < nReactants
         ? FRAGMENT.REACTANT // eslint-disable-line no-nested-ternary
         : j < nReactants + nProducts

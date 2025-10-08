449c8dd03b010b858c6e2e82b1185aefcfae71d3
Mikhail Zhirnov <zhirnoff85@gmail.com>
2025-10-08 13:27:02 +0300

    - add test; - add test file; - add screenshots

---
+++ ketcher-autotests/tests/specs/Chromium-popup/Snap-Functionality/snap-functionality.spec.ts
@@ -0,0 +1,55 @@
+/* eslint-disable @typescript-eslint/no-empty-function */
+/* eslint-disable max-len */
+/* eslint-disable no-magic-numbers */
+import { test } from '@fixtures';
+import { Page } from '@playwright/test';
+import { CommonTopRightToolbar } from '@tests/pages/common/CommonTopRightToolbar';
+import { Peptide } from '@tests/pages/constants/monomers/Peptides';
+import {
+  openFileAndAddToCanvasAsNewProject,
+  takeEditorScreenshot,
+} from '@utils';
+import { getMonomerLocator } from '@utils/macromolecules/monomer';
+
+let page: Page;
+
+test.describe('Snap Functionality', () => {
+  test.beforeAll(async ({ initFlexCanvas }) => {
+    page = await initFlexCanvas();
+  });
+  test.beforeEach(async ({ FlexCanvas: _ }) => {});
+  test.afterAll(async ({ closePage }) => {
+    await closePage();
+  });
+
+  test('Case 1: Snap a selected group of connected monomers relative to adjacent monomers', async () => {
+    /*
+     * Version 3.8
+     * Test case: https://github.com/epam/ketcher/issues/6287
+     * Description: Snap a selected group of connected monomers relative to adjacent monomers
+     * Scenario:
+     * 1. Create a structure consisting of several monomers connected via bonds.
+     * 2. Select a subset of monomers that form a connected group within the structure.
+     * 3. Drag this group toward the monomers that are directly bonded to it.
+     */
+    await openFileAndAddToCanvasAsNewProject(page, 'KET/six-peptides.ket');
+    await CommonTopRightToolbar(page).setZoomInputValue('80');
+    await takeEditorScreenshot(page, {
+      hideMonomerPreview: true,
+      hideMacromoleculeEditorScrollBars: true,
+    });
+    await page.keyboard.down('Shift');
+    for (const peptide of [Peptide.D, Peptide.E, Peptide.F]) {
+      await getMonomerLocator(page, peptide).click();
+    }
+    await page.keyboard.up('Shift');
+    await getMonomerLocator(page, Peptide.F);
+    await page.mouse.down();
+    await page.mouse.move(632, 322, { steps: 10 });
+    await takeEditorScreenshot(page, {
+      hideMonomerPreview: true,
+      hideMacromoleculeEditorScrollBars: true,
+    });
+    await page.mouse.up();
+  });
+});

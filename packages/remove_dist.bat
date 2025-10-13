2ded2fce1b94fc823c73d3bd510047d009720676
Alexey Girin <agirin@mail.ru>
2025-10-13 20:36:39 +0300

    f11

--- packages/remove_dist.bat
+++
@@ -1,4 +0,0 @@
-rd /Q /S C:\WorkDir\ketcher\packages\ketcher-standalone\dist\
-rd /Q /S C:\WorkDir\ketcher\packages\ketcher-react\dist\
-rd /Q /S C:\WorkDir\ketcher\packages\ketcher-macromolecules\dist\
-rd /Q /S C:\WorkDir\ketcher\packages\ketcher-core\dist\

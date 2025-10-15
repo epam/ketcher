c3f699c448980a10398a209d329d7d598fe7e3c0
Alexey Girin <agirin@mail.ru>
2025-10-15 11:34:29 +0300

    f22

--- ketcher-autotests/tests/specs/Chromium-popup/Monomer-creation/monomer-creation3.spec.ts
+++ ketcher-autotests/tests/specs/Chromium-popup/Monomer-creation/monomer-creation3.spec.ts
@@ -32,6 +32,10 @@ import {
 import { NotificationMessageBanner } from '@tests/pages/molecules/canvas/createMonomer/NotificationMessageBanner';
 import { ErrorMessage } from '@tests/pages/constants/notificationMessageBanner/Constants';
 import { MonomerType } from '@tests/pages/constants/createMonomerDialog/Constants';
+import { CommonTopRightToolbar } from '@tests/pages/common/CommonTopRightToolbar';
+import { Library } from '@tests/pages/macromolecules/Library';
+import { Chem } from '@tests/pages/constants/monomers/Chem';
+import { MonomerPreviewTooltip } from '@tests/pages/macromolecules/canvas/MonomerPreviewTooltip';
 
 let page: Page;
 test.beforeAll(async ({ initMoleculesCanvas }) => {
@@ -1186,4 +1190,48 @@ test(`23. Check that if the user clicks on Cancel all monomer information is rem
 
   await CreateMonomerDialog(page).discard();
   await takeEditorScreenshot(page);
+});
+
+test(`24. Check that if the user clicks on Summit, the new monomer gets saved to the library, the previously selected structure becomes an expanded monomer, and the monomer is selected)`, async () => {
+  /*
+   * Test task: https://github.com/epam/ketcher/issues/7657
+   * Description: Check that if the user clicks on Summit, the new monomer gets saved to the library, the previously selected structure becomes an expanded monomer, and the monomer is selected
+   *
+   * Case:
+   *      1. Open Molecules canvas
+   *      2. Load molecule on canvas
+   *      3. Select whole molecule
+   *      4. Press Create Monomer button
+   *      5. Set monomer properties and click Submit button
+   *      6. Validate that the previously selected structure becomes an expanded monomer, and the monomer is selected
+   *      7. Validate the new monomer gets saved to the library
+   *
+   * Version 3.8
+   */
+  await pasteFromClipboardAndOpenAsNewProject(
+    page,
+    'CC%91%92.[*:2]%91.[*:3]%92 |$;;_R2;_R3$|',
+  );
+  await clickOnCanvas(page, 0, 0);
+  await selectAllStructuresOnCanvas(page);
+  await LeftToolbar(page).createMonomer();
+
+  // to make molecule visible
+  await CommonLeftToolbar(page).handTool();
+  await page.mouse.move(600, 200);
+  await dragMouseTo(425, 200, page);
+
+  await CreateMonomerDialog(page).selectType(MonomerType.CHEM);
+  await CreateMonomerDialog(page).setSymbol('CHEM');
+  await CreateMonomerDialog(page).setName('CHEM Test monomer');
+  await CreateMonomerDialog(page).submit();
+
+  await takeEditorScreenshot(page);
+  await CommonTopRightToolbar(page).turnOnMacromoleculesEditor();
+  await Library(page).hoverMonomer(Chem.CHEM);
+  await MonomerPreviewTooltip(page).waitForBecomeVisible();
+  await takeElementScreenshot(
+    page,
+    MonomerPreviewTooltip(page).monomerPreviewTooltipWindow,
+  );
 });

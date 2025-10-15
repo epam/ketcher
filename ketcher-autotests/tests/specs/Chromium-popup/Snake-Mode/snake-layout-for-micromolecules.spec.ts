2582bb4e588c85b95465b447702a8eed7231c42e
Mikhail Zhirnov <zhirnoff85@gmail.com>
2025-10-15 14:44:18 +0300

    - add tests; - add test files; - add screenshots

--- ketcher-autotests/tests/specs/Chromium-popup/Snake-Mode/snake-layout-for-micromolecules.spec.ts
+++ ketcher-autotests/tests/specs/Chromium-popup/Snake-Mode/snake-layout-for-micromolecules.spec.ts
@@ -160,4 +160,114 @@ test.describe('Snake Layout for Microstructures', () => {
       hideMacromoleculeEditorScrollBars: true,
     });
   });
+
+  test('Case 6: Check that small molecules connected to RNA/DNA monomers layouted below the chains', async () => {
+    /*
+     * Version 3.9
+     * Test case: https://github.com/epam/ketcher/issues/7560
+     * Description: The small molecules connected to RNA/DNA monomers layouted below the chains and
+     * all chains made up from monomers layouted like usual at the top of the canvas
+     * Scenario:
+     * 1. Go to Macromolecules mode - Flex mode
+     * 2. Add chains made up from monomers connected to micro structures and molecules
+     * 3. Switch to Snake mode
+     * 4. Check layout
+     */
+    await CommonTopRightToolbar(page).setZoomInputValue('20');
+    await openFileAndAddToCanvasAsNewProject(
+      page,
+      'KET/rna-connected-to-microsrtructures.ket',
+    );
+    await takeEditorScreenshot(page, {
+      hideMonomerPreview: true,
+      hideMacromoleculeEditorScrollBars: true,
+    });
+    await MacromoleculesTopToolbar(page).selectLayoutModeTool(LayoutMode.Snake);
+    await takeEditorScreenshot(page, {
+      hideMonomerPreview: true,
+      hideMacromoleculeEditorScrollBars: true,
+    });
+  });
+
+  test('Case 7: Check that small molecules NOT connected to RNA/DNA monomers layouted below the ones who are connected to monomers', async () => {
+    /*
+     * Version 3.9
+     * Test case: https://github.com/epam/ketcher/issues/7560
+     * Description: The small molecules NOT connected to RNA/DNA monomers layouted below the ones who are connected to monomers
+     * Scenario:
+     * 1. Go to Macromolecules mode - Flex mode
+     * 2. Add chains made up from monomers connected to micro structures and small molecules NOT connected to monomers
+     * 3. Switch to Snake mode
+     * 4. Check layout
+     */
+    await CommonTopRightToolbar(page).setZoomInputValue('20');
+    await openFileAndAddToCanvasAsNewProject(
+      page,
+      'KET/rna-with-connected-and-not-connected-molecules.ket',
+    );
+    await takeEditorScreenshot(page, {
+      hideMonomerPreview: true,
+      hideMacromoleculeEditorScrollBars: true,
+    });
+    await MacromoleculesTopToolbar(page).selectLayoutModeTool(LayoutMode.Snake);
+    await takeEditorScreenshot(page, {
+      hideMonomerPreview: true,
+      hideMacromoleculeEditorScrollBars: true,
+    });
+  });
+
+  test('Case 8: Check that if there are multiple connected SMs to one monomer in RNA/DNA chain, when layout is applied, they pulled in a row in a way that doesnt allow their bonds to overlap, if possible', async () => {
+    /*
+     * Version 3.9
+     * Test case: https://github.com/epam/ketcher/issues/7560
+     * Description: If there are multiple connected SMs to one monomer in RNA/DNA chain, when layout is applied, they
+     * pulled in a row in a way that doesnt allow their bonds to overlap, if possible
+     * Scenario:
+     * 1. Go to Macromolecules mode - Flex mode
+     * 2. Add chains made up from monomers and CHEMS connected to small molecules
+     * 3. Switch to Snake mode
+     * 4. Check layout
+     */
+    await CommonTopRightToolbar(page).setZoomInputValue('20');
+    await openFileAndAddToCanvasAsNewProject(
+      page,
+      'KET/micromolecules-connected-to-chems-in-RNA-chain.ket',
+    );
+    await takeEditorScreenshot(page, {
+      hideMonomerPreview: true,
+      hideMacromoleculeEditorScrollBars: true,
+    });
+    await MacromoleculesTopToolbar(page).selectLayoutModeTool(LayoutMode.Snake);
+    await takeEditorScreenshot(page, {
+      hideMonomerPreview: true,
+      hideMacromoleculeEditorScrollBars: true,
+    });
+  });
+
+  test('Case 9: Check that connected SMs arranged in a row based on the location of the RNA/DNA monomer they are connected to in the grid (column x row)', async () => {
+    /*
+     * Version 3.9
+     * Test case: https://github.com/epam/ketcher/issues/7560
+     * Description: The connected SMs arranged in a row based on the location of the RNA/DNA monomer they are connected to in the grid (column x row)
+     * Scenario:
+     * 1. Go to Macromolecules mode - Flex mode
+     * 2. Add chains made up from monomers connected to small molecules
+     * 3. Switch to Snake mode
+     * 4. Check layout
+     */
+    await CommonTopRightToolbar(page).setZoomInputValue('20');
+    await openFileAndAddToCanvasAsNewProject(
+      page,
+      'KET/rna-connected-to-small-molecules-in-different-rows.ket',
+    );
+    await takeEditorScreenshot(page, {
+      hideMonomerPreview: true,
+      hideMacromoleculeEditorScrollBars: true,
+    });
+    await MacromoleculesTopToolbar(page).selectLayoutModeTool(LayoutMode.Snake);
+    await takeEditorScreenshot(page, {
+      hideMonomerPreview: true,
+      hideMacromoleculeEditorScrollBars: true,
+    });
+  });
 });

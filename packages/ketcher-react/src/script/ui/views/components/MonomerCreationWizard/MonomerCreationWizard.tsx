9e8b5ee7eb2164d3f8e05bd511b8ba0d77aaf323
Kristina <kristina_kasyanovskaya@epam.com>
2025-10-22 18:34:56 +0500

    #7932 - Add warning messages to the monomer creation wizard in case of unusual leaving group atoms  (#8343)
    * fix: fix buttons logic
    
    * fix: fix primary button logic

--- packages/ketcher-react/src/script/ui/views/components/MonomerCreationWizard/MonomerCreationWizard.tsx
+++ packages/ketcher-react/src/script/ui/views/components/MonomerCreationWizard/MonomerCreationWizard.tsx
@@ -831,8 +831,9 @@ const MonomerCreationWizard = () => {
                 },
                 onCancel: () => setLeavingGroupDialogMessage(''),
               }}
-              buttons={['Cancel', 'OK']}
-              buttonsNameMap={{ OK: 'Cancel', Cancel: 'Yes' }}
+              buttons={['OK', 'Cancel']}
+              buttonsNameMap={{ OK: 'Yes', Cancel: 'Cancel' }}
+              primaryButtons={['Cancel']}
             >
               <div className={styles.DialogMessage}>
                 {leavingGroupDialogMessage}

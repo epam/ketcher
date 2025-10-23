63e2c3888e202ed66464cc5b5ae33b324f359489
Kristina <kristina_kasyanovskaya@epam.com>
2025-10-23 18:14:27 +0500

    #8328 - Modification drop-down list is not ordered as required (Natural Amino Acid first, others alphabetically) (#8342)
    * fix: fix Modification drop-down list
    
    * fix: fix add-modification-type option
    
    * fix: fix error message
    
    * fix: add limit on the modififcations fields

--- packages/ketcher-react/src/script/ui/views/components/MonomerCreationWizard/MonomerCreationWizard.tsx
+++ packages/ketcher-react/src/script/ui/views/components/MonomerCreationWizard/MonomerCreationWizard.tsx
@@ -36,6 +36,7 @@ import {
   MonomerTypeSelectConfig,
   NotificationMessages,
   NotificationTypes,
+  MAX_MODIFICATION_TYPES,
 } from './MonomerCreationWizard.constants';
 import { validateMonomerLeavingGroups } from './MonomerLeavingGroupValidator';
 import { useAppContext } from '../../../../../hooks';
@@ -432,7 +433,12 @@ const MonomerCreationWizard = () => {
   };
 
   const handleAddModificationType = () => {
-    setModificationTypes((types) => [...types, '']);
+    setModificationTypes((types) => {
+      if (types.length >= MAX_MODIFICATION_TYPES) {
+        return types;
+      }
+      return [...types, ''];
+    });
   };
 
   const deleteModificationType = (indexToDelete: number) => {
@@ -581,7 +587,7 @@ const MonomerCreationWizard = () => {
     attachmentPointEditPopupData !== null && ketcherEditorRootElement !== null;
 
   const displayModificationTypes =
-    wizardState.values.type === KetMonomerClass.AminoAcid && naturalAnalogue;
+    wizardState.values.type === KetMonomerClass.AminoAcid;
 
   return (
     <div className={styles.monomerCreationWizard}>
@@ -769,6 +775,9 @@ const MonomerCreationWizard = () => {
                       type="button"
                       className={styles.addModificationTypeButton}
                       onClick={handleAddModificationType}
+                      disabled={
+                        modificationTypes.length >= MAX_MODIFICATION_TYPES
+                      }
                     >
                       Add modification type
                     </button>

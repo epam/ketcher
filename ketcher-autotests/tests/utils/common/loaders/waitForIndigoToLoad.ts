e7f23ff9587a4eb244f6e14781bdbb01782f0e59
Alexey Girin <agirin@mail.ru>
2025-10-24 21:27:30 +0300

    f5

--- ketcher-autotests/tests/utils/common/loaders/waitForIndigoToLoad.ts
+++ ketcher-autotests/tests/utils/common/loaders/waitForIndigoToLoad.ts
@@ -1,10 +1,7 @@
-import { Locator, Page, expect } from '@playwright/test';
+import { Page, expect } from '@playwright/test';
 import { IndigoFunctionsToolbar } from '@tests/pages/molecules/IndigoFunctionsToolbar';
 
-let someIndigoButton: Locator | null = null;
 export const waitForIndigoToLoad = async (page: Page) => {
-  if (someIndigoButton === null) {
-    someIndigoButton = IndigoFunctionsToolbar(page).aromatizeButton;
-  }
+  const { aromatizeButton: someIndigoButton } = IndigoFunctionsToolbar(page);
   await expect(someIndigoButton).toBeEnabled({ timeout: 20_000 });
 };

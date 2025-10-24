090e411611b0216f615bc3df5d464f0dbce9228e
Alexey Girin <agirin@mail.ru>
2025-10-24 23:56:38 +0300

    f10

--- ketcher-autotests/tests/specs/Structure-Creating-&-Editing/SMARTS-attributes/Reaction-flags-attributes/reaction-flags-attributes.spec.ts
+++ ketcher-autotests/tests/specs/Structure-Creating-&-Editing/SMARTS-attributes/Reaction-flags-attributes/reaction-flags-attributes.spec.ts
@@ -1,49 +1,24 @@
-import { Page, test, expect } from '@fixtures';
+import { test, expect } from '@fixtures';
 import {
   checkSmartsValue,
   checkSmartsWarnings,
-  clickInTheMiddleOfTheScreen,
   doubleClickOnAtom,
+  pasteFromClipboardAndOpenAsNewProject,
   takeEditorScreenshot,
   waitForPageInit,
 } from '@utils';
-import { CommonLeftToolbar } from '@tests/pages/common/CommonLeftToolbar';
-import { MicroBondType } from '@tests/pages/constants/bondSelectionTool/Constants';
 import { AtomPropertiesDialog } from '@tests/pages/molecules/canvas/AtomPropertiesDialog';
 import { Inversion } from '@tests/pages/constants/atomProperties/Constants';
 
 const expectedSmarts = '[#6](-[#6])(-[#6])-[#6]';
 
-async function drawStructure(page: Page, numberOfClicks: number) {
-  await CommonLeftToolbar(page).selectBondTool(MicroBondType.Single);
-  for (let i = 0; i < numberOfClicks; i++) {
-    await clickInTheMiddleOfTheScreen(page);
-  }
-}
-
-async function drawStructureAndDoubleClickOnAtom(
-  page: Page,
-  numberOfBondsAtStructure: number,
-  atomType: string,
-  numberOfAtom: number,
-) {
-  await waitForPageInit(page);
-  await drawStructure(page, numberOfBondsAtStructure);
-  await page.keyboard.press('Escape');
-  await doubleClickOnAtom(page, atomType, numberOfAtom);
-  await expect(AtomPropertiesDialog(page).window).toBeVisible();
-}
-
 test.describe('Checking atom properties attributes in SMARTS format', () => {
   test.beforeEach(async ({ page }) => {
     const numberOfAtom = 0;
-    const numberOfBondsAtStructure = 3;
-    await drawStructureAndDoubleClickOnAtom(
-      page,
-      numberOfBondsAtStructure,
-      'C',
-      numberOfAtom,
-    );
+    await waitForPageInit(page);
+    await pasteFromClipboardAndOpenAsNewProject(page, 'C(C)(C)C');
+    await doubleClickOnAtom(page, 'C', numberOfAtom);
+    await expect(AtomPropertiesDialog(page).window).toBeVisible();
   });
 
   test('Setting reaction flag - Inverts', async ({ page }) => {

077c59f02c472622447d46e254ac06d18eb62f85
Alexey Girin <agirin@mail.ru>
2025-11-21 17:21:25 +0300

    d3

--- ketcher-autotests/tests/specs/Chromium-popup/Monomer-creation/monomer-creation6.spec.ts
+++ ketcher-autotests/tests/specs/Chromium-popup/Monomer-creation/monomer-creation6.spec.ts
@@ -33,7 +33,7 @@ test(`1. Check warning messages on Amino acid monomer if R1 attachment point wit
    *
    * Case:
    *      1. Open Molecules canvas
-   *      2. Load molecule on canvas
+   *      2. Load molecule on canvas (R1 = H, R2 = H)
    *      3. Press Create Monomer button
    *      4. Set mandatory fields in Create Monomer dialog for amino acid monomer
    *      5. Set R1 and R2 attachment point with a leaving group not equal H (e.g. O atom)
@@ -56,8 +56,8 @@ test(`1. Check warning messages on Amino acid monomer if R1 attachment point wit
 
   const createMonomerDialog = CreateMonomerDialog(page);
   await createMonomerDialog.selectType(MonomerType.AminoAcid);
-  await createMonomerDialog.setSymbol('Temp AminoAcid1');
-  await createMonomerDialog.setName('Temp AminoAcid1');
+  await createMonomerDialog.setSymbol('TempAminoAcid1');
+  await createMonomerDialog.setName('TempAminoAcid1');
   await createMonomerDialog.selectNaturalAnalogue(AminoAcidNaturalAnalogue.A);
 
   await RightToolbar(page).clickAtom(Atom.Oxygen);
@@ -67,8 +67,196 @@ test(`1. Check warning messages on Amino acid monomer if R1 attachment point wit
 
   await createMonomerDialog.submit();
 
-  expect(WarningMessageDialog(page).window).toBeVisible();
+  await expect(WarningMessageDialog(page).window).toBeVisible();
   expect(await WarningMessageDialog(page).getWarningMessage()).toContain(
     'Amino acid monomers typically have a hydrogen as the leaving group for R1, and a hydroxyl as a leaving group for R2. Do you wish to proceed with the current attachment points?',
   );
+  await WarningMessageDialog(page).cancel();
+  await createMonomerDialog.discard();
+});
+
+test(`2. Check warning messages on Amino acid monomer if R2 attachment point with a leaving group is not equal OH`, async () => {
+  /*
+   * Test task: https://github.com/epam/ketcher/issues/8587
+   * Description: Check warning messages on Amino acid monomer if R2 attachment point with a leaving group is not equal OH
+   *
+   * Case:
+   *      1. Open Molecules canvas
+   *      2. Load molecule on canvas (R1 = H, R2 = H)
+   *      3. Press Create Monomer button
+   *      4. Set mandatory fields in Create Monomer dialog for amino acid monomer
+   *      5. Press Submit button
+   *      6. Verify that warning message dialog is opened
+   *      7. Verify that the correct warning message is displayed in the dialog
+   *
+   * Version 3.10
+   */
+  await pasteFromClipboardAndOpenAsNewProject(
+    page,
+    '[*:1]CCC%91.[*:2]%91 |$_R1;;;;_R2$|',
+  );
+  await clickOnCanvas(page, 0, 0);
+  await selectAllStructuresOnCanvas(page);
+  await LeftToolbar(page).createMonomer();
+
+  // shifting canvas to make tooltip appear fully
+  await shiftCanvas(page, -150, 50);
+
+  const createMonomerDialog = CreateMonomerDialog(page);
+  await createMonomerDialog.selectType(MonomerType.AminoAcid);
+  await createMonomerDialog.setSymbol('TempAminoAcid1');
+  await createMonomerDialog.setName('TempAminoAcid1');
+  await createMonomerDialog.selectNaturalAnalogue(AminoAcidNaturalAnalogue.A);
+
+  await createMonomerDialog.submit();
+
+  await expect(WarningMessageDialog(page).window).toBeVisible();
+  expect(await WarningMessageDialog(page).getWarningMessage()).toContain(
+    'Amino acid monomers typically have a hydrogen as the leaving group for R1, and a hydroxyl as a leaving group for R2. Do you wish to proceed with the current attachment points?',
+  );
+  await WarningMessageDialog(page).cancel();
+  await createMonomerDialog.discard();
+});
+
+test(`3. Check warning messages on Sugar monomer if R1 attachment point with a leaving group is not equal H`, async () => {
+  /*
+   * Test task: https://github.com/epam/ketcher/issues/8587
+   * Description: Check warning messages on Sugar monomer if R1 attachment point with a leaving group is not equal H
+   *
+   * Case:
+   *      1. Open Molecules canvas
+   *      2. Load molecule on canvas (R1 = H, R2 = H, R3 = H)
+   *      3. Press Create Monomer button
+   *      4. Set mandatory fields in Create Monomer dialog for amino acid monomer
+   *      5. Set R1 and R3 attachment point with a leaving group not equal H (e.g. O atom)
+   *      6. Press Submit button
+   *      7. Verify that warning message dialog is opened
+   *      8. Verify that the correct warning message is displayed in the dialog
+   *
+   * Version 3.10
+   */
+  await pasteFromClipboardAndOpenAsNewProject(
+    page,
+    '[*:1]CC%91C%92.[*:2]%92.[*:3]%91 |$_R1;;;;_R2;_R3$|',
+  );
+  await clickOnCanvas(page, 0, 0);
+  await selectAllStructuresOnCanvas(page);
+  await LeftToolbar(page).createMonomer();
+
+  // shifting canvas to make tooltip appear fully
+  await shiftCanvas(page, -150, 50);
+
+  const createMonomerDialog = CreateMonomerDialog(page);
+  await createMonomerDialog.selectType(MonomerType.Sugar);
+  await createMonomerDialog.setSymbol('TempSugar1');
+  await createMonomerDialog.setName('TempSugar1');
+
+  await RightToolbar(page).clickAtom(Atom.Oxygen);
+  await getAtomLocator(page, { atomLabel: 'H' }).first().click({ force: true });
+  await getAtomLocator(page, { atomLabel: 'H' }).nth(1).click({ force: true });
+  // (R1 = OH, R2 = H, R3 = OH)
+
+  await createMonomerDialog.submit();
+
+  await expect(WarningMessageDialog(page).window).toBeVisible();
+  expect(await WarningMessageDialog(page).getWarningMessage()).toContain(
+    'Sugar monomers typically have a hydrogen as the leaving group for R1 and R2, and a hydroxyl as a leaving group for R3. Do you wish to proceed with the current attachment points?',
+  );
+
+  await WarningMessageDialog(page).cancel();
+  await createMonomerDialog.discard();
+});
+
+test(`4. Check warning messages on Sugar monomer if R2 attachment point with a leaving group is not equal H`, async () => {
+  /*
+   * Test task: https://github.com/epam/ketcher/issues/8587
+   * Description: Check warning messages on Sugar monomer if R2 attachment point with a leaving group is not equal H
+   *
+   * Case:
+   *      1. Open Molecules canvas
+   *      2. Load molecule on canvas (R1 = H, R2 = H, R3 = H)
+   *      3. Press Create Monomer button
+   *      4. Set mandatory fields in Create Monomer dialog for amino acid monomer
+   *      5. Set R2 and R3 attachment point with a leaving group not equal H (e.g. O atom)
+   *      6. Press Submit button
+   *      7. Verify that warning message dialog is opened
+   *      8. Verify that the correct warning message is displayed in the dialog
+   *
+   * Version 3.10
+   */
+  await pasteFromClipboardAndOpenAsNewProject(
+    page,
+    '[*:1]CC%91C%92.[*:2]%92.[*:3]%91 |$_R1;;;;_R2;_R3$|',
+  );
+  await clickOnCanvas(page, 0, 0);
+  await selectAllStructuresOnCanvas(page);
+  await LeftToolbar(page).createMonomer();
+
+  // shifting canvas to make tooltip appear fully
+  await shiftCanvas(page, -150, 50);
+
+  const createMonomerDialog = CreateMonomerDialog(page);
+  await createMonomerDialog.selectType(MonomerType.Sugar);
+  await createMonomerDialog.setSymbol('TempSugar1');
+  await createMonomerDialog.setName('TempSugar1');
+
+  await RightToolbar(page).clickAtom(Atom.Oxygen);
+  await getAtomLocator(page, { atomLabel: 'H' }).nth(1).click({ force: true });
+  await getAtomLocator(page, { atomLabel: 'H' }).nth(1).click({ force: true });
+  // (R1 = H, R2 = OH, R3 = OH)
+
+  await createMonomerDialog.submit();
+
+  await expect(WarningMessageDialog(page).window).toBeVisible();
+  expect(await WarningMessageDialog(page).getWarningMessage()).toContain(
+    'Sugar monomers typically have a hydrogen as the leaving group for R1 and R2, and a hydroxyl as a leaving group for R3. Do you wish to proceed with the current attachment points?',
+  );
+
+  await WarningMessageDialog(page).cancel();
+  await createMonomerDialog.discard();
+});
+
+test(`5. Check warning messages on Sugar monomer if R3 attachment point with a leaving group is not equal OH`, async () => {
+  /*
+   * Test task: https://github.com/epam/ketcher/issues/8587
+   * Description: Check warning messages on Sugar monomer if R3 attachment point with a leaving group is not equal OH
+   *
+   * Case:
+   *      1. Open Molecules canvas
+   *      2. Load molecule on canvas (R1 = H, R2 = H, R3 = H)
+   *      3. Press Create Monomer button
+   *      4. Set mandatory fields in Create Monomer dialog for amino acid monomer
+   *      5. Press Submit button
+   *      6. Verify that warning message dialog is opened
+   *      7. Verify that the correct warning message is displayed in the dialog
+   *
+   * Version 3.10
+   */
+  await pasteFromClipboardAndOpenAsNewProject(
+    page,
+    '[*:1]CC%91C%92.[*:2]%92.[*:3]%91 |$_R1;;;;_R2;_R3$|',
+  );
+  await clickOnCanvas(page, 0, 0);
+  await selectAllStructuresOnCanvas(page);
+  await LeftToolbar(page).createMonomer();
+
+  // shifting canvas to make tooltip appear fully
+  await shiftCanvas(page, -150, 50);
+
+  const createMonomerDialog = CreateMonomerDialog(page);
+  await createMonomerDialog.selectType(MonomerType.Sugar);
+  await createMonomerDialog.setSymbol('TempSugar1');
+  await createMonomerDialog.setName('TempSugar1');
+
+  // (R1 = H, R2 = H, R3 = H)
+
+  await createMonomerDialog.submit();
+
+  await expect(WarningMessageDialog(page).window).toBeVisible();
+  expect(await WarningMessageDialog(page).getWarningMessage()).toContain(
+    'Sugar monomers typically have a hydrogen as the leaving group for R1 and R2, and a hydroxyl as a leaving group for R3. Do you wish to proceed with the current attachment points?',
+  );
+
+  await WarningMessageDialog(page).cancel();
+  await createMonomerDialog.discard();
 });

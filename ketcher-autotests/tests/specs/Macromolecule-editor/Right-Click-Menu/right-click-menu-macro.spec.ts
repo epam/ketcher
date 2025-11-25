9bfeb1cff89a890d0432ab65de2302aae8cab290
KristinaKasyanovskaya <kristina_kasyanovskaya@epam.com>
2025-11-25 18:52:43 +0500

    test: update test logic

--- ketcher-autotests/tests/specs/Macromolecule-editor/Right-Click-Menu/right-click-menu-macro.spec.ts
+++ ketcher-autotests/tests/specs/Macromolecule-editor/Right-Click-Menu/right-click-menu-macro.spec.ts
@@ -161,7 +161,7 @@ test(`2. Verify context menu in Snake and Flex modes when right-clicking a part 
     ContextMenu(page, peptideD).contextMenuBody,
   );
   await expect(copyOption).toBeEnabled();
-  await expect(pasteOption).toBeDisabled();
+  await expect(pasteOption).toBeEnabled();
   await expect(deleteOption).toBeEnabled();
 });
 

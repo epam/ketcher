0a317c2d98f3c20ade7d5b3581628d58d69c7731
Alexey Girin <agirin@mail.ru>
2025-11-26 23:52:35 +0300

    f3

--- ketcher-autotests/tests/specs/Macromolecule-editor/Polymer-Bond-Tool/preview-for-hovering-over-bond.spec.ts
+++ ketcher-autotests/tests/specs/Macromolecule-editor/Polymer-Bond-Tool/preview-for-hovering-over-bond.spec.ts
@@ -1,5 +1,5 @@
 /* eslint-disable no-magic-numbers */
-import { test, Page } from '@fixtures';
+import { test } from '@fixtures';
 import {
   moveMouseAway,
   openFileAndAddToCanvasAsNewProjectMacro,

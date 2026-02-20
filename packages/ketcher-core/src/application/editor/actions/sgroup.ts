d336838b5a87a282754cc8ea200dce87174ebbb2
azamatalt <108387789+azamatalt@users.noreply.github.com>
2026-02-20 13:00:20 +0300

    Fix empty collection access in sequence tests and sgroup action

--- packages/ketcher-core/src/application/editor/actions/sgroup.ts
+++ packages/ketcher-core/src/application/editor/actions/sgroup.ts
@@ -88,7 +88,7 @@ export function fromSeveralSgroupAddition(
   atoms,
   attrs,
 ) {
-  const attachmentPoints = [];
+  const attachmentPoints = attrs.attachmentPoints ?? [];
 
   const descriptors = attrs.fieldValue;
   if (typeof descriptors === 'string' || type !== 'DAT') {

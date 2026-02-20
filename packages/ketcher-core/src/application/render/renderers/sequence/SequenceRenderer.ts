9d418e38444cbf5acba70c7418f7cd03cfc8e52c
Gulia <gulia_isaevacologlu@epam.com>
2026-02-20 18:04:48 +0300

    fix:remove unused function

--- packages/ketcher-core/src/application/render/renderers/sequence/SequenceRenderer.ts
+++ packages/ketcher-core/src/application/render/renderers/sequence/SequenceRenderer.ts
@@ -9,7 +9,6 @@ import {
   MonomerToAtomBond,
   Nucleotide,
   Phosphate,
-  Pool,
   Sugar,
   Vec2,
 } from 'domain/entities';
@@ -1263,43 +1262,4 @@ export class SequenceRenderer {
   private static removeDelegatedEvents() {
     SequenceEventDelegationManager.instance.removeDelegatedEvents();
   }
-}
-
-const getCustomToString = (value: object): string | undefined => {
-  const toStringCandidate = (value as { toString: () => string }).toString;
-  if (
-    typeof toStringCandidate === 'function' &&
-    toStringCandidate !== Object.prototype.toString
-  ) {
-    return toStringCandidate.call(value);
-  }
-  return undefined;
-};
-
-export function sequenceReplacer(key: string, value: unknown): unknown {
-  if (key === 'renderer') {
-    return `<${typeof value}>`;
-  } else if (key === 'baseRenderer') {
-    return `<${typeof value}>`;
-  } else if (['R1', 'R2', 'R3'].includes(key)) {
-    return `<${typeof value}>`;
-  } else if (value instanceof Pool) {
-    return {
-      // eslint-disable-next-line dot-notation
-      nextId: value['nextId'],
-      items: Array.from(value),
-    };
-  } else if (
-    value instanceof Object &&
-    !['Object', 'Array'].includes(value.constructor.name)
-  ) {
-    const valueObj = value as object;
-    const repr = getCustomToString(valueObj);
-    return {
-      ctor: value.constructor.name,
-      ...(repr && { repr }),
-      ...value,
-    };
-  }
-  return value;
 }

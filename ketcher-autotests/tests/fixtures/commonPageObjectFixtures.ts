bf496f8a9d45b7f7fcbf474af313a4e63b1764ce
Alexey Girin <alexey_girin@epam.com>
2026-03-11 16:06:32 +0300

    f2

--- ketcher-autotests/tests/fixtures/commonPageObjectFixtures.ts
+++ ketcher-autotests/tests/fixtures/commonPageObjectFixtures.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/ban-types */
 import {
   CommonTopLeftToolbar,
@@ -12,7 +13,11 @@ import {
   MacromoleculesTopToolbar,
   MacromoleculesTopToolbarType,
 } from '@tests/pages/macromolecules/MacromoleculesTopToolbar';
-import { Page } from '@playwright/test';
+import { Locator, Locator, Locator, Page } from '@playwright/test';
+import {
+  AntisenseStrandType,
+  LayoutMode,
+} from '@tests/pages/constants/macromoleculesTopToolbar/Constants';
 
 type CommonPageObjects = {
   CommonTopRightToolbar: (page: Page) => CommonTopRightToolbarType;
@@ -20,23 +25,102 @@ type CommonPageObjects = {
   CommonTopLeftToolbar: (page: Page) => CommonTopLeftToolbarType;
 };
 
-export const test = base.extend<{}, CommonPageObjects>({
+export const test = base.extend<_, CommonPageObjects>({
   CommonTopRightToolbar: [
-    async (_, use) => {
+    async (
+      _: any,
+      use: (
+        arg0: (page: Page) => {
+          help(): Promise<void>;
+          about(): Promise<void>;
+          setZoomInputValue(value: string): Promise<void>;
+          selectZoomOutTool(count?: number): Promise<void>;
+          selectZoomInTool(count?: number): Promise<void>;
+          resetZoom(): Promise<void>;
+          turnOnMacromoleculesEditor(options?: {
+            enableFlexMode?: boolean;
+            goToPeptides?: boolean;
+            disableChainLengthRuler?: boolean;
+            disableAutozoom?: boolean;
+          }): Promise<void>;
+          turnOnMicromoleculesEditor(): Promise<void>;
+          zoomValueEditbox: Locator;
+          zoomOutButton: Locator;
+          zoomInButton: Locator;
+          zoomDefaultButton: Locator;
+          ketcherModeSwitcherCombobox: Locator;
+          fullScreenButton: Locator;
+          helpButton: Locator;
+          aboutButton: Locator;
+          zoomSelector: Locator;
+        },
+      ) => any,
+    ) => {
       await use(CommonTopRightToolbar);
     },
     { scope: 'worker', auto: true },
   ],
 
   CommonTopLeftToolbar: [
-    async (_, use) => {
+    async (
+      _: any,
+      use: (
+        arg0: (page: Page) => {
+          clearCanvas(maxAttempts?: number): Promise<void>;
+          openFile(): Promise<void>;
+          saveFile(): Promise<void>;
+          undo(): Promise<void>;
+          redo(): Promise<void>;
+          clearCanvasButton: Locator;
+          openButton: Locator;
+          saveButton: Locator;
+          undoButton: Locator;
+          redoButton: Locator;
+        },
+      ) => any,
+    ) => {
       await use(CommonTopLeftToolbar);
     },
     { scope: 'worker', auto: true },
   ],
 
   MacromoleculesTopToolbar: [
-    async (_, use) => {
+    async (
+      _: any,
+      use: (
+        arg0: (page: Page) => {
+          createAntisenseStrand(): Promise<void>;
+          expandCreateAntisenseStrandDropdown(): Promise<void>;
+          selectAntisenseStrand(
+            antisenseStrandType?: AntisenseStrandType,
+          ): Promise<void>;
+          arrangeAsARing(): Promise<void>;
+          calculateProperties(options?: { timeout?: number }): Promise<void>;
+          turnSyncEditModeOn(): Promise<void>;
+          turnSyncEditModeOff(): Promise<void>;
+          expandSwitchLayoutModeDropdown(): Promise<void>;
+          selectLayoutModeTool(layoutMode: LayoutMode): Promise<void>;
+          rna(): Promise<void>;
+          dna(): Promise<void>;
+          peptides(): Promise<void>;
+          arrowScrollLeft(): Promise<void>;
+          arrowScrollRight(): Promise<void>;
+          arrangeAsRingButton: Locator;
+          createAntisenseStrandDropdownButton: Locator;
+          createAntisenseStrandDropdownExpandButton: Locator;
+          arrangeAsARingButton: Locator;
+          calculatePropertiesButton: Locator;
+          syncSequenceEditModeButton: Locator;
+          switchLayoutModeDropdownButton: Locator;
+          switchLayoutModeDropdownExpandButton: Locator;
+          rnaButton: Locator;
+          dnaButton: Locator;
+          peptidesButton: Locator;
+          arrowScrollLeftButton: Locator;
+          arrowScrollRightButton: Locator;
+        },
+      ) => any,
+    ) => {
       await use(MacromoleculesTopToolbar);
     },
     { scope: 'worker', auto: true },

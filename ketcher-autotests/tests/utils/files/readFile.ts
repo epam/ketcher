c38d5c68766a74eb86306602a5488f9448d4eeb5
Alexey Girin <alexey_girin@epam.com>
2026-03-20 22:22:54 +0300

    f2

--- ketcher-autotests/tests/utils/files/readFile.ts
+++ ketcher-autotests/tests/utils/files/readFile.ts
@@ -198,6 +198,7 @@ async function setupStructureFormatComboboxes(
   page: Page,
   structureFormat: StructureFormat,
 ) {
+  let structureType;
   let sequenceOrFastaType: SequenceMonomerType = SequenceMonomerType.RNA;
   let peptideType: PeptideLetterCodeType = PeptideLetterCodeType.oneLetterCode;
 
@@ -330,18 +331,6 @@ export async function saveToFile(filename: string, data: string) {
     });
     return await fs.promises.writeFile(resolvedFilePath, data, 'utf-8');
   }
-}
-
-export async function openPasteFromClipboard(
-  page: Page,
-  fillStructure: string,
-) {
-  await CommonTopLeftToolbar(page).openFile();
-  await OpenStructureDialog(page).pasteFromClipboard();
-  await PasteFromClipboardDialog(page).fillTextArea(fillStructure);
-  // The 'Add to Canvas' button step is removed.
-  // If you need to use this function in another context and include the button press, you can do so separately.
-  // await waitForLoad(page);
 }
 
 export async function copyContentToClipboard(page: Page, content: string) {

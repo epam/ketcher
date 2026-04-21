a96c182fc4a93acf0672fb604534f830ce38e9fc
Sergei Bakirov <sergei_bakirov@epam.com>
2026-04-21 17:19:32 +0300

    - fixed test case links, descriptions

--- ketcher-autotests/tests/specs/Chromium-popup/Circular-structures/formation-of-fused-rings.spec.ts
+++ ketcher-autotests/tests/specs/Chromium-popup/Circular-structures/formation-of-fused-rings.spec.ts
@@ -31,14 +31,14 @@ test.describe('Copolymer S-Group type', () => {
     FlexCanvas: _,
   }) => {
     /*
-     * Test case: https://github.com/epam/ketcher/issues/9695
+     * Test case: https://github.com/epam/ketcher/issues/9761
      * Description: Check that when there are more than one chain of monomers connected to each other and they are arranged as ring structures,
      * the layout applied in a way that allows for the first structure to be fixed in its place and the rest of the monomers arranged in an n-agon in relation to it
      * Scenario:
      * 1. Go to Flex mode
      * 2. Load from file: 3-fused-rings.ket
      * 3. Select all structures on canvas
-     * 4. Arrange three chins of monomers as rings via keyboard shortcut
+     * 4. Arrange three chains of monomers as rings via keyboard shortcut
      * 5. Check that all circular structures formed in relation to the smaller ring and
      * that when forming the second and third circular structure, the bond length stay the same as it was between the monomers in the first layouted n-agon, 1,5Å
      * Version 3.12.0
@@ -61,15 +61,19 @@ test.describe('Copolymer S-Group type', () => {
     FlexCanvas: _,
   }) => {
     /*
-     * Test case: https://github.com/epam/ketcher/issues/9695
+     * Test case: https://github.com/epam/ketcher/issues/9761
      * Description: Check that when one circular structure is already formed and the user makes a selection
      * that includes monomers connected to it outside of the circle and also monomers that are a part the circle in order to make a bicircular structure,
      * the layout applied in a way that allows for the first structure to be fixed in its place and the rest of the monomers arranged in an n-agon in relation to it
+     * This test also validates the following requirements:
+     * 1. Indigo first check if part of the selection belongs to an already formed regular n-agon
+     * 2. The part of the selection that already belongs to an n-agon stay fixed and the second circular structure formed in relation to it
+     * 3. When forming the second circular structure, the bond length stay the same as it was between the monomers in the first layouted n-agon, 1,5Å
      * Scenario:
      * 1. Go to Flex mode
      * 2. Load from file: 3-fused-rings-preformed.ket
      * 3. Select all structures on canvas
-     * 4. Arrange three chins of monomers as rings via keyboard shortcut
+     * 4. Arrange three chains of monomers as rings via keyboard shortcut
      * 5. Check that all circular structures formed in relation to the smaller ring and
      * that when forming the second and third circular structure, the bond length stay the same as it was between the monomers in the first layouted n-agon, 1,5Å
      * Version 3.12.0

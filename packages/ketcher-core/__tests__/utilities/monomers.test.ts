cd4d899344bfb878b2dcc892883a04caabbef07c
Guranda Lemonjava <gurandalemonjava@Gurandas-MacBook-Pro.local>
2026-05-07 21:53:39 +0400

    #8173 - add unit test

---
+++ packages/ketcher-core/__tests__/utilities/monomers.test.ts
@@ -0,0 +1,15 @@
+import { isIdtAliasLengthValid } from 'utilities';
+
+describe('isIdtAliasLengthValid', () => {
+  it.each([
+    ['', true],
+    ['ABCDE12345', true],
+    ['ABCDE123456', false],
+    ['/modif/', true],
+    ['/modif', true],
+    ['modif/', true],
+    ['/ABCDE123456/', true],
+  ])('returns %p as %p', (alias: string, expected: boolean) => {
+    expect(isIdtAliasLengthValid(alias)).toBe(expected);
+  });
+});

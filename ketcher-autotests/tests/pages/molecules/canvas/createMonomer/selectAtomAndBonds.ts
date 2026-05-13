757d25f7f8e7af01f3e7ea9552f50ef3be27237f
Gulia <gulia_isaevacologlu@epam.com>
2026-05-13 19:50:26 +0300

    test: fix nucleotide atom selection

--- ketcher-autotests/tests/pages/molecules/canvas/createMonomer/selectAtomAndBonds.ts
+++ ketcher-autotests/tests/pages/molecules/canvas/createMonomer/selectAtomAndBonds.ts
@@ -1,8 +1,29 @@
-import { Page } from '@playwright/test';
+import { Locator, Page } from '@playwright/test';
 import { CommonLeftToolbar } from '@tests/pages/common/CommonLeftToolbar';
 import { clickOnCanvas } from '@utils/clicks';
 import { getAtomLocator } from '@utils/canvas/atoms/getAtomLocator/getAtomLocator';
 import { getBondLocator } from '@utils/macromolecules/polymerBond';
+
+const CENTER_DIVISOR = 2;
+
+async function shiftClickLocatorCenter(page: Page, locator: Locator) {
+  await locator.waitFor({ state: 'attached' });
+  const boundingBox = await locator.boundingBox();
+
+  if (!boundingBox) {
+    throw new Error('Unable to get bounding box for selected canvas element');
+  }
+
+  await page.keyboard.down('Shift');
+  try {
+    await page.mouse.click(
+      boundingBox.x + boundingBox.width / CENTER_DIVISOR,
+      boundingBox.y + boundingBox.height / CENTER_DIVISOR,
+    );
+  } finally {
+    await page.keyboard.up('Shift');
+  }
+}
 
 export async function selectAtomAndBonds(
   page: Page,
@@ -16,18 +37,12 @@ export async function selectAtomAndBonds(
   await clickOnCanvas(page, 0, 0);
   if (options.atomIds) {
     for (const atomId of options.atomIds) {
-      await getAtomLocator(page, { atomId }).click({
-        force: true,
-        modifiers: ['Shift'],
-      });
+      await shiftClickLocatorCenter(page, getAtomLocator(page, { atomId }));
     }
   }
   if (options.bondIds) {
     for (const bondId of options.bondIds) {
-      await getBondLocator(page, { bondId }).click({
-        force: true,
-        modifiers: ['Shift'],
-      });
+      await shiftClickLocatorCenter(page, getBondLocator(page, { bondId }));
     }
   }
 }

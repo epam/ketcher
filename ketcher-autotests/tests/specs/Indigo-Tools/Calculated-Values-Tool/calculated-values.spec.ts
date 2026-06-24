41a43a983f531e2b6c68d1c2fd4392d3e0c0c66d
Katsiaryna Pustakhod <katsiaryna.pustakhod@vertexinc.com>
2026-06-24 10:45:55 +0200

    #3890 - Continue test fixes.

--- ketcher-autotests/tests/specs/Indigo-Tools/Calculated-Values-Tool/calculated-values.spec.ts
+++ ketcher-autotests/tests/specs/Indigo-Tools/Calculated-Values-Tool/calculated-values.spec.ts
@@ -247,14 +247,14 @@ test.describe('Calculated Values Tools', () => {
       CalculatedValuesDialog(page).chemicalFormulaInput,
     ).toContainText('C9H9O2');
     await expect(CalculatedValuesDialog(page).molecularWeightInput).toHaveValue(
-      '72.063',
+      '149.169',
     );
     await expect(CalculatedValuesDialog(page).exactMassInput).toHaveValue(
-      '72.021',
+      '149.060',
     );
     await expect(
       CalculatedValuesDialog(page).elementalAnalysisInput,
-    ).toHaveValue('C 50.0 H 5.6 O 44.4');
+    ).toHaveValue('C 72.5 H 6.1 O 21.4');
   });
 
   test('Calculation of exact mass for the reaction components', async () => {

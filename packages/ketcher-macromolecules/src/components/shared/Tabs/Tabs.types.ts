ca2ce1544bec9aa5e3272b6a2e2a7230ed611247
Alexey Girin <alexey_girin@epam.com>
2026-07-06 16:06:57 +0300

    Update Tabs.types.ts

--- packages/ketcher-macromolecules/src/components/shared/Tabs/Tabs.types.ts
+++ packages/ketcher-macromolecules/src/components/shared/Tabs/Tabs.types.ts
@@ -3,7 +3,7 @@ import { FC } from 'react';
 export type TabPanelData = {
   caption: string;
   tooltip?: string;
-  component: FC<unknown>;
+  component: FC<never>;
   testId: string;
   props?: Record<string, unknown>;
 };

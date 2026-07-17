8f810b9a2abf87f3968e376fca7435efc7d4c1a4
Alexey Girin <alexey_girin@epam.com>
2026-07-17 11:09:12 +0300

    f2

--- ketcher-autotests/tests/specs/Chromium-popup/Bugs/ketcher-3.15.0-bugs.spec.ts
+++ ketcher-autotests/tests/specs/Chromium-popup/Bugs/ketcher-3.15.0-bugs.spec.ts
@@ -16,7 +16,6 @@ import {
   takeEditorScreenshot,
   dragMouseTo,
   takeTopToolbarScreenshot,
-  setMolecule,
   selectByAtomAndBondIds,
   clickInTheMiddleOfTheCanvas,
   pasteFromClipboardAndOpenAsNewProject,

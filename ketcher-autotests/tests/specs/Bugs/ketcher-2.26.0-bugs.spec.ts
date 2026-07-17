c9143e15b55116f8588b0503b8b33a142ad8cd7e
Alexey Girin <alexey_girin@epam.com>
2026-07-17 13:58:07 +0300

    #10495 - Autotests: Get rig of setMolecule function

--- ketcher-autotests/tests/specs/Bugs/ketcher-2.26.0-bugs.spec.ts
+++ ketcher-autotests/tests/specs/Bugs/ketcher-2.26.0-bugs.spec.ts
@@ -1006,9 +1006,8 @@ test.describe('Ketcher bugs in 2.26.0', () => {
       'KET/monomers-connected-to-microstructures.ket',
     );
     await takeEditorScreenshot(page);
-    await CommonTopRightToolbar(page).turnOnMacromoleculesEditor({
-      enableFlexMode: true,
-    });
+    await CommonTopRightToolbar(page).turnOnMacromoleculesEditor();
+    await MacromoleculesTopToolbar(page).selectLayoutModeTool(LayoutMode.Flex);
     await verifyFileExport(
       page,
       'Molfiles-V3000/monomers-connected-to-microstructures-expected.mol',

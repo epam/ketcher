39e3f095e243dd0556d41aab187d430083d733fd
Alexey Girin <alexey_girin@epam.com>
2026-07-25 17:21:04 +0300

    Fix remaining ESLint errors: convert && chains to if statements in ContextMenuTrigger and raphael-ext require eslint-disable

--- packages/ketcher-core/src/application/render/raphael-ext.ts
+++ packages/ketcher-core/src/application/render/raphael-ext.ts
@@ -18,9 +18,12 @@
 
 import { Vec2 } from 'domain/entities/vec2';
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/no-require-imports
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 const raphaelModule: any =
-  typeof window !== 'undefined' ? require('raphael') : undefined;
+  typeof window !== 'undefined'
+    ? // eslint-disable-next-line @typescript-eslint/no-require-imports
+      require('raphael')
+    : undefined;
 
 // Some environments (vite, webpack etc) might resolve this import differently
 // this is a workaround to make it work in all environments

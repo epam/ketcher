ce8eb4686c06ca033d05db827b9be48f269d2906
Avetis Badalyan <avetis_badalyan@epam.com>
2026-07-27 10:55:25 +0400

    #10543 – Refactor: Explicit/implicit Any type clean up from packages\ketcher-macromolecules\src\components\modal\Open\Open.tsx file

--- packages/ketcher-macromolecules/src/components/modal/Open/Open.tsx
+++ packages/ketcher-macromolecules/src/components/modal/Open/Open.tsx
@@ -265,7 +265,7 @@ const onOk = async ({
   let inputFormat;
   let fileData = struct;
 
-  const showParsingError = (stringError) => {
+  const showParsingError = (stringError: string) => {
     const errorMessage = 'Convert error! ' + stringError;
     dispatch(
       openErrorModal({
@@ -315,7 +315,7 @@ const onOk = async ({
   }
 };
 const isAnalyzingFile = false;
-const errorHandler = (error) => console.log(error);
+const errorHandler = (error: string) => console.log(error);
 
 const Open = ({ isModalOpen, onClose }: RequiredModalProps) => {
   const dispatch = useAppDispatch();
@@ -357,8 +357,8 @@ const Open = ({ isModalOpen, onClose }: RequiredModalProps) => {
       windowContext.isKetcherFullscreenBeforeFilePicker = false;
     }
 
-    const onLoad = (fileContent) => {
-      setStructStr(fileContent);
+    const onLoad = (fileContent: unknown) => {
+      setStructStr(fileContent as string);
       setCurrentState(MODAL_STATES.textEditor);
     };
     const onError = () => errorHandler('Error processing file');

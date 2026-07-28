cae5f64281752ce45837751e5438103426bbb234
Alexey Girin <alexey_girin@epam.com>
2026-07-28 11:20:36 +0300

    f2

---
+++ packages/ketcher-macromolecules/src/types/ketcher-react.d.ts
@@ -0,0 +1,25 @@
+declare module 'ketcher-react' {
+  export type IconName = string;
+  export type PresetPosition = string;
+
+  export const Icon: any;
+  export const IconButton: any;
+  export const Button: any;
+  export const Input: any;
+  export const Accordion: any;
+  export const IndigoProvider: any;
+  export const StructRender: any;
+  export const preview: any;
+  export const AmbiguousMonomerPreview: any;
+  export const ArrowScroll: any;
+  export const EditorClassName: any;
+  export const KETCHER_MACROMOLECULES_ROOT_NODE_SELECTOR: string;
+  export const getFullscreenElement: any;
+  export const calculateBondPreviewPosition: any;
+  export const PresetPosition: any;
+
+  export function usePortalStyle(...args: any[]): any;
+  export function calculateAmbiguousMonomerPreviewTop(...args: any[]): any;
+  export function calculateMonomerPreviewTop(...args: any[]): any;
+  export function calculateNucleoElementPreviewTop(...args: any[]): any;
+}

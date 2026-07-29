adc13d052a690e307602290814903a782409efe3
Avet Badalyan <avetbadalyan@gmail.com>
2026-07-29 20:37:15 +0400

    #3705 – Change bond type using keyboard shortcuts while hovering on bond (#10695)
    Co-authored-by: Avetis Badalyan <avetis_badalyan@epam.com>

--- packages/ketcher-react/src/script/ui/state/handleHotkeysOverItem.ts
+++ packages/ketcher-react/src/script/ui/state/handleHotkeysOverItem.ts
@@ -9,6 +9,7 @@ import {
   Atom,
   Action,
   KetcherLogger,
+  bondChangingAction,
 } from 'ketcher-core';
 import { STRUCT_TYPE } from 'src/constants';
 import { openDialog } from './modal';
@@ -263,6 +264,9 @@ function getToolHandler(itemType: string, toolName = '') {
       hand: ({ dispatch }: HandlersProps) =>
         dispatch(onAction({ tool: 'hand' })),
     },
+    bonds: {
+      bond: (props: HandlersProps) => handleBondTypeChangeTool(props),
+    },
     sgroups: {
       atom: (props: HandlersProps) => handleSgroupsTool(props),
     },
@@ -320,6 +324,21 @@ function handleBondTool({ hoveredItemId, newAction, editor }: HandlersProps) {
     { label: 'C' },
   )[0];
   editor.update(newBond);
+}
+
+function handleBondTypeChangeTool({
+  hoveredItemId,
+  newAction,
+  editor,
+}: HandlersProps) {
+  const restruct = editor.render.ctab;
+  const bond = restruct.bonds.get(hoveredItemId)?.b;
+  if (!bond) return;
+
+  const action = bondChangingAction(restruct, hoveredItemId, bond, {
+    ...newAction.opts,
+  });
+  editor.update(action);
 }
 
 function handleChargeTool({ hoveredItemId, newAction, editor }: HandlersProps) {

f12cfa83c90136afd81ec2a8444fffd50369016c
HelenaIsh <32093844+HelenaIsh@users.noreply.github.com>
2026-07-30 11:41:36 +0200

    #2950 - No warning message after entering 0 in some numeric fields in Settings (#10683)
    * #2950 - No warning message after entering 0 in some numeric fields in Settings
    
    * chore: improve styling
    
    * chore: fix test
    
    * chore: fix test
    
    * refactor: reuse usePopoverAnchor hook

--- packages/ketcher-react/src/script/ui/component/form/form/form.tsx
+++ packages/ketcher-react/src/script/ui/component/form/form/form.tsx
@@ -14,7 +14,7 @@
  * limitations under the License.
  ***************************************************************************/
 
-import { Component, useCallback, useState } from 'react';
+import { Component } from 'react';
 
 import { type ValidationError, type Schema, Validator } from 'jsonschema';
 import { ErrorPopover } from './errorPopover';
@@ -31,7 +31,7 @@ import clsx from 'clsx';
 import { connect } from 'react-redux';
 import { getSelectOptionsFromSchema } from '../../../utils';
 import { updateFormState } from '../../../state/modal/form';
-import { useFormContext } from '../../../../../hooks';
+import { useFormContext, usePopoverAnchor } from '../../../../../hooks';
 import { cloneDeep, omit } from 'lodash';
 import { Icon, IconButton } from 'components';
 import { Tooltip } from '@mui/material';
@@ -309,17 +309,6 @@ function Label({
   );
 }
 
-function usePopoverAnchor() {
-  const [anchorEl, setAnchorEl] = useState<HTMLElement | null>(null);
-  const handleOpen = useCallback((event: React.MouseEvent) => {
-    setAnchorEl(event.currentTarget as HTMLElement);
-  }, []);
-  const handleClose = useCallback(() => {
-    setAnchorEl(null);
-  }, []);
-  return { anchorEl, handleOpen, handleClose };
-}
-
 function Field(props: Readonly<FieldProps>) {
   const {
     name,
@@ -353,6 +342,7 @@ function Field(props: Readonly<FieldProps>) {
       className={className}
       onExtraChange={onExtraChange}
       extraValue={extraValue}
+      {...(labelPos === false && { error: dataError })}
       {...(extraName && {
         extraSchema: rest.extraSchema || schema?.properties?.[extraName],
       })}
@@ -370,7 +360,43 @@ function Field(props: Readonly<FieldProps>) {
     />
   );
 
-  if (labelPos === false) return formField;
+  if (labelPos === false) {
+    if (Component) {
+      return formField;
+    }
+
+    const isSelectableControl =
+      rest.type === 'radio' || rest.type === 'checkbox';
+    const showError = Boolean(dataError) && !isSelectableControl;
+
+    return (
+      <>
+        <span className={clsx({ [classes.dataError]: showError }, className)}>
+          <span
+            className={classes.inputWrapper}
+            onMouseEnter={handlePopoverOpen}
+            onMouseLeave={handlePopoverClose}
+            data-testid={
+              props['data-testid']
+                ? `${props['data-testid']}-input-span`
+                : undefined
+            }
+            role="none"
+          >
+            {formField}
+          </span>
+        </span>
+        {showError && dataError && anchorEl && (
+          <ErrorPopover
+            anchorEl={anchorEl}
+            open={!!anchorEl}
+            error={dataError}
+            onClose={handlePopoverClose}
+          />
+        )}
+      </>
+    );
+  }
   return (
     <Label
       className={clsx({ [classes.dataError]: dataError }, className)}

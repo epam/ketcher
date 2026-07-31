94b4f06e29b12fae3396e6debe1479d4abeb427c
ArmanKarapetyan <arman_karapetyan@epam.com>
2026-07-31 13:08:39 +0400

    Refactor environment variable access in editorSlice

--- packages/ketcher-macromolecules/src/state/common/editorSlice.ts
+++ packages/ketcher-macromolecules/src/state/common/editorSlice.ts
@@ -42,11 +42,6 @@ export const molarMeasurementUnitToNumber = {
   [MolarMeasurementUnit.milliMol]: 10 ** 3,
 };
 
-const env =
-  typeof process !== 'undefined'
-    ? process.env
-    : ({} as Record<string, string | undefined>);
-
 interface AppMeta {
   buildDate: string;
   indigoVersion: string;
@@ -99,10 +94,10 @@ const initialState: EditorState = {
   unipositiveIonsValue: 140,
   oligonucleotidesValue: 200,
   app: {
-    buildDate: env.BUILD_DATE ?? '',
-    indigoVersion: env.INDIGO_VERSION ?? '',
-    indigoMachine: env.INDIGO_MACHINE ?? '',
-    version: env.VERSION ?? '',
+    buildDate: process.env.BUILD_DATE ?? '',
+    indigoVersion: process.env.INDIGO_VERSION ?? '',
+    indigoMachine: process.env.INDIGO_MACHINE ?? '',
+    version: process.env.VERSION ?? '',
   },
   selectedMenuGroupItems: {},
 };

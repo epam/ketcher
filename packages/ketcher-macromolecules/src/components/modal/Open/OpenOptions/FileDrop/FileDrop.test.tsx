08a36c6a07b409818366437c8f212b24f4f86dab
Alexey Girin <alexey_girin@epam.com>
2026-08-07 19:04:46 +0300

    а5

--- packages/ketcher-macromolecules/src/components/modal/Open/OpenOptions/FileDrop/FileDrop.test.tsx
+++ packages/ketcher-macromolecules/src/components/modal/Open/OpenOptions/FileDrop/FileDrop.test.tsx
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  ***************************************************************************/
-
 import { render, screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { IconName } from 'ketcher-react';

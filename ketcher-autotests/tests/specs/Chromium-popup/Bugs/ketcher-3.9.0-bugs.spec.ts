ab995843b70b8216796f9373f12f1d2d7642d03f
Alexey Girin <alexey_girin@epam.com>
2026-08-07 13:07:59 +0300

    f4

--- ketcher-autotests/tests/specs/Chromium-popup/Bugs/ketcher-3.9.0-bugs.spec.ts
+++ ketcher-autotests/tests/specs/Chromium-popup/Bugs/ketcher-3.9.0-bugs.spec.ts
@@ -465,7 +465,7 @@ test.describe('Ketcher bugs in 3.9.0: ', () => {
   }) => {
     /*
      * Test case: https://github.com/epam/ketcher/issues/8351
-     * Bug: https://github.com/epam/ketcher/issues/7359
+     * Bug: https://github.com/epam/ketcher/issues/7572
      * Description:  "Create a monomer" option is missing in right-click menu when selection meets conditions
      *
      * Scenario:
@@ -487,8 +487,8 @@ test.describe('Ketcher bugs in 3.9.0: ', () => {
     if (atomBoundingBox) {
       await selectCanvasArea(
         page,
-        { x: atomBoundingBox.x - 40, y: atomBoundingBox.y - 40 },
-        { x: atomBoundingBox.x + 40, y: atomBoundingBox.y + 40 },
+        { x: atomBoundingBox.x - 50, y: atomBoundingBox.y - 50 },
+        { x: atomBoundingBox.x + 50, y: atomBoundingBox.y + 50 },
       );
     }
     expect(

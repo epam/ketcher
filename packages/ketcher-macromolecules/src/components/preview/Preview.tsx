2111a81cc38e255f73d3c386c08002bbcf19f08e
Alexey Girin <alexey_girin@epam.com>
2026-08-07 22:17:36 +0300

    f8

--- packages/ketcher-macromolecules/src/components/preview/Preview.tsx
+++ packages/ketcher-macromolecules/src/components/preview/Preview.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable react-you-might-not-need-an-effect/no-chain-state-updates */
+/* eslint-disable react-you-might-not-need-an-effect/no-event-handler */
 /****************************************************************************
  * Copyright 2021 EPAM Systems
  *
@@ -111,7 +113,7 @@ export const Preview = () => {
       setIsPreviewVisible(false);
       previewRef.current.setAttribute('style', '');
     }
-  }, [preview]);
+  }, [editor?.ketcherRootElementBoundingClientRect, isPreviewVisible, preview]);
 
   if (!preview) {
     return null;

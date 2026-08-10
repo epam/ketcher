bdd140df8b3aab298639414c52bf43eca71deed1
Aleksandr Afanasev <Aleksandr_Afanasev1@epam.com>
2026-08-10 13:47:52 +0400

    #4262 - fixed rna builder auto expansion

--- packages/ketcher-macromolecules/src/components/monomerLibrary/RnaBuilder/RnaEditor/RnaEditor.tsx
+++ packages/ketcher-macromolecules/src/components/monomerLibrary/RnaBuilder/RnaEditor/RnaEditor.tsx
@@ -46,7 +46,6 @@ export const RnaEditor = ({ duplicatePreset }) => {
 
   useEffect(() => {
     if (activePreset) {
-      if (activePreset.name || isEditMode) setExpanded(true);
       return;
     }
 

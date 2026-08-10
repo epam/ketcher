bb64daee46429eb9d1930bed2a7319eac8c2b587
OnurKucukoz <onur@kucukoz.com>
2026-08-10 22:13:01 +0300

    #10434 – Refactor explicit any type clean up from packages/ketcher-core/src/application/render/restruct/reatom.ts file

--- packages/ketcher-core/src/application/render/restruct/reatom.ts
+++ packages/ketcher-core/src/application/render/restruct/reatom.ts
@@ -2139,7 +2139,7 @@ const atomCustomQueryPatterns: readonly AtomCustomQueryPattern[] = [
   {
     propertyName: 'unsaturatedAtom',
     getValue: (atom) => atom.unsaturatedAtom,
-    format: (value) => (Number(value) === 1 ? 'u' : ''),
+    format: (value) => (value === 'true' || Number(value) === 1 ? 'u' : ''),
   },
   {
     propertyName: 'explicitValence',

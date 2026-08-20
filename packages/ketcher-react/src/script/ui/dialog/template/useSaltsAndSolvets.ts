0b275ff2770983d298713b40c749a55c5896128c
Mykyta Kurkan <kurkan.n.s@gmail.com>
2026-08-20 13:15:13 +0300

    #11245 Refactor: eliminate no-event-handler violation in useSaltsAndSolvets

--- packages/ketcher-react/src/script/ui/dialog/template/useSaltsAndSolvets.ts
+++ packages/ketcher-react/src/script/ui/dialog/template/useSaltsAndSolvets.ts
@@ -9,7 +9,7 @@ export default function useSaltsAndSolvents(
   saltsAndSolvents: Template[],
   filter: string,
 ) {
-  const [isFirstRender, setIsFirstRender] = useState(true);
+  const isFirstRenderRef = useRef(true);
   const timerId = useRef<null | ReturnType<typeof setTimeout>>(null);
   const [filteredSaltsAndSolvents, setFilteredSaltsAndSolvents] = useState(
     saltsAndSolvents[SALTS_AND_SOLVENTS],
@@ -36,8 +36,8 @@ export default function useSaltsAndSolvents(
   }, [saltsAndSolvents, addToSaSWithBatches]);
 
   useEffect(() => {
-    if (isFirstRender) {
-      setIsFirstRender(false);
+    if (isFirstRenderRef.current) {
+      isFirstRenderRef.current = false;
       return;
     }
     clearTimeout(timerId.current as unknown as number);

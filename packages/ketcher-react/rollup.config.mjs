e6efa1b435ea0081c28b2909a6fbb06ebf94941f
Karen Ghalachyan <44671938+Karen1704@users.noreply.github.com>
2026-08-28 13:32:01 +0400

    fix: disable production CSS source maps (#4216)

--- packages/ketcher-react/rollup.config.mjs
+++ packages/ketcher-react/rollup.config.mjs
@@ -83,7 +83,7 @@ const config = {
       plugins: [autoprefixer({ grid: 'autoplace' })],
       extract: 'index.css',
       minimize: isProduction,
-      sourceMap: !isProduction,
+      sourceMap: !isProduction, 
       include: [includePattern, '../ketcher-macromolecules/dist/index.css'],
     }),
     svgrPlugin({ include: includePattern }),

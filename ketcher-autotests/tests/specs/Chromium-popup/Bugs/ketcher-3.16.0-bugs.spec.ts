e7d38868accd3551e74e5cffc31017227dd99250
d-mrzv <denis_morozov@epam.com>
2026-09-02 18:53:53 +0300

    first test case works

--- ketcher-autotests/tests/specs/Chromium-popup/Bugs/ketcher-3.16.0-bugs.spec.ts
+++ ketcher-autotests/tests/specs/Chromium-popup/Bugs/ketcher-3.16.0-bugs.spec.ts
@@ -1,47 +1,13 @@
-import { CommonTopRightToolbar } from '@tests/pages/common/CommonTopRightToolbar';
 import { Page, test, expect } from '@fixtures';
-import { CommonLeftToolbar } from '@tests/pages/common/CommonLeftToolbar';
-import { ContextMenu } from '@tests/pages/common/ContextMenu';
-import { SelectionToolType } from '@tests/pages/constants/areaSelectionTool/Constants';
 import {
-  selectAllStructuresOnCanvas,
   openFileAndAddToCanvas,
-  pasteFromClipboardAndAddToMacromoleculesCanvas,
-  MacroFileType,
-  takeElementScreenshot,
   takeEditorScreenshot,
-  dragMouseTo,
-  takeTopToolbarScreenshot,
-  selectByAtomAndBondIds,
-  clickInTheMiddleOfTheCanvas,
-  pasteFromClipboardAndOpenAsNewProject,
-  selectPartOfMolecules,
   copyToClipboardByKeyboard,
   pasteFromClipboardByKeyboard,
   clickOnCanvas,
+  selectRectangleArea,
+  getCoordinatesOfTheMiddleOfTheScreen,
 } from '@utils';
-import { Library } from '@tests/pages/macromolecules/Library';
-import { MacromoleculesTopToolbar } from '@tests/pages/macromolecules/MacromoleculesTopToolbar';
-import { LeftToolbar } from '@tests/pages/molecules/LeftToolbar';
-import { getAtomLocator } from '@utils/canvas/atoms/getAtomLocator/getAtomLocator';
-import { getMonomerLocator } from '@utils/macromolecules/monomer';
-import { MonomerType } from '@tests/pages/constants/createMonomerDialog/Constants';
-import { CreateMonomerDialog } from '@tests/pages/molecules/canvas/CreateMonomerDialog';
-import { NucleotidePresetSection } from '@tests/pages/molecules/canvas/createMonomer/NucleotidePresetSection';
-import {
-  MicroAtomOption,
-  MonomerOnMicroOption,
-} from '@tests/pages/constants/contextMenu/Constants';
-import { getAbbreviationLocator } from '@utils/canvas/s-group-signes/getAbbreviationLocator';
-import { RNASection } from '@tests/pages/constants/library/Constants';
-import { TopRightToolbar } from '@tests/pages/molecules/TopRightToolbar';
-import { AtomPropertiesDialog } from '@tests/pages/molecules/canvas/AtomPropertiesDialog';
-import { ImplicitHCount } from '@tests/pages/constants/atomProperties/Constants';
-import { NotificationMessageBanner } from '@tests/pages/molecules/canvas/createMonomer/NotificationMessageBanner';
-import { ErrorMessage } from '@tests/pages/constants/notificationMessageBanner/Constants';
-import { Preset } from '@tests/pages/constants/monomers/Presets';
-import { verifySMARTSExport } from '@utils/files/receiveFileComparisonData';
-
 let page: Page;
 test.beforeAll(async ({ initMoleculesCanvas }) => {
   page = await initMoleculesCanvas();
@@ -69,15 +35,15 @@ test('Case 1: Copy/paste action with large structions shouldnt cause expections'
       consoleErrors.push(msg.text());
     }
   });
-
   await openFileAndAddToCanvas(
     page,
     'KET/all-possible-query-features-with-out-custom-query.ket',
   );
-  await selectPartOfMolecules(page, 150);
+  const { x, y } = await getCoordinatesOfTheMiddleOfTheScreen(page);
+  await selectRectangleArea(page, x - 250, y - 210, x + 100, y + 110);
   await copyToClipboardByKeyboard(page);
   await pasteFromClipboardByKeyboard(page);
-  await clickOnCanvas(page, 0, 0);
+  await clickOnCanvas(page, 0, 100, { from: 'pageCenter' });
   const clipboardErrors = consoleErrors.filter((error) =>
     error.includes('No valid data on clipboard'),
   );

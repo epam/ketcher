a7d99103f93f9c77a1fc2d716940c9b25bfbfb8d
noemi_verebelyi <noemi_verebelyi@epam.com>
2026-09-10 16:55:52 +0200

    #4058 – Fix Daylight SMILES export for R groups

--- packages/ketcher-core/src/application/formatters/formatProperties.ts
+++ packages/ketcher-core/src/application/formatters/formatProperties.ts
@@ -62,6 +62,7 @@ const formatProperties: FormatPropertiesMap = {
     ChemicalMimeType.DaylightSmiles,
     ['.smi', '.smiles'],
     true,
+    { 'smiles-saving-format': 'daylight' },
   ),
   smilesExt: new SupportedFormatProperties(
     'Extended SMILES',

3dec9c72e775f09df64291e3c9a8d460fc6924c2
Nyildizzz <nyildiz61.ny@gmail.com>
2026-09-10 21:03:14 +0300

    refactor: reorganize imports and update sequence edit hook usage in MonomerGroup

--- packages/ketcher-macromolecules/src/components/monomerLibrary/monomerLibraryGroup/MonomerGroup.tsx
+++ packages/ketcher-macromolecules/src/components/monomerLibrary/monomerLibraryGroup/MonomerGroup.tsx
@@ -23,11 +23,12 @@ import {
   MonomerOrAmbiguousType,
   isAmbiguousMonomerLibraryItem,
 } from 'ketcher-core';
-import { useAppSelector, useDebouncedShowPreview } from 'hooks';
 import {
-  selectEditor,
-  selectIsSequenceEditInRNABuilderMode,
-} from 'state/common';
+  useAppSelector,
+  useDebouncedShowPreview,
+  useSequenceEditInRNABuilderMode,
+} from 'hooks';
+import { selectEditor } from 'state/common';
 import { selectGroupItemValidations } from 'state/rna-builder';
 import { PreviewType } from 'state';
 import {
@@ -47,9 +48,7 @@ const MonomerGroup = ({
 }: IMonomerGroupProps) => {
   const editor = useAppSelector(selectEditor);
   const activeGroupItemValidations = useAppSelector(selectGroupItemValidations);
-  const isSequenceEditInRNABuilderMode = useAppSelector(
-    selectIsSequenceEditInRNABuilderMode,
-  );
+  const isSequenceEditInRNABuilderMode = useSequenceEditInRNABuilderMode();
   const isMonomerDisabled = (monomer: MonomerOrAmbiguousType) => {
     if (disabled) {
       return disabled;

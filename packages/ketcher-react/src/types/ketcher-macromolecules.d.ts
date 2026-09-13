a7cb2470cca0b0fa9d55386cdf73c9b9ac9efd0a
Hovik Poghosyan <hovik.poghosyan2005@gmail.com>
2026-09-13 23:28:23 +0400

    fix test-id getting for the helm alias-s input close icon

--- packages/ketcher-react/src/types/ketcher-macromolecules.d.ts
+++ packages/ketcher-react/src/types/ketcher-macromolecules.d.ts
@@ -1,9 +1,17 @@
 declare module 'ketcher-macromolecules' {
   import type * as React from 'react';
+
   interface MacromoleculesEditorProps {
     ketcherId: string;
     togglerComponent?: JSX.Element;
   }
+
   const MacromoleculesEditor: React.ComponentType<MacromoleculesEditorProps>;
   export default MacromoleculesEditor;
+}
+
+declare module '@mui/utils/types' {
+  interface DataAttributesOverrides {
+    'data-testid'?: string;
+  }
 }
